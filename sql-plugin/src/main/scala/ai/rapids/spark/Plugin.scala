10c4e8d704d79bb5b6360a066fa44d41f2965e4a
Robert (Bobby) Evans <bobby@apache.org>
2019-07-24 10:25:40 -0500

    Added in support for union

--- sql-plugin/src/main/scala/ai/rapids/spark/Plugin.scala
+++ sql-plugin/src/main/scala/ai/rapids/spark/Plugin.scala
@@ -19,7 +19,6 @@ package ai.rapids.spark
 import ai.rapids.cudf.{Cuda, Rmm, RmmAllocationMode}
 
 import org.apache.spark.internal.Logging
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{SparkSession, SparkSessionExtensions}
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.plans.physical.{HashPartitioning, Partitioning}
@@ -33,7 +32,6 @@ import org.apache.spark.sql.execution.datasources.v2.parquet.ParquetScan
 import org.apache.spark.sql.execution.exchange.ShuffleExchangeExec
 import org.apache.spark.sql.sources.v2.reader.Scan
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.vectorized.{ColumnarBatch, ColumnVector}
 import org.apache.spark.{ExecutorPlugin, SparkConf, SparkEnv}
 
 trait GpuExec extends SparkPlan {
@@ -47,70 +45,6 @@ trait GpuExec extends SparkPlan {
   }
 
   override def hashCode(): Int = super.hashCode()
-}
-
-class GpuProjectExec(projectList: Seq[GpuExpression], child: SparkPlan)
-  extends ProjectExec(projectList.asInstanceOf[Seq[NamedExpression]], child) with GpuExec {
-
-  // Disable code generation for now...
-  override def supportCodegen: Boolean = false
-
-  override def doExecuteColumnar() : RDD[ColumnarBatch] = {
-    val boundProjectList: Seq[Any] = GpuBindReferences.bindReferences(projectList, child.output)
-    val rdd = child.executeColumnar()
-    AutoCloseColumnBatchIterator.map(rdd,
-      (cb: ColumnarBatch) => {
-        val newColumns = boundProjectList.map(
-          expr => {
-            val result = expr.asInstanceOf[GpuExpression].columnarEval(cb)
-            // TODO it is possible for a constant to be returned that we need to
-            // create a columnVector for, might be a special sub-class
-            // that only stores a single value.
-            result.asInstanceOf[ColumnVector]
-          }).toArray
-        new ColumnarBatch(newColumns, cb.numRows())
-      }
-    )
-  }
-}
-
-class GpuFilterExec(condition: GpuExpression, child: SparkPlan)
-  extends FilterExec(condition, child) with GpuExec {
-
-  // Disable code generation for now...
-  override def supportCodegen: Boolean = false
-
-  override def doExecuteColumnar(): RDD[ColumnarBatch] = {
-    val numOutputRows = longMetric("numOutputRows")
-    val boundCondition: Any = GpuBindReferences.bindReference(condition, child.output)
-    val rdd = child.executeColumnar()
-    AutoCloseColumnBatchIterator.map(rdd,
-      (cb: ColumnarBatch) => {
-        val boundExpression = boundCondition.asInstanceOf[GpuExpression]
-        val evalCv = boundExpression.columnarEval(cb)
-        var cols = Seq[GpuColumnVector]()
-        var rowCount = 0
-        try {
-          val gpuEvalCv = evalCv.asInstanceOf[GpuColumnVector]
-
-          // rebuild the columns, but with new filtered columns
-          for (i <- 0 until cb.numCols()) {
-            val colBase = cb.column(i).asInstanceOf[GpuColumnVector].getBase
-            val filtered = colBase.filter(gpuEvalCv.getBase)
-            cols = (cols :+ GpuColumnVector.from(filtered))
-            rowCount = filtered.getRowCount().intValue() // all columns have the same # of rows
-          }
-        } finally {
-          if (evalCv != null && evalCv.isInstanceOf[GpuColumnVector]) {
-            evalCv.asInstanceOf[GpuColumnVector].close();
-          }
-        }
-        numOutputRows += rowCount
-
-        new ColumnarBatch(cols.toArray, rowCount)
-      }
-    )
-  }
 }
 
 class CannotReplaceException(str: String) extends RuntimeException(str) {
@@ -289,6 +223,8 @@ case class GpuOverrides(session: SparkSession) extends Rule[SparkPlan] with Logg
         case shuffle: ShuffleExchangeExec =>
           new GpuShuffleExchangeExec(replaceWithGpuPartitioning(shuffle.outputPartitioning),
             replaceWithGpuPlan(shuffle.child), shuffle.canChangeNumPartitions)
+        case union: UnionExec =>
+          new GpuUnionExec(union.children.map(replaceWithGpuPlan))
         case p =>
           logWarning(s"GPU Processing for ${p.getClass} is not currently supported.")
           p.withNewChildren(p.children.map(replaceWithGpuPlan))
