d4c286499ceef78d363cd4abd86d88f839a0610f
Robert Evans <roberte@nvidia.com>
2020-04-24 15:11:17 -0700

    Added in some more integration tests and fixed a few more corner cases

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/bitwise.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/bitwise.scala
@@ -16,41 +16,98 @@
 
 package org.apache.spark.sql.rapids
 
-import ai.rapids.cudf.{BinaryOp, UnaryOp}
-import ai.rapids.spark.{CudfBinaryExpression, GpuColumnVector, GpuUnaryExpression}
+import ai.rapids.cudf.{BinaryOp, ColumnVector, DType, Scalar, UnaryOp}
+import ai.rapids.spark.{Arm, CudfBinaryExpression, GpuBinaryExpression, GpuColumnVector, GpuExpression, GpuUnaryExpression}
+
 import org.apache.spark.sql.catalyst.expressions.{ExpectsInputTypes, Expression, ImplicitCastInputTypes}
 import org.apache.spark.sql.types._
 
-case class GpuShiftLeft(left: Expression, right: Expression) extends CudfBinaryExpression with ImplicitCastInputTypes {
+object ShiftHelper extends Arm {
+  // From the java language specification for Java 8
+  // If the promoted type of the left-hand operand is int, then only the five lowest-order bits of
+  // the right-hand operand are used as the shift distance. It is as if the right-hand operand
+  // were subjected to a bitwise logical AND operator & ( §15.22.1) with the mask value
+  // 0x1f (0b11111). The shift distance actually used is therefore always in the range 0 to 31,
+  // inclusive.
+  //
+  // If  the  promoted  type  of  the  left-hand  operand  is  long,  then  only  the  six
+  // lowest-order  bits  of  the  right-hand  operand  are  used  as  the  shift  distance.  It  is
+  // as  if  the right-hand operand were subjected to a bitwise logical AND operator & ( §15.22.1)
+  // with the mask value 0x3f (0b111111). The shift distance actually used is therefore always in
+  // the range 0 to 63, inclusive.
+  private def maskForDistance(t: DType): Int = t match {
+    case DType.INT32 =>  0x1F // 0b11111
+    case DType.INT64 =>  0x3F //0b111111
+  }
+
+  def fixupDistanceNoClose(t: DType, distance: ColumnVector): ColumnVector = {
+    withResource(Scalar.fromInt(maskForDistance(t))) { mask =>
+      distance.bitAnd(mask)
+    }
+  }
+
+  def fixupDistanceNoClose(t: DType, distance: Scalar): Scalar = {
+    if (distance.isValid) {
+      Scalar.fromInt(distance.getInt & maskForDistance(t))
+    } else {
+      distance.incRefCount()
+    }
+  }
+}
+
+trait GpuShiftBase extends GpuBinaryExpression with ImplicitCastInputTypes {
+  def shiftOp: BinaryOp
+
+  override def doColumnar(lhs: GpuColumnVector, rhs: GpuColumnVector): GpuColumnVector = {
+    val lBase = lhs.getBase
+    withResource(ShiftHelper.fixupDistanceNoClose(lBase.getType, rhs.getBase)) { distance =>
+      GpuColumnVector.from(lBase.binaryOp(shiftOp, distance, lBase.getType))
+    }
+  }
+
+  override def doColumnar(lhs: Scalar, rhs: GpuColumnVector): GpuColumnVector = {
+    withResource(ShiftHelper.fixupDistanceNoClose(lhs.getType, rhs.getBase)) { distance =>
+      GpuColumnVector.from(lhs.binaryOp(shiftOp, distance, lhs.getType))
+    }
+  }
+
+  override def doColumnar(lhs: GpuColumnVector, rhs: Scalar): GpuColumnVector = {
+    val lBase = lhs.getBase
+    withResource(ShiftHelper.fixupDistanceNoClose(lBase.getType, rhs)) { distance =>
+      GpuColumnVector.from(lBase.binaryOp(shiftOp, distance, lBase.getType))
+    }
+  }
+}
+
+case class GpuShiftLeft(left: GpuExpression, right: GpuExpression) extends GpuShiftBase {
   override def inputTypes: Seq[AbstractDataType] =
     Seq(TypeCollection(IntegerType, LongType), IntegerType)
 
-  override def binaryOp: BinaryOp = BinaryOp.SHIFT_LEFT
+  override def shiftOp: BinaryOp = BinaryOp.SHIFT_LEFT
 
   override def dataType: DataType = left.dataType
 }
 
-case class GpuShiftRight(left: Expression, right: Expression) extends CudfBinaryExpression with ImplicitCastInputTypes {
+case class GpuShiftRight(left: GpuExpression, right: GpuExpression) extends GpuShiftBase {
   override def inputTypes: Seq[AbstractDataType] =
     Seq(TypeCollection(IntegerType, LongType), IntegerType)
 
-  override def binaryOp: BinaryOp = BinaryOp.SHIFT_RIGHT
+  override def shiftOp: BinaryOp = BinaryOp.SHIFT_RIGHT
 
   override def dataType: DataType = left.dataType
 
 }
 
-case class GpuShiftRightUnsigned(left: Expression, right: Expression) extends CudfBinaryExpression
-  with ImplicitCastInputTypes {
+case class GpuShiftRightUnsigned(left: GpuExpression, right: GpuExpression) extends GpuShiftBase {
   override def inputTypes: Seq[AbstractDataType] =
     Seq(TypeCollection(IntegerType, LongType), IntegerType)
 
-  override def binaryOp: BinaryOp = BinaryOp.SHIFT_RIGHT_UNSIGNED
+  override def shiftOp: BinaryOp = BinaryOp.SHIFT_RIGHT_UNSIGNED
 
   override def dataType: DataType = left.dataType
 }
 
-case class GpuBitwiseAnd(left: Expression, right: Expression) extends CudfBinaryArithmetic {
+case class GpuBitwiseAnd(left: GpuExpression, right: GpuExpression) extends CudfBinaryArithmetic {
   override def inputType: AbstractDataType = IntegralType
 
   override def symbol: String = "&"
@@ -58,7 +115,7 @@ case class GpuBitwiseAnd(left: Expression, right: Expression) extends CudfBinary
   override def binaryOp: BinaryOp = BinaryOp.BITWISE_AND
 }
 
-case class GpuBitwiseOr(left: Expression, right: Expression) extends CudfBinaryArithmetic {
+case class GpuBitwiseOr(left: GpuExpression, right: GpuExpression) extends CudfBinaryArithmetic {
   override def inputType: AbstractDataType = IntegralType
 
   override def symbol: String = "|"
@@ -66,7 +123,7 @@ case class GpuBitwiseOr(left: Expression, right: Expression) extends CudfBinaryA
   override def binaryOp: BinaryOp = BinaryOp.BITWISE_OR
 }
 
-case class GpuBitwiseXor(left: Expression, right: Expression) extends CudfBinaryArithmetic {
+case class GpuBitwiseXor(left: GpuExpression, right: GpuExpression) extends CudfBinaryArithmetic {
   override def inputType: AbstractDataType = IntegralType
 
   override def symbol: String = "^"
@@ -74,7 +131,7 @@ case class GpuBitwiseXor(left: Expression, right: Expression) extends CudfBinary
   override def binaryOp: BinaryOp = BinaryOp.BITWISE_XOR
 }
 
-case class GpuBitwiseNot(child: Expression) extends GpuUnaryExpression with ExpectsInputTypes {
+case class GpuBitwiseNot(child: GpuExpression) extends GpuUnaryExpression with ExpectsInputTypes {
   override def inputTypes: Seq[AbstractDataType] = Seq(IntegralType)
 
   override def dataType: DataType = child.dataType
