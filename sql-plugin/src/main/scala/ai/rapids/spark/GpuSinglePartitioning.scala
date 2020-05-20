0a0cb7f535adea5ed652e609f5ab81267d0abbf0
Ryan Lee (SW-GPU) <ryanlee@nvidia.com>
2020-05-20 15:58:34 -0700

    line length fixes for multiple small files

--- sql-plugin/src/main/scala/ai/rapids/spark/GpuSinglePartitioning.scala
+++ sql-plugin/src/main/scala/ai/rapids/spark/GpuSinglePartitioning.scala
@@ -20,16 +20,17 @@ import org.apache.spark.sql.catalyst.expressions.Expression
 import org.apache.spark.sql.types.{DataType, IntegerType}
 import org.apache.spark.sql.vectorized.ColumnarBatch
 
-case class GpuSinglePartitioning(expressions: Seq[GpuExpression]) extends GpuExpression with GpuPartitioning {
+case class GpuSinglePartitioning(expressions: Seq[GpuExpression])
+  extends GpuExpression with GpuPartitioning {
   /**
-    * Returns the result of evaluating this expression on the entire
-    * [[ColumnarBatch]]. The result of calling this may be a single [[GpuColumnVector]] or a scalar
-    * value. Scalar values typically happen if they are a part of the expression i.e. col("a") + 100.
+    * Returns the result of evaluating this expression on the entire [[ColumnarBatch]].
+    * The result of calling this may be a single [[GpuColumnVector]] or a scalar value.
+    * Scalar values typically happen if they are a part of the expression i.e. col("a") + 100.
     * In this case the 100 is a literal that Add would have to be able to handle.
     *
     * By convention any [[GpuColumnVector]] returned by [[columnarEval]]
-    * is owned by the caller and will need to be closed by them. This can happen by putting it into
-    * a [[ColumnarBatch]] and closing the batch or by closing the vector directly if it is a
+    * is owned by the caller and will need to be closed by them. This can happen by putting it
+    * into a [[ColumnarBatch]] and closing the batch or by closing the vector directly if it is a
     * temporary value.
     */
   override def columnarEval(batch: ColumnarBatch): Any = {
