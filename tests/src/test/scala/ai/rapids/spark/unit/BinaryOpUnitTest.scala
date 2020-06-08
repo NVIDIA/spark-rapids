5882d64df2b173f8cc896dcf38314fc38b29d7e5
Raza Jafri <rjafri@nvidia.com>
2020-06-08 11:29:24 -0700

    addressed review comments

--- tests/src/test/scala/ai/rapids/spark/unit/BinaryOpUnitTest.scala
+++ tests/src/test/scala/ai/rapids/spark/unit/BinaryOpUnitTest.scala
@@ -24,40 +24,37 @@ import org.apache.spark.sql.rapids.GpuPmod
 class BinaryOpUnitTest extends GpuUnitTests {
 
   test("test pmod") {
-    testNumericDataTypes { c =>
-      c match {
-        case (convert, convertS) =>
-          withResource(convert(ColumnVector.fromBoxedInts(2, 0, 1, null))) { expected0 =>
-            withResource(convert(ColumnVector.fromBoxedInts(2, null, 0, null))) { expected1 =>
-              withResource(convert(ColumnVector.fromBoxedInts(3, 0, 3, null))) { expected2 =>
-                //vector 0
-                val vector0 = convert(ColumnVector.fromBoxedInts(-7, 3, 4, null))
-                // assign to new handles for code readability
-                val vector01 = vector0.incRefCount()
-                val vector02 = vector0.incRefCount()
+    testNumericDataTypes { case (convertVector, convertScalar) =>
+      withResource(convertVector(ColumnVector.fromBoxedInts(2, 0, 1, null))) { expected0 =>
+        withResource(convertVector(ColumnVector.fromBoxedInts(2, null, 0, null))) { expected1 =>
+          withResource(convertVector(ColumnVector.fromBoxedInts(3, 0, 3, null))) { expected2 =>
+            //vector 0
+            val vector0 = convertVector(ColumnVector.fromBoxedInts(-7, 3, 4, null))
+            // assign to new handles for code readability
+            val vector01 = vector0.incRefCount()
+            val vector02 = vector0.incRefCount()
 
-                //vector 1
-                val vector1 = convert(ColumnVector.fromBoxedInts(3, 0, 1, null))
+            //vector 1
+            val vector1 = convertVector(ColumnVector.fromBoxedInts(3, 0, 1, null))
 
-                //saving dataType to a variable for convenience
-                val dataType = getSparkType(vector0.getBase.getType)
+            //saving dataType to a variable for convenience
+            val dataType = getSparkType(vector0.getBase.getType)
 
-                //lhs = vector rhs = scalar
-                val expr0 = GpuLiteral(vector0, dataType)
-                val exprS = GpuLiteral(convertS(3), dataType)
-                checkEvaluation(GpuPmod(expr0, exprS), expected0)
+            //lhs = vector rhs = scalar
+            val exprVectorLhs0 = GpuLiteral(vector0, dataType)
+            val exprScalar = GpuLiteral(convertScalar(3), dataType)
+            checkEvaluation(GpuPmod(exprVectorLhs0, exprScalar), expected0)
 
-                //lhs = vector rhs = vector
-                val expr01 = GpuLiteral(vector01, dataType)
-                val expr1 = GpuLiteral(vector1, dataType)
-                checkEvaluation(GpuPmod(expr01, expr1), expected1)
+            //lhs = vector rhs = vector
+            val exprVectorLhs1 = GpuLiteral(vector01, dataType)
+            val exprVectorRhs0 = GpuLiteral(vector1, dataType)
+            checkEvaluation(GpuPmod(exprVectorLhs1, exprVectorRhs0), expected1)
 
-                //lhs = scalar rhs = vector
-                val expr02 = GpuLiteral(vector02, dataType)
-                checkEvaluation(GpuPmod(exprS, expr02), expected2)
-              }
-            }
+            //lhs = scalar rhs = vector
+            val exprVectorRhs1 = GpuLiteral(vector02, dataType)
+            checkEvaluation(GpuPmod(exprScalar, exprVectorRhs1), expected2)
           }
+        }
       }
     }
   }
