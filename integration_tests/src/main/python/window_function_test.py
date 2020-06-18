28db9e99ca179ef04fb187f166e9906cb21c3019
Mithun RK <mythrocks@gmail.com>
2020-06-18 15:59:44 -0700

    Integration tests for corner cases in window functions.
    Review changes:
    1. Clarified the need to cast to timestamp
    2. xfail for count(nullable) counting nulls
    3. Removed unnecessary dependencies from hash_aggregate_test.py

--- integration_tests/src/main/python/window_function_test.py
+++ integration_tests/src/main/python/window_function_test.py
@@ -20,21 +20,13 @@ from pyspark.sql.types import *
 from marks import *
 from spark_session import with_cpu_session
 
-_no_nans_float_conf = {'spark.rapids.sql.variableFloatAgg.enabled': 'true',
-                       'spark.rapids.sql.hasNans': 'false',
-                       'spark.rapids.sql.castStringToFloat.enabled': 'true'}
-
-# The input lists or schemas that are used by StructGen.
-
-
 _grpkey_longs_with_no_nulls = [
-    ('a', RepeatSeqGen(LongGen(nullable=False), length=2048)),
+    ('a', RepeatSeqGen(LongGen(nullable=False), length=20)),
     ('b', IntegerGen()),
     ('c', IntegerGen())]
 
-# grouping longs with nulls present
 _grpkey_longs_with_nulls = [
-    ('a', RepeatSeqGen(LongGen(nullable=(True, 10.0)), length=2048)),
+    ('a', RepeatSeqGen(LongGen(nullable=(True, 10.0)), length=20)),
     ('b', IntegerGen()),
     ('c', IntegerGen())]
 
@@ -44,64 +36,45 @@ _grpkey_longs_with_timestamps = [
     ('c', IntegerGen())]
 
 _grpkey_longs_with_nullable_timestamps = [
-    ('a', RepeatSeqGen(LongGen(nullable=False), length=2048)),
+    ('a', RepeatSeqGen(LongGen(nullable=False), length=20)),
     ('b', DateGen(nullable=(True, 5.0), start=date(year=2020, month=1, day=1), end=date(year=2020, month=12, day=31))),
     ('c', IntegerGen())]
 
-# List of schemas
-_init_list_rows_query = [_grpkey_longs_with_no_nulls, _grpkey_longs_with_nulls, _grpkey_longs_with_timestamps]
-_init_list_range_query = [_grpkey_longs_with_timestamps, _grpkey_longs_with_nullable_timestamps]
-
-
-def get_struct_gens(init_list=_init_list_rows_query, marked_params=[]):
-    """
-    A method to build the structGen inputs along with their passed in markers to allow testing
-    specific params with their relevant markers.
-    :arg init_list list of schemas to be tested, defaults to _init_list_no_nans from above.
-    :arg marked_params A list of tuples of (schema, list of pytest markers)
-    Look at params_markers_for_avg_sum as an example.
-    """
-    list = init_list.copy()
-    for index in range(0, len(list)):
-        for test_case,marks in marked_params:
-            if list[index] == test_case:
-                list[index] = pytest.param(list[index], marks=marks)
-    return list
-
-
-params_markers_for_avg_sum=[]
-
 
 @ignore_order
-@pytest.mark.parametrize('data_gen', get_struct_gens(
-    marked_params=params_markers_for_avg_sum), ids=idfn)
+@pytest.mark.parametrize('data_gen', [_grpkey_longs_with_no_nulls,
+                                      _grpkey_longs_with_nulls,
+                                      _grpkey_longs_with_timestamps,
+                                      _grpkey_longs_with_nullable_timestamps], ids=idfn)
 def test_window_aggs_for_rows(data_gen):
     df = with_cpu_session(
-        lambda spark : gen_df(spark, data_gen, length=100))
+        lambda spark : gen_df(spark, data_gen, length=2048))
     df.createOrReplaceTempView("window_agg_table")
     assert_gpu_and_cpu_are_equal_collect(
         lambda spark: spark.sql(
             'select '
             ' sum(c) over '
-            '   (partition by a order by b asc rows between 1 preceding and 1 following) as sum_c_asc, '
+            '   (partition by a order by b,c asc rows between 1 preceding and 1 following) as sum_c_asc, '
             ' max(c) over '
-            '   (partition by a order by b desc rows between 2 preceding and 1 following) as max_c_desc, '
+            '   (partition by a order by b desc, c desc rows between 2 preceding and 1 following) as max_c_desc, '
             ' min(c) over '
-            '   (partition by a order by b asc  rows between 2 preceding and current row) as min_c_asc, '
+            '   (partition by a order by b,c rows between 2 preceding and current row) as min_c_asc, '
             ' count(1) over '
-            '   (partition by a order by b asc  rows between UNBOUNDED preceding and UNBOUNDED following) as count_1, '
+            '   (partition by a order by b,c rows between UNBOUNDED preceding and UNBOUNDED following) as count_1, '
             ' row_number() over '
-            '   (partition by a order by b asc  rows between UNBOUNDED preceding and CURRENT ROW) as row_num '
-            'from window_agg_table order by 1 desc, 2 desc'),
-        conf=_no_nans_float_conf)
+            '   (partition by a order by b,c rows between UNBOUNDED preceding and CURRENT ROW) as row_num '
+            'from window_agg_table '))
 
 
+# Test for RANGE queries, with timestamp order-by expressions.
+# Non-timestamp order-by columns are currently unsupported for RANGE queries.
+# See https://github.com/NVIDIA/spark-rapids/issues/216
 @ignore_order
-@pytest.mark.parametrize('data_gen', get_struct_gens(init_list=_init_list_range_query,
-    marked_params=params_markers_for_avg_sum), ids=idfn)
+@pytest.mark.parametrize('data_gen', [_grpkey_longs_with_timestamps,
+                                      _grpkey_longs_with_nullable_timestamps], ids=idfn)
 def test_window_aggs_for_ranges(data_gen):
     df = with_cpu_session(
-        lambda spark : gen_df(spark, data_gen, length=100))
+        lambda spark : gen_df(spark, data_gen, length=2048))
     df.createOrReplaceTempView("window_agg_table")
     assert_gpu_and_cpu_are_equal_collect(
         lambda spark: spark.sql(
@@ -124,6 +97,38 @@ def test_window_aggs_for_ranges(data_gen):
             ' max(c) over '
             '   (partition by a order by cast(b as timestamp) asc  '
             '       range between UNBOUNDED preceding and UNBOUNDED following) as max_c_unbounded '
-            'from window_agg_table'),
-        conf=_no_nans_float_conf)
+            'from window_agg_table'))
 
+
+@pytest.mark.xfail(reason="[UNSUPPORTED] Ranges over non-timestamp columns "
+                          "(https://github.com/NVIDIA/spark-rapids/issues/216)")
+@ignore_order
+@pytest.mark.parametrize('data_gen', [_grpkey_longs_with_timestamps], ids=idfn)
+def test_window_aggs_for_ranges_of_dates(data_gen):
+    df = with_cpu_session(
+        lambda spark : gen_df(spark, data_gen, length=2048))
+    df.createOrReplaceTempView("window_agg_table")
+    assert_gpu_and_cpu_are_equal_collect(
+        lambda spark: spark.sql(
+            'select '
+            ' sum(c) over '
+            '   (partition by a order by b asc  '
+            '       range between 1 preceding and 1 following) as sum_c_asc '
+            'from window_agg_table'))
+
+
+@pytest.mark.xfail(reason="[BUG] `COUNT(x)` should not count null values of `x` "
+                          "(https://github.com/NVIDIA/spark-rapids/issues/218)")
+@ignore_order
+@pytest.mark.parametrize('data_gen', [_grpkey_longs_with_no_nulls], ids=idfn)
+def test_window_aggs_for_rows_count_non_null(data_gen):
+    df = with_cpu_session(
+        lambda spark : gen_df(spark, data_gen, length=2048))
+    df.createOrReplaceTempView("window_agg_table")
+    assert_gpu_and_cpu_are_equal_collect(
+        lambda spark: spark.sql(
+            'select '
+            ' count(c) over '
+            '   (partition by a order by b,c '
+            '       rows between UNBOUNDED preceding and UNBOUNDED following) as count_non_null '
+            'from window_agg_table '))
