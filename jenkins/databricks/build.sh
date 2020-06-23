f8d659200d57623dc9b200fb22dc2b79c8db1d54
Thomas Graves <tgraves@nvidia.com>
2020-06-23 16:44:52 -0500

    Explicit check pipestatus from running tests

--- jenkins/databricks/build.sh
+++ jenkins/databricks/build.sh
@@ -92,8 +92,11 @@ sudo ln -s /databricks/jars/ $SPARK_HOME/jars || true
 sudo chmod 777 /databricks/data/logs/
 sudo chmod 777 /databricks/data/logs/*
 echo { \"port\":\"15002\" } > ~/.databricks-connect
-false
 $SPARK_HOME/bin/spark-submit ./runtests.py 2>&1 | tee out
+if [ "${PIPESTATUS[0]}" -ne "0" ]; then
+    echo "Failure running tests"
+    exit 2
+fi
 
 cd /home/ubuntu
 tar -zcvf spark-rapids-built.tgz spark-rapids
