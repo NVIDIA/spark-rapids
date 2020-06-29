91d3c8e34ea4ed7e20418d67fe52fecebe8d91bb
Thomas Graves <tgraves@nvidia.com>
2020-06-29 08:30:44 -0500

    Change xfail to be annotation

--- integration_tests/src/main/python/parquet_test.py
+++ integration_tests/src/main/python/parquet_test.py
@@ -146,10 +146,9 @@ def test_simple_partitioned_read(spark_tmp_path):
     assert_gpu_and_cpu_are_equal_collect(
             lambda spark : spark.read.parquet(data_path))
 
+@pytest.mark.xfail(condition=is_databricks_runtime(),
+    reason='https://github.com/NVIDIA/spark-rapids/issues/192')
 def test_read_merge_schema(spark_tmp_path):
-    if is_databricks_runtime():
-        pytest.xfail(reason="test_read_merge_schema fails on Databricks "
-        "(https://github.com/NVIDIA/spark-rapids/issues/192)")
     # Once https://github.com/NVIDIA/spark-rapids/issues/133 and https://github.com/NVIDIA/spark-rapids/issues/132 are fixed 
     # we should go with a more standard set of generators
     parquet_gens = [byte_gen, short_gen, int_gen, long_gen, float_gen, double_gen,
