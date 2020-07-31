f52ecc77d68068ebc168cd8b9dc0c1f923d9141d
Andy Grove <andygrove@nvidia.com>
2020-07-31 10:29:56 -0600

    show both plans
    Signed-off-by: Andy Grove <andygrove@nvidia.com>

--- tests/src/test/scala/com/nvidia/spark/rapids/HashAggregatesSuite.scala
+++ tests/src/test/scala/com/nvidia/spark/rapids/HashAggregatesSuite.scala
@@ -46,8 +46,8 @@ class HashAggregatesSuite extends SparkQueryCompareTestSuite {
           case _ => None
         }
       }
-      assert(gpuAgg.isDefined,
-        s"as the GPU plan expected a GPU aggregate but did not find any! Plan: $plan")
+      assert(gpuAgg.isDefined, s"as the GPU plan expected a GPU aggregate but did not find any! " +
+          s"plan: $plan; executedPlan: $executedPlan")
     }
   }
 
