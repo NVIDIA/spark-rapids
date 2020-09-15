3526677b7740edfb8a2df4592c4f13adcb1dc5ad
Andy Grove <andygrove@nvidia.com>
2020-09-15 09:17:04 -0600

    rebase
    Signed-off-by: Andy Grove <andygrove@nvidia.com>

--- tests/src/test/scala/com/nvidia/spark/rapids/AdaptiveQueryExecSuite.scala
+++ tests/src/test/scala/com/nvidia/spark/rapids/AdaptiveQueryExecSuite.scala
@@ -263,7 +263,7 @@ class AdaptiveQueryExecSuite
 
   test("Exchange reuse") {
 
-    assumeSpark301orLater()
+    assumeSpark301orLater
 
     val conf = new SparkConf()
         .set(SQLConf.ADAPTIVE_EXECUTION_ENABLED.key, "true")
@@ -363,16 +363,6 @@ class AdaptiveQueryExecSuite
       case p: PartialReducerPartitionSpec => p.reducerIndex
     }.distinct
     assert(rightSkew.length == rightSkewNum)
-  }
-
-  private def assumeSpark301orLater(): Unit = {
-    // all AQE tests requires Spark 3.0.1 or later
-    val isValidTestForSparkVersion = ShimLoader.getSparkShims.getSparkShimVersion match {
-      case SparkShimVersion(3, 0, 0) => false
-      case DatabricksShimVersion(3, 0, 0) => false
-      case _ => true
-    }
-    assume(isValidTestForSparkVersion)
   }
 
   private def setupTestData(spark: SparkSession): Unit = {
