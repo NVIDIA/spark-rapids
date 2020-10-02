2c9b3dabd8e11ae27ae77c576b010706970eb134
Thomas Graves <tgraves@nvidia.com>
2020-10-02 10:17:14 -0500

    Update limit test
    Signed-off-by: Thomas Graves <tgraves@nvidia.com>

--- tests/src/test/scala/com/nvidia/spark/rapids/LimitExecSuite.scala
+++ tests/src/test/scala/com/nvidia/spark/rapids/LimitExecSuite.scala
@@ -16,38 +16,47 @@
 
 package com.nvidia.spark.rapids
 
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.types.DataTypes
 
 class LimitExecSuite extends SparkQueryCompareTestSuite {
 
-  testSparkResultsAreEqual("limit more than rows", intCsvDf) {
+ /** CollectLimitExec is off by default, turn it on for tests */
+  def enableCollectLimitExec(conf: SparkConf = new SparkConf()): SparkConf = {
+    conf.set("spark.rapids.sql.exec.CollectLimitExec", "true")
+  }
+
+  testSparkResultsAreEqual("limit more than rows", intCsvDf,
+      conf = enableCollectLimitExec()) {
       frame => frame.limit(10).repartition(2)
   }
 
   testSparkResultsAreEqual("limit less than rows equal to batchSize", intCsvDf,
-    conf = makeBatchedBytes(1)) {
+    conf = enableCollectLimitExec(makeBatchedBytes(1))) {
     frame => frame.limit(1).repartition(2)
   }
 
   testSparkResultsAreEqual("limit less than rows applied with batches pending", intCsvDf,
-    conf = makeBatchedBytes(3)) {
+    conf = enableCollectLimitExec(makeBatchedBytes(3))) {
     frame => frame.limit(2).repartition(2)
   }
 
   testSparkResultsAreEqual("limit with no real columns", intCsvDf,
-    conf = makeBatchedBytes(3), repart = 0) {
+    conf = enableCollectLimitExec(makeBatchedBytes(3)), repart = 0) {
     frame => frame.limit(2).selectExpr("pi()")
   }
 
-  testSparkResultsAreEqual("collect with limit", testData) {
+  testSparkResultsAreEqual("collect with limit", testData,
+    conf = enableCollectLimitExec()) {
     frame => {
       import frame.sparkSession.implicits._
       val results: Array[Row] = frame.limit(16).repartition(2).collect()
       results.map(row => if (row.isNullAt(0)) 0 else row.getInt(0)).toSeq.toDF("c0")
     }
   }
-  testSparkResultsAreEqual("collect with limit, repart=4", testData, repart = 4) {
+  testSparkResultsAreEqual("collect with limit, repart=4", testData,
+    conf = enableCollectLimitExec(), repart = 4) {
     frame => {
       import frame.sparkSession.implicits._
       val results: Array[Row] = frame.limit(16).collect()
