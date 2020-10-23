1d517da829464b806e988c2f5dca5f9fce4d15ad
rwlee <rlee35@u.rochester.edu>
2020-10-23 15:41:00 -0700

    remove getBinary accessor

--- sql-plugin/src/main/java/com/nvidia/spark/rapids/RapidsHostColumnVector.java
+++ sql-plugin/src/main/java/com/nvidia/spark/rapids/RapidsHostColumnVector.java
@@ -159,7 +159,7 @@ public final class RapidsHostColumnVector extends ColumnVector {
 
   @Override
   public byte[] getBinary(int rowId) {
-    return cudfCv.getBytesFromList(rowId);
+    throw new IllegalStateException("Binary data access is currently not supported by rapids cudf");
   }
 
   @Override
