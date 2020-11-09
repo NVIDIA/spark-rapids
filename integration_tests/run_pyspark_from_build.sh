b0aaf4b50975da86ec0575f9788ff60ae2c1115c
Robert (Bobby) Evans <bobby@apache.org>
2020-11-09 07:11:56 -0600

    Fixed some issues with findspark and memory on the GPU

--- integration_tests/run_pyspark_from_build.sh
+++ integration_tests/run_pyspark_from_build.sh
@@ -40,18 +40,23 @@ else
     fi
     if [[ "${TEST_PARALLEL}" == "" ]];
     then
-        # For now just assume that we are going to use the GPU on the system with the most memory
-        # and then divide it up into 3GiB chunks
-        # we subtract one for the main controlling process that will still
-        # launch an application, but it will never be used
-        TEST_PARALLEL=`nvidia-smi --query-gpu=memory.total --format=csv,noheader | awk '{if (MAX < $1){ MAX = $1}} END {print int(MAX / (2.3 * 1024)) - 1}'`
+        # For now just assume that we are going to use the GPU on the
+        # system with the most free memory and then divide it up into chunks.
+        # We use free memory to try and avoid issues if the GPU also is working
+        # on graphics, which happens some times.
+        # We subtract one for the main controlling process that will still
+        # launch an application.  It will not run thing on the GPU but it needs
+        # to still launch a spark application.
+        TEST_PARALLEL=`nvidia-smi --query-gpu=memory.free --format=csv,noheader | awk '{if (MAX < $1){ MAX = $1}} END {print int(MAX / (2.3 * 1024)) - 1}'`
         echo "AUTO DETECTED PARALLELISM OF $TEST_PARALLEL"
     fi
     if python -c 'import findspark';
     then
         echo "FOUND findspark"
+        FIND_SPARK=1
     else
         TEST_PARALLEL=0
+        FIND_SPARK=0
         echo "findspark not installed cannot run tests in parallel"
     fi
     if python -c 'import xdist.plugin';
@@ -75,16 +80,18 @@ else
     RUN_DIR="$SCRIPTPATH"/target/run_dir
     mkdir -p "$RUN_DIR"
     cd "$RUN_DIR"
-    export PYSP_TEST_spark_driver_extraClassPath="${ALL_JARS// /:}"
-    export PYSP_TEST_spark_driver_extraJavaOptions="-ea -Duser.timezone=GMT $COVERAGE_SUBMIT_FLAGS"
-    export PYSP_TEST_spark_executor_extraJavaOptions='-ea -Duser.timezone=GMT'
-    export PYSP_TEST_spark_ui_showConsoleProgress='false'
-    export PYSP_TEST_spark_sql_session_timeZone='UTC'
-    export PYSP_TEST_spark_sql_shuffle_partitions='12'
-    export PYSP_TEST_spark_rapids_memory_gpu_allocFraction=$MEMORY_FRACTION
-    export PYSP_TEST_spark_rapids_memory_gpu_maxAllocFraction=$MEMORY_FRACTION
+    if [[ "${FIND_SPARK}" == "1" ]];
+    then
+        export PYSP_TEST_spark_driver_extraClassPath="${ALL_JARS// /:}"
+        export PYSP_TEST_spark_driver_extraJavaOptions="-ea -Duser.timezone=GMT $COVERAGE_SUBMIT_FLAGS"
+        export PYSP_TEST_spark_executor_extraJavaOptions='-ea -Duser.timezone=GMT'
+        export PYSP_TEST_spark_ui_showConsoleProgress='false'
+        export PYSP_TEST_spark_sql_session_timeZone='UTC'
+        export PYSP_TEST_spark_sql_shuffle_partitions='12'
+        export PYSP_TEST_spark_rapids_memory_gpu_allocFraction=$MEMORY_FRACTION
+        export PYSP_TEST_spark_rapids_memory_gpu_maxAllocFraction=$MEMORY_FRACTION
 
-    python \
+        python \
           "$SCRIPTPATH"/runtests.py --rootdir "$SCRIPTPATH" "$SCRIPTPATH"/src/main/python \
           $TEST_PARALLEL \
           -v -rfExXs "$TEST_TAGS" \
@@ -92,4 +99,19 @@ else
           "$TEST_ARGS" \
           $RUN_TEST_PARAMS \
           "$@"
+    else
+        "$SPARK_HOME"/bin/spark-submit --jars "${ALL_JARS// /,}" \
+          --conf "spark.driver.extraJavaOptions=-ea -Duser.timezone=GMT $COVERAGE_SUBMIT_FLAGS" \
+          --conf 'spark.executor.extraJavaOptions=-ea -Duser.timezone=GMT' \
+          --conf 'spark.sql.session.timeZone=UTC' \
+          --conf 'spark.sql.shuffle.partitions=12' \
+          $SPARK_SUBMIT_FLAGS \
+          "$SCRIPTPATH"/runtests.py --rootdir "$SCRIPTPATH" "$SCRIPTPATH"/src/main/python \
+          -v -rfExXs "$TEST_TAGS" \
+          --std_input_path="$SCRIPTPATH"/src/test/resources/ \
+          "$TEST_ARGS" \
+          $RUN_TEST_PARAMS \
+          "$@"
+
+    fi
 fi
