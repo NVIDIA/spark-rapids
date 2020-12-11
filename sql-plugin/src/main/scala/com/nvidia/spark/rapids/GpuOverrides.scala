c5514e268e85f244df041e99e02a2c3463af857e
Robert (Bobby) Evans <bobby@apache.org>
2020-12-11 07:30:19 -0600

    Reuse gpu expression conversion rules when checking sort order (#1367)
    Signed-off-by: Robert (Bobby) Evans <bobby@apache.org>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
@@ -401,42 +401,44 @@ object GpuOverrides {
     !regexList.exists(pattern => s.contains(pattern))
   }
 
-  private def canonicalizeToCpuForSortOrder(exp: Expression): Expression = exp match {
-    case g: GpuLiteral => Literal(g.value, g.dataType).canonicalized
-    case g: GpuKnownFloatingPointNormalized =>
-      KnownFloatingPointNormalized(canonicalizeToCpuForSortOrder(g.child)).canonicalized
-    case g: GpuNormalizeNaNAndZero =>
-      NormalizeNaNAndZero(canonicalizeToCpuForSortOrder(g.child)).canonicalized
-    case g: GpuAlias =>
-      ShimLoader.getSparkShims.alias(canonicalizeToCpuForSortOrder(g.child), g.name)(
-        g.exprId,
-        g.qualifier,
-        g.explicitMetadata)
-          .canonicalized
-    case g: GpuSubstring =>
-      Substring(
-        canonicalizeToCpuForSortOrder(g.str),
-        canonicalizeToCpuForSortOrder(g.pos),
-        canonicalizeToCpuForSortOrder(g.len))
-          .canonicalized
-    case o: GpuExpression =>
-      throw new IllegalStateException(s"${o.getClass} is not expected to be a part of a SortOrder")
-    case other => other.canonicalized
+  private def convertExprToGpuIfPossible(expr: Expression, conf: RapidsConf): Expression = {
+    if (expr.find(_.isInstanceOf[GpuExpression]).isDefined) {
+      // already been converted
+      expr
+    } else {
+      val wrapped = wrapExpr(expr, conf, None)
+      wrapped.tagForGpu()
+      if (wrapped.canExprTreeBeReplaced) {
+        wrapped.convertToGpu()
+      } else {
+        expr
+      }
+    }
   }
 
-  private def gpuOrderingSemanticEquals(found: Expression, required: Expression): Boolean = {
-    found.deterministic && required.deterministic &&
-        canonicalizeToCpuForSortOrder(found) == canonicalizeToCpuForSortOrder(required)
-  }
+  private def gpuOrderingSemanticEquals(
+      found: Expression,
+      required: Expression,
+      conf: RapidsConf): Boolean =
+    found.deterministic &&
+        required.deterministic &&
+        convertExprToGpuIfPossible(found, conf).canonicalized ==
+            convertExprToGpuIfPossible(required, conf).canonicalized
 
-  private def orderingSatisfies(found: SortOrder, required: SortOrder): Boolean = {
+  private def orderingSatisfies(
+      found: SortOrder,
+      required: SortOrder,
+      conf: RapidsConf): Boolean = {
     val foundChildren = ShimLoader.getSparkShims.sortOrderChildren(found)
-    foundChildren.exists(gpuOrderingSemanticEquals(_, required.child)) &&
-        found.direction == required.direction &&
-        found.nullOrdering == required.nullOrdering
+    found.direction == required.direction &&
+        found.nullOrdering == required.nullOrdering &&
+        foundChildren.exists(gpuOrderingSemanticEquals(_, required.child, conf))
   }
 
-  private def orderingSatisfies(ordering1: Seq[SortOrder], ordering2: Seq[SortOrder]): Boolean = {
+  private def orderingSatisfies(
+      ordering1: Seq[SortOrder],
+      ordering2: Seq[SortOrder],
+      conf: RapidsConf): Boolean = {
     // We cannot use SortOrder.orderingSatisfies because there is a corner case where
     // some operators like a Literal can be a part of SortOrder, which then results in errors
     // because we may have converted it over to a GpuLiteral at that point and a Literal
@@ -447,7 +449,7 @@ object GpuOverrides {
       false
     } else {
       ordering2.zip(ordering1).forall {
-        case (o2, o1) => orderingSatisfies(o1, o2)
+        case (o2, o1) => orderingSatisfies(o1, o2, conf)
       }
     }
   }
@@ -2306,7 +2308,7 @@ case class GpuOverrides() extends Rule[SparkPlan] with Logging {
     // Now that we've performed any necessary shuffles, add sorts to guarantee output orderings:
     children = children.zip(requiredChildOrderings).map { case (child, requiredOrdering) =>
       // If child.outputOrdering already satisfies the requiredOrdering, we do not need to sort.
-      if (GpuOverrides.orderingSatisfies(child.outputOrdering, requiredOrdering)) {
+      if (GpuOverrides.orderingSatisfies(child.outputOrdering, requiredOrdering, conf)) {
         child
       } else {
         val sort = SortExec(requiredOrdering, global = false, child = child)
