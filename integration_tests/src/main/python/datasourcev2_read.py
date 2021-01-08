1777628c54b2e5609e1b9b5b9e1aae83b8ab69f6
Thomas Graves <tgraves@apache.org>
2021-01-08 10:00:09 -0600

    update datasource v2 test

--- integration_tests/src/main/python/datasourcev2_read.py
+++ integration_tests/src/main/python/datasourcev2_read.py
@@ -14,12 +14,9 @@
 
 import pytest
 
-from asserts import assert_gpu_and_cpu_are_equal_collect, assert_gpu_and_cpu_writes_are_equal_collect, assert_gpu_fallback_collect
-from datetime import date, datetime, timezone
-from data_gen import *
-from marks import *
+from asserts import assert_gpu_and_cpu_are_equal_collect,
 from pyspark.sql.types import *
-from spark_session import with_cpu_session, with_gpu_session
+from spark_session import with_cpu_session
 
 def read_parquet_df(data_path):
     return lambda spark : spark.read.parquet(data_path)
@@ -27,45 +24,38 @@ def read_parquet_df(data_path):
 def read_parquet_sql(data_path):
     return lambda spark : spark.sql('select * from parquet.`{}`'.format(data_path))
 
+def createPeopleCSVDf(spark):
+    peopleCSVLocation = "people.csv"
+    return spark.read.format("csv")\
+        .option("header", "false")\
+        .option("inferSchema", "true")\
+        .load(peopleCSVLocation)\
+        .withColumnRenamed("_c0", "name")\
+        .withColumnRenamed("_c1", "age")\
+        .withColumnRenamed("_c2", "job")
 
-# Override decimal_gens because decimal with negative scale is unsupported in parquet reading
-decimal_gens = [DecimalGen(), DecimalGen(precision=7, scale=3), DecimalGen(precision=10, scale=10),
-                DecimalGen(precision=9, scale=0), DecimalGen(precision=18, scale=15)]
 
-parquet_gens_list = [[byte_gen, short_gen, int_gen, long_gen, float_gen, double_gen,
-    string_gen, boolean_gen, date_gen,
-    TimestampGen(start=datetime(1900, 1, 1, tzinfo=timezone.utc)), ArrayGen(byte_gen),
-    ArrayGen(long_gen), ArrayGen(string_gen), ArrayGen(date_gen),
-    ArrayGen(TimestampGen(start=datetime(1900, 1, 1, tzinfo=timezone.utc))),
-    ArrayGen(DecimalGen()),
-    ArrayGen(ArrayGen(byte_gen)),
-    StructGen([['child0', ArrayGen(byte_gen)], ['child1', byte_gen], ['child2', float_gen], ['child3', DecimalGen()]]),
-    ArrayGen(StructGen([['child0', string_gen], ['child1', double_gen], ['child2', int_gen]]))] +
-                     map_gens_sample + decimal_gens,
-                     pytest.param([timestamp_gen], marks=pytest.mark.xfail(reason='https://github.com/NVIDIA/spark-rapids/issues/132'))]
+catalogName = "columnar"
+tableName = "people"
+columnarTableName = catalogName + "." + tableName
 
-# test with original parquet file reader, the multi-file parallel reader for cloud, and coalesce file reader for
-# non-cloud
-original_parquet_file_reader_conf = {'spark.rapids.sql.format.parquet.reader.type': 'PERFILE'}
-multithreaded_parquet_file_reader_conf = {'spark.rapids.sql.format.parquet.reader.type': 'MULTITHREADED'}
-coalesce_parquet_file_reader_conf = {'spark.rapids.sql.format.parquet.reader.type': 'COALESCING'}
-reader_opt_confs = [original_parquet_file_reader_conf, multithreaded_parquet_file_reader_conf,
-                    coalesce_parquet_file_reader_conf]
+def setupInMemoryTableWithPartitioning(spark):
+    spark.sql("create database IF NOT EXISTS " + catalogName)
+    peopleCSVDf = createPeopleCSVDf(spark)
+    peopleCSVDf.createOrReplaceTempView("people_csv")
+    spark.table("people_csv").write.partitionBy("job").saveAsTable(columnarTableName)
 
-@pytest.mark.parametrize('parquet_gens', parquet_gens_list, ids=idfn)
-@pytest.mark.parametrize('read_func', [read_parquet_df, read_parquet_sql])
-@pytest.mark.parametrize('reader_confs', reader_opt_confs)
-@pytest.mark.parametrize('v1_enabled_list', ["", "parquet"])
-def test_read_round_trip(spark_tmp_path, parquet_gens, read_func, reader_confs, v1_enabled_list):
-    gen_list = [('_c' + str(i), gen) for i, gen in enumerate(parquet_gens)]
-    data_path = spark_tmp_path + '/PARQUET_DATA'
-    with_cpu_session(
-            lambda spark : gen_df(spark, gen_list).write.parquet(data_path),
-            conf={'spark.sql.legacy.parquet.datetimeRebaseModeInWrite': 'CORRECTED'})
-    all_confs = reader_confs.copy()
-    all_confs.update({'spark.sql.sources.useV1SourceList': v1_enabled_list, 'spark.sql.legacy.parquet.datetimeRebaseModeInRead': 'CORRECTED'})
-    # once https://github.com/NVIDIA/spark-rapids/issues/1126 is in we can remove spark.sql.legacy.parquet.datetimeRebaseModeInRead config which is a workaround
-    # for nested timestamp/date support
-    assert_gpu_and_cpu_are_equal_collect(read_func(data_path),
-            conf=all_confs)
+def setupInMemoryTableNoPartitioning(spark):
+    spark.sql("create database IF NOT EXISTS " + catalogName)
+    peopleCSVDf = createPeopleCSVDf(spark)
+    peopleCSVDf.createOrReplaceTempView("people_csv")
+    spark.table("people_csv").write.saveAsTable(columnarTableName)
+
+def readTable(spark):
+    spark.table(columnarTableName)\
+        .orderBy("name", "age")
+
+def test_read_round_trip_partitioned(spark_tmp_path):
+    with_cpu_session(lambda spark : setupInMemoryTableWithPartitioning(spark))
+    assert_gpu_and_cpu_are_equal_collect(readTable)
 
