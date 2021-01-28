fe5e19ac2d881c20c02df2200c6c376f959b7c50
Robert (Bobby) Evans <bobby@apache.org>
2021-01-28 15:19:47 -0600

    Add in flag for integration tests to not skip required tests (#1611)
    Signed-off-by: Robert (Bobby) Evans <bobby@apache.org>

--- integration_tests/run_pyspark_from_build.sh
+++ integration_tests/run_pyspark_from_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2020, NVIDIA CORPORATION.
+# Copyright (c) 2020-2021, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -66,6 +66,12 @@ else
         echo "xdist not installed cannot run tests in parallel"
     fi
 
+    TEST_TYPE_PARAM=""
+    if [[ "${TEST_TYPE}" != "" ]];
+    then
+        TEST_TYPE_PARAM="--test_type $TEST_TYPE"
+    fi
+
     if [[ ${TEST_PARALLEL} -lt 2 ]];
     then
         # With xdist 0 and 1 are the same parallelsm but
@@ -96,6 +102,7 @@ else
           -v -rfExXs "$TEST_TAGS" \
           --std_input_path="$SCRIPTPATH"/src/test/resources/ \
           --color=yes \
+          $TEST_TYPE_PARAM \
           "$TEST_ARGS" \
           $RUN_TEST_PARAMS \
           "$@"
@@ -110,6 +117,7 @@ else
           -v -rfExXs "$TEST_TAGS" \
           --std_input_path="$SCRIPTPATH"/src/test/resources/ \
           --color=yes \
+          $TEST_TYPE_PARAM \
           "$TEST_ARGS" \
           $RUN_TEST_PARAMS \
           "$@"
