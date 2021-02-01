d47765a46010f74d806511e6e6451e72c591508a
Kuhu Shukla <kuhus@nvidia.com>
2021-01-31 22:00:01 -0600

    Fix javadoc
    Signed-off-by: Kuhu Shukla <kuhus@nvidia.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/complexTypeExtractors.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/complexTypeExtractors.scala
@@ -186,9 +186,8 @@ case class GpuGetMapValue(child: Expression, key: Expression)
   override def right: Expression = key
 }
 
-/**
-  * Checks if the array (left) has the element (right)
-  */
+/** Checks if the array (left) has the element (right)
+*/
 case class GpuArrayContains(left: Expression, right: Expression)
   extends GpuBinaryExpression with NullIntolerant {
 
