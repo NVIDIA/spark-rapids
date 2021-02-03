5f54cee424555e6e3183130a53048edb52512209
Thomas Graves <tgraves@apache.org>
2021-02-03 10:21:04 -0600

    update v2 source testing

--- integration_tests/src/main/scala/com/nvidia/spark/rapids/tests/datasourcev2/TestingV2Source.scala
+++ integration_tests/src/main/scala/com/nvidia/spark/rapids/tests/datasourcev2/TestingV2Source.scala
@@ -15,12 +15,30 @@
  */
 package com.nvidia.spark.rapids.tests.datasourcev2.parquet
 
-import org.apache.spark.sql.sources.v2._
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.sources.v2.reader._
+import java.util
+
 import scala.collection.JavaConverters._
-import org.apache.spark.sql.SparkSession
+
+import org.apache.arrow.memory.RootAllocator
+import org.apache.arrow.vector.{IntVector, ValueVector}
+import org.apache.arrow.vector.complex.{ListVector, MapVector}
+import org.apache.arrow.vector.types.{DateUnit, FloatingPointPrecision, TimeUnit}
+import org.apache.arrow.vector.types.pojo.{ArrowType, Field, FieldType}
+
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.connector.catalog._
+import org.apache.spark.sql.connector.catalog.TableCapability.BATCH_READ
+import org.apache.spark.sql.connector.expressions.Transform
+import org.apache.spark.sql.connector.read._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.util.CaseInsensitiveStringMap
+import org.apache.spark.sql.vectorized.{ArrowColumnVector, ColumnarBatch}
+
+
+object TestingV2Source {
+  val schema =
+    new StructType(Array(StructField("int1", IntegerType), StructField("int2", IntegerType)))
+}
 
 trait TestingV2Source extends TableProvider {
   override def inferSchema(options: CaseInsensitiveStringMap): StructType = {
@@ -37,12 +55,37 @@ trait TestingV2Source extends TableProvider {
   def getTable(options: CaseInsensitiveStringMap): Table
 }
 
+abstract class SimpleScanBuilder extends ScanBuilder
+  with Batch with Scan {
+
+  override def build(): Scan = this
+
+  override def toBatch: Batch = this
+
+  override def readSchema(): StructType = TestingV2Source.schema
+
+  override def createReaderFactory(): PartitionReaderFactory
+}
+
+abstract class SimpleBatchTable extends Table with SupportsRead  {
+
+  override def schema(): StructType = TestingV2Source.schema
+
+  override def name(): String = this.getClass.toString
+
+  override def capabilities(): util.Set[TableCapability] = Set(BATCH_READ).asJava
+}
+
+case class ArrowInputPartition(dt: Seq[DataType], numRows: Int) extends InputPartition
+
+
 class ColumnarDataSourceV2 extends TestingV2Source {
 
   class MyScanBuilder extends SimpleScanBuilder {
 
     override def planInputPartitions(): Array[InputPartition] = {
-      Array(RangeInputPartition(0, 50), RangeInputPartition(50, 90))
+      Array(ArrowInputPartition(Seq(IntegerType, IntegerType), 100),
+        ArrowInputPartition(Seq(IntegerType, IntegerType), 100))
     }
 
     override def createReaderFactory(): PartitionReaderFactory = {
@@ -67,30 +110,37 @@ object ColumnarReaderFactory extends PartitionReaderFactory {
   }
 
   override def createColumnarReader(partition: InputPartition): PartitionReader[ColumnarBatch] = {
-    val RangeInputPartition(start, end) = partition
+    val ArrowInputPartition(dataTypes, numRows) = partition
     new PartitionReader[ColumnarBatch] {
-      private lazy val i = new OnHeapColumnVector(BATCH_SIZE, IntegerType)
-      private lazy val j = new OnHeapColumnVector(BATCH_SIZE, IntegerType)
-      private lazy val batch = new ColumnarBatch(Array(i, j))
+      private var batch: ColumnarBatch = _
 
-      private var current = start
+      private var current = 0
 
       override def next(): Boolean = {
-        i.reset()
-        j.reset()
-
         var count = 0
-        while (current < end && count < BATCH_SIZE) {
-          i.putInt(count, current)
-          j.putInt(count, -current)
-          current += 1
-          count += 1
+        val batchSize = if (current < numRows) {
+          if (current + BATCH_SIZE > numRows) {
+            numRows - current
+          } else {
+            BATCH_SIZE
+          }
+        } else {
+          0
         }
 
-        if (count == 0) {
+        if (batchSize == 0) {
           false
         } else {
+          var dtypeNum = 0
+          val vecs = dataTypes.map { dtype =>
+            val vector = setupArrowVector(s"v$current$dtypeNum", dtype)
+            dtypeNum += 1
+            fillArrowVec(dtype, vector, numRows)
+            new ArrowColumnVector(vector)
+          }
+          batch = new ColumnarBatch(vecs.toArray)
           batch.setNumRows(count)
+          current += batchSize
           true
         }
       }
@@ -99,6 +149,83 @@ object ColumnarReaderFactory extends PartitionReaderFactory {
 
       override def close(): Unit = batch.close()
     }
+  }
+
+  // this was copied from Spark ArrowUtils
+  /** Maps data type from Spark to Arrow. NOTE: timeZoneId required for TimestampTypes */
+  private def toArrowType(dt: DataType, timeZoneId: String): ArrowType = dt match {
+    case BooleanType => ArrowType.Bool.INSTANCE
+    case ByteType => new ArrowType.Int(8, true)
+    case ShortType => new ArrowType.Int(8 * 2, true)
+    case IntegerType => new ArrowType.Int(8 * 4, true)
+    case LongType => new ArrowType.Int(8 * 8, true)
+    case FloatType => new ArrowType.FloatingPoint(FloatingPointPrecision.SINGLE)
+    case DoubleType => new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)
+    case StringType => ArrowType.Utf8.INSTANCE
+    case BinaryType => ArrowType.Binary.INSTANCE
+    // case DecimalType.Fixed(precision, scale) => new ArrowType.Decimal(precision, scale)
+    case DateType => new ArrowType.Date(DateUnit.DAY)
+    case TimestampType =>
+      if (timeZoneId == null) {
+        throw new UnsupportedOperationException(
+          s"${TimestampType.catalogString} must supply timeZoneId parameter")
+      } else {
+        new ArrowType.Timestamp(TimeUnit.MICROSECOND, timeZoneId)
+      }
+    case _ =>
+      throw new UnsupportedOperationException(s"Unsupported data type: ${dt.catalogString}")
+  }
+
+  // this was copied from Spark ArrowUtils
+  /** Maps field from Spark to Arrow. NOTE: timeZoneId required for TimestampType */
+  private def toArrowField(
+      name: String, dt: DataType, nullable: Boolean, timeZoneId: String): Field = {
+    dt match {
+      case ArrayType(elementType, containsNull) =>
+        val fieldType = new FieldType(nullable, ArrowType.List.INSTANCE, null)
+        new Field(name, fieldType,
+          Seq(toArrowField("element", elementType, containsNull, timeZoneId)).asJava)
+      case StructType(fields) =>
+        val fieldType = new FieldType(nullable, ArrowType.Struct.INSTANCE, null)
+        new Field(name, fieldType,
+          fields.map { field =>
+            toArrowField(field.name, field.dataType, field.nullable, timeZoneId)
+          }.toSeq.asJava)
+      case MapType(keyType, valueType, valueContainsNull) =>
+        val mapType = new FieldType(nullable, new ArrowType.Map(false), null)
+        // Note: Map Type struct can not be null, Struct Type key field can not be null
+        new Field(name, mapType,
+          Seq(toArrowField(MapVector.DATA_VECTOR_NAME,
+            new StructType()
+              .add(MapVector.KEY_NAME, keyType, nullable = false)
+              .add(MapVector.VALUE_NAME, valueType, nullable = valueContainsNull),
+            nullable = false,
+            timeZoneId)).asJava)
+      case dataType =>
+        val fieldType = new FieldType(nullable, toArrowType(dataType, timeZoneId), null)
+        new Field(name, fieldType, Seq.empty[Field].asJava)
+    }
+  }
+
+  private def fillArrowVec(dt: DataType, vec: ValueVector, numRows: Int): Unit = dt match {
+    case IntegerType => {
+      val vector = vec.asInstanceOf[IntVector]
+      (0 until numRows).foreach { i =>
+        vector.setSafe(i, i)
+      }
+      vector.setNull(numRows)
+      vector.setValueCount(numRows + 1)
+    }
+    case _ =>
+      throw new UnsupportedOperationException(s"Unsupported data type: ${dt.catalogString}")
+  }
+
+  private def setupArrowVector(name: String, dataType: DataType): ValueVector = {
+    val rootAllocator = new RootAllocator(Long.MaxValue)
+    val allocator = rootAllocator.newChildAllocator(s"$name", 0, Long.MaxValue)
+    val vector = toArrowField(s"field$name", dataType, nullable = true, null)
+      .createVector(allocator).asInstanceOf[IntVector]
+    vector
   }
 }
 
