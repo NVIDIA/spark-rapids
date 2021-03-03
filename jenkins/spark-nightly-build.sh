19d1f0504993b38e3691fbaa506c86fe02b21512
Jason Lowe <jlowe@nvidia.com>
2021-03-03 08:04:41 -0600

    Add shim for Spark 3.0.3 (#1834)
    * Add shim for Spark 3.0.3
    
    Signed-off-by: Jason Lowe <jlowe@nvidia.com>
    
    * Add premerge testing for Spark 3.0.2 and Spark 3.0.3

--- jenkins/spark-nightly-build.sh
+++ jenkins/spark-nightly-build.sh
@@ -25,6 +25,7 @@ mvn -U -B -Pinclude-databricks,snapshot-shims clean deploy $MVN_URM_MIRROR -Dmav
 # Run unit tests against other spark versions
 mvn -U -B -Pspark301tests,snapshot-shims test $MVN_URM_MIRROR -Dmaven.repo.local=$M2DIR
 mvn -U -B -Pspark302tests,snapshot-shims test $MVN_URM_MIRROR -Dmaven.repo.local=$M2DIR
+mvn -U -B -Pspark303tests,snapshot-shims test $MVN_URM_MIRROR -Dmaven.repo.local=$M2DIR
 mvn -U -B -Pspark311tests,snapshot-shims test $MVN_URM_MIRROR -Dmaven.repo.local=$M2DIR
 
 # Parse cudf and spark files from local mvn repo
