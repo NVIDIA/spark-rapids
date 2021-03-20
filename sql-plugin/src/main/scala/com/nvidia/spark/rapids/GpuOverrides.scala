b1b2c8ff60a4977538e271f05a7e46d183284fd8
Raza Jafri <razajafri@users.noreply.github.com>
2021-03-19 18:43:13 -0700

    Update sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
    Co-authored-by: Gera Shegalov <gshegalov@nvidia.com>
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
@@ -2690,7 +2690,7 @@ object GpuOverrides {
       "The backend for the union operator",
       ExecChecks(TypeSig.commonCudfTypes + TypeSig.NULL + TypeSig.DECIMAL +
         TypeSig.STRUCT.nested(TypeSig.commonCudfTypes + TypeSig.NULL + TypeSig.DECIMAL)
-        .withPsNote(TypeEnum.STRUCT, "unionByName will not optionally fill missing columns with " +
+        .withPsNote(TypeEnum.STRUCT, "unionByName will not optionally impute nulls for missing struct fields  " +
           "nulls when the col has structs"), TypeSig.all),
       (union, conf, p, r) => new SparkPlanMeta[UnionExec](union, conf, p, r) {
         override def convertToGpu(): GpuExec =
