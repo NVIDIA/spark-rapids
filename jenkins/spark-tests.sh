3d2f7d31c6f7b130426d2f193318279dfe41069d
Raza Jafri <rjafri@nvidia.com>
2021-04-06 13:19:12 -0700

    addressed review comments
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>

--- jenkins/spark-tests.sh
+++ jenkins/spark-tests.sh
@@ -90,12 +90,15 @@ jps
 
 echo "----------------------------START TEST------------------------------------"
 pushd $RAPIDS_INT_TESTS_HOME
-#only run cache tests with our serializer in nightly test for Spark version >= 3.1.1
-if [ "$(printf '%s\n' "3.1.1" "$SPARK_VER" | sort -V | head -n1)" = "3.1.1" ]; then
-  spark-submit $BASE_SPARK_SUBMIT_ARGS --conf spark.sql.cache.serializer=com.nvidia.spark.rapids.shims.spark311.ParquetCachedBatchSerializer --jars $RAPIDS_TEST_JAR ./runtests.py -v -rfExXs --std_input_path="$WORKSPACE/integration_tests/src/test/resources/" -k cache_test.py
-fi
 spark-submit $BASE_SPARK_SUBMIT_ARGS --jars $RAPIDS_TEST_JAR ./runtests.py -v -rfExXs --std_input_path="$WORKSPACE/integration_tests/src/test/resources/"
 spark-submit $BASE_SPARK_SUBMIT_ARGS $CUDF_UDF_TEST_ARGS --jars $RAPIDS_TEST_JAR ./runtests.py -m "cudf_udf" -v -rfExXs --cudf_udf
+#only run cache tests with our serializer in nightly test for Spark version >= 3.1.1
+if [ "$(printf '%s\n' "3.1.1" "$SPARK_VER" | sort -V | head -n1)" = "3.1.1" ]; then
+  SHIM_PACKAGE=${SPARK_VER} | sed 's/\.//g'
+  spark-submit $BASE_SPARK_SUBMIT_ARGS --conf spark.sql.cache.serializer=com.nvidia.spark.rapids
+  .shims.spark${SHIM_PACKAGE}.ParquetCachedBatchSerializer --jars $RAPIDS_TEST_JAR
+  ./runtests.py -v -rfExXs --std_input_path="$WORKSPACE/integration_tests/src/test/resources/" -k cache_test.py
+fi
 popd
 stop-slave.sh
 stop-master.sh
