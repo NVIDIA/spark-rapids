96898cfa76581e52f790af6eee27e490bc004f2b
Rong Ou <rong.ou@gmail.com>
2021-04-07 17:19:28 -0700

    really fix it
    Signed-off-by: Rong Ou <rong.ou@gmail.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsGdsStore.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsGdsStore.scala
@@ -82,7 +82,6 @@ class RapidsGdsStore(
       closeOnExcept(DeviceMemoryBuffer.allocate(size)) { buffer =>
         CuFile.readFileToDeviceBuffer(buffer, path, fileOffset)
         logDebug(s"Created device buffer for $path $fileOffset:$size via GDS")
-        buffer.incRefCount()
         buffer
       }
     }
