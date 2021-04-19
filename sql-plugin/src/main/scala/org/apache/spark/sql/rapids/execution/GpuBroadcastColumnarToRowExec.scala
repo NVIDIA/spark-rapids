e77bd9e6ff387c3a38d5a5dcd9ceea3b604afb70
Andy Grove <andygrove@nvidia.com>
2021-04-19 12:55:54 -0600

    Add license header and revert variable rename that caused compilation error
    Signed-off-by: Andy Grove <andygrove@nvidia.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/execution/GpuBroadcastColumnarToRowExec.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/execution/GpuBroadcastColumnarToRowExec.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2021, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.spark.sql.rapids.execution
 
 import java.util.UUID
@@ -27,9 +42,9 @@ import org.apache.spark.util.KnownSizeEstimation
  * converts the columnar results containing cuDF tables into Spark rows so that the results
  * can feed a CPU BroadcastHashJoin. This is required for exchange reuse in AQE.
  *
- * @param broadcast GpuBroadcastExchangeExecBase
+ * @param child GpuBroadcastExchangeExecBase
  */
-case class GpuBroadcastColumnarToRowExec(broadcast: GpuBroadcastExchangeExecBase)
+case class GpuBroadcastColumnarToRowExec(child: GpuBroadcastExchangeExecBase)
     extends UnaryExecNode with GpuExec {
 
   import GpuMetric._
@@ -88,13 +103,14 @@ case class GpuBroadcastColumnarToRowExec(broadcast: GpuBroadcastExchangeExecBase
             sparkContext.setJobGroup(_runId.toString, s"broadcast exchange (runId ${_runId})",
               interruptOnCancel = true)
 
+            val broadcastChild = child.child
             val f = GpuColumnarToRowExecParent
-                .makeIteratorFunc(broadcast.child.output, numOutputRows, NoopMetric, totalTime)
+                .makeIteratorFunc(broadcastChild.output, numOutputRows, NoopMetric, totalTime)
 
             val collectRange = new NvtxWithMetrics("broadcast collect", NvtxColor.GREEN,
               collectTime)
             val serializedBatch: SerializeConcatHostBuffersDeserializeBatch = try {
-              val data = broadcast.child.executeColumnar().map(cb => try {
+              val data = broadcastChild.executeColumnar().map(cb => try {
                 new SerializeBatchDeserializeHostBuffer(cb)
               } finally {
                 cb.close()
@@ -118,7 +134,7 @@ case class GpuBroadcastColumnarToRowExec(broadcast: GpuBroadcastExchangeExecBase
 
               // Convert batches to rows and create a broadcast relation
               val rows = f(Seq(batch).iterator).toArray
-              val relation = broadcast.mode.transform(rows)
+              val relation = child.mode.transform(rows)
 
               val dataSize = relation match {
                 case map: KnownSizeEstimation =>
