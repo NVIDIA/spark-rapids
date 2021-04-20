16c65fc76a43757a114f200138d88251f4e4b079
Tim Liu <timl@nvidia.com>
2021-04-20 12:00:12 +0800

    init scripts user is root

--- jenkins/databricks/init_cuda11_runtime.sh
+++ jenkins/databricks/init_cuda11_runtime.sh
@@ -20,6 +20,6 @@
 
 wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda_11.0.2_450.51.05_linux.run
 
-sudo sh cuda_11.0.2_450.51.05_linux.run --toolkit --silent
+sh cuda_11.0.2_450.51.05_linux.run --toolkit --silent
 
 rm cuda_11.0.2_450.51.05_linux.run
