8212710f3a57ca8846d8e718add8add22d34c5fd
Bobby Wang <wbo4958@gmail.com>
2021-05-13 15:02:11 +0800

    improvement

--- integration_tests/src/main/python/array_test.py
+++ integration_tests/src/main/python/array_test.py
@@ -64,8 +64,8 @@ def test_make_array_of_array(data_gen):
     (s1, s2) = gen_scalars_for_sql(data_gen, 2, force_no_nulls=not isinstance(data_gen, NullGen))
     assert_gpu_and_cpu_are_equal_collect(
             lambda spark : binary_op_df(spark, data_gen).selectExpr(
-                'array(array(1), array(2), array(null), array())',
-                'array(array(), array(null))',
+                'array(array(1, 2, 3), array(2), array(null), array())',
+                'array(array(), array(null), array(a, b))',
                 'array(array(b, a, null, {}, {}), array(a, b), array(), array(null))'.format(s1, s2)))
 
 
