3bcc9c49e3b623dc0790f22f77dd6931f6ebc0c0
Alessandro Bellina <abellina@nvidia.com>
2021-05-21 10:55:42 -0500

    Update attempt earlier to remove need fix in log

--- shuffle-plugin/src/main/scala/com/nvidia/spark/rapids/shuffle/ucx/UCX.scala
+++ shuffle-plugin/src/main/scala/com/nvidia/spark/rapids/shuffle/ucx/UCX.scala
@@ -183,16 +183,16 @@ class UCX(executor: BlockManagerId, rapidsConf: RapidsConf) extends AutoCloseabl
         var attempt = 0
         while (listener.isEmpty && attempt < maxRetries) {
           val sockAddress = new InetSocketAddress(executor.host, startPort + attempt)
+          attempt += 1
           try {
             ucpListenerParams.setSockAddr(sockAddress)
             listener = Option(worker.newListener(ucpListenerParams))
           } catch {
             case _: UcxException =>
               logDebug(s"Failed to bind UcpListener on $sockAddress. " +
-                s"Attempt ${attempt + 1} out of $maxRetries.")
+                s"Attempt $attempt out of $maxRetries.")
               listener = None
           }
-          attempt += 1
         }
         if (listener.isEmpty) {
           throw new BindException(s"Couldn't start UcpListener " +
