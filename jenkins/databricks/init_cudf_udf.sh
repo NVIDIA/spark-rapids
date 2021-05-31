6dc4377f4f344d5f4fc7d7f2ce481f0daf03d193
pxLi <pxli@nyu.edu>
2021-05-31 15:54:29 +0800

    Init branch-21.08 (#2543)
    Signed-off-by: Peixin Li <pxli@nyu.edu>

--- jenkins/databricks/init_cudf_udf.sh
+++ jenkins/databricks/init_cudf_udf.sh
@@ -18,7 +18,7 @@
 # The initscript to set up environment for the cudf_udf tests on Databricks
 # Will be automatically pushed into the dbfs:/databricks/init_scripts once it is updated.
 
-CUDF_VER=${CUDF_VER:-21.06}
+CUDF_VER=${CUDF_VER:-21.08}
 
 # Use mamba to install cudf-udf packages to speed up conda resolve time
 base=$(conda info --base)
