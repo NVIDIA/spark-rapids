45b5c14335ed6c88905724257f81fdfd51c889b8
Thomas Graves <tgraves@nvidia.com>
2021-06-01 09:08:19 -0500

    Round rank and make percentage

--- rapids-4-spark-tools/src/main/scala/org/apache/spark/sql/rapids/tool/profiling/ApplicationInfo.scala
+++ rapids-4-spark-tools/src/main/scala/org/apache/spark/sql/rapids/tool/profiling/ApplicationInfo.scala
@@ -722,7 +722,7 @@ class ApplicationInfo(
   def qualificationDurationSumSQL: String = {
     s"""select first(sqlID) as sqlID, first(appName) as appName,
        |first(appID) as appID,
-       |sum(dfDuration) / first(appDuration) as dfRankTotal,
+       |ROUND((sum(dfDuration) * 100) / first(appDuration), 2) as dfRankTotal,
        |concat_ws(",", collect_list(potentialProblems)) as potentialProblems,
        |sum(dfDuration) as dfDurationFinal,
        |first(appDuration) as appDuration
