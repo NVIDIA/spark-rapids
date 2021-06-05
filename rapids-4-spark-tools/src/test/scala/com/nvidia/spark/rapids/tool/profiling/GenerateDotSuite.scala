272d55285cd69180a66fe822dad8168ad99ee42a
Andy Grove <andygrove@nvidia.com>
2021-06-04 20:20:35 -0600

    Implement test for qualification tool sql metric aggregates (#2591)
    * Fix package name
    
    Signed-off-by: Andy Grove <andygrove@nvidia.com>
    
    * Rename to ToolTestUtils
    
    Signed-off-by: Andy Grove <andygrove@nvidia.com>
    
    * Implement basic structure for test
    
    Signed-off-by: Andy Grove <andygrove@nvidia.com>
    
    * Test checks that number of tasks is correct
    
    Signed-off-by: Andy Grove <andygrove@nvidia.com>
    
    * Add more tests
    
    Signed-off-by: Andy Grove <andygrove@nvidia.com>
    
    * Add cpuTime test, commented out
    
    Signed-off-by: Andy Grove <andygrove@nvidia.com>
    
    * Change executorCpuTime logic to match qualification tool
    
    Signed-off-by: Andy Grove <andygrove@nvidia.com>
    
    * use dedicated spark session in test
    
    * create new SparkSession before each test
    
    Signed-off-by: Andy Grove <andygrove@nvidia.com>

--- rapids-4-spark-tools/src/test/scala/com/nvidia/spark/rapids/tool/profiling/GenerateDotSuite.scala
+++ rapids-4-spark-tools/src/test/scala/com/nvidia/spark/rapids/tool/profiling/GenerateDotSuite.scala
@@ -15,11 +15,9 @@
  */
 package com.nvidia.spark.rapids.tool.profiling
 
-import java.io.{File, FilenameFilter}
-
 import scala.io.Source
 
-import com.google.common.io.Files
+import com.nvidia.spark.rapids.tool.ToolTestUtils
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 import org.apache.spark.internal.Logging
@@ -32,76 +30,50 @@ class GenerateDotSuite extends FunSuite with BeforeAndAfterAll with Logging {
   }
 
   test("Generate DOT") {
-    val eventLogDir = Files.createTempDir()
-    eventLogDir.deleteOnExit()
+    TrampolineUtil.withTempDir { eventLogDir =>
+      val eventLog = ToolTestUtils.generateEventLog(eventLogDir, "dot") { spark =>
+        import spark.implicits._
+        val t1 = Seq((1, 2), (3, 4)).toDF("a", "b")
+        t1.createOrReplaceTempView("t1")
+        spark.sql("SELECT a, MAX(b) FROM t1 GROUP BY a ORDER BY a")
+      }
 
-    lazy val spark = SparkSession
-      .builder()
-      .master("local[*]")
-      .appName("Rapids Spark Profiling Tool Unit Tests")
-      .config("spark.eventLog.enabled", "true")
-      .config("spark.eventLog.dir", eventLogDir.getAbsolutePath)
-      .getOrCreate()
+      // create new session for tool to use
+      val spark2 = SparkSession
+        .builder()
+        .master("local[*]")
+        .appName("Rapids Spark Profiling Tool Unit Tests")
+        .getOrCreate()
 
-    // generate some events
-    import spark.implicits._
-    val t1 = Seq((1, 2), (3, 4)).toDF("a", "b")
-    t1.createOrReplaceTempView("t1")
-    val df = spark.sql("SELECT a, MAX(b) FROM t1 GROUP BY a ORDER BY a")
-    df.collect()
+      TrampolineUtil.withTempDir { dotFileDir =>
+        val appArgs = new ProfileArgs(Array(
+          "--output-directory",
+          dotFileDir.getAbsolutePath,
+          "--generate-dot",
+          eventLog))
+        ProfileMain.mainInternal(spark2, appArgs)
 
-    // close the event log
-    spark.close()
+        // assert that a file was generated
+        val dotDirs = ToolTestUtils.listFilesMatching(dotFileDir, _.startsWith("local"))
+        assert(dotDirs.length === 2)
 
-    // find the event log
-    val files = listFilesMatching(eventLogDir, !_.startsWith("."))
-    assert(files.length === 1)
-    val eventLog = files.head.getAbsolutePath
-
-    // create new session for tool to use
-    val spark2 = SparkSession
-      .builder()
-      .master("local[*]")
-      .appName("Rapids Spark Profiling Tool Unit Tests")
-      .getOrCreate()
-
-    val dotFileDir = Files.createTempDir()
-    dotFileDir.deleteOnExit()
-
-    val appArgs = new ProfileArgs(Array(
-      "--output-directory",
-      dotFileDir.getAbsolutePath,
-      "--generate-dot",
-      eventLog
-    ))
-    ProfileMain.mainInternal(spark2, appArgs)
-
-    // assert that a file was generated
-    val dotDirs = listFilesMatching(dotFileDir, _.startsWith("local"))
-    assert(dotDirs.length === 2)
-
-    // assert that the generated files looks something like what we expect
-    var hashAggCount = 0
-    for (dir <- dotDirs) {
-      val dotFiles = listFilesMatching(dir, _.endsWith(".dot"))
-      assert(dotFiles.length === 1)
-      val source = Source.fromFile(dotFiles.head)
-      try {
-        val lines = source.getLines().toArray
-        assert(lines.head === "digraph G {")
-        assert(lines.last === "}")
-        hashAggCount += lines.count(_.contains("HashAggregate"))
-      } finally {
-        source.close()
+        // assert that the generated files looks something like what we expect
+        var hashAggCount = 0
+        for (dir <- dotDirs) {
+          val dotFiles = ToolTestUtils.listFilesMatching(dir, _.endsWith(".dot"))
+          assert(dotFiles.length === 1)
+          val source = Source.fromFile(dotFiles.head)
+          try {
+            val lines = source.getLines().toArray
+            assert(lines.head === "digraph G {")
+            assert(lines.last === "}")
+            hashAggCount += lines.count(_.contains("HashAggregate"))
+          } finally {
+            source.close()
+          }
+        }
+        assert(hashAggCount === 2)
       }
     }
-    assert(hashAggCount === 2)
   }
-
-  private def listFilesMatching(dir: File, matcher: String => Boolean): Array[File] = {
-    dir.listFiles(new FilenameFilter {
-      override def accept(file: File, s: String): Boolean = matcher(s)
-    })
-  }
-
 }
