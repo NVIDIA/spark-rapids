b7f250b584030c89bc4f5e9b9100e5f0115c7113
Mithun RK <mythrocks@gmail.com>
2021-06-07 23:55:48 -0700

    Delegate to GpuBaseWindowExecMeta, from Databricks Shims.

--- shims/spark311db/src/main/scala/com/nvidia/spark/rapids/shims/spark311db/GpuRunningWindowExec.scala
+++ shims/spark311db/src/main/scala/com/nvidia/spark/rapids/shims/spark311db/GpuRunningWindowExec.scala
@@ -17,7 +17,7 @@
 package com.nvidia.spark.rapids.shims.spark311db
 
 import com.databricks.sql.execution.window.RunningWindowFunctionExec
-import com.nvidia.spark.rapids.{BaseExprMeta, DataFromReplacementRule, GpuExec, GpuOverrides, GpuWindowExec, RapidsConf, RapidsMeta, SparkPlanMeta}
+import com.nvidia.spark.rapids.{BaseExprMeta, DataFromReplacementRule, GpuBaseWindowExecMeta, GpuExec, GpuOverrides, GpuWindowExec, RapidsConf, RapidsMeta, SparkPlanMeta}
 
 import org.apache.spark.sql.catalyst.expressions.{Expression, NamedExpression, SortOrder}
 
@@ -28,31 +28,10 @@ class GpuRunningWindowExecMeta(runningWindowFunctionExec: RunningWindowFunctionE
     conf: RapidsConf,
     parent: Option[RapidsMeta[_, _, _]],
     rule: DataFromReplacementRule)
-    extends SparkPlanMeta[RunningWindowFunctionExec](runningWindowFunctionExec, conf, parent, rule) {
+    extends GpuBaseWindowExecMeta[RunningWindowFunctionExec](runningWindowFunctionExec, conf, parent, rule) {
 
-  val windowExpressions: Seq[BaseExprMeta[NamedExpression]] =
-    runningWindowFunctionExec.windowExpressionList.map(GpuOverrides.wrapExpr(_, conf, Some(this)))
-  val partitionSpec: Seq[BaseExprMeta[Expression]] =
-    runningWindowFunctionExec.partitionSpec.map(GpuOverrides.wrapExpr(_, conf, Some(this)))
-  val orderSpec: Seq[BaseExprMeta[SortOrder]] =
-    runningWindowFunctionExec.orderSpec.map(GpuOverrides.wrapExpr(_, conf, Some(this)))
-
-  override def tagPlanForGpu(): Unit = {
-    windowExpressions.map(meta => meta.wrapped)
-        .filter(expr => !expr.isInstanceOf[NamedExpression])
-        .foreach(_ => willNotWorkOnGpu(because = "Unexpected query plan with Windowing functions; " +
-            "cannot convert for GPU execution. " +
-            "(Detail: WindowExpression not wrapped in `NamedExpression`.)"))
-  }
-
-  override def convertToGpu(): GpuExec = {
-    GpuWindowExec(
-      windowExpressions.map(_.convertToGpu()),
-      partitionSpec.map(_.convertToGpu()),
-      orderSpec.map(_.convertToGpu().asInstanceOf[SortOrder]),
-      childPlans.head.convertIfNeeded(),
-      true
-    )
-  }
+  override def getInputWindowExpressions: Seq[NamedExpression] = runningWindowFunctionExec.windowExpressionList
+  override def getPartitionSpecs: Seq[Expression] = runningWindowFunctionExec.partitionSpec
+  override def getOrderSpecs: Seq[SortOrder] = runningWindowFunctionExec.orderSpec
+  override def getResultColumnsOnly: Boolean = true
 }
-
