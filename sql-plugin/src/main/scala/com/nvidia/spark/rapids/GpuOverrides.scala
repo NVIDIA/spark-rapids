3b3d511427b9b53801183f36007b82c65c8c019b
Liangcai Li <firestarmanllc@gmail.com>
2021-06-21 08:49:56 +0800

    Support `KnownNotNull` running on GPU (#2707)
    * Support KnownNotNull running on GPU
    
    Signed-off-by: Firestarman <firestarmanllc@gmail.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
@@ -445,6 +445,9 @@ object GpuOverrides {
 
   private[this] val _commonTypes = TypeSig.commonCudfTypes + TypeSig.NULL + TypeSig.DECIMAL
 
+  private[this] val _notNullCudfTypes = (TypeSig.commonCudfTypes + TypeSig.DECIMAL +
+    TypeSig.BINARY + TypeSig.CALENDAR + TypeSig.ARRAY + TypeSig.MAP + TypeSig.STRUCT).nested()
+
   val pluginSupportedOrderableSig: TypeSig = _commonTypes + TypeSig.STRUCT.nested(_commonTypes)
 
   private[this] def isStructType(dataType: DataType) = dataType match {
@@ -1339,6 +1342,14 @@ object GpuOverrides {
       (a, conf, p, r) => new UnaryExprMeta[KnownFloatingPointNormalized](a, conf, p, r) {
         override def convertToGpu(child: Expression): GpuExpression =
           GpuKnownFloatingPointNormalized(child)
+      }),
+    expr[KnownNotNull](
+      "Tag an expression as known to not be null",
+      ExprChecks.unaryProjectNotLambdaInputMatchesOutput(
+        _notNullCudfTypes, TypeSig.all),
+      (k, conf, p, r) => new UnaryExprMeta[KnownNotNull](k, conf, p, r) {
+        override def convertToGpu(child: Expression): GpuExpression =
+          GpuKnownNotNull(child)
       }),
     expr[DateDiff](
       "Returns the number of days from startDate to endDate",
