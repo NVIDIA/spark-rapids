c9e01873185a2ce6e8382fba2995ae04bf818a1f
Andy Grove <andygrove@nvidia.com>
2021-06-23 16:14:05 -0600

    Revert temp debug println
    Signed-off-by: Andy Grove <andygrove@nvidia.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/CostBasedOptimizer.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/CostBasedOptimizer.scala
@@ -60,7 +60,7 @@ class CostBasedOptimizer extends Optimizer with Logging {
    * @return A list of optimizations that were applied
    */
   def optimize(conf: RapidsConf, plan: SparkPlanMeta[SparkPlan]): Seq[Optimization] = {
-    println("CBO optimizing plan")
+    logTrace("CBO optimizing plan")
     val cpuCostModel = new CpuCostModel(conf)
     val gpuCostModel = new GpuCostModel(conf)
     val optimizations = new ListBuffer[Optimization]()
