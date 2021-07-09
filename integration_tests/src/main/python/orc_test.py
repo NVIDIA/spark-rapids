5b32a8db3e6be34faf2b53379c31fe2cc599bd0e
Firestarman <firestarmanllc@gmail.com>
2021-07-09 15:40:47 +0800

    Update the tests
    Signed-off-by: Firestarman <firestarmanllc@gmail.com>

--- integration_tests/src/main/python/orc_test.py
+++ integration_tests/src/main/python/orc_test.py
@@ -49,18 +49,22 @@ orc_basic_gens = [byte_gen, short_gen, int_gen, long_gen, float_gen, double_gen,
     string_gen, boolean_gen, DateGen(start=date(1590, 1, 1)),
     TimestampGen(start=datetime(1590, 1, 1, tzinfo=timezone.utc))]
 
-orc_basic_struct_gen = StructGen([['child'+str(ind), sub_gen] for ind, sub_gen in enumerate(orc_basic_gens)])
+# Set to true after the issue https://github.com/rapidsai/cudf/issues/8704 is fixed.
+struct_gen_nullable = False
+
+orc_basic_struct_gen = StructGen([['child'+str(ind), sub_gen] for ind, sub_gen in enumerate(orc_basic_gens)],
+    struct_gen_nullable)
 
 # Some array gens, but not all because of nesting
 orc_array_gens_sample = [ArrayGen(sub_gen) for sub_gen in orc_basic_gens] + [
-    ArrayGen(ArrayGen(short_gen, max_length=10), max_length=10),
-    ArrayGen(ArrayGen(string_gen, max_length=10), max_length=10),
-    ArrayGen(StructGen([['child0', byte_gen], ['child1', string_gen], ['child2', float_gen]]))]
+    ArrayGen(ArrayGen(short_gen, max_length=10)),
+    ArrayGen(ArrayGen(string_gen, max_length=10)),
+    ArrayGen(StructGen([['child0', byte_gen], ['child1', string_gen], ['child2', float_gen]], struct_gen_nullable))]
 
 # Some struct gens, but not all because of nesting
 orc_struct_gens_sample = [orc_basic_struct_gen,
-    StructGen([['child0', byte_gen], ['child1', orc_basic_struct_gen]]),
-    StructGen([['child0', ArrayGen(short_gen)], ['child1', double_gen]])]
+    StructGen([['child0', byte_gen], ['child1', orc_basic_struct_gen]], struct_gen_nullable),
+    StructGen([['child0', ArrayGen(short_gen)], ['child1', double_gen]], struct_gen_nullable)]
 
 orc_gens_list = [orc_basic_gens,
     orc_array_gens_sample,
