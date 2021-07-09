2844e45639916790c9c0e63e683b4d1c9f384e58
Thomas Graves <tgraves@apache.org>
2021-07-09 10:21:37 -0500

    simplify weighted score for read data types
    Signed-off-by: Thomas Graves <tgraves@apache.org>

--- tools/src/main/scala/com/nvidia/spark/rapids/tool/qualification/PluginTypeChecker.scala
+++ tools/src/main/scala/com/nvidia/spark/rapids/tool/qualification/PluginTypeChecker.scala
@@ -23,16 +23,25 @@ import org.apache.spark.internal.Logging
 
 class PluginTypeChecker extends Logging {
 
-  // map of file format => Map[datatype => supported string]
-  val allSplit = new HashMap[String, Map[String, Seq[String]]]()
-  val allSupportedReadSources = readSupportedTypesForPlugin
+  private val NS = "NS"
+  private val PS = "PS"
+  private val PSPART = "PS*"
+  private val SPART = "S*"
+  // configured off
+  private val CO = "CO"
+  private val NA = "NA"
+
+  // map of file format => Map[support category => Seq[Datatypes for that category]]
+  // contains the details of formats to which ones have datatypes not supported,
+  // partially supported or S*
+  val formatsToSupportedCategory = readSupportedTypesForPlugin
 
   // file format should be like this:
   // Format,Direction,BOOLEAN,BYTE,SHORT,INT,LONG,FLOAT,DOUBLE,DATE,...
   // CSV,read,S,S,S,S,S,S,S,S,S*,S,NS,NA,NS,NA,NA,NA,NA,NA
-  private def readSupportedTypesForPlugin: Map[String, Map[String, String]] = {
+  private def readSupportedTypesForPlugin: Map[String, Map[String, Seq[String]]] = {
     // get the types the Rapids Plugin supports
-    val allSupportedReadSources = HashMap.empty[String, Map[String, String]]
+    val allSupportedReadSources = HashMap.empty[String, Map[String, Seq[String]]]
     val dsFile = "supportedDataSource.csv"
     val source = Source.fromResource(dsFile)
     try {
@@ -52,34 +61,18 @@ class PluginTypeChecker extends Logging {
         if (direction.equals("read")) {
           val dataTypesToSup = header.drop(2).zip(cols.drop(2)).toMap
           logWarning("datatypes to sup are: " + dataTypesToSup)
-          val nsTypes = dataTypesToSup.filter { case (dt, sup) =>
-            sup.equals("NA") || sup.equals("NS") || sup.equals("CO")
-          }.keys.toSeq
-          val allNsTypes = nsTypes.flatMap { t =>
-            getOtherTypes(t) :+ t
-          }
-          val psTypes = dataTypesToSup.filter { case (dt, sup) =>
-            sup.equals("PS") || sup.equals("PS*")
-          }.keys.toSeq
-          val allPsTypes = psTypes.flatMap { t =>
-            getOtherTypes(t) :+ t
-          }
-          val sPartTypes = dataTypesToSup.filter { case (dt, sup) =>
-            // we care for decimal
-            sup.equals("S*")
-          }.keys.toSeq
-          val allsPartTypes = sPartTypes.flatMap { t =>
-            getOtherTypes(t) :+ t
-          }
-          val allBySup = HashMap("NS" -> allNsTypes, "PS" -> allPsTypes, "S*" -> allsPartTypes)
-          allSplit.put(format, allBySup.toMap)
-          allSupportedReadSources(format) = dataTypesToSup
+          val nsTypes = dataTypesToSup.filter { case (_, sup) =>
+            sup.equals(NA) || sup.equals(NS) || sup.equals(CO)
+          }.keys.toSeq.map(_.toLowerCase)
+          val allNsTypes = nsTypes.flatMap(t => getOtherTypes(t) :+ t)
+          val allBySup = HashMap(NS -> allNsTypes)
+          allSupportedReadSources.put(format, allBySup.toMap)
         }
       }
     } finally {
       source.close()
     }
-    allSupportedReadSources.toMap
+    allSupportedReadSources
   }
 
   def getOtherTypes(typeRead: String): Seq[String] = {
@@ -95,53 +88,32 @@ class PluginTypeChecker extends Logging {
     }
   }
 
+  // Parsing the schema string is very complex when you get into nested types, so for now
+  // we do the simpler thing of checking to see if the schema string contains types we
+  // don't support.
   // NOTE, UDT doesn't show up in the event log, when its written, it gets written as
   // other types since parquet/orc has to know about it
   def scoreReadDataTypes(format: String, schema: String): Double = {
-    logWarning("data source is: " + format + " all sploit is: " + allSplit)
+    val schemaLower = schema.toLowerCase
+    logWarning("data source is: " + format + " all sploit is: " + formatsToSupportedCategory)
     val formatInLower = format.toLowerCase
-    val score = if (allSupportedReadSources.contains(formatInLower)) {
-      logWarning(s"data source format ${formatInLower} is supported by plugin")
-      val typesBySup = allSplit(formatInLower)
-      val hasNotSupported = typesBySup("NS").exists(schema.contains(_))
-      logWarning(s"has not supported is: $hasNotSupported")
-      val formatScore = if (hasNotSupported) {
-        0.0
-      } else {
-        val hasSupportedPart = typesBySup("S*").exists(schema.contains(_))
-        logWarning(s"has supported partial supported is: $hasSupportedPart")
-        if (hasSupportedPart) {
-          // timestamps or decimals, check for decimals specifically
-          // could look for decimal(4,2) precision but might have operators
-          // that make it worse so just assume worst case
-          if (schema.contains("decimal")) {
-            0.0
-          } else {
-            1.0
-          }
+    val typesBySup = formatsToSupportedCategory.get(formatInLower)
+    val score = typesBySup match {
+      case Some(dtSupMap) =>
+        // check if any of the not supported types are in the schema
+        if (dtSupMap(NS).exists(t => schemaLower.contains(t.toLowerCase()))) {
+          0.0
         } else {
-          val hasPartSupported = typesBySup("PS").exists(schema.contains(_))
-          logWarning(s"has part supported is: $hasPartSupported")
-          if (hasPartSupported) {
-            // TODO - is this to pesimistic?
-            0.75
-          } else {
-            // either supported or we don't know
-            1.0
-          }
+          // started out giving different weights based on partial support and so forth
+          // but decided to be optimistic and not penalize if we don't know, perhaps
+          // make it smarter later.
+          // schema could also be incomplete, but similarly don't penalize since we don't
+          // know.
+          1.0
         }
-      }
-
-      if (formatScore == 0.0) {
+      case None =>
+        // assume we don't support that format
         0.0
-      } else {
-        // schema could be incomplete but just report what we were able to parse.
-        // Instead could mark some off but if the rest is fine we penalize for nothiner.
-        formatScore
-      }
-    } else {
-      // format not supported
-      0.0
     }
     logWarning(s"read score is $score")
     score
