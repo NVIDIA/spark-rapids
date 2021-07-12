0c73e1ef23f6cc99b5018099b2199add6c1e6402
Jason Lowe <jlowe@nvidia.com>
2021-07-12 15:34:16 -0500

    Work around libcudf sort-hinted aggregations bug

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/aggregate.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/aggregate.scala
@@ -500,7 +500,10 @@ class GpuHashAggregateIterator(
       override def next(): ColumnarBatch = {
         // batches coming out of the sort need to be merged
         withResource(keyBatchingIter.next()) { batch =>
-          computeAggregate(GpuColumnVector.extractColumns(batch), merge = true, isSorted = true)
+          // TODO: Normally we would want to hint to cudf that the data is already sorted on the
+          // grouping keys here, but this doesn't always produce the expected result due to a bug.
+          // See https://github.com/rapidsai/cudf/issues/8717 for details.
+          computeAggregate(GpuColumnVector.extractColumns(batch), merge = true, isSorted = false)
         }
       }
     }
