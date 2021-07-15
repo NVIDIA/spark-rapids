5eeb562f68e521390252fe3444fb40d89eec4241
Niranjan Artal <nartal@nvidia.com>
2021-07-15 15:23:31 -0700

    fix description
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- tools/src/main/scala/com/nvidia/spark/rapids/tool/qualification/QualificationArgs.scala
+++ tools/src/main/scala/com/nvidia/spark/rapids/tool/qualification/QualificationArgs.scala
@@ -47,7 +47,7 @@ Usage: java -cp rapids-4-spark-tools_2.12-<version>.jar:$SPARK_HOME/jars/*
     opt[String](required = false,
       descr = "Filter event logs whose application name matches " +
           "exactly or is a substring of input string. Regular expressions not supported." +
-          "For complement of eventlogs, use ~EVENTLOGNAME. Filter event logs based on the " +
+          "For complement of eventlogs, use ~APPLICATION_NAME. Filter event logs based on the " +
           "complement of a selection criterion. i.e Select all event logs except the ones which " +
           "have application name as the input string.")
   val matchEventLogs: ScallopOption[String] =
