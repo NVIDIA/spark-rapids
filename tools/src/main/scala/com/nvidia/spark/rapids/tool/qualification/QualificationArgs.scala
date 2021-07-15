19ed8aced8a2700b4b291fa927589299c7aaaf2f
Niranjan Artal <nartal@nvidia.com>
2021-07-15 15:20:03 -0700

    Update QualificationArgs
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- tools/src/main/scala/com/nvidia/spark/rapids/tool/qualification/QualificationArgs.scala
+++ tools/src/main/scala/com/nvidia/spark/rapids/tool/qualification/QualificationArgs.scala
@@ -45,8 +45,11 @@ Usage: java -cp rapids-4-spark-tools_2.12-<version>.jar:$SPARK_HOME/jars/*
           "eg: 100-oldest (for processing oldest 100 event logs)")
   val applicationName: ScallopOption[String] =
     opt[String](required = false,
-      descr = "Filter event logs whose application name matches exactly with input string" +
-          "i.e no regular expressions supported.")
+      descr = "Filter event logs whose application name matches " +
+          "exactly or is a substring of input string. Regular expressions not supported." +
+          "For complement of eventlogs, use ~EVENTLOGNAME. Filter event logs based on the " +
+          "complement of a selection criterion. i.e Select all event logs except the ones which " +
+          "have application name as the input string.")
   val matchEventLogs: ScallopOption[String] =
     opt[String](required = false,
       descr = "Filter event logs whose filenames contain the input string")
