1b6fbfc80eafc7e478cbd94310e38fb61a7c55b6
Thomas Graves <tgraves@nvidia.com>
2021-07-15 18:39:54 -0500

    qualification tool: add filtering by app start time (#2940)
    * Add logic to filter by time period
    
    Signed-off-by: Thomas Graves <tgraves@apache.org>
    
    * change name of param
    
    * fix matching default for days

--- tools/src/main/scala/com/nvidia/spark/rapids/tool/qualification/QualificationArgs.scala
+++ tools/src/main/scala/com/nvidia/spark/rapids/tool/qualification/QualificationArgs.scala
@@ -17,6 +17,8 @@ package com.nvidia.spark.rapids.tool.qualification
 
 import org.rogach.scallop.{ScallopConf, ScallopOption}
 
+import org.apache.spark.sql.rapids.tool.AppFilterImpl
+
 class QualificationArgs(arguments: Seq[String]) extends ScallopConf(arguments) {
 
   banner("""
@@ -47,6 +49,11 @@ Usage: java -cp rapids-4-spark-tools_2.12-<version>.jar:$SPARK_HOME/jars/*
     opt[String](required = false,
       descr = "Filter event logs whose application name matches exactly with input string" +
           "i.e no regular expressions supported.")
+  val startAppTime: ScallopOption[String] =
+    opt[String](required = false,
+      descr = "Filter event logs whose application start occurred within the past specified " +
+        "time period. Valid time periods are min(minute),h(hours),d(days),w(weeks)," +
+        "m(months). If a period is not specified it defaults to days.")
   val matchEventLogs: ScallopOption[String] =
     opt[String](required = false,
       descr = "Filter event logs whose filenames contain the input string")
@@ -98,6 +105,12 @@ Usage: java -cp rapids-4-spark-tools_2.12-<version>.jar:$SPARK_HOME/jars/*
   validate(readScorePercent) {
     case percent if (percent >= 0) && (percent <= 100) => Right(Unit)
     case _ => Left("Error, read score percent must be between 0 and 100.")
+  }
+
+  validate(startAppTime) {
+    case time if (AppFilterImpl.parseAppTimePeriod(time) > 0L) => Right(Unit)
+    case _ => Left("Time period specified, must be greater than 0 and valid periods " +
+      "are min(minute),h(hours),d(days),w(weeks),m(months).")
   }
 
   verify()
