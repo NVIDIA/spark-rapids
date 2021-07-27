662b88569140abaa41d052067349a0977d38dc59
Thomas Graves <tgraves@apache.org>
2021-07-27 17:36:57 -0500

    fix

--- tools/src/main/scala/com/nvidia/spark/rapids/tool/profiling/Analysis.scala
+++ tools/src/main/scala/com/nvidia/spark/rapids/tool/profiling/Analysis.scala
@@ -412,11 +412,11 @@ class Analysis(apps: Seq[ApplicationInfo], fileWriter: Option[ToolTextFileWriter
             case Some(avg) =>
               Seq(app.index.toString, tc.stageId.toString, tc.stageAttemptId.toString,
                 tc.taskId.toString, tc.attempt.toString,
-                ToolUtils.calculateAverage(tc.duration, 1000, 2).toString,
+                f"${tc.duration.toDouble / 1000}%1.2f".toString,
                 f"${avg.avgDuration / 1000}%1.2f".toString,
-                ToolUtils.calculateAverage(tc.sr_totalBytesRead, 1024/1024, 2).toString,
+                f"${tc.sr_totalBytesRead.toDouble / 1024 / 1024}%1.2f".toString,
                 f"${avg.avgShuffleReadBytes / 1024 / 1024}%1.2f".toString,
-                ToolUtils.calculateAverage(tc.peakExecutionMemory, 1024/1024, 2).toString,
+                f"${tc.peakExecutionMemory.toDouble / 1024 / 1024}%1.2f".toString,
                 tc.successful.toString,
                 ProfileUtils.truncateFailureStr(tc.endReason))
             case None =>
