a484336939e29e3c9d3838deff994aa6a2d63f8a
Thomas Graves <tgraves@apache.org>
2021-07-29 17:18:49 -0500

    fix

--- tools/src/test/scala/com/nvidia/spark/rapids/tool/profiling/GenerateDotSuite.scala
+++ tools/src/test/scala/com/nvidia/spark/rapids/tool/profiling/GenerateDotSuite.scala
@@ -55,7 +55,7 @@ class GenerateDotSuite extends FunSuite with BeforeAndAfterAll with Logging {
           dotFileDir.getAbsolutePath,
           "--generate-dot",
           eventLog))
-        ProfileMain.mainInternal(appArgs)12
+        ProfileMain.mainInternal(appArgs)
 
         val tempSubDir = new File(dotFileDir, Profiler.SUBDIR)
 
