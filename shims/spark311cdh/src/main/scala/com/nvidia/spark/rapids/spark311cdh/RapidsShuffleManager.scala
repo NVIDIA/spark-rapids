a9f48d9ecf8fd5e7ca9105c2df3e5bfe7208aeaa
Thomas Graves <tgraves@nvidia.com>
2021-08-20 08:43:55 -0500

    cdh changes

--- shims/spark311cdh/src/main/scala/com/nvidia/spark/rapids/spark311cdh/RapidsShuffleManager.scala
+++ shims/spark311cdh/src/main/scala/com/nvidia/spark/rapids/spark311cdh/RapidsShuffleManager.scala
@@ -17,7 +17,7 @@
 package com.nvidia.spark.rapids.spark311cdh
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.rapids.shims.spark311.RapidsShuffleInternalManager
+import org.apache.spark.sql.rapids.shims.spark311cdh.RapidsShuffleInternalManager
 
 /** A shuffle manager optimized for the RAPIDS Plugin for Apache Spark. */
 sealed class RapidsShuffleManager(
