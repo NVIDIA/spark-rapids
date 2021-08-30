2e2048093c8bbef4ae34b582623fd543e81d3913
Thomas Graves <tgraves@nvidia.com>
2021-08-30 15:15:15 +0000

    Cleanup extra dependencies and rename dependencies
    Signed-off-by: Thomas Graves <tgraves@nvidia.com>

--- jenkins/databricks/build.sh
+++ jenkins/databricks/build.sh
@@ -25,7 +25,7 @@ BASE_SPARK_VERSION_TO_INSTALL_DATABRICKS_JARS=$4
 BUILD_PROFILES=${BUILD_PROFILES:-'databricks311,!snapshot-shims'}
 BASE_SPARK_VERSION=${BASE_SPARK_VERSION:-'3.1.1'}
 # set this to anything (true) to skip building with mvn and it will only install the dependencies into .m2
-INSTALL_DEPS_ONLY=""
+INSTALL_DEPS_ONLY="true"
 # the version of Spark used when we install the Databricks jars in .m2
 # 3.1.0-databricks is add because its actually based on Spark 3.1.1
 BASE_SPARK_VERSION_TO_INSTALL_DATABRICKS_JARS=${BASE_SPARK_VERSION_TO_INSTALL_DATABRICKS_JARS:-$BASE_SPARK_VERSION}
@@ -308,8 +308,8 @@ else
     mvn -B install:install-file \
        -Dmaven.repo.local=$M2DIR \
        -Dfile=$JARDIR/$PARQUETFORMATJAR \
-       -DgroupId=org.apache.spark \
-       -DartifactId=spark-parquet-format-db_$SCALA_VERSION \
+       -DgroupId=org.apache.parquet \
+       -DartifactId=parquet-format \
        -Dversion=$SPARK_VERSION_TO_INSTALL_DATABRICKS_JARS \
        -Dpackaging=jar
 
