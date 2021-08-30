2c31105124bcb4c85f1ce0c2d0ad4b8b81343bbd
Raza Jafri <rjafri@nvidia.com>
2021-08-30 13:29:03 -0700

    removed unnecessary changes to the SparkBaseShim
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>

--- shims/spark311/src/main/scala/com/nvidia/spark/rapids/shims/spark311/SparkBaseShims.scala
+++ shims/spark311/src/main/scala/com/nvidia/spark/rapids/shims/spark311/SparkBaseShims.scala
@@ -834,12 +834,4 @@ abstract class SparkBaseShims extends SparkShims {
 
   override def hasCastFloatTimestampUpcast: Boolean = false
 
-  override def int96ParquetRebaseReadKey: String =
-    SQLConf.LEGACY_PARQUET_INT96_REBASE_MODE_IN_READ.key
-  override def int96ParquetRebaseWriteKey: String =
-    SQLConf.LEGACY_PARQUET_INT96_REBASE_MODE_IN_WRITE.key
-  override def int96ParquetRebaseRead(conf: SQLConf): String =
-    conf.getConf(SQLConf.LEGACY_PARQUET_INT96_REBASE_MODE_IN_READ)
-  override def int96ParquetRebaseWrite(conf: SQLConf): String =
-    conf.getConf(SQLConf.LEGACY_PARQUET_INT96_REBASE_MODE_IN_WRITE)
 }
