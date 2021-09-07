9ffe27d27ef5422fdd056de0e37485c804ffa2b7
Thomas Graves <tgraves@nvidia.com>
2021-09-07 16:32:15 +0000

    Working on 311db
    Signed-off-by: Thomas Graves <tgraves@nvidia.com>

--- shims/spark311db/src/main/scala/com/nvidia/spark/rapids/shims/spark311db/SparkBaseShims.scala
+++ shims/spark311db/src/main/scala/com/nvidia/spark/rapids/shims/spark311db/SparkBaseShims.scala
@@ -56,7 +56,7 @@ import org.apache.spark.sql.execution.window.WindowExecBase
 import org.apache.spark.sql.internal.{SQLConf, StaticSQLConf}
 import org.apache.spark.sql.rapids._
 import org.apache.spark.sql.rapids.execution.{GpuBroadcastExchangeExecBase, GpuBroadcastNestedLoopJoinExecBase, GpuShuffleExchangeExecBase, JoinTypeChecks}
-import org.apache.spark.sql.rapids.execution.python.GpuPythonUDF
+import org.apache.spark.sql.rapids.execution.python.{GpuAggregateInPandasExecMeta, GpuArrowEvalPythonExec, GpuMapInPandasExecMeta, GpuPythonUDF}
 import org.apache.spark.sql.rapids.shims.spark311db._
 import org.apache.spark.sql.sources.BaseRelation
 import org.apache.spark.sql.types._
