fe243c23475c29092c9eff63e09bd63c28b1399c
Chong Gao <res_life@163.com>
2021-09-09 19:58:36 +0800

    Orc read map (#3394)
    * orc read map
    
    Signed-off-by: Chong Gao <res_life@163.com>

--- integration_tests/src/main/python/orc_test.py
+++ integration_tests/src/main/python/orc_test.py
@@ -82,9 +82,23 @@ orc_struct_gens_sample = [orc_basic_struct_gen,
     StructGen([['child0', byte_gen], ['child1', orc_basic_struct_gen]]),
     StructGen([['child0', ArrayGen(short_gen)], ['child1', double_gen]])]
 
+orc_basic_map_gens = [simple_string_to_string_map_gen] + [MapGen(f(nullable=False), f()) for f in [
+    BooleanGen, ByteGen, ShortGen, IntegerGen, LongGen, FloatGen, DoubleGen,
+    lambda nullable=True: TimestampGen(start=datetime(1900, 1, 1, tzinfo=timezone.utc), nullable=nullable),
+    lambda nullable=True: DateGen(start=date(1590, 1, 1), nullable=nullable)]]
+
+# Some map gens, but not all because of nesting
+orc_map_gens_sample = orc_basic_map_gens + [
+    MapGen(StringGen(pattern='key_[0-9]', nullable=False), ArrayGen(string_gen), max_length=10),
+    MapGen(RepeatSeqGen(IntegerGen(nullable=False), 10), long_gen, max_length=10),
+    MapGen(StringGen(pattern='key_[0-9]', nullable=False), simple_string_to_string_map_gen),
+    MapGen(StructGen([['child0', byte_gen], ['child1', long_gen]], nullable=False),
+           StructGen([['child0', byte_gen], ['child1', long_gen]]))]
+
 orc_gens_list = [orc_basic_gens,
     orc_array_gens_sample,
     orc_struct_gens_sample,
+    orc_map_gens_sample,
     pytest.param([date_gen], marks=pytest.mark.xfail(reason='https://github.com/NVIDIA/spark-rapids/issues/131')),
     pytest.param([timestamp_gen], marks=pytest.mark.xfail(reason='https://github.com/NVIDIA/spark-rapids/issues/131'))]
 
