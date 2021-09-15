ec5643de2101be77514047da5841c275c5e68668
Jason Lowe <jlowe@nvidia.com>
2021-09-15 17:16:42 -0500

    Use NonFatal

--- udf-compiler/src/main/scala/com/nvidia/spark/udf/GpuScalaUDF.scala
+++ udf-compiler/src/main/scala/com/nvidia/spark/udf/GpuScalaUDF.scala
@@ -16,6 +16,8 @@
 
 package com.nvidia.spark.udf
 
+import scala.util.control.NonFatal
+
 import com.nvidia.spark.rapids.shims.v2.ShimExpression
 
 import org.apache.spark.SparkException
@@ -56,7 +58,7 @@ case class GpuScalaUDFLogical(udf: ScalaUDF) extends ShimExpression with Logging
           throw e
         }
         udf
-      case e: Exception =>
+      case NonFatal(e) =>
         val udfName = udf.udfName.getOrElse("<unknown>")
         logWarning(s"Unable to translate UDF $udfName: $e")
         udf
