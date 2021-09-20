dadc0d9ff48541da1aba16944f33cead42d9b7fe
Bobby Wang <wbo4958@gmail.com>
2021-09-20 20:41:03 +0800

    Range window supports DayTime on 3.2+ (#3547)
    Signed-off-by: Bobby Wang <wbo4958@gmail.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/TypeChecks.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/TypeChecks.scala
@@ -66,6 +66,8 @@ trait TypeSigUtil {
    */
   def mapDataTypeToTypeEnum(dataType: DataType): TypeEnum.Value
 
+  /** Get numeric and interval TypeSig */
+  def getNumericAndInterval(): TypeSig
 }
 
 /**
@@ -654,7 +656,7 @@ object TypeSig {
   /**
    * numeric + CALENDAR
    */
-  val numericAndInterval: TypeSig = numeric + CALENDAR
+  val numericAndInterval: TypeSig = TypeSigUtil.getNumericAndInterval()
 
   /**
    * All types that CUDF supports sorting/ordering on.
