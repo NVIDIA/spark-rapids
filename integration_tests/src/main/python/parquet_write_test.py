4b578704ab973ba308cc517a3495558abfcf17a4
Alessandro Bellina <abellina@nvidia.com>
2021-09-23 12:48:50 -0500

    Revert "Disable test_non_empty_ctas test (#3588)"
    This reverts commit f6c7521bd9e06209614a789c96759572f58dce20.
    
    Signed-off-by: Alessandro Bellina <abellina@nvidia.com>

--- integration_tests/src/main/python/parquet_write_test.py
+++ integration_tests/src/main/python/parquet_write_test.py
@@ -366,7 +366,6 @@ def test_roundtrip_with_rebase_values(spark_tmp_path, ts_write_data_gen, date_ti
         data_path,
         conf=all_confs)
 
-@pytest.mark.xfail(reason='https://github.com/NVIDIA/spark-rapids/issues/3476')
 @pytest.mark.allow_non_gpu("DataWritingCommandExec", "HiveTableScanExec")
 @pytest.mark.parametrize('allow_non_empty', [True, False])
 def test_non_empty_ctas(spark_tmp_path, spark_tmp_table_factory, allow_non_empty):
