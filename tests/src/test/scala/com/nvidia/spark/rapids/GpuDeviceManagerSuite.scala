2118c47e0d6bc34d12796d02af967fcd91511f6c
Rong Ou <rong.ou@gmail.com>
2021-09-28 13:57:55 -0700

    fix tests
    Signed-off-by: Rong Ou <rong.ou@gmail.com>

--- tests/src/test/scala/com/nvidia/spark/rapids/GpuDeviceManagerSuite.scala
+++ tests/src/test/scala/com/nvidia/spark/rapids/GpuDeviceManagerSuite.scala
@@ -17,15 +17,23 @@
 package com.nvidia.spark.rapids
 
 import ai.rapids.cudf.{Cuda, DeviceMemoryBuffer}
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite}
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.rapids.execution.TrampolineUtil
+import org.apache.spark.sql.SparkSession
 
-class GpuDeviceManagerSuite extends FunSuite with Arm {
+class GpuDeviceManagerSuite extends FunSuite with Arm with BeforeAndAfter {
+
+  before {
+    TrampolineUtil.cleanupAnyExistingSession()
+  }
+
+  after {
+    TrampolineUtil.cleanupAnyExistingSession()
+  }
 
   test("RMM pool limit") {
-    TrampolineUtil.cleanupAnyExistingSession()
     val totalGpuSize = Cuda.memGetInfo().total
     val initPoolFraction = 0.1
     val maxPoolFraction = 0.2
@@ -39,41 +47,37 @@ class GpuDeviceManagerSuite extends FunSuite with Arm {
         .set(RapidsConf.RMM_ALLOC_MIN_FRACTION.key, minPoolFraction.toString)
         .set(RapidsConf.RMM_ALLOC_MAX_FRACTION.key, maxPoolFraction.toString)
         .set(RapidsConf.RMM_ALLOC_RESERVE.key, "0")
-    try {
-      TestUtils.withGpuSparkSession(conf) { _ =>
-        val initPoolSize = (totalGpuSize * initPoolFraction).toLong
-        val allocSize = Math.max(initPoolSize - 1024 * 1024, 0)
-        // initial allocation should fit within initial pool size
-        withResource(DeviceMemoryBuffer.allocate(allocSize)) { _ =>
-          // this should grow the pool
-          withResource(DeviceMemoryBuffer.allocate(allocSize / 2)) { _ =>
-            assertThrows[OutOfMemoryError] {
-              // this should exceed the specified pool limit
-              DeviceMemoryBuffer.allocate(allocSize).close()
-            }
+    TestUtils.withGpuSparkSession(conf) { _ =>
+      val initPoolSize = (totalGpuSize * initPoolFraction).toLong
+      val allocSize = Math.max(initPoolSize - 1024 * 1024, 0)
+      // initial allocation should fit within initial pool size
+      withResource(DeviceMemoryBuffer.allocate(allocSize)) { _ =>
+        // this should grow the pool
+        withResource(DeviceMemoryBuffer.allocate(allocSize / 2)) { _ =>
+          assertThrows[OutOfMemoryError] {
+            // this should exceed the specified pool limit
+            DeviceMemoryBuffer.allocate(allocSize).close()
           }
         }
       }
-    } finally {
-      TrampolineUtil.cleanupAnyExistingSession()
     }
   }
 
   test("RMM reserve larger than max") {
-    TrampolineUtil.cleanupAnyExistingSession()
     val rapidsConf = new RapidsConf(Map(RapidsConf.RMM_ALLOC_RESERVE.key -> "200g"))
     assertThrows[IllegalArgumentException] {
+      SparkSession.builder().master("local[1]").getOrCreate()
       GpuDeviceManager.initializeMemory(None, Some(rapidsConf))
     }
   }
 
   test("RMM init equals max") {
-    TrampolineUtil.cleanupAnyExistingSession()
     val rapidsConf = new RapidsConf(Map(
       RapidsConf.RMM_ALLOC_RESERVE.key -> "0",
       RapidsConf.RMM_ALLOC_FRACTION.key -> "0.3",
       RapidsConf.RMM_ALLOC_MAX_FRACTION.key -> "0.3"))
     try {
+      SparkSession.builder().master("local[1]").getOrCreate()
       GpuDeviceManager.initializeMemory(None, Some(rapidsConf))
     } finally {
       GpuDeviceManager.shutdown()
