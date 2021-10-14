fc7b51e98393845aa02b6ced22fd68cbc33e03c5
Bobby Wang <wbo4958@gmail.com>
2021-10-14 13:03:42 +0800

    remove sortOrderChildren
    Signed-off-by: Bobby Wang <wbo4958@gmail.com>

--- shims/spark320/src/main/scala/com/nvidia/spark/rapids/shims/spark320/Spark320Shims.scala
+++ shims/spark320/src/main/scala/com/nvidia/spark/rapids/shims/spark320/Spark320Shims.scala
@@ -802,8 +802,6 @@ class Spark320Shims extends Spark32XShims {
     queryStage.shuffle.asInstanceOf[GpuShuffleExchangeExecBase]
   }
 
-  override def sortOrderChildren(s: SortOrder): Seq[Expression] = s.children
-
   override def sortOrder(
       child: Expression,
       direction: SortDirection,
