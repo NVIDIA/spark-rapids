bd4bbeb80f1a69441424b7c3cdac5b55537eb37d
Thomas Graves <tgraves@nvidia.com>
2021-10-18 10:30:10 -0500

    update javadoc

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/ExplainPlan.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/ExplainPlan.scala
@@ -38,6 +38,7 @@ object ExplainPlan {
    * @param df The Spark DataFrame to get the query plan from
    * @param explain If ALL returns all the data, otherwise just returns what does not
    *                work on the GPU. Default is ALL.
+   * @return String containing the explained plan.
    */
   def explainPotentialGPUPlan(df: DataFrame, explain: String = "ALL"): String = {
     val gpuOverrideClass = ShimLoader.loadGpuOverrides()
