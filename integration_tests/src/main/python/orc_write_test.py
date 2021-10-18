ced52d9a12648e3df8726a2f347cc3854024b3fa
Chong Gao <res_life@163.com>
2021-10-18 22:13:52 +0800

    Test
    Signed-off-by: Chong Gao <res_life@163.com>

--- integration_tests/src/main/python/orc_write_test.py
+++ integration_tests/src/main/python/orc_write_test.py
@@ -40,7 +40,7 @@ simple_string_to_string_map_gen = MapGen(StringGen(pattern='key_[0-9]', nullable
 orc_write_basic_map_gens = [simple_string_to_string_map_gen] + [MapGen(f(nullable=False), f(), max_length=20) for f in [
     BooleanGen, ByteGen, ShortGen, IntegerGen, LongGen, FloatGen, DoubleGen,
     lambda nullable=True: TimestampGen(start=datetime(1900, 1, 1, tzinfo=timezone.utc), nullable=nullable),
-    lambda nullable=True: DateGen(start=date(1900, 1, 1), nullable=nullable)]]
+    lambda nullable=True: DateGen(start=date(1590, 1, 1), nullable=nullable)]]
 
 orc_write_gens_list = [orc_write_basic_gens,
         orc_write_struct_gens_sample,
