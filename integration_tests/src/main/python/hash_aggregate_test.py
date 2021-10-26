3743bfa96a392112ebe6327cbad950585b3d5ea1
Andy Grove <andygrove@nvidia.com>
2021-10-26 09:38:41 -0600

    Update comments

--- integration_tests/src/main/python/hash_aggregate_test.py
+++ integration_tests/src/main/python/hash_aggregate_test.py
@@ -1174,20 +1174,13 @@ def test_hash_groupby_approx_percentile_partial_fallback_to_cpu(aqe_enabled):
         'spark.sql.adaptive.enabled': aqe_enabled
     })
 
-    def create_and_show_df(spark):
-        # This simulates calling  "df.show" by introducing a `CAST(approx_percentile(...) AS string)` on the
-        # final aggregate and this is not supported on GPU so falls back to CPU and the purpose of this test
-        # is to make sure that the partial aggregate also falls back to CPU (the query will fail if
-        # the partial aggregate runs on the GPU because the t-digest buffer is not compatible with Spark's
-        # CPU buffer for approx percentile).
-
+    def approx_percentile_query(spark):
         df = gen_df(spark, [('k', StringGen(nullable=False)),
                             ('v', DoubleGen())], length=100)
         df.createOrReplaceTempView("t")
-        df2 = spark.sql("select k, approx_percentile(v, array(0.1, 0.2)) from t group by k")
-        return df2
+        return spark.sql("select k, approx_percentile(v, array(0.1, 0.2)) from t group by k")
 
-    assert_gpu_fallback_collect(lambda spark: create_and_show_df(spark), 'ApproximatePercentile', conf)
+    assert_gpu_fallback_collect(lambda spark: approx_percentile_query(spark), 'ApproximatePercentile', conf)
 
 # The percentile approx tests differ from other tests because we do not expect the CPU and GPU to produce the same
 # results due to the different algorithms being used. Instead we compute an exact percentile on the CPU and then
