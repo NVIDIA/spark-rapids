ddc3129d830180916d42f9f8b7b491563758f1e2
Chong Gao <res_life@163.com>
2021-11-02 11:00:47 +0800

    Revert "Test"
    This reverts commit 2d838ffe624cebd31e06634794efdac65e2f0c13.

--- integration_tests/src/main/python/orc_write_test.py
+++ integration_tests/src/main/python/orc_write_test.py
@@ -39,7 +39,7 @@ orc_write_array_gens_sample = [ArrayGen(sub_gen) for sub_gen in orc_write_basic_
 orc_write_basic_map_gens = [simple_string_to_string_map_gen] + [MapGen(f(nullable=False), f()) for f in [
     BooleanGen, ByteGen, ShortGen, IntegerGen, LongGen, FloatGen, DoubleGen,
     lambda nullable=True: TimestampGen(start=datetime(1900, 1, 1, tzinfo=timezone.utc), nullable=nullable),
-    lambda nullable=True: DateGen(start=date(1900, 1, 1), nullable=nullable)]]
+    lambda nullable=True: DateGen(start=date(1590, 1, 1), nullable=nullable)]]
 
 orc_write_gens_list = [orc_write_basic_gens,
         orc_write_struct_gens_sample,
