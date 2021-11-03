47ea788f169c6be20f079e73b1589d91a6399ad7
Chong Gao <res_life@163.com>
2021-11-03 12:02:12 +0800

    Update
    Signed-off-by: Chong Gao <res_life@163.com>

--- integration_tests/run_pyspark_from_build.sh
+++ integration_tests/run_pyspark_from_build.sh
@@ -71,7 +71,8 @@ else
         TEST_PARALLEL=`nvidia-smi --query-gpu=memory.free --format=csv,noheader | awk '{if (MAX < $1){ MAX = $1}} END {print int(MAX / (2.3 * 1024)) - 1}'`
         echo "AUTO DETECTED PARALLELISM OF $TEST_PARALLEL"
 
-        # adjust TEST_PARALLEL according to cpu cores and free memory
+        # adjust TEST_PARALLEL according to cpu cores and free memory;
+        # first read from ENV; Note it's hardly to get CPU cores and real free memory in docker
         if [[ "${PYSP_TEST_CPU_CORES}" != "" ]]; then
           cpu_cores=${PYSP_TEST_CPU_CORES}
         else
@@ -82,16 +83,16 @@ else
         else
           free_mem_mib=$(awk '/MemFree/ { printf "%d\n", $2/1024 }' /proc/meminfo)
         fi
-        max_parallel_for_cpu_cores=$(($cpu_cores - 1))
+        max_parallel_for_cpu_cores=$((${cpu_cores} - 1))
         # default heap size for spark is 1G, add reserve 50M for other processes in OS.
-        max_parallel_for_free_memory=$(($free_mem_mib - 50 / 1024))
+        max_parallel_for_free_memory=$(( (${free_mem_mib} - 50) / 1024 ))
         if [[ TEST_PARALLEL -gt $max_parallel_for_cpu_cores ]]; then
           echo "set TEST_PARALLEL from $TEST_PARALLEL to $max_parallel_for_cpu_cores according to cpu cores $cpu_cores"
-          TEST_PARALLEL=max_parallel_for_cpu_cores
+          TEST_PARALLEL=${max_parallel_for_cpu_cores}
         fi
-        if [[ TEST_PARALLEL -gt $max_parallel_for_free_memory ]]; then
+        if [[ TEST_PARALLEL -gt ${max_parallel_for_free_memory} ]]; then
           echo "set TEST_PARALLEL from $TEST_PARALLEL to $max_parallel_for_free_memory according to free memory $free_mem_mib mib"
-          TEST_PARALLEL=max_parallel_for_free_memory
+          TEST_PARALLEL=${max_parallel_for_free_memory}
         fi
     fi
     if python -c 'import findspark';
