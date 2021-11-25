da6c053e827600abbe40ddb4529a76cba3c1b86a
remzi <13716567376yh@gmail.com>
2021-11-25 10:02:48 +0800

    find cast timestamp to string bug
    Signed-off-by: remzi <13716567376yh@gmail.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/TypeChecks.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/TypeChecks.scala
@@ -1293,13 +1293,8 @@ class CastChecks extends ExprChecks {
   val calendarChecks: TypeSig = none
   val sparkCalendarSig: TypeSig = CALENDAR + STRING
 
-<<<<<<< HEAD
   val arrayChecks: TypeSig = STRING + ARRAY.nested(commonCudfTypes + DECIMAL_128_FULL + NULL +
       ARRAY + BINARY + STRUCT + MAP + STRING) +
-=======
-  val arrayChecks: TypeSig = ARRAY.nested(commonCudfTypes + DECIMAL_128_FULL + NULL +
-      ARRAY + BINARY + STRUCT + MAP) +
->>>>>>> branch-22.02
       psNote(TypeEnum.ARRAY, "The array's child type must also support being cast to " +
           "the desired child type")
 
