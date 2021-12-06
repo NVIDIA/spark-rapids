e021a00da1302fc228eb82fec34cbefca17d13dd
Alessandro Bellina <abellina@nvidia.com>
2021-12-06 14:04:01 -0600

    Remove incRefCount

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/execution/GpuBroadcastNestedLoopJoinExecBase.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/execution/GpuBroadcastNestedLoopJoinExecBase.scala
@@ -439,12 +439,9 @@ abstract class GpuBroadcastNestedLoopJoinExecBase(
       buildTime: GpuMetric,
       buildDataSize: GpuMetric): ColumnarBatch = {
     withResource(new NvtxWithMetrics("build join table", NvtxColor.GREEN, buildTime)) { _ =>
-      withResource(GpuBroadcastHelper.getBroadcastBatch(
-          broadcastRelation, broadcast.schema)) { builtBatch =>
-        GpuColumnVector.incRefCounts(builtBatch)
-        buildDataSize += GpuColumnVector.getTotalDeviceMemoryUsed(builtBatch)
-        builtBatch
-      }
+      val builtBatch = GpuBroadcastHelper.getBroadcastBatch(broadcastRelation, broadcast.schema)
+      buildDataSize += GpuColumnVector.getTotalDeviceMemoryUsed(builtBatch)
+      builtBatch
     }
   }
 
