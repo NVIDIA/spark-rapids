134404047f23273cf6c956aaf1e925d748ca62b5
Firestarman <firestarmanllc@gmail.com>
2021-12-13 17:17:11 +0800

    premerge debug 3
    Signed-off-by: Firestarman <firestarmanllc@gmail.com>

--- jenkins/spark-premerge-build.sh
+++ jenkins/spark-premerge-build.sh
@@ -108,7 +108,7 @@ ci_2() {
     export TEST_TYPE="pre-commit"
     export TEST_PARALLEL=4
     # separate process to avoid OOM kill
-    TEST_PARALLEL=5 TEST='if_else or case_when' ./integration_tests/run_pyspark_from_build.sh
+    TEST_PARALLEL=5 TEST='case_when' ./integration_tests/run_pyspark_from_build.sh
     # TEST_PARALLEL=5 TEST='conditionals_test or window_function_test' ./integration_tests/run_pyspark_from_build.sh
     # TEST_PARALLEL=5 TEST='struct_test or time_window_test' ./integration_tests/run_pyspark_from_build.sh
     # TEST='not conditionals_test and not window_function_test and not struct_test and not time_window_test' \
