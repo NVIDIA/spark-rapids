8c5e6de7113234e5eb23d16ab7ff0811a7262dee
Chong Gao <res_life@163.com>
2021-12-31 11:01:33 +0800

    Refactor
    Signed-off-by: Chong Gao <res_life@163.com>

--- integration_tests/src/main/python/hash_aggregate_test.py
+++ integration_tests/src/main/python/hash_aggregate_test.py
@@ -1697,20 +1697,20 @@ def test_groupby_std_variance_partial_replace_fallback(data_gen,
         conf=local_conf)
 
 @ignore_order
-@pytest.mark.parametrize('data_type',  all_gen + [NullGen()], ids=idfn)
-def test_max_single_level_struct(data_type):
-    data_gen = [
+@pytest.mark.parametrize('data_gen',  all_gen + [NullGen()], ids=idfn)
+def test_max_single_level_struct(data_gen):
+    df_gen = [
         ('a', StructGen([
-                ('aa', data_type),
-                ('ab', data_type)])),
+                ('aa', data_gen),
+                ('ab', data_gen)])),
         ('b', IntegerGen())]
     assert_gpu_and_cpu_are_equal_sql(
-        lambda spark : gen_df(spark, data_gen, length=1024),
+        lambda spark : gen_df(spark, df_gen, length=1024),
         "hash_agg_table",
         'select b, max(a) from hash_agg_table group by b',
         _no_nans_float_conf)
     assert_gpu_and_cpu_are_equal_sql(
-        lambda spark : gen_df(spark, data_gen, length=1024),
+        lambda spark : gen_df(spark, df_gen, length=1024),
         "hash_agg_table",
         'select max(a) from hash_agg_table',
         _no_nans_float_conf)
