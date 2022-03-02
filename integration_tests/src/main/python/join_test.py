e4c0a40b6f29da85d48fcdaae0dfa49e87fe2fc6
Gera Shegalov <gera@apache.org>
2022-03-01 22:10:29 -0800

    undo import py

--- integration_tests/src/main/python/join_test.py
+++ integration_tests/src/main/python/join_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import py
 import pytest
 from _pytest.mark.structures import ParameterSet
 from pyspark.sql.functions import broadcast
