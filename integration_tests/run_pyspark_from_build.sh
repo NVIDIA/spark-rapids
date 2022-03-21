087a576f1ef2cf5a73e53be5bf991f4d274fdaa4
Bobby Wang <wbo4958@gmail.com>
2022-03-21 19:18:14 +0800

    modify ci

--- integration_tests/run_pyspark_from_build.sh
+++ integration_tests/run_pyspark_from_build.sh
@@ -52,6 +52,18 @@ else
         TEST_JARS=$(echo "$SCRIPTPATH"/target/rapids-4-spark-integration-tests*-$SPARK_SHIM_VER.jar)
     fi
 
+    # `./run_pyspark_from_build.sh` runs all tests including avro_test.py with spark-avro.jar
+    #                               in the classpath.
+    #
+    # `./run_pyspark_from_build.sh -k xxx ` runs all xxx tests with spark-avro.jar in the classpath
+    #
+    # `CI_EXCLUDE_AVRO=true ./run_pyspark_from_build.sh -k not avro_test.py` run all tests excluding
+    #                               those in avro_test.py
+    if [[ "${CI_EXCLUDE_AVRO}" != "" ]];
+    then
+       AVRO_JARS=""
+    fi
+
     # Only 3 jars: cudf.jar dist.jar integration-test.jar
     ALL_JARS="$CUDF_JARS $PLUGIN_JARS $TEST_JARS $AVRO_JARS"
     echo "AND PLUGIN JARS: $ALL_JARS"
