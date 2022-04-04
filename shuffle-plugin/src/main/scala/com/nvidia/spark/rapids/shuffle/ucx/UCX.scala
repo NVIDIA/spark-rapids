5224d328dda285c2b139ff9b98ec3fb53d20b504
Alessandro Bellina <abellina@nvidia.com>
2022-04-04 17:32:44 -0500

    Update UCX to 1.12.1 in blossom dockerfile

--- shuffle-plugin/src/main/scala/com/nvidia/spark/rapids/shuffle/ucx/UCX.scala
+++ shuffle-plugin/src/main/scala/com/nvidia/spark/rapids/shuffle/ucx/UCX.scala
@@ -136,7 +136,6 @@ class UCX(transport: UCXShuffleTransport, executor: BlockManagerId, rapidsConf: 
       }
 
       var workerParams = new UcpWorkerParams()
-        .setClientId(localExecutorId)
 
       if (rapidsConf.shuffleUcxUseWakeup) {
         workerParams = workerParams
@@ -776,7 +775,6 @@ class UCX(transport: UCXShuffleTransport, executor: BlockManagerId, rapidsConf: 
       // enables `onError` callback
       .setPeerErrorHandlingMode()
       .setErrorHandler(this)
-      .sendClientId()
 
     /**
      * Get a `ClientConnection` after optionally connecting to a peer given by `peerExecutorId`,
@@ -808,8 +806,7 @@ class UCX(transport: UCXShuffleTransport, executor: BlockManagerId, rapidsConf: 
       onWorkerThreadAsync(() => {
         endpoints.computeIfAbsent(peerExecutorId, _ => {
           val sockAddr = new InetSocketAddress(peerHost, peerPort)
-          val ep = worker.newEndpoint(
-            epParams.setSocketAddress(sockAddr))
+          val ep = worker.newEndpoint(epParams.setSocketAddress(sockAddr))
           logDebug(s"Initiator: created an endpoint $ep to $peerExecutorId")
           reverseLookupEndpoints.put(ep, peerExecutorId)
           ep
@@ -819,17 +816,17 @@ class UCX(transport: UCXShuffleTransport, executor: BlockManagerId, rapidsConf: 
 
     // UcpListenerConnectionHandler interface - called from progress thread
     // handles an incoming connection to our UCP Listener
+    // TODO: in the future, this function may reject `ConnectionRequest`s
+    //  given a peer id we already established a connection to:
+    //  https://github.com/openucx/ucx/pull/6859
     override def onConnectionRequest(connectionRequest: UcpConnectionRequest): Unit = {
-      val clientId = connectionRequest.getClientId
-
-      logDebug(s"Accepting UcpListener request from ${connectionRequest.getClientAddress} " +
-        s"peer executorId $clientId")
+      logInfo(s"Got UcpListener request from ${connectionRequest.getClientAddress}")
 
       // accept it
       val ep = worker.newEndpoint(epParams.setConnectionRequest(connectionRequest))
 
       logInfo(s"Created ConnectionRequest endpoint $ep " +
-        s"for ${connectionRequest.getClientAddress} and peer executorId $clientId")
+        s"for ${connectionRequest.getClientAddress}")
 
       // Register a `Control` active message for a handshake response
       val responseAm = UCXActiveMessage(
