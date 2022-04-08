f49fa5193336c7c8802291981f5812556dd167f6
Thomas Graves <tgraves@nvidia.com>
2022-04-07 19:27:00 -0500

    Update Databricks 10.4 for changes to the QueryStageExec and ClusteredDistribution [databricks] (#5165)
    * Update Databricks 10.4 for ClusterDistribution and QueryStageExec
    changes
    
    Signed-off-by: Thomas Graves <tgraves@nvidia.com>
    
    * revert build script changes
    
    * remove unused import
    
    * revert build changes
    
    * fix copyright

--- sql-plugin/src/main/320+/scala/com/nvidia/spark/rapids/shims/Spark320PlusShims.scala
+++ sql-plugin/src/main/320+/scala/com/nvidia/spark/rapids/shims/Spark320PlusShims.scala
@@ -54,7 +54,7 @@ import org.apache.spark.sql.execution.datasources.v2.BatchScanExec
 import org.apache.spark.sql.execution.datasources.v2.csv.CSVScan
 import org.apache.spark.sql.execution.datasources.v2.orc.OrcScan
 import org.apache.spark.sql.execution.datasources.v2.parquet.ParquetScan
-import org.apache.spark.sql.execution.exchange.{BroadcastExchangeExec, ENSURE_REQUIREMENTS, ReusedExchangeExec, ShuffleExchangeExec}
+import org.apache.spark.sql.execution.exchange.{BroadcastExchangeExec, ENSURE_REQUIREMENTS, ShuffleExchangeExec}
 import org.apache.spark.sql.execution.joins._
 import org.apache.spark.sql.execution.python._
 import org.apache.spark.sql.execution.window.WindowExecBase
@@ -916,15 +916,6 @@ trait Spark320PlusShims extends SparkShims with RebaseShims with Logging {
   override def replacePartitionDirectoryFiles(partitionDir: PartitionDirectory,
       replaceFunc: Path => Path): Seq[Path] = {
     partitionDir.files.map(f => replaceFunc(f.getPath))
-  }
-
-  /**
-   * Case class ShuffleQueryStageExec holds an additional field shuffleOrigin
-   * affecting the unapply method signature
-   */
-  override def reusedExchangeExecPfn: PartialFunction[SparkPlan, ReusedExchangeExec] = {
-    case ShuffleQueryStageExec(_, e: ReusedExchangeExec, _) => e
-    case BroadcastQueryStageExec(_, e: ReusedExchangeExec, _) => e
   }
 
   /** dropped by SPARK-34234 */
