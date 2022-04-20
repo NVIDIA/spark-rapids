b854336476f68bdd115ad86d85ddf1b013eb8057
Peixin Li <pxli@nyu.edu>
2022-04-20 18:02:27 +0800

    address comments

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -87,9 +87,9 @@ $DEPLOY_CMD -Durl=$SERVER_URL -DrepositoryId=$SERVER_ID \
 # Distribution jar is a shaded artifact so use the reduced dependency pom.
 $DEPLOY_CMD -Durl=$SERVER_URL -DrepositoryId=$SERVER_ID \
             $SRC_DOC_JARS \
-            -Dfile=$FPATH.jar -Dtypes=jar -Dfiles=$FPATH.jar \
-            -DgroupId=com.nvidia -DartifactId=$ART_ID -Dversion=$ART_VER \
-            -DpomFile="$POM_FPATH" -Dclassifiers=$CUDA_CLASSIFIER
+            -Dfile=$FPATH.jar -DgroupId=com.nvidia -DartifactId=$ART_ID -Dversion=$ART_VER \
+            -Dfiles=$FPATH.jar -Dtypes=jar -Dclassifiers=$CUDA_CLASSIFIER \
+            -DpomFile="$POM_FPATH"
 
 ###### Deploy profiling tool jar(s) ######
 TOOL_PL=${TOOL_PL:-"tools"}
