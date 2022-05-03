5eff01c408cb663e356ad7b749cc162b3c059d7e
Thomas Graves <tgraves@apache.org>
2022-05-03 11:30:13 -0500

    move logic parsing

--- tools/src/main/scala/com/nvidia/spark/rapids/tool/planparser/FileSourceScanExecParser.scala
+++ tools/src/main/scala/com/nvidia/spark/rapids/tool/planparser/FileSourceScanExecParser.scala
@@ -30,6 +30,72 @@ case class FileSourceScanExecParser(
 
   val fullExecName = "FileSourceScanExec"
 
+  // strip off the struct<> part that Spark adds to the ReadSchema
+  private def formatSchemaStr(schema: String): String = {
+    schema.stripPrefix("struct<").stripSuffix(">")
+  }
+  // This tries to get just the field specified by tag in a string that
+  // may contain multiple fields.  It looks for a comma to delimit fields.
+  private def getFieldWithoutTag(str: String, tag: String): String = {
+    val index = str.indexOf(tag)
+    // remove the tag from the final string retruned
+    val subStr = str.substring(index + tag.size)
+    val endIndex = subStr.indexOf(", ")
+    if (endIndex != -1) {
+      subStr.substring(0, endIndex)
+    } else {
+      subStr
+    }
+  }
+
+  private def parseRead: Unit = {
+    val schemaTag = "ReadSchema: "
+    val schema = if (node.desc.contains(schemaTag)) {
+      formatSchemaStr(getFieldWithoutTag(node.desc, schemaTag))
+    } else {
+      ""
+    }
+    logWarning(s"schema is: $schema")
+    val locationTag = "Location: "
+    val location = if (node.desc.contains(locationTag)) {
+      getFieldWithoutTag(node.desc, locationTag)
+    } else if (node.name.contains("JDBCRelation")) {
+      // see if we can report table or query
+      val JDBCPattern = raw".*JDBCRelation\((.*)\).*".r
+      node.name match {
+        case JDBCPattern(tableName) => tableName
+        case _ => "unknown"
+      }
+    } else {
+      "unknown"
+    }
+    logWarning(s"location is: $location")
+
+    val pushedFilterTag = "PushedFilters: "
+    val pushedFilters = if (node.desc.contains(pushedFilterTag)) {
+      getFieldWithoutTag(node.desc, pushedFilterTag)
+    } else {
+      "unknown"
+    }
+    logWarning(s"pushedFilters is: $pushedFilters")
+
+    val formatTag = "Format: "
+    val fileFormat = if (node.desc.contains(formatTag)) {
+      val format = getFieldWithoutTag(node.desc, formatTag)
+      if (node.name.startsWith("Gpu")) {
+        s"${format}(GPU)"
+      } else {
+        format
+      }
+    } else if (node.name.contains("JDBCRelation")) {
+      "JDBC"
+    } else {
+      "unknown"
+    }
+    logWarning(s"fileFormat is: $fileFormat")
+
+  }
+
   override def parse: Seq[ExecInfo] = {
     // can we use scan time?
     val accumId = node.metrics.find(_.name == "scan time").map(_.accumulatorId)
