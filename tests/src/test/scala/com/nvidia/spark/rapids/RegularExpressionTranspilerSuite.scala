b0b7d67f29e609761f390cdda5cb80f40051f3a8
Andy Grove <andygrove73@gmail.com>
2022-05-17 14:49:37 -0600

    Update tests/src/test/scala/com/nvidia/spark/rapids/RegularExpressionTranspilerSuite.scala

--- tests/src/test/scala/com/nvidia/spark/rapids/RegularExpressionTranspilerSuite.scala
+++ tests/src/test/scala/com/nvidia/spark/rapids/RegularExpressionTranspilerSuite.scala
@@ -294,8 +294,7 @@ class RegularExpressionTranspilerSuite extends FunSuite with Arm {
   test("replace_replace - ?, *, +, and {0, n} repetitions") {
     val patterns = Seq("D?", "D*", "D+", "D{0,}", "D{0,1}", "D{0,5}", "[1a-zA-Z]{0,}",
         "[1a-zA-Z]{0,2}", "A+")
-    val inputs = Seq("SS", "DD", "SDSDSDS", "DDDD", "DDDDDD", "ABCDEFG",
-        "\u000e\u0011\u0006h¹TEST\ud720AA")
+    val inputs = Seq("SS", "DD", "SDSDSDS", "DDDD", "DDDDDD", "ABCDEFG")
     assertCpuGpuMatchesRegexpReplace(patterns, inputs)
   }
 
