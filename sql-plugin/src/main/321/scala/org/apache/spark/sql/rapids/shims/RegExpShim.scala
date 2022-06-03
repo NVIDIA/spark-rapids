e3b81d69f92f179f9d754e08624380c069e23088
Andy Grove <andygrove@nvidia.com>
2022-06-03 11:35:16 -0600

    shim layer for regexp

---
+++ sql-plugin/src/main/321/scala/org/apache/spark/sql/rapids/shims/RegExpShim.scala
@@ -0,0 +1,6 @@
+package com.nvidia.spark.rapids.shims
+
+object RegExpShim {
+  // Handle regexp_replace inconsistency from https://issues.apache.org/jira/browse/SPARK-39107
+  def reproduceEmptyStringBug(): Boolean = true
+}
