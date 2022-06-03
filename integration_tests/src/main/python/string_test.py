951d455c504df71c845c9a7a9186583da63fbdd6
Anthony Chang <antchang@nvidia.com>
2022-06-03 15:23:00 -0700

    Update integration test
    Signed-off-by: Anthony Chang <antchang@nvidia.com>

--- integration_tests/src/main/python/string_test.py
+++ integration_tests/src/main/python/string_test.py
@@ -1076,7 +1076,7 @@ def test_rlike_fallback_possessive_quantifier():
         conf=_regexp_conf)
 
 def test_replace_empty_string():
-    gen = StringGen('')
+    gen = mk_str_gen('.{0,5}TEST[\ud720 A]{0,5}')
     assert_gpu_and_cpu_are_equal_collect(
             lambda spark: unary_op_df(spark, gen).selectExpr(
                 'regexp_replace(a, "A*", "_REPLACED_")'),
