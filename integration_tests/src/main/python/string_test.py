e7a6e2c74f1d1347e8ed36c17c63169d89c21200
Anthony Chang <antchang@nvidia.com>
2022-06-08 09:26:33 -0700

    Update comment
    Signed-off-by: Anthony Chang <antchang@nvidia.com>

--- integration_tests/src/main/python/string_test.py
+++ integration_tests/src/main/python/string_test.py
@@ -547,9 +547,7 @@ def test_re_replace():
                 'REGEXP_REPLACE(a, "TEST", NULL)'),
         conf=_regexp_conf)
 
-# Note regexp_replace with empty string will not match 
-# unless we are using Spark 3.1.4, 3.2.2, or 3.3.0
-# See https://issues.apache.org/jira/browse/SPARK-39107
+# We have shims to support empty strings for zero-repetition patterns
 # See https://github.com/NVIDIA/spark-rapids/issues/5456
 def test_re_replace_repetition():
     gen = mk_str_gen('.{0,5}TEST[\ud720 A]{0,5}')
