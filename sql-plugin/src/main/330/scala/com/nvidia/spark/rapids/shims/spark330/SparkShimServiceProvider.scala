90dd97c898ef523d33284eaf9853107778f6de01
Jason Lowe <jlowe@nvidia.com>
2022-06-16 13:05:25 -0500

    Remove SNAPSHOT support from Spark 3.3.0 shim (#5858)
    Signed-off-by: Jason Lowe <jlowe@nvidia.com>

--- sql-plugin/src/main/330/scala/com/nvidia/spark/rapids/shims/spark330/SparkShimServiceProvider.scala
+++ sql-plugin/src/main/330/scala/com/nvidia/spark/rapids/shims/spark330/SparkShimServiceProvider.scala
@@ -20,7 +20,7 @@ import com.nvidia.spark.rapids.SparkShimVersion
 
 object SparkShimServiceProvider {
   val VERSION = SparkShimVersion(3, 3, 0)
-  val VERSIONNAMES = Seq(s"$VERSION", s"$VERSION-SNAPSHOT")
+  val VERSIONNAMES = Seq(s"$VERSION")
 }
 
 class SparkShimServiceProvider extends com.nvidia.spark.rapids.SparkShimServiceProvider {
