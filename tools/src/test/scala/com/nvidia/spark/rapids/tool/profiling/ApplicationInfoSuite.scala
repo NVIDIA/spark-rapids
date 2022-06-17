8413b5bab8ed6da3e1e3194fe0bd51d467f6eb4e
Thomas Graves <tgraves@apache.org>
2022-06-17 08:57:40 -0500

    update test for node ids

--- tools/src/test/scala/com/nvidia/spark/rapids/tool/profiling/ApplicationInfoSuite.scala
+++ tools/src/test/scala/com/nvidia/spark/rapids/tool/profiling/ApplicationInfoSuite.scala
@@ -466,12 +466,12 @@ class ApplicationInfoSuite extends FunSuite with Logging {
     assert(firstRow.nodeID === 0)
     assert(firstRow.sqlID === 0)
     assert(firstRow.parent === "WholeStageCodegen (6)")
-    assert(firstRow.child === "HashAggregate")
+    assert(firstRow.child === "HashAggregate(1)")
     val lastRow = wholeStageMapping(9)
     assert(lastRow.nodeID === 17)
     assert(lastRow.sqlID === 0)
     assert(lastRow.parent === "WholeStageCodegen (3)")
-    assert(lastRow.child === "SerializeFromObject")
+    assert(lastRow.child === "SerializeFromObject(19)")
   }
 
 
