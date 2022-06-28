4acc25849f82d01c731c92610ed54eebb96c12b2
Robert (Bobby) Evans <bobby@apache.org>
2022-06-28 14:25:07 -0500

    Make first/last test for sorted deterministic
    Signed-off-by: Robert (Bobby) Evans <bobby@apache.org>

--- integration_tests/src/main/python/hash_aggregate_test.py
+++ integration_tests/src/main/python/hash_aggregate_test.py
@@ -1113,9 +1113,10 @@ def test_sorted_groupby_first_last(data_gen):
         f.first('b'), f.last('b'), f.first('b', True), f.last('b', True))
     assert_gpu_and_cpu_are_equal_collect(
         # First and last are not deterministic when they are run in a real distributed setup.
-        # We set parallelism 1 to prevent nondeterministic results because of distributed setup.
+        # We set parallelism and partitions to 1 to prevent nondeterministic results because
+        # of distributed setups.
         lambda spark: agg_fn(gen_df(spark, gen_fn, num_slices=1)),
-        conf = {'spark.rapids.sql.explain': 'ALL'})
+        conf = {'spark.sql.shuffle.partitions': '1'})
 
 @ignore_order
 @pytest.mark.parametrize('data_gen', all_gen, ids=idfn)
