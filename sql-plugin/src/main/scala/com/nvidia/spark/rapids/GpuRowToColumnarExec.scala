429aa0817e38967dbb7951c73d0dd46174a53f01
Ahmed Hussein (amahussein) <a@ahussein.me>
2022-07-06 10:09:37 -0500

    add null check to the string copy
    Signed-off-by: Ahmed Hussein (amahussein) <a@ahussein.me>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuRowToColumnarExec.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuRowToColumnarExec.scala
@@ -734,7 +734,7 @@ object GeneratedInternalRowToCudfRowIterator extends Logging {
       val colLength = jcudfRowVisitor.getColLength()
       val ret = jcudfRowVisitor.getColLength() match {
         // strings return -15
-        case -15 => s"$cudfDataOffsetTmp += copyUTF8StringInto($rowBaseObj, $rowBaseOffset + ${sparkValidityOffset + (colIndex * 8)}, $cudfColOff, $cudfDataOffsetTmp);"
+        case -15 => s"$cudfDataOffsetTmp += copyUTF8StringInto($colIndex, $rowBaseObj,  $rowBaseOffset, $sparkValidityOffset, $cudfColOff, $cudfDataOffsetTmp);"
         case 1 => s"Platform.putByte(null, startAddress + $cudfColOff, Platform.getByte($rowBaseObj, $rowBaseOffset + ${sparkValidityOffset + (colIndex * 8)}));"
         case 2 => s"Platform.putShort(null, startAddress + $cudfColOff, Platform.getShort($rowBaseObj, $rowBaseOffset + ${sparkValidityOffset + (colIndex * 8)}));"
         case 4 => s"Platform.putInt(null, startAddress + $cudfColOff, Platform.getInt($rowBaseObj, $rowBaseOffset + ${sparkValidityOffset + (colIndex * 8)}));"
@@ -835,7 +835,17 @@ object GeneratedInternalRowToCudfRowIterator extends Logging {
          |  }
          |
          |  private int copyUTF8StringInto(
-         |      Object src, long baseOffset, int cudfColOffset, int dataDstOffset) {
+         |      int ordinal, Object src, long baseOffset, int sparkValidityOffset, int cudfColOffset, int dataDstOffset) {
+         |    // check that the field is not null
+         |    if (!org.apache.spark.unsafe.bitset.BitSetMethods.isSet(src, baseOffset, ordinal)) {
+         |      // this string is null
+         |      // the size should be 0, the address is 0 since no data is stored.
+         |      Platform.putInt(null, cudfColOffset, 0);
+         |      Platform.putInt(null, cudfColOffset + 4, 0);
+         |      return 0;
+         |    }
+         |    // calculate the spark offset
+         |    baseOffset = baseOffset + sparkValidityOffset + (ordinal * 8);
          |    // get the offset and the size from Spark UnsafeFormat
          |    final long strOffsetAndSize = Platform.getLong(src, baseOffset);
          |    final int strOffset = (int) (strOffsetAndSize >> 32);
@@ -844,7 +854,7 @@ object GeneratedInternalRowToCudfRowIterator extends Logging {
          |    Platform.putInt(null, cudfColOffset, dataDstOffset);
          |    Platform.putInt(null, cudfColOffset + 4, strSize);
          |    // copy the data
-         |    Platform.copyMemory(src, baseOffset, null, dataDstOffset, strSize);
+         |    Platform.copyMemory(src, strOffset, null, dataDstOffset, strSize);
          |    return strSize;
          |  }
          |
