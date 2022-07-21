1cabb0701c1fc1f502a9f3593ea8238d1f6028ca
Allen Xu <allxu@nvidia.com>
2022-07-21 22:39:28 +0800

    port whole parsePartitions method from Spark3.3 to Gpu side
    Signed-off-by: Allen Xu <allxu@nvidia.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/execution/datasources/rapids/GpuPartitioningUtils.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/execution/datasources/rapids/GpuPartitioningUtils.scala
@@ -16,14 +16,30 @@
 
 package org.apache.spark.sql.execution.datasources.rapids
 
+import java.lang.{Double => JDouble, Long => JLong}
+import java.math.{BigDecimal => JBigDecimal}
+import java.time.ZoneId
+
+import scala.util.Try
+import scala.util.control.NonFatal
+
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.catalyst.util.{CaseInsensitiveMap, DateTimeUtils}
-import org.apache.spark.sql.execution.datasources.{PartitioningUtils, PartitionSpec}
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.catalog.ExternalCatalogUtils.{unescapePathName, DEFAULT_PARTITION_NAME}
+import org.apache.spark.sql.catalyst.expressions.{Cast, Literal}
+import org.apache.spark.sql.catalyst.util.{CaseInsensitiveMap, DateFormatter, DateTimeUtils, TimestampFormatter}
+import org.apache.spark.sql.errors.QueryExecutionErrors
+import org.apache.spark.sql.execution.datasources.{PartitionPath, PartitionSpec}
 import org.apache.spark.sql.execution.datasources.PartitioningAwareFileIndex.BASE_PATH_PARAM
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.execution.datasources.PartitioningUtils.{parsePartition, resolvePartitions, timestampPartitionPattern, PartitionValues, TypedPartValue}
+import org.apache.spark.sql.types._
+import org.apache.spark.unsafe.types.UTF8String
+
+
+
 
 object GpuPartitioningUtils {
 
@@ -68,7 +84,7 @@ object GpuPartitioningUtils {
       val basePaths = getBasePaths(sparkSession.sessionState.newHadoopConfWithOptions(parameters),
         basePathOption, rootPaths, leafFiles)
 
-      PartitioningUtils.parsePartitions(
+      parsePartitions(
         leafDirs,
         typeInference = sparkSession.sessionState.conf.partitionColumnTypeInferenceEnabled,
         basePaths = basePaths,
@@ -110,11 +126,10 @@ object GpuPartitioningUtils {
    * mainly copied from PartitioningAwareFileIndex.basePaths
    */
   private def getBasePaths(
-    hadoopConf: Configuration,
-    basePathOption: Option[Path],
-    rootPaths: Seq[Path],
-    leafFiles: Seq[Path]): Set[Path] = {
-
+      hadoopConf: Configuration,
+      basePathOption: Option[Path],
+      rootPaths: Seq[Path],
+      leafFiles: Seq[Path]): Set[Path] = {
     basePathOption match {
       case Some(userDefinedBasePath) =>
         val fs = userDefinedBasePath.getFileSystem(hadoopConf)
@@ -138,5 +153,174 @@ object GpuPartitioningUtils {
           if (leafFiles.contains(qualifiedPath)) qualifiedPath.getParent else qualifiedPath }.toSet
     }
   }
+
+  // To fix issue: https://github.com/NVIDIA/spark-rapids/issues/6026 and
+  // https://issues.apache.org/jira/browse/SPARK-39012
+  // Port latest Spark code into spark-rapids
+  def castPartValueToDesiredType(
+      desiredType: DataType,
+      value: String,
+      zoneId: ZoneId): Any = desiredType match {
+    case _ if value == DEFAULT_PARTITION_NAME => null
+    case NullType => null
+    case StringType => UTF8String.fromString(unescapePathName(value))
+    case ByteType | ShortType | IntegerType => Integer.parseInt(value)
+    case LongType => JLong.parseLong(value)
+    case FloatType | DoubleType => JDouble.parseDouble(value)
+    case _: DecimalType => Literal(new JBigDecimal(value)).value
+    case DateType =>
+      Cast(Literal(value), DateType, Some(zoneId.getId)).eval()
+    // Timestamp types
+    case dt if AnyTimestampType.acceptsType(dt) =>
+      Try {
+        Cast(Literal(unescapePathName(value)), dt, Some(zoneId.getId)).eval()
+      }.getOrElse {
+        Cast(Cast(Literal(value), DateType, Some(zoneId.getId)), dt).eval()
+      }
+    case it: AnsiIntervalType =>
+      Cast(Literal(unescapePathName(value)), it).eval()
+    case BinaryType => value.getBytes()
+    case BooleanType => value.toBoolean
+    case dt => throw QueryExecutionErrors.typeUnsupportedError(dt)
+  }
+
+  /**
+   * Given a group of qualified paths, tries to parse them and returns a partition specification.
+   * For example, given:
+   * {{{
+   *   hdfs://<host>:<port>/path/to/partition/a=1/b=hello/c=3.14
+   *   hdfs://<host>:<port>/path/to/partition/a=2/b=world/c=6.28
+   * }}}
+   * it returns:
+   * {{{
+   *   PartitionSpec(
+   *     partitionColumns = StructType(
+   *       StructField(name = "a", dataType = IntegerType, nullable = true),
+   *       StructField(name = "b", dataType = StringType, nullable = true),
+   *       StructField(name = "c", dataType = DoubleType, nullable = true)),
+   *     partitions = Seq(
+   *       Partition(
+   *         values = Row(1, "hello", 3.14),
+   *         path = "hdfs://<host>:<port>/path/to/partition/a=1/b=hello/c=3.14"),
+   *       Partition(
+   *         values = Row(2, "world", 6.28),
+   *         path = "hdfs://<host>:<port>/path/to/partition/a=2/b=world/c=6.28")))
+   * }}}
+   */
+  private[datasources] def parsePartitions(
+    paths: Seq[Path],
+    typeInference: Boolean,
+    basePaths: Set[Path],
+    userSpecifiedSchema: Option[StructType],
+    caseSensitive: Boolean,
+    validatePartitionColumns: Boolean,
+    timeZoneId: String): PartitionSpec = {
+    parsePartitions(paths, typeInference, basePaths, userSpecifiedSchema, caseSensitive,
+      validatePartitionColumns, DateTimeUtils.getZoneId(timeZoneId))
+  }
+
+  private[datasources] def parsePartitions(
+    paths: Seq[Path],
+    typeInference: Boolean,
+    basePaths: Set[Path],
+    userSpecifiedSchema: Option[StructType],
+    caseSensitive: Boolean,
+    validatePartitionColumns: Boolean,
+    zoneId: ZoneId): PartitionSpec = {
+    val userSpecifiedDataTypes = if (userSpecifiedSchema.isDefined) {
+      val nameToDataType = userSpecifiedSchema.get.fields.map(f => f.name -> f.dataType).toMap
+      if (!caseSensitive) {
+        CaseInsensitiveMap(nameToDataType)
+      } else {
+        nameToDataType
+      }
+    } else {
+      Map.empty[String, DataType]
+    }
+
+    // SPARK-26990: use user specified field names if case insensitive.
+    val userSpecifiedNames = if (userSpecifiedSchema.isDefined && !caseSensitive) {
+      CaseInsensitiveMap(userSpecifiedSchema.get.fields.map(f => f.name -> f.name).toMap)
+    } else {
+      Map.empty[String, String]
+    }
+
+    val dateFormatter = DateFormatter(DateFormatter.defaultPattern)
+    val timestampFormatter = TimestampFormatter(
+      timestampPartitionPattern,
+      zoneId,
+      isParsing = true)
+    // First, we need to parse every partition's path and see if we can find partition values.
+    val (partitionValues, optDiscoveredBasePaths) = paths.map { path =>
+      parsePartition(path, typeInference, basePaths, userSpecifiedDataTypes,
+        validatePartitionColumns, zoneId, dateFormatter, timestampFormatter)
+    }.unzip
+
+    // We create pairs of (path -> path's partition value) here
+    // If the corresponding partition value is None, the pair will be skipped
+    val pathsWithPartitionValues = paths.zip(partitionValues).flatMap(x => x._2.map(x._1 -> _))
+
+    if (pathsWithPartitionValues.isEmpty) {
+      // This dataset is not partitioned.
+      PartitionSpec.emptySpec
+    } else {
+      // This dataset is partitioned. We need to check whether all partitions have the same
+      // partition columns and resolve potential type conflicts.
+
+      // Check if there is conflicting directory structure.
+      // For the paths such as:
+      // var paths = Seq(
+      //   "hdfs://host:9000/invalidPath",
+      //   "hdfs://host:9000/path/a=10/b=20",
+      //   "hdfs://host:9000/path/a=10.5/b=hello")
+      // It will be recognised as conflicting directory structure:
+      //   "hdfs://host:9000/invalidPath"
+      //   "hdfs://host:9000/path"
+      // TODO: Selective case sensitivity.
+      val discoveredBasePaths = optDiscoveredBasePaths.flatten.map(_.toString.toLowerCase())
+      assert(
+        discoveredBasePaths.distinct.size == 1,
+        "Conflicting directory structures detected. Suspicious paths:\b" +
+          discoveredBasePaths.distinct.mkString("\n\t", "\n\t", "\n\n") +
+          "If provided paths are partition directories, please set " +
+          "\"basePath\" in the options of the data source to specify the " +
+          "root directory of the table. If there are multiple root directories, " +
+          "please load them separately and then union them.")
+
+      val resolvedPartitionValues = resolvePartitions(pathsWithPartitionValues, caseSensitive)
+
+      // Creates the StructType which represents the partition columns.
+      val fields = {
+        val PartitionValues(columnNames, typedValues) = resolvedPartitionValues.head
+        columnNames.zip(typedValues).map { case (name, TypedPartValue(_, dataType)) =>
+          // We always assume partition columns are nullable since we've no idea whether null values
+          // will be appended in the future.
+          val resultName = userSpecifiedNames.getOrElse(name, name)
+          val resultDataType = userSpecifiedDataTypes.getOrElse(name, dataType)
+          StructField(resultName, resultDataType, nullable = true)
+        }
+      }
+
+      // Finally, we create `Partition`s based on paths and resolved partition values.
+      val partitions = resolvedPartitionValues.zip(pathsWithPartitionValues).map {
+        case (PartitionValues(columnNames, typedValues), (path, _)) =>
+          val rowValues = columnNames.zip(typedValues).map { case (columnName, typedValue) =>
+            try {
+              castPartValueToDesiredType(typedValue.dataType, typedValue.value, zoneId)
+            } catch {
+              case NonFatal(_) =>
+                if (validatePartitionColumns) {
+                  throw QueryExecutionErrors.failedToCastValueToDataTypeForPartitionColumnError(
+                    typedValue.value, typedValue.dataType, columnName)
+                } else null
+            }
+          }
+          PartitionPath(InternalRow.fromSeq(rowValues), path)
+      }
+
+      PartitionSpec(StructType(fields), partitions)
+    }
+  }
+
 
 }
