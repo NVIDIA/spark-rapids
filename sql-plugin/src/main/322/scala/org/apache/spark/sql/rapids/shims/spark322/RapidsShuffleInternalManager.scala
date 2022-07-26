bc1bdcb58dcc7daa47403b3549c9020740cfb16a
Alessandro Bellina <abellina@nvidia.com>
2022-07-26 17:44:46 -0500

    Scalastyle

--- sql-plugin/src/main/322/scala/org/apache/spark/sql/rapids/shims/spark322/RapidsShuffleInternalManager.scala
+++ sql-plugin/src/main/322/scala/org/apache/spark/sql/rapids/shims/spark322/RapidsShuffleInternalManager.scala
@@ -17,7 +17,6 @@
 package org.apache.spark.sql.rapids.shims.spark322
 
 import org.apache.spark.{SparkConf, TaskContext}
-
 import org.apache.spark.shuffle._
 import org.apache.spark.shuffle.sort.BypassMergeSortShuffleHandle
 import org.apache.spark.sql.rapids.{ProxyRapidsShuffleInternalManagerBase, RapidsShuffleInternalManagerBase}
