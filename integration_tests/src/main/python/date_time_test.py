2dfff8edbd95ada78d5f16f55608587acf484863
Nghia Truong <nghiatruong.vn@gmail.com>
2022-08-10 15:50:13 -0700

    Remove 9S format
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- integration_tests/src/main/python/date_time_test.py
+++ integration_tests/src/main/python/date_time_test.py
@@ -365,7 +365,8 @@ def test_date_format_f_incompat(data_gen, date_format):
     assert_gpu_fallback_collect(
         lambda spark : unary_op_df(spark, data_gen).selectExpr("date_format(a, '{}')".format(date_format)), 'ProjectExec', conf)
 
-maybe_supported_date_formats = ['dd-MM-yyyy']
+#maybe_supported_date_formats = ['dd-MM-yyyy', 'yyyy-MM-dd HH:mm:ss.SSS']
+maybe_supported_date_formats = ['yyyy-MM-dd HH:mm:ss.SSSSSSSSS']
 @pytest.mark.parametrize('date_format', maybe_supported_date_formats, ids=idfn)
 @pytest.mark.parametrize('data_gen', date_n_time_gens, ids=idfn)
 @allow_non_gpu('ProjectExec,Alias,DateFormatClass,Literal,Cast')
@@ -375,11 +376,15 @@ def test_date_format_maybe(data_gen, date_format):
         'ProjectExec')
 
 @pytest.mark.parametrize('date_format', maybe_supported_date_formats, ids=idfn)
-@pytest.mark.parametrize('data_gen', date_n_time_gens, ids=idfn)
+@pytest.mark.parametrize('data_gen', [timestamp_gen], ids=idfn)
 def test_date_format_maybe_incompat(data_gen, date_format):
     conf = {"spark.rapids.sql.incompatibleDateFormats.enabled": "true"}
-    assert_gpu_and_cpu_are_equal_collect(
-        lambda spark : unary_op_df(spark, data_gen).selectExpr("date_format(a, '{}')".format(date_format)), conf)
+    def doit(spark):
+        df = unary_op_df(spark, data_gen)
+        print("MY STUFF", df.collect())
+        return df.selectExpr("date_format(a, '{}')".format(date_format))
+    assert_gpu_and_cpu_are_equal_collect(doit, conf)
+        #lambda spark : unary_op_df(spark, data_gen).selectExpr("date_format(a, '{}')".format(date_format)), conf)
 
 # Reproduce conditions for https://github.com/NVIDIA/spark-rapids/issues/5670
 # where we had a failure due to GpuCast canonicalization with timezone.
