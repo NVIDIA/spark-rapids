125727525dabae317500d157e6887c4debd7d089
remzi <13716567376yh@gmail.com>
2022-08-17 14:36:51 +0800

    refactor
    Signed-off-by: remzi <13716567376yh@gmail.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/collectionOperations.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/collectionOperations.scala
@@ -482,25 +482,29 @@ case class GpuFloatArrayMin(child: Expression) extends GpuArrayMin(child) {
 
     withResource(base.getChildColumnView(0)) {child => 
       withResource(child.isNan()){child_is_nan =>
-        // if all values in each array are nans or nulls
+        // if all values in each list are nans or nulls
         val all_nan_or_null = {
           val child_is_nan_or_null = withResource(child.isNull()) {_.or(child_is_nan)}
           val nan_or_null_list = withResource(child_is_nan_or_null) {base.replaceListChild(_)}
           withResource(nan_or_null_list) {_.listReduce(listAll)}
         }
+        // if all values in one list are nans or nulls
+        // return nan if the list contains nan, else return null
+        val true_option = withResource(base.replaceListChild(child_is_nan)) {nan_list =>
+          withResource(nan_list.listReduce(listAny)) {any_nan =>
+            withResource(Seq(getNanSalar, getNullScalar)) {
+              case Seq(nan_scalar, null_scalar) => any_nan.ifElse(nan_scalar, null_scalar)
+            }
+          }
+        }
+        // if a list contains values other than nan or null
+        // we replace all nans to nulls, and then find the min value.
+        val false_option = withResource(child.nansToNulls()) {no_nan_child =>
+          withResource(base.replaceListChild(no_nan_child)) { no_nan_list =>
+            no_nan_list.listReduce(SegmentedReductionAggregation.min())
+          }
+        }
         withResource(all_nan_or_null) {all_nan_or_null =>
-          val true_option = withResource(base.replaceListChild(child_is_nan)) {nan_list =>
-            withResource(nan_list.listReduce(listAny)) {any_nan =>
-              withResource(Seq(getNanSalar, getNullScalar)) {
-                case Seq(nan_scalar, null_scalar) => any_nan.ifElse(nan_scalar, null_scalar)
-              }
-            }
-          }
-          val false_option = withResource(child.nansToNulls()) {no_nan_child =>
-            withResource(base.replaceListChild(no_nan_child)) { no_nan_list =>
-              no_nan_list.listReduce(SegmentedReductionAggregation.min())
-            }
-          }
           withResource(Seq(true_option, false_option)){case Seq(true_option, false_option) =>
             all_nan_or_null.ifElse(true_option, false_option)
           }
