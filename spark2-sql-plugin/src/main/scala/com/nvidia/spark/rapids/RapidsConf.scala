ffb84fdc5385ee8c24d17f0a04aff37e60eeeee7
Tim Liu <timl@nvidia.com>
2022-08-17 11:04:58 +0800

    Change spark-rapids version to v22.08.0
    Signed-off-by: Tim Liu <timl@nvidia.com>

--- spark2-sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
+++ spark2-sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
@@ -1544,7 +1544,7 @@ object RapidsConf {
         |On startup use: `--conf [conf key]=[conf value]`. For example:
         |
         |```
-        |$SPARK_HOME/bin/spark-shell --jars rapids-4-spark_2.12-22.08.0-SNAPSHOT-cuda11.jar \
+        |$SPARK_HOME/bin/spark-shell --jars rapids-4-spark_2.12-22.08.0-cuda11.jar \
         |--conf spark.plugins=com.nvidia.spark.SQLPlugin \
         |--conf spark.rapids.sql.concurrentGpuTasks=2
         |```
