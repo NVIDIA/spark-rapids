c3ebbbb1fa2d638fb655ba2b3b1b29caf9b4fc0e
Navin Kumar <navink@nvidia.com>
2022-08-22 23:41:37 +0000

    Updated AQE tests to ensure that leafexecnodes are tested for Databricks

--- integration_tests/src/main/python/aqe_test.py
+++ integration_tests/src/main/python/aqe_test.py
@@ -19,53 +19,81 @@ from pyspark.sql.types import *
 from asserts import assert_gpu_and_cpu_are_equal_collect, assert_gpu_fallback_collect, assert_cpu_and_gpu_are_equal_collect_with_capture
 from conftest import is_databricks_runtime, is_emr_runtime
 from data_gen import *
-from marks import ignore_order, allow_non_gpu, incompat, validate_execs_in_gpu_plan, approximate_float
+from marks import ignore_order, allow_non_gpu, incompat, validate_execs_in_gpu_plan
 from spark_session import with_cpu_session, with_spark_session
 
-_adaptive_conf = { "spark.sql.adaptive.enabled": "true",
-                   "spark.rapids.sql.castFloatToString.enabled": "true",
-                   "spark.rapids.sql.castDecimalToString.enabled": "true" }
+_adaptive_conf = { "spark.sql.adaptive.enabled": "true" }
 
 # Dynamic switching of join strategies
-
 # Dynamic coalescing of shuffle partitions
+# Dynamically Handle Skew Joins
+
 _adaptive_coalese_conf = copy_and_update(_adaptive_conf, {
     "spark.sql.adaptive.coalescePartitions.enabled": "true"
 })
 
 
-# Dynamically Handle Skew Joins
-
-def create_skew_df(spark, data_gen, length):
-    rand = random.Random(0)
-    data_gen.start(rand)
-    next_val = None
-    while next_val is None:
-        next_val = data_gen.gen()
-    root = two_col_df(spark, data_gen, SetValuesGen(data_gen.data_type, [next_val]), length)
+def create_skew_df(spark, length):
+    root = spark.range(0, length)
+    mid = length / 2
     left = root.select(
-        when(col('a') < col('b') / 2, col('b')).
-            otherwise('a').alias("key1"),
-        col('a').alias("value1")
+        when(col('id') < mid / 2, mid).
+            otherwise('id').alias("key1"),
+        col('id').alias("value1")
     )
     right = root.select(
-        when(col('a') < col('b'), col('b')).
-            otherwise('a').alias("key2"),
-        col('a').alias("value2")
+        when(col('id') < mid, mid).
+            otherwise('id').alias("key2"),
+        col('id').alias("value2")
     )
     return left, right
 
 
 @ignore_order(local=True)
-@approximate_float(abs=1e-6)
-@allow_non_gpu("ShuffleExchangeExec")
-@pytest.mark.parametrize("data_gen", numeric_gens + decimal_gens, ids=idfn)
-def test_skew_join(data_gen):
+def test_aqe_skew_join():
     def do_join(spark):
-        left, right = create_skew_df(spark, data_gen, length=512)
+        left, right = create_skew_df(spark, 500)
         left.createOrReplaceTempView("skewData1")
         right.createOrReplaceTempView("skewData2")
         return spark.sql("SELECT * FROM skewData1 join skewData2 ON key1 = key2")
 
-    assert_gpu_and_cpu_are_equal_collect(do_join, _adaptive_conf)
+    assert_gpu_and_cpu_are_equal_collect(do_join, conf=_adaptive_conf)
 
+@ignore_order(local=True)
+@pytest.mark.parametrize("data_gen", integral_gens, ids=idfn)
+def test_aqe_join_parquet(spark_tmp_path, data_gen):
+    data_path = spark_tmp_path + '/PARQUET_DATA'
+    with_cpu_session(
+        lambda spark: unary_op_df(spark, data_gen).orderBy('a').write.parquet(data_path)
+    )
+
+    def do_it(spark):
+        spark.read.parquet(data_path).createOrReplaceTempView('df1')
+        spark.read.parquet(data_path).createOrReplaceTempView('df2')
+        return spark.sql("select count(*) from df1,df2 where df1.a = df2.a")
+
+    assert_gpu_and_cpu_are_equal_collect(do_it, conf=_adaptive_conf)
+
+
+@ignore_order(local=True)
+@pytest.mark.parametrize("data_gen", integral_gens, ids=idfn)
+def test_aqe_join_parquet_batch(spark_tmp_path, data_gen):
+    # force v2 source for parquet to use BatchScanExec
+    conf = copy_and_update(_adaptive_conf, {
+        "spark.sql.sources.useV1SourceList": ""
+    })
+
+    first_data_path = spark_tmp_path + '/PARQUET_DATA/key=0'
+    with_cpu_session(
+            lambda spark : unary_op_df(spark, data_gen).write.parquet(first_data_path))
+    second_data_path = spark_tmp_path + '/PARQUET_DATA/key=1'
+    with_cpu_session(
+            lambda spark : unary_op_df(spark, data_gen).write.parquet(second_data_path))
+    data_path = spark_tmp_path + '/PARQUET_DATA'
+
+    def do_it(spark):
+        spark.read.parquet(data_path).createOrReplaceTempView('df1')
+        spark.read.parquet(data_path).createOrReplaceTempView('df2')
+        return spark.sql("select count(*) from df1,df2 where df1.a = df2.a")
+
+    assert_gpu_and_cpu_are_equal_collect(do_it, conf=conf)
