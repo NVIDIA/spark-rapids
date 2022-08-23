39b7ea81852857d152a03772093d7a36c9e1f9fb
Nghia Truong <nghiatruong.vn@gmail.com>
2022-08-23 12:06:01 -0700

    Add fallback test
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- integration_tests/src/main/python/date_time_test.py
+++ integration_tests/src/main/python/date_time_test.py
@@ -214,12 +214,22 @@ def test_to_unix_timestamp(data_gen, ansi_enabled):
         lambda spark : unary_op_df(spark, data_gen).selectExpr("to_unix_timestamp(a)"),
         {'spark.sql.ansi.enabled': ansi_enabled})
 
+
 @pytest.mark.parametrize('time_zone', ["UTC", "UTC+0", "UTC-0", "GMT", "GMT+0", "GMT-0"], ids=idfn)
 @pytest.mark.parametrize('data_gen', [timestamp_gen], ids=idfn)
 def test_from_utc_timestamp(data_gen, time_zone):
     assert_gpu_and_cpu_are_equal_collect(
         lambda spark : unary_op_df(spark, data_gen).selectExpr("from_utc_timestamp(a, '{}')".format(time_zone)))
-        
+
+@allow_non_gpu('ProjectExec, FromUTCTimestamp')
+@pytest.mark.parametrize('time_zone', ["PST", "MST", "EST", "VST", "NST", "AST"], ids=idfn)
+@pytest.mark.parametrize('data_gen', [timestamp_gen], ids=idfn)
+def test_from_utc_timestamp_fallback(data_gen, time_zone):
+    assert_gpu_fallback_collect(
+        lambda spark : unary_op_df(spark, data_gen).selectExpr("from_utc_timestamp(a, '{}')".format(time_zone)),
+    'ProjectExec')
+
+
 @pytest.mark.parametrize('invalid,fmt', [
     ('2021-01/01', 'yyyy-MM-dd'),
     ('2021/01-01', 'yyyy/MM/dd'),
