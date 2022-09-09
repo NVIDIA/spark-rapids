d5076807af75c56b0421b386a4e908acfd7709fb
Navin Kumar <navink@nvidia.com>
2022-09-09 00:50:22 +0000

    WIP: Windowing fix for Databricks to handle missing child expressions of GpuWindowFunction
    Signed-off-by: Navin Kumar <navink@nvidia.com>

--- integration_tests/src/main/python/window_function_test.py
+++ integration_tests/src/main/python/window_function_test.py
@@ -907,6 +907,23 @@ def test_running_window_function_exec_for_all_aggs():
         from window_collect_table
         ''')
 
+# Test the Databricks optimization that sometimes drops children from the 
+# initial list of expressions that are still children of GpuWindowFunction in GpuWindowExec
+@ignore_order(local=True)
+@pytest.mark.parametrize('data_gen', integral_gens, ids=idfn)
+def test_join_sum_window(data_gen):
+    conf = { 'spark.rapids.sql.batchSizeBytes': '100',
+            'spark.rapids.sql.explain': 'NONE'}
+
+    def do_it(spark):
+        agg_table = gen_df(spark, StructGen([('a_1', LongRangeGen()), ('c', data_gen)], nullable=False))
+        part_table = gen_df(spark, StructGen([('a_2', LongRangeGen()), ('b', byte_gen)], nullable=False))
+        agg_table.createOrReplaceTempView("agg")
+        part_table.createOrReplaceTempView("part")
+        return spark.sql("select b, sum(c) as sum_c, sum(c)/sum(sum(c)) over (partition by b) as r_c from agg, part where a_1 = a_2 group by b order by b, r_c")
+
+    assert_gpu_and_cpu_are_equal_collect(do_it, conf = conf)
+
 # Generates some repeated values to test the deduplication of GpuCollectSet.
 # And GpuCollectSet does not yet support struct type.
 _gen_data_for_collect_set = [
