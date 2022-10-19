615af9cdf17d82769a833fd460ff7e1a35303df1
Robert (Bobby) Evans <bobby@apache.org>
2022-10-19 08:29:36 -0500

    Add in support for zorder operators on databricks
    Signed-off-by: Robert (Bobby) Evans <bobby@apache.org>

--- integration_tests/src/main/python/delta_zorder_test.py
+++ integration_tests/src/main/python/delta_zorder_test.py
@@ -23,21 +23,21 @@ from spark_session import is_databricks_runtime, with_cpu_session, with_gpu_sess
 # the important part is to not have InterleaveBits or PartitionerExpr
 @allow_non_gpu("SerializeFromObjectExec", "MapElementsExec", "MapPartitionsExec", "DeserializeToObjectExec",
         "ProjectExec", "FilterExec", "SortExec", "ShuffleExchangeExec", "FileSourceScanExec",
-        "ObjectHashAggregateExec", "CollectLimitExec")
+        "ObjectHashAggregateExec", "CollectLimitExec", "CreateTableExec", "AppendDataExecV1")
 @delta_lake
-@pytest.mark.skipif(is_databricks_runtime(), reason='We do not support zorder for databricks yet')
 @ignore_order(local=True)
 def test_delta_zorder(spark_tmp_table_factory):
     table = spark_tmp_table_factory.get()
-    def setup_delta_table(spark):
+
+    def optimize_table(spark):
+        # We need to drop the table and rerun each time because in some
+        # versions delta will keep track if it has already been optimized or not
+        # and will not re-run if it has been optimized
         df = two_col_df(spark, long_gen, string_gen, length=4096)
         spark.sql("DROP TABLE IF EXISTS {}".format(table)).show()
         spark.sql("CREATE TABLE {} (a BIGINT, b STRING) USING DELTA".format(table)).show()
         df.write.insertInto(table)
 
-    with_cpu_session(setup_delta_table)
-
-    def optimize_table(spark):
         # The optimize returns stats and metadata about the operation, which is different
         # from one run to another, so we cannot just compare them...
         spark.sql("OPTIMIZE {} ZORDER BY a, b".format(table)).show()
