054cfef1c36e744dd14a4ad455b6b11948d20d93
Raza Jafri <razajafri@users.noreply.github.com>
2022-11-03 15:18:43 -0700

    Removed PromotePrecision for Spark 3.4 [databricks] (#6937)
    * Bubbled up the PromotePrecision
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * remove GpuPromotePrecision
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * changed the comment
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * Revert "remove GpuPromotePrecision"
    
    This reverts commit d0d4f8f02e3df2ecd84e2f3d9d97fc5dd8f5696f.
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * Changes to properly get the Exprs from the parent shim
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * removed PromotePrecision from 31x shims
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * regenerated configs.md
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * cleaned up the diff for easier review
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * Fixed the build error for 320
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * removed the override from get320Exprs as it doesn't override anything
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * cleaned up code for easier review
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * Removed CheckOverflow override from the common GpuOverrides and moving it o the shimmed one because of CheckOverflow changes in 340
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * removed CheckOverflow from 3.4 shim as we don't support the cases where it is added in 3.4
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * Added CheckOverflow to DB
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * Reverted the SparkBaseShim class and introduced a DecimalArithmeticOverrides class
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * added override
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    * addressed nits
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>
    Co-authored-by: Raza Jafri <rjafri@nvidia.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
@@ -24,7 +24,7 @@ import scala.util.control.NonFatal
 
 import ai.rapids.cudf.DType
 import com.nvidia.spark.rapids.RapidsConf.{SUPPRESS_PLANNING_FAILURE, TEST_CONF}
-import com.nvidia.spark.rapids.shims.{AQEUtils, DeltaLakeUtils, GetMapValueMeta, GpuBatchScanExec, GpuHashPartitioning, GpuRangePartitioning, GpuSpecifiedWindowFrameMeta, GpuTypeShims, GpuWindowExpressionMeta, OffsetWindowFunctionMeta, SparkShimImpl}
+import com.nvidia.spark.rapids.shims.{AQEUtils, DecimalArithmeticOverrides, DeltaLakeUtils, GetMapValueMeta, GpuBatchScanExec, GpuHashPartitioning, GpuRangePartitioning, GpuSpecifiedWindowFrameMeta, GpuTypeShims, GpuWindowExpressionMeta, OffsetWindowFunctionMeta, SparkShimImpl}
 
 import org.apache.spark.internal.Logging
 import org.apache.spark.rapids.shims.GpuShuffleExchangeExec
@@ -896,112 +896,7 @@ object GpuOverrides extends Logging {
           }
         }
       }),
-    expr[PromotePrecision](
-      "PromotePrecision before arithmetic operations between DecimalType data",
-      ExprChecks.unaryProjectInputMatchesOutput(TypeSig.DECIMAL_128,
-        TypeSig.DECIMAL_128),
-      (a, conf, p, r) => new UnaryExprMeta[PromotePrecision](a, conf, p, r) {
-        override def convertToGpu(child: Expression): GpuExpression = GpuPromotePrecision(child)
-      }),
-    expr[CheckOverflow](
-      "CheckOverflow after arithmetic operations between DecimalType data",
-      ExprChecks.unaryProjectInputMatchesOutput(TypeSig.DECIMAL_128,
-        TypeSig.DECIMAL_128),
-      (a, conf, p, r) => new ExprMeta[CheckOverflow](a, conf, p, r) {
-        private[this] def extractOrigParam(expr: BaseExprMeta[_]): BaseExprMeta[_] =
-          expr.wrapped match {
-            case lit: Literal if lit.dataType.isInstanceOf[DecimalType] =>
-              // Lets figure out if we can make the Literal value smaller
-              val (newType, value) = lit.value match {
-                case null =>
-                  (DecimalType(0, 0), null)
-                case dec: Decimal =>
-                  val stripped = Decimal(dec.toJavaBigDecimal.stripTrailingZeros())
-                  val p = stripped.precision
-                  val s = stripped.scale
-                  val t = if (s < 0 && !SQLConf.get.allowNegativeScaleOfDecimalEnabled) {
-                    // need to adjust to avoid errors about negative scale
-                    DecimalType(p - s, 0)
-                  } else {
-                    DecimalType(p, s)
-                  }
-                  (t, stripped)
-                case other =>
-                  throw new IllegalArgumentException(s"Unexpected decimal literal value $other")
-              }
-              expr.asInstanceOf[LiteralExprMeta].withNewLiteral(Literal(value, newType))
-            // Avoid unapply for PromotePrecision and Cast because it changes between Spark versions
-            case p: PromotePrecision if p.child.isInstanceOf[CastBase] &&
-                p.child.dataType.isInstanceOf[DecimalType] =>
-              val c = p.child.asInstanceOf[CastBase]
-              val to = c.dataType.asInstanceOf[DecimalType]
-              val fromType = DecimalUtil.optionallyAsDecimalType(c.child.dataType)
-              fromType match {
-                case Some(from) =>
-                  val minScale = math.min(from.scale, to.scale)
-                  val fromWhole = from.precision - from.scale
-                  val toWhole = to.precision - to.scale
-                  val minWhole = if (to.scale < from.scale) {
-                    // If the scale is getting smaller in the worst case we need an
-                    // extra whole part to handle rounding up.
-                    math.min(fromWhole + 1, toWhole)
-                  } else {
-                    math.min(fromWhole, toWhole)
-                  }
-                  val newToType = DecimalType(minWhole + minScale, minScale)
-                  if (newToType == from) {
-                    // We can remove the cast totally
-                    val castExpr = expr.childExprs.head
-                    castExpr.childExprs.head
-                  } else if (newToType == to) {
-                    // The cast is already ideal
-                    expr
-                  } else {
-                    val castExpr = expr.childExprs.head.asInstanceOf[CastExprMeta[_]]
-                    castExpr.withToTypeOverride(newToType)
-                  }
-                case _ =>
-                  expr
-              }
-            case _ => expr
-          }
-        private[this] lazy val binExpr = childExprs.head
-        private[this] lazy val lhs = extractOrigParam(binExpr.childExprs.head)
-        private[this] lazy val rhs = extractOrigParam(binExpr.childExprs(1))
-        private[this] lazy val lhsDecimalType =
-          DecimalUtil.asDecimalType(lhs.wrapped.asInstanceOf[Expression].dataType)
-        private[this] lazy val rhsDecimalType =
-          DecimalUtil.asDecimalType(rhs.wrapped.asInstanceOf[Expression].dataType)
 
-        override def convertToGpu(): GpuExpression = {
-          // Prior to Spark 3.4.0
-          // Division and Multiplication of Decimal types is a little odd. Spark will cast the
-          // inputs to a common wider value where the scale is the max of the two input scales,
-          // and the precision is max of the two input non-scale portions + the new scale. Then it
-          // will do the divide or multiply as a BigDecimal value but lie about the return type.
-          // Finally here in CheckOverflow it will reset the scale and check the precision so that
-          // Spark knows it fits in the final desired result.
-          // Here we try to strip out the extra casts, etc to get to as close to the original
-          // query as possible. This lets us then calculate what CUDF needs to get the correct
-          // answer, which in some cases is a lot smaller.
-
-          a.child match {
-            case _: Divide =>
-              // GpuDecimalDivide includes the overflow check in it.
-              GpuDecimalDivide(lhs.convertToGpu(), rhs.convertToGpu(), wrapped.dataType)
-            case _: Multiply =>
-              // GpuDecimal*Multiply includes the overflow check in it
-              val intermediatePrecision =
-                GpuDecimalMultiply.nonRoundedIntermediatePrecision(lhsDecimalType,
-                  rhsDecimalType, a.dataType)
-              GpuDecimalMultiply(lhs.convertToGpu(), rhs.convertToGpu(), wrapped.dataType,
-                useLongMultiply = intermediatePrecision > DType.DECIMAL128_MAX_PRECISION)
-            case _ =>
-              GpuCheckOverflow(childExprs.head.convertToGpu(),
-                wrapped.dataType, wrapped.nullOnOverflow)
-          }
-        }
-      }),
     expr[ToDegrees](
       "Converts radians to degrees",
       ExprChecks.mathUnary,
@@ -3604,7 +3499,7 @@ object GpuOverrides extends Logging {
   // Shim expressions should be last to allow overrides with shim-specific versions
   val expressions: Map[Class[_ <: Expression], ExprRule[_ <: Expression]] =
     commonExpressions ++ TimeStamp.getExprs ++ GpuHiveOverrides.exprs ++
-        SparkShimImpl.getExprs ++ ZOrderRules.exprs
+        ZOrderRules.exprs ++ DecimalArithmeticOverrides.exprs ++ SparkShimImpl.getExprs
 
   def wrapScan[INPUT <: Scan](
       scan: INPUT,
