d5ba2e234cca531e267d01f727b9476d6e57b512
Peixin <pxli@nyu.edu>
2022-11-18 16:29:41 +0800

    Skip test_hash_groupby_collect_with_single_distinct (#7107)
    Signed-off-by: Peixin Li <pxli@nyu.edu>
    
    Signed-off-by: Peixin Li <pxli@nyu.edu>

--- integration_tests/src/main/python/hash_aggregate_test.py
+++ integration_tests/src/main/python/hash_aggregate_test.py
@@ -731,7 +731,7 @@ def test_hash_reduction_collect_set_on_nested_array_type(data_gen):
 
 @ignore_order(local=True)
 @pytest.mark.parametrize('data_gen', _full_gen_data_for_collect_op, ids=idfn)
-@pytest.mark.xfail(reason='https://github.com/NVIDIA/spark-rapids/issues/7104')
+@pytest.mark.skip(reason='https://github.com/NVIDIA/spark-rapids/issues/7092,7104')
 def test_hash_groupby_collect_with_single_distinct(data_gen):
     # test collect_ops with other distinct aggregations
     assert_gpu_and_cpu_are_equal_collect(
