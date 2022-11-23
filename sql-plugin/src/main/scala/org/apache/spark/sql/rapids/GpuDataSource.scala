08f55f275ddabd308f40e9f7879500273d3973ec
Raza Jafri <rjafri@nvidia.com>
2022-11-23 12:42:08 -0800

    fix 3.4 build
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/GpuDataSource.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/GpuDataSource.scala
@@ -86,9 +86,9 @@ case class GpuDataSource(
   }
 
   bucketSpec.foreach { bucket =>
-    SchemaUtils.checkColumnNameDuplication(
+    RapidsSchemaUtils.checkColumnNameDuplication(
       bucket.bucketColumnNames, "in the bucket definition", equality)
-    SchemaUtils.checkColumnNameDuplication(
+    RapidsSchemaUtils.checkColumnNameDuplication(
       bucket.sortColumnNames, "in the sort definition", equality)
   }
 
@@ -175,7 +175,7 @@ case class GpuDataSource(
     // we have the existing tests for the cases (e.g., `ParquetHadoopFsRelationSuite`).
     // See SPARK-18108 and SPARK-21144 for related discussions.
     try {
-      SchemaUtils.checkColumnNameDuplication(
+      RapidsSchemaUtils.checkColumnNameDuplication(
         (dataSchema ++ partitionSchema).map(_.name),
         "in the data schema and the partition schema",
         equality)
@@ -286,17 +286,17 @@ case class GpuDataSource(
 
     relation match {
       case hs: HadoopFsRelation =>
-        SchemaUtils.checkSchemaColumnNameDuplication(
+        RapidsSchemaUtils.checkSchemaColumnNameDuplication(
           hs.dataSchema,
           "in the data schema",
           equality)
-        SchemaUtils.checkSchemaColumnNameDuplication(
+        RapidsSchemaUtils.checkSchemaColumnNameDuplication(
           hs.partitionSchema,
           "in the partition schema",
            equality)
         DataSourceUtils.verifySchema(hs.fileFormat, hs.dataSchema)
       case _ =>
-        SchemaUtils.checkSchemaColumnNameDuplication(
+        RapidsSchemaUtils.checkSchemaColumnNameDuplication(
           relation.schema,
           "in the data schema",
            equality)
