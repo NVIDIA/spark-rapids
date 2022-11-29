6af828c7e7dd51070e18049e16ec9c35dcf8bffd
Alessandro Bellina <abellina@nvidia.com>
2022-11-29 15:06:09 -0600

    Fixes unregisterShuffle bugs in the driver and a missed match for the GpuResolver
    Signed-off-by: Alessandro Bellina <abellina@nvidia.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/RapidsShuffleInternalManagerBase.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/RapidsShuffleInternalManagerBase.scala
@@ -1330,17 +1330,20 @@ abstract class RapidsShuffleInternalManagerBase(conf: SparkConf, val isDriver: B
 
   override def unregisterShuffle(shuffleId: Int): Boolean = {
     unregisterGpuShuffle(shuffleId)
-    shuffleBlockResolver match {
-      case isbr: IndexShuffleBlockResolver =>
-        Option(taskIdMapsForShuffle.remove(shuffleId)).foreach { mapTaskIds =>
-          mapTaskIds.iterator.foreach { mapTaskId =>
-            isbr.removeDataByMap(shuffleId, mapTaskId)
+    if (!isDriver) {
+      shuffleBlockResolver match {
+        case isbr: IndexShuffleBlockResolver =>
+          Option(taskIdMapsForShuffle.remove(shuffleId)).foreach { mapTaskIds =>
+            mapTaskIds.iterator.foreach { mapTaskId =>
+              isbr.removeDataByMap(shuffleId, mapTaskId)
+            }
           }
-        }
-      case _ if taskIdMapsForShuffle.size() > 0 =>
-        throw new IllegalStateException(
-          "unregisterShuffle called with unexpected resolver " +
-          s"$shuffleBlockResolver and blocks left to be cleaned")
+        case _: GpuShuffleBlockResolver => // noop
+        case _ =>
+          throw new IllegalStateException(
+            "unregisterShuffle called with unexpected resolver " +
+              s"$shuffleBlockResolver and blocks left to be cleaned")
+      }
     }
     wrapped.unregisterShuffle(shuffleId)
   }
