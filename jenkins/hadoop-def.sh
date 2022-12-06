bf88a0d185b7e93500f3c25f5309e4ef03835a2c
YanxuanLiu <yanxuanl@nvidia.com>
2022-12-06 22:23:07 +0800

    handle version suffix and custom url
    Signed-off-by: YanxuanLiu <yanxuanl@nvidia.com>

--- jenkins/hadoop-def.sh
+++ jenkins/hadoop-def.sh
@@ -16,14 +16,26 @@
 #
 # Argument(s):
 #   SPARK_VER: spark version. e.g. 3.3.0
+#   CUSTOM_URL: custom download url
 #
 
 set -e
 
-if [[ `echo -e "$1\n3.3.0" | sort -V | head -n 1` == "3.3.0" ]]; then # spark version >= 3.3.0
+if [[ $# -eq 0 ]]; then
+    echo "No arguments provided, use default: bin-hadoop3.2"
+    exit 1
+fi
+
+# Split spark version into base version (e.g. 3.3.0) and suffix (e.g. SNAPSHOT)
+PRE_IFS=$IFS
+IFS="-" read -r -a SPARK_VER <<< "$1"
+IFS=$PRE_IFS
+if [[ ${#SPARK_VER[@]} > 1 ]]; then # version with suffix
+    BIN_HADOOP_VER="bin-hadoop3.2"
+elif [[ -n "$2" ]] && [[ "$2" != "apache default url" ]]; then # without suffix, provided custom-url and url != default url
+    BIN_HADOOP_VER="bin-hadoop3.2"
+elif [[ `echo -e "${SPARK_VER[0]}\n3.3.0" | sort -V | head -n 1` == "3.3.0" ]]; then # no custom url and spark version >= 3.3.0
     BIN_HADOOP_VER="bin-hadoop3"
-    BIN_HADOOP_URL="to be defined"
 else
     BIN_HADOOP_VER="bin-hadoop3.2"
-    BIN_HADOOP_URL="to be defined"
 fi
