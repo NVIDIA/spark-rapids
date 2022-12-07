755924cb257aeb64cbbbeda5b2a6b1b8e9974a59
YanxuanLiu <yanxuanl@nvidia.com>
2022-12-07 12:17:36 +0800

    optimize url
    Signed-off-by: YanxuanLiu <yanxuanl@nvidia.com>

--- jenkins/hadoop-def.sh
+++ jenkins/hadoop-def.sh
@@ -15,26 +15,22 @@
 # limitations under the License.
 #
 # Argument(s):
-#   SPARK_VER: spark version. e.g. 3.3.0
-#   CUSTOM_URL: custom download url
+#   1 SPARK_VER: spark version. e.g. 3.3.0
+#   2 CUSTOM_URL: custom download url
 #
 
 set -e
 
-if [[ $# -eq 0 ]]; then
-    echo "No arguments provided, use default: bin-hadoop3.2"
-    exit 1
-fi
-
+spark_version=${1:-"3.1.1"}
 # Split spark version into base version (e.g. 3.3.0) and suffix (e.g. SNAPSHOT)
 PRE_IFS=$IFS
-IFS="-" read -r -a SPARK_VER <<< "$1"
+IFS="-" read -r -a spark_version <<< "$1"
 IFS=$PRE_IFS
-if [[ ${#SPARK_VER[@]} > 1 ]]; then # version with suffix
+if [[ ${#spark_version[@]} > 1 ]]; then # version with suffix
     BIN_HADOOP_VER="bin-hadoop3.2"
-elif [[ -n "$2" ]] && [[ "$2" != "apache default url" ]]; then # without suffix, provided custom-url and url != default url
+elif [[ -n "$2" ]] && [[ "$2" != "https://repo.maven.apache.org/maven2" ]]; then # without suffix, provided custom-url and url != default url
     BIN_HADOOP_VER="bin-hadoop3.2"
-elif [[ `echo -e "${SPARK_VER[0]}\n3.3.0" | sort -V | head -n 1` == "3.3.0" ]]; then # no custom url and spark version >= 3.3.0
+elif [[ `echo -e "${spark_version[0]}\n3.3.0" | sort -V | head -n 1` == "3.3.0" ]]; then # no custom url and spark version >= 3.3.0
     BIN_HADOOP_VER="bin-hadoop3"
 else
     BIN_HADOOP_VER="bin-hadoop3.2"
