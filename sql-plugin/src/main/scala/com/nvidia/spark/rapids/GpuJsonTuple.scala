4307b49e404c8c108a3a507a321e25397c37f0a5
Cindy Jiang <cindyj@nvidia.com>
2022-12-14 17:43:59 -0800

    added GpuJsonTuple class and scala file
    Signed-off-by: Cindy Jiang <cindyj@nvidia.com>

---
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuJsonTuple.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nvidia.spark.rapids
+
+import ai.rapids.cudf.{ColumnVector, ColumnView}
+import com.nvidia.spark.rapids.GpuExpressionsUtils.columnarEvalToColumn
+import com.nvidia.spark.rapids.RapidsPluginImplicits._
+import com.nvidia.spark.rapids.shims.ShimExpression
+
+import org.apache.spark.sql.catalyst.expressions.{ExpectsInputTypes, Expression}
+import org.apache.spark.sql.types.{DataType, StringType}
+import org.apache.spark.sql.vectorized.ColumnarBatch
+
+case class GpuJsonTuple(json: Expression, paths: Seq[Expression]) extends GpuExpression 
+  with ShimExpression with ExpectsInputTypes {
+  override def dataType: DataType = StringType
+  override def inputTypes: Seq[DataType] = Seq.fill(paths.length + 1)(StringType)
+  override def nullable: Boolean = true
+  override def prettyName: String = "json_tuple"
+
+  override def columnarEval(batch: ColumnarBatch): Any = {
+    def resolveScalar(any: Any): GpuScalar = {
+      withResourceIfAllowed(any) {
+        case s: GpuScalar => s
+        case _ => throw new UnsupportedOperationException("JSON path must be a scalar value")
+      }
+    }
+    withResource(columnarEvalToColumn(json, batch).getBase) { json =>
+      withResource(paths.safeMap(p => resolveScalar(p.columnarEval(batch)).getBase)) { scalars =>
+        withResource(scalars.safeMap(json.getJSONObject(_))) { cols =>
+          ColumnVector.stringConcatenate(cols.toArray[ColumnView])
+        }
+      }
+    }
+  }
+}
