af238755909c8608285e1adc7af3132a8d31bd3b
Robert (Bobby) Evans <bobby@apache.org>
2022-12-15 14:32:15 -0600

    Avoid shutting down RMM until all allocations have cleared
    Signed-off-by: Robert (Bobby) Evans <bobby@apache.org>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuDeviceManager.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuDeviceManager.scala
@@ -16,7 +16,7 @@
 
 package com.nvidia.spark.rapids
 
-import java.util.concurrent.ThreadFactory
+import java.util.concurrent.{ThreadFactory, TimeUnit}
 
 import scala.collection.mutable.ArrayBuffer
 import scala.util.control.NonFatal
@@ -143,6 +143,17 @@ object GpuDeviceManager extends Logging {
     singletonMemoryInitialized = Errored
     RapidsBufferCatalog.close()
     GpuShuffleEnv.shutdown()
+    // try to avoid segfault on RMM shutdown
+    val timeout = System.nanoTime() + TimeUnit.SECONDS.toNanos(10)
+    while(Rmm.getTotalBytesAllocated > 0 && System.nanoTime() < timeout) {
+      Thread.sleep(10)
+    }
+    if (System.nanoTime() >= timeout) {
+      val remaining = Rmm.getTotalBytesAllocated
+      if (remaining > 0) {
+        logWarning(s"Shutting down RMM even though there are outstanding allocations $remaining")
+      }
+    }
     Rmm.shutdown()
     singletonMemoryInitialized = Uninitialized
   }
