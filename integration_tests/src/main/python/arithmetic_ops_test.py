a33295c593d8c38d625b1e8350ff2567155dc43a
Raza Jafri <rjafri@nvidia.com>
2022-12-14 18:49:09 -0800

    skip tests for Spark 340 and 330db
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>

--- integration_tests/src/main/python/arithmetic_ops_test.py
+++ integration_tests/src/main/python/arithmetic_ops_test.py
@@ -251,7 +251,8 @@ def test_int_division(data_gen):
                 'a DIV b'))
 
 @pytest.mark.parametrize('lhs', [DecimalGen(6, 5), DecimalGen(5, 4), DecimalGen(3, -2), _decimal_gen_30_2], ids=idfn)
-@pytest.mark.parametrize('rhs', [DecimalGen(13, 2), DecimalGen(6, 3), _decimal_gen_38_0, _decimal_gen_36_neg5], ids=idfn)
+@pytest.mark.parametrize('rhs', [DecimalGen(13, 2), DecimalGen(6, 3), _decimal_gen_38_0,
+                                 pytest.param(_decimal_gen_36_neg5, marks=pytest.mark.skipif(not is_before_spark_340() or is_databricks113_or_later(), reason='SPARK-41207'))], ids=idfn)
 def test_int_division_mixed(lhs, rhs):
     assert_gpu_and_cpu_are_equal_collect(
             lambda spark : two_col_df(spark, lhs, rhs).selectExpr(
@@ -910,7 +911,7 @@ div_overflow_exprs = [
 @pytest.mark.parametrize('ansi_enabled', ['false', 'true'])
 def test_div_overflow_exception_when_ansi(expr, ansi_enabled):
     ansi_conf = {'spark.sql.ansi.enabled': ansi_enabled}
-    err_exp = 'java.lang.ArithmeticException' if is_before_spark_330() \
+    err_exp = 'java.lang.ArithmeticException' if is_before_spark_330() and not is_databricks113_or_later() \
         else 'org.apache.spark.SparkArithmeticException'
     err_mess = ': Overflow in integral divide' if is_before_spark_340() \
         else ': [ARITHMETIC_OVERFLOW] Overflow in integral divide'
