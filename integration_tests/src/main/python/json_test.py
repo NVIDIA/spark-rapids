62105edd1f4bae6e078fafa1862ab522f77536da
Nghia Truong <nghiatruong.vn@gmail.com>
2023-01-12 13:41:51 -0800

    Update test
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- integration_tests/src/main/python/json_test.py
+++ integration_tests/src/main/python/json_test.py
@@ -18,7 +18,7 @@ from asserts import assert_gpu_and_cpu_are_equal_collect, assert_gpu_and_cpu_err
 from data_gen import *
 from conftest import is_databricks_runtime
 from marks import approximate_float, allow_non_gpu, ignore_order
-
+from pyspark.sql.functions import from_json
 from spark_session import with_cpu_session, with_gpu_session, is_before_spark_330, is_spark_330_or_later
 
 json_supported_gens = [
@@ -366,6 +366,5 @@ def test_from_json_map():
     json_string_gen = StringGen(r'{"a": "[0-9]{0,5}"(, "b": "[A-Z]{0,5}")?}')
     assert_gpu_and_cpu_are_equal_collect(
         lambda spark : unary_op_df(spark, json_string_gen) \
-            .selectExpr("from_json(a, \"MAP<STRING,STRING>\") as parsed") \
-            .selectExpr("parsed[\"a\"] as pa", "parsed[\"b\"] as pb"),
+            .select(from_json(f.col('a'), 'MAP<STRING,STRING>')),
         conf={"spark.rapids.sql.expression.JsonToStructs": "true"})
