e41116b52efafaf2f1e016ec4a034c32d1623b2b
remzi <13716567376yh@gmail.com>
2023-01-13 14:45:11 +0800

    fix errors in 332 and 330cdh
    Signed-off-by: remzi <13716567376yh@gmail.com>

--- sql-plugin/src/main/332/scala/com/nvidia/spark/rapids/shims/SparkShims.scala
+++ sql-plugin/src/main/332/scala/com/nvidia/spark/rapids/shims/SparkShims.scala
@@ -16,4 +16,21 @@
 
 package com.nvidia.spark.rapids.shims
 
-object SparkShimImpl extends Spark331PlusShims with AnsiCastRuleShims
+import com.nvidia.spark.rapids._
+
+import org.apache.spark.sql.execution.command.{CreateDataSourceTableAsSelectCommand, DataWritingCommand, RunnableCommand}
+
+object SparkShimImpl extends Spark331PlusShims with AnsiCastRuleShims {
+  override def getDataWriteCmds: Map[Class[_ <: DataWritingCommand],
+      DataWritingCommandRule[_ <: DataWritingCommand]] = {
+    Seq(GpuOverrides.dataWriteCmd[CreateDataSourceTableAsSelectCommand](
+    "Create table with select command",
+    (a, conf, p, r) => new CreateDataSourceTableAsSelectCommandMeta(a, conf, p, r))
+    ).map(r => (r.getClassFor.asSubclass(classOf[DataWritingCommand]), r)).toMap
+  }
+
+  override def getRunnableCmds: Map[Class[_ <: RunnableCommand],
+      RunnableCommandRule[_ <: RunnableCommand]] = {
+    Map.empty
+  }
+}
