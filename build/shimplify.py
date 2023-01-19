aaf09997922f581ec6a8250636475ac7335ac999
Gera Shegalov <gera@apache.org>
2023-01-19 14:27:58 -0800

    idempotent overwrite

--- build/shimplify.py
+++ build/shimplify.py
@@ -42,6 +42,7 @@ def upsert_shim_json(filename, bv_list):
     for bv in bv_list:
         shim_comment.append(json.dumps({'spark' :  bv}))
     shim_comment.append(tag + ' ***/')
+    delete_prior_comment_if_allowed(contents, tag, filename)
     log.debug("Inserting comment {} to {}".format(shim_comment, filename))
     package_line = next(i for i in xrange(len(contents)) if str(contents[i]).startswith('package'))
     log.debug("filename={} package_line_number={}".format(filename, package_line))
@@ -51,11 +52,44 @@ def upsert_shim_json(filename, bv_list):
         file.writelines(contents)
 
 
+def delete_prior_comment_if_allowed(contents, tag, filename):
+    opening_shim_comment_line = None
+    closing_shim_comment_line = None
+    overShimplify = project.getProperty("overShimplify")
+    overwrite = (str(True).lower() == overShimplify)
+    try:
+        opening_shim_comment_line = next(i for i in xrange(len(contents)) if tag in str(contents[i]))
+        closing_shim_comment_line = next(i for i in xrange(opening_shim_comment_line + 1, len(contents)) if tag in str(contents[i]))
+    except StopIteration:
+        if not (opening_shim_comment_line is None) and (closing_shim_comment_line is None):
+            raise Exception("no closing comment for {}:{}".format(filename, opening_shim_comment_line))
+        pass
+    if not (opening_shim_comment_line is None) and not overwrite:
+        raise Exception("found shim comment from prior execution at {}:{}".format(filename, opening_shim_comment_line))
+    log.debug("removing comments {}:{}:{}".format(filename, opening_shim_comment_line, closing_shim_comment_line))
+    del contents[opening_shim_comment_line:(closing_shim_comment_line + 1)]
+
 def task_impl():
+    """Simplifies the old version range directory system
+
+    Each shim Scala/Java file receives a comment describing all Spark builds it
+    it belongs to. Lines are sorted by the Spark buildver lexicographically.
+    Each line is assumed to be a JSON to keep it extensible.
+    /*** spark-rapids-shim-json-lines
+    {"spark": "312"}
+    {"spark": "323"}
+    spark-rapids-shim-json-lines ***/
+
+    The canonical location of a source file shared by multiple shims is the
+    first src/main/<top_buildver_in_the_comment>
+
+    You can find all shim files for a particular shim easily by executing:
+    git grep '{"spark": "312"}'
+    """
     log.info('############# Starting Jython Task Shimplify #######')
     if_attr = attributes.get('if')
     if_val = 'true' if if_attr is None else project.getProperty(if_attr)
-    if ((if_val is None) or (if_val.strip() != str(True).lower())):
+    if (if_val is None) or (if_val.strip() != str(True).lower()):
         log.info('Skipping shimplify! Set -Dshimplify=true to convert old shims')
         return
     shims_attr = attributes.get('shims')
