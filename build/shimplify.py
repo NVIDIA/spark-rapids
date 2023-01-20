4b8bf3f663178d933562140dfc8f893224135e64
Gera Shegalov <gera@apache.org>
2023-01-19 23:27:46 -0800

    configurable rename

--- build/shimplify.py
+++ build/shimplify.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""implementation of shimplify Ant task """
+
 import errno
 import json
 import logging
@@ -26,7 +28,30 @@ formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 log.addHandler(ch)
 
-def upsert_shim_json(filename, bv_list):
+
+def __is_enabled_property(p):
+    assert not (p is None), "Invalid property: None"
+    prop_val = project.getProperty(p)
+    return str(True).lower() == prop_val
+
+
+def __is_enabled_attr(attr):
+    assert not (attr is None), "Invalid attribute: None"
+    attr_val = attributes.get(attr)
+    return not (attr_val is None) and __is_enabled_property(attr_val)
+
+
+
+task_enabled = __is_enabled_attr('if')
+
+# should we move files?
+move_files = __is_enabled_property('shimplifyMove')
+
+# allowed to overwrite the existing comment
+over_shimplify = __is_enabled_property("overShimplify")
+
+
+def __upsert_shim_json(filename, bv_list):
     with open(filename, 'r') as file:
         contents = file.readlines()
     tag = 'spark-rapids-shim-json-lines'
@@ -35,60 +60,58 @@ def upsert_shim_json(filename, bv_list):
     for bv in bv_list:
         shim_comment.append(json.dumps({'spark' :  bv}))
     shim_comment.append(tag + ' ***/')
-    delete_prior_comment_if_allowed(contents, tag, filename)
-    log.debug("Inserting comment {} to {}".format(shim_comment, filename))
+    __delete_prior_comment_if_allowed(contents, tag, filename)
+    log.debug("Inserting comment %s to %s", shim_comment, filename)
     package_line = next(i for i in xrange(len(contents)) if str(contents[i]).startswith('package'))
-    log.debug("filename={} package_line_number={}".format(filename, package_line))
+    log.debug("filename=%s package_line_number=%d", filename, package_line)
     shim_comment_str = '\n'.join(shim_comment) + '\n'
     contents.insert(package_line, shim_comment_str)
     with open(filename, 'w') as file:
         file.writelines(contents)
 
-def delete_prior_comment_if_allowed(contents, tag, filename):
+
+def __delete_prior_comment_if_allowed(contents, tag, filename):
     opening_shim_comment_line = None
     closing_shim_comment_line = None
-    overShimplify = project.getProperty("overShimplify")
-    overwrite = (str(True).lower() == overShimplify)
     try:
         opening_shim_comment_line = next(i for i in xrange(len(contents)) if tag in str(contents[i]))
         closing_shim_comment_line = next(i for i in xrange(opening_shim_comment_line + 1, len(contents)) if tag in str(contents[i]))
-    except StopIteration:
+    except StopIteration as si_exc:
         if not (opening_shim_comment_line is None) and (closing_shim_comment_line is None):
-            raise Exception("no closing comment for {}:{}".format(filename, opening_shim_comment_line))
-        pass
+            raise Exception("%s: no closing comment for %s:%d" % (si_exc, filename, opening_shim_comment_line))
+
     # no work
     if opening_shim_comment_line is None:
         return
-    if not overwrite:
-        raise Exception(
-            "found shim comment from prior execution at {}:{}, use -DoverShimplify=true to overwrite"\
-                .format(filename, opening_shim_comment_line))
+    if not over_shimplify:
+        raise Exception("found shim comment from prior execution at %s:%d, "
+                        "use -DoverShimplify=true to overwrite" % (filename, opening_shim_comment_line))
     assert not (opening_shim_comment_line is None) and not (closing_shim_comment_line is None)
-    log.debug("removing comments {}:{}:{}".format(filename, opening_shim_comment_line, closing_shim_comment_line))
+    log.debug("removing comments %s:%d:%d", filename, opening_shim_comment_line, closing_shim_comment_line)
     del contents[opening_shim_comment_line:(closing_shim_comment_line + 1)]
 
-def git_rename(shim_file, first_shim):
-    log.debug("git rename {} to the dir of {}".format(shim_file, first_shim))
+
+def __git_rename(shim_file, first_shim):
+    log.debug("git rename %s to the canonical dir of %s", shim_file, first_shim)
     parent_pom_dir = str(project.getProperty('spark.rapids.source.basedir'))
-    # sql-plugin/src/main/320+-nondb/scala/org/apache/spark/sql/delta/rapids/shims/GpuCheckDeltaInvariant.scala
+    # sql-plugin/src/main/320+-nondb/scala/org/apache/spark/...
     rel_path = os.path.relpath(shim_file, parent_pom_dir)
-    log.debug("spark-rapids root dir: {}".format(parent_pom_dir))
-    log.debug("shim file path relative to root dir: {}".format(rel_path))
+    log.debug("spark-rapids root dir: %s", parent_pom_dir)
+    log.debug("shim file path relative to root dir: %s", rel_path)
     path_parts = rel_path.split(os.sep)
     path_parts[3] = first_shim
     new_shim_file = os.sep.join([parent_pom_dir] + path_parts)
     if shim_file == new_shim_file:
-        log.info("{} is already at the right location, skipping git rename")
+        log.info("%s is already at the right location, skipping git rename", shim_file)
         return
     new_shim_dir = os.path.dirname(new_shim_file)
-    log.debug("creating new shim path {}".format(new_shim_dir))
+    log.debug("creating new shim path %s", new_shim_dir)
     try:
         os.makedirs(new_shim_dir)
     except OSError as ose:
-        if ose.errno == errno.EEXIST and os.path.isdir(new_shim_dir):
-            pass
-        else:
+        if not (ose.errno == errno.EEXIST and os.path.isdir(new_shim_dir)):
             raise
+
     git_cmd = ['git', 'mv', shim_file, new_shim_file]
     ret_code = subprocess.call(git_cmd)
     if ret_code != 0:
@@ -113,11 +136,9 @@ def task_impl():
     git grep '{"spark": "312"}'
     """
     log.info('############# Starting Jython Task Shimplify #######')
+    log.info("config: task_enabled=%s over_shimplify=%s move_files=%s", task_enabled, over_shimplify, move_files)
     log.info("review changes and `git restore` if necessary")
-    if_attr = attributes.get('if')
-    if_val = None if if_attr is None else project.getProperty(if_attr)
-    should_skip = if_val is None or if_val != str(True).lower()
-    if should_skip:
+    if not task_enabled:
         log.info('Skipping shimplify! Set -Dshimplify=true to convert old shims')
         return
     shims_attr = attributes.get('shims')
@@ -130,7 +151,7 @@ def task_impl():
     files2bv = {}
     for build_ver in shims_arr:
         shim_source_files = project.getReference("spark{}.fileset".format(build_ver))
-        log.debug("build_ver={} shim_src.size={}".format(build_ver, shim_source_files.size()))
+        log.debug("build_ver=%s shim_src.size=%i", build_ver, shim_source_files.size())
         for resource in shim_source_files:
             shim_file = str(resource)
             if shim_file in files2bv.keys():
@@ -139,8 +160,10 @@ def task_impl():
                 files2bv[shim_file] = [build_ver]
 
     for shim_file, bv_list in files2bv.items():
-        log.debug("calling upsert_shim_json shim_file={} bv_list={}".format(shim_file, bv_list))
-        upsert_shim_json(shim_file, bv_list)
-        git_rename(shim_file, bv_list[0])
+        log.debug("calling upsert_shim_json shim_file=%s bv_list=%s", shim_file, bv_list)
+        __upsert_shim_json(shim_file, bv_list)
+        if move_files:
+            __git_rename(shim_file, bv_list[0])
+
 
 task_impl()
