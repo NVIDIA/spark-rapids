5b34fd3a752ac1617f4a2e436b93e93aef44fe91
Gera Shegalov <gera@apache.org>
2023-01-21 21:44:41 -0800

    flake8 lint
    Signed-off-by: Gera Shegalov <gera@apache.org>

--- build/shimplify.py
+++ build/shimplify.py
@@ -20,16 +20,36 @@ import logging
 import os
 import subprocess
 
+# wrap Ant interactions to reduce linting warnings
+# TODO pass only Python types if possible
 
-def __is_enabled_property(p):
-    assert not (p is None), "Invalid property: None"
-    prop_val = project.getProperty(p)
+
+def __project():
+    return project
+
+
+def __attributes():
+    return attributes
+
+
+# get Ant project property as string
+def __ant_proj_prop(name):
+    return str(__project().getProperty(name))
+
+
+def __ant_attr(name):
+    return str(__attributes().get(name))
+
+
+def __is_enabled_property(prop_name):
+    assert not (prop_name is None), "Invalid property: None"
+    prop_val = __ant_proj_prop(prop_name)
     return str(True).lower() == prop_val
 
 
 def __is_enabled_attr(attr):
     assert not (attr is None), "Invalid attribute: None"
-    attr_val = attributes.get(attr)
+    attr_val = __ant_attr(attr)
     return not (attr_val is None) and __is_enabled_property(attr_val)
 
 
@@ -41,6 +61,7 @@ move_files = __is_enabled_property('shimplifyMove')
 # allowed to overwrite the existing comment
 over_shimplify = __is_enabled_property('overShimplify')
 
+# enable log tracing
 shimplify_trace = __is_enabled_property('traceShimplify')
 
 
@@ -57,12 +78,12 @@ def __upsert_shim_json(filename, bv_list):
     tag = 'spark-rapids-shim-json-lines'
     shim_comment = []
     shim_comment.append('/*** ' + tag)
-    for bv in bv_list:
-        shim_comment.append(json.dumps({'spark' :  bv}))
+    for build_ver in bv_list:
+        shim_comment.append(json.dumps({'spark':  build_ver}))
     shim_comment.append(tag + ' ***/')
     __delete_prior_comment_if_allowed(contents, tag, filename)
     log.debug("Inserting comment %s to %s", shim_comment, filename)
-    package_line = next(i for i in xrange(len(contents)) if str(contents[i]).startswith('package'))
+    package_line = next(i for i in range(len(contents)) if str(contents[i]).startswith('package'))
     log.debug("filename=%s package_line_number=%d", filename, package_line)
     shim_comment_str = '\n'.join(shim_comment) + '\n'
     contents.insert(package_line, shim_comment_str)
@@ -74,26 +95,31 @@ def __delete_prior_comment_if_allowed(contents, tag, filename):
     opening_shim_comment_line = None
     closing_shim_comment_line = None
     try:
-        opening_shim_comment_line = next(i for i in xrange(len(contents)) if tag in str(contents[i]))
-        closing_shim_comment_line = next(i for i in xrange(opening_shim_comment_line + 1, len(contents)) if tag in str(contents[i]))
+        opening_shim_comment_line = next(i for i in range(len(contents))
+                                         if tag in str(contents[i]))
+        shim_comment_and_below = range(opening_shim_comment_line + 1, len(contents))
+        closing_shim_comment_line = next(i for i in shim_comment_and_below
+                                         if tag in str(contents[i]))
     except StopIteration as si_exc:
         if not (opening_shim_comment_line is None) and (closing_shim_comment_line is None):
-            raise Exception("%s: no closing comment for %s:%d" % (si_exc, filename, opening_shim_comment_line))
+            raise Exception("%s: no closing comment for %s:%d"
+                            % (si_exc, filename, opening_shim_comment_line))
 
     # no work
     if opening_shim_comment_line is None:
         return
     if not over_shimplify:
-        raise Exception("found shim comment from prior execution at %s:%d, "
-                        "use -DoverShimplify=true to overwrite" % (filename, opening_shim_comment_line))
+        raise Exception("found shim comment from prior execution at %s:%d, use -DoverShimplify=true"
+                        "to overwrite" % (filename, opening_shim_comment_line))
     assert not (opening_shim_comment_line is None) and not (closing_shim_comment_line is None)
-    log.debug("removing comments %s:%d:%d", filename, opening_shim_comment_line, closing_shim_comment_line)
+    log.debug("removing comments %s:%d:%d", filename, opening_shim_comment_line,
+              closing_shim_comment_line)
     del contents[opening_shim_comment_line:(closing_shim_comment_line + 1)]
 
 
 def __git_rename(shim_file, first_shim):
     log.debug("git rename %s to the canonical dir of %s", shim_file, first_shim)
-    parent_pom_dir = str(project.getProperty('spark.rapids.source.basedir'))
+    parent_pom_dir = __ant_proj_prop('spark.rapids.source.basedir')
     # sql-plugin/src/main/320+-nondb/scala/org/apache/spark/...
     rel_path = os.path.relpath(shim_file, parent_pom_dir)
     log.debug("spark-rapids root dir: %s", parent_pom_dir)
@@ -136,12 +162,13 @@ def task_impl():
     git grep '{"spark": "312"}'
     """
     log.info('############# Starting Jython Task Shimplify #######')
-    log.info("config: task_enabled=%s over_shimplify=%s move_files=%s", task_enabled, over_shimplify, move_files)
+    log.info("config: task_enabled=%s over_shimplify=%s move_files=%s",
+             task_enabled, over_shimplify, move_files)
     log.info("review changes and `git restore` if necessary")
     if not task_enabled:
         log.info('Skipping shimplify! Set -Dshimplify=true to convert old shims')
         return
-    shims_attr = attributes.get('shims')
+    shims_attr = __ant_attr('shims')
     # remove whitespace
     shims_attr_csv = str(shims_attr).translate(None, ' \n\r')
     shims_arr = shims_attr_csv.split(',')
@@ -150,7 +177,10 @@ def task_impl():
     # map file -> [shims it's part of]
     files2bv = {}
     for build_ver in shims_arr:
-        shim_source_files = project.getReference("spark{}.fileset".format(build_ver))
+        # alternatively we can use range dirs instead of files, which is more efficient
+        # keeping at file level for flexibility until/unless the shim layer becomes unexpectedly
+        # large
+        shim_source_files = __project().getReference("spark{}.fileset".format(build_ver))
         log.debug("build_ver=%s shim_src.size=%i", build_ver, shim_source_files.size())
         for resource in shim_source_files:
             shim_file = str(resource)
