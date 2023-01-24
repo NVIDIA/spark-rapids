838f09497bd3ca0a9b011e1724e586b546b7ab90
Tim Liu <timl@nvidia.com>
2023-01-24 11:01:31 +0800

    Refact deploy script to support gpg and nvsec signature [skip ci]
    Signed-off-by: Tim Liu <timl@nvidia.com>

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -17,76 +17,81 @@
 
 # Argument(s):
 #   SIGN_FILE:  true/false, whether to sign the jar/pom file to de deployed
-#   DATABRICKS: true/fasle, whether deploying for databricks
-#   VERSIONS_BUILT: The spark versions built before calling this script
 #
 # Used environment(s):
 #   SQL_PL:         The path of module 'sql-plugin', relative to project root path.
 #   DIST_PL:        The path of module 'dist', relative to project root path.
-#   AGGREGATOR_PL:  The path of the module 'aggregator', relative to project root path.
-#   TESTS_PL:       The path of the module 'integration_tests', relative to the project root path.
 #   SERVER_ID:      The repository id for this deployment.
 #   SERVER_URL:     The url where to deploy artifacts.
 #   GPG_PASSPHRASE: The passphrase used to sign files, only required when <SIGN_FILE> is true.
-#   FINAL_AGG_VERSION_TOBUILD: The spark version of the final build and aggregation.
+#   SIGN_TOOL:      Tool to sign files, e.g., gpg, nvsec, only required when $1 is 'true'
+#   GPG_PASSPHRASE: gpg passphrase to sign artifacts, only required when <SIGN_TOOL> is gpg
+#   NVSEC_CFG_FILE: nvsec credentials to sign artifacts, only required when <SIGN_TOOL> is nvsec
+#   MVN_SETTINGS:   Maven configuration file
+#   POM_FILE:       Project pom file to be deployed
+#   OUT_PATH:       The path where jar files are
+#   CLASSIFIERS:    Comma separated classifiers, e.g., "cuda11"
 ###
 
 set -ex
-SIGN_FILE=$1
-DATABRICKS=$2
-VERSIONS_BUILT=$3
 
-export M2DIR=${M2DIR:-"$WORKSPACE/.m2"}
+SIGN_FILE=${1:-"false"}
+DIST_PL=${DIST_PL:-"dist"}
+SQL_PL=${SQL_PL:-"sql-plugin"}
+POM_FILE=${POM_FILE:-`find "$DIST_PL/target/extra-resources/" -name pom.xml`}
+OUT_PATH=${OUT_PATH:-"$DIST_PL/target"}
+SIGN_TOOL=${SIGN_TOOL:-"gpg"}
+MVN_SETTINGS=${MVN_SETTINGS:-"jenkins/settings.xml"}
 
+MVN="mvn -B -Dmaven.wagon.http.retryHandler.count=3 -DretryFailedDeploymentCount=3 -s $MVN_SETTINGS"
 ###### Build the path of jar(s) to be deployed ######
-
-cd $WORKSPACE
-
-###### Databricks built tgz file so we need to untar and deploy from that
-if [ "$DATABRICKS" == true ]; then
-    rm -rf deploy
-    mkdir -p deploy
-    cd deploy
-    tar -zxf ../spark-rapids-built.tgz
-    cd spark-rapids
-fi
-
-ART_ID=`mvn help:evaluate -q -pl $DIST_PL -Dexpression=project.artifactId -DforceStdout`
-ART_VER=`mvn help:evaluate -q -pl $DIST_PL -Dexpression=project.version -DforceStdout`
+ART_ID=`$MVN help:evaluate -q -pl $DIST_PL -Dexpression=project.artifactId -DforceStdout`
+ART_VER=`$MVN help:evaluate -q -f $DIST_PL -Dexpression=project.version -DforceStdout`
 CUDA_CLASSIFIER=`mvn help:evaluate -q -pl $DIST_PL -Dexpression=cuda.version -DforceStdout`
+FPATH="$OUT_PATH/$ART_ID-$ART_VER"
+cp $FPATH-$CUDA_CLASSIFIER.jar $FPATH.jar
 
-FPATH="$DIST_PL/target/$ART_ID-$ART_VER-$CUDA_CLASSIFIER"
-POM_FPATH="$DIST_PL/target/extra-resources/META-INF/maven/com.nvidia/$ART_ID/pom.xml"
+# dist does not have javadoc and sources jars, use 'sql-plugin' instead
+source jenkins/version-def.sh >/dev/null 2&>1
+echo $SPARK_BASE_SHIM_VERSION
+SQL_ART_ID=`mvn help:evaluate -q -pl $SQL_PL -Dexpression=project.artifactId -DforceStdout`
+SQL_ART_VER=`mvn help:evaluate -q -pl $SQL_PL -Dexpression=project.version -DforceStdout`
+JS_FPATH="${SQL_PL}/target/spark${SPARK_BASE_SHIM_VERSION}/${SQL_ART_ID}-${SQL_ART_VER}"
+cp $JS_FPATH-sources.jar $FPATH-sources.jar
+cp $JS_FPATH-javadoc.jar $FPATH-javadoc.jar
 
 echo "Plan to deploy ${FPATH}.jar to $SERVER_URL (ID:$SERVER_ID)"
 
-FINAL_AGG_VERSION_TOBUILD=${FINAL_AGG_VERSION_TOBUILD:-'311'}
 ###### Choose the deploy command ######
-MVN="mvn -Dmaven.wagon.http.retryHandler.count=3 -DretryFailedDeploymentCount=3"
 if [ "$SIGN_FILE" == true ]; then
-    # No javadoc and sources jar is generated for shade artifact only. Use 'sql-plugin' instead
-    SQL_ART_ID=`mvn help:evaluate -q -pl $SQL_PL -Dexpression=project.artifactId -DforceStdout`
-    SQL_ART_VER=`mvn help:evaluate -q -pl $SQL_PL -Dexpression=project.version -DforceStdout`
-    JS_FPATH="${SQL_PL}/target/spark${FINAL_AGG_VERSION_TOBUILD}/${SQL_ART_ID}-${SQL_ART_VER}"
-    SRC_DOC_JARS="-Dsources=${JS_FPATH}-sources.jar -Djavadoc=${JS_FPATH}-javadoc.jar"
-    DEPLOY_CMD="$MVN -B gpg:sign-and-deploy-file -s jenkins/settings.xml -Dgpg.passphrase=$GPG_PASSPHRASE"
+    case $SIGN_TOOL in
+        nvsec)
+            cp $NVSEC_CFG_FILE ~/.nvsec.cfg
+            DEPLOY_CMD="$MVN gpg:sign-and-deploy-file -Dgpg.executable=nvsec_sign"
+            ;;
+        gpg)
+            DEPLOY_CMD="$MVN gpg:sign-and-deploy-file -Dgpg.passphrase=$GPG_PASSPHRASE "
+            ;;
+        *)
+            echo "Error unsupported sign type : $SIGN_TYPE !"
+            echo "Please set variable SIGN_TOOL 'nvsec'or 'gpg'"
+            exit -1
+            ;;
+    esac
 else
     DEPLOY_CMD="$MVN -B deploy:deploy-file -s jenkins/settings.xml"
 fi
-
+DEPLOY_CMD="$DEPLOY_CMD -Durl=$SERVER_URL -DrepositoryId=$SERVER_ID"
 echo "Deploy CMD: $DEPLOY_CMD"
 
-
 ###### Deploy the parent pom file ######
-
-$DEPLOY_CMD -Durl=$SERVER_URL -DrepositoryId=$SERVER_ID \
-            -Dfile=./pom.xml -DpomFile=./pom.xml
+$DEPLOY_CMD -Dfile=./pom.xml -DpomFile=./pom.xml
 
 ###### Deploy the artifact jar(s) ######
-
-# Distribution jar is a shaded artifact so use the reduced dependency pom.
-$DEPLOY_CMD -Durl=$SERVER_URL -DrepositoryId=$SERVER_ID \
-            $SRC_DOC_JARS \
-            -Dfile=$FPATH.jar -DgroupId=com.nvidia -DartifactId=$ART_ID -Dversion=$ART_VER \
-            -Dfiles=$FPATH.jar -Dtypes=jar -Dclassifiers=$CUDA_CLASSIFIER \
-            -DpomFile="$POM_FPATH"
+$DEPLOY_CMD -DpomFile=$POM_FILE \
+            -Dfile=$FPATH.jar \
+            -Dsources=$FPATH-sources.jar \
+            -Djavadoc=$FPATH-javadoc.jar \
+            -Dfiles=$FPATH-$CUDA_CLASSIFIER.jar \
+            -Dtypes=jar \
+            -Dclassifiers=$CUDA_CLASSIFIER
