871c9d68001a7c9c328d75b48b17cd406d008546
rishi <sririshindra@gmail.com>
2023-01-31 17:37:06 -0800

    Address review comments.
    Signed-off-by: rishi <sririshindra@gmail.com>

--- sql-plugin/src/main/330cdh/scala/org/apache/spark/sql/hive/cdh/HiveOverrides.scala
+++ sql-plugin/src/main/330cdh/scala/org/apache/spark/sql/hive/cdh/HiveOverrides.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2023, NVIDIA CORPORATION.
+ * Copyright (c) 2023, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import org.apache.spark.sql.hive.execution.HiveTableScanExec
 
 object HiveOverrides {
 
+  // This is a workaround for the mismatches listed in #7423
   def getHiveTableExecRule() = {
     GpuOverrides.exec[HiveTableScanExec](
       desc = "CDH-specific override to disable HiveTableScanExec, " +
