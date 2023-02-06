c2f0ab726c9bf2decacd09f9e53fb3ddb96ff966
Gera Shegalov <gera@apache.org>
2023-02-06 07:39:22 -0800

    Add missing spark shim test suites  (#7673)
    * Add missing shim scalatest suites
    
    - remove buildver from class name for easier scripting, it's already
      present in the package name
    
    Signed-off-by: Gera Shegalov <gera@apache.org>
    
    * Fully qulified shims suite name
    
    Signed-off-by: Gera Shegalov <gera@apache.org>
    
    * 340 suite
    
    Signed-off-by: Gera Shegalov <gera@apache.org>
    
    * scalastyle
    
    Signed-off-by: Gera Shegalov <gera@apache.org>
    
    * Missed adding FQSuiteName
    
    Signed-off-by: Gera Shegalov <gera@apache.org>
    
    * fix 340
    
    Signed-off-by: Gera Shegalov <gera@apache.org>
    
    ---------
    
    Signed-off-by: Gera Shegalov <gera@apache.org>

--- sql-plugin/src/test/320/scala/com/nvidia/spark/rapids/shims/spark320/SparkShimsSuite.scala
+++ sql-plugin/src/test/320/scala/com/nvidia/spark/rapids/shims/spark320/SparkShimsSuite.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, NVIDIA CORPORATION.
+ * Copyright (c) 2021-2023, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,20 +14,28 @@
  * limitations under the License.
  */
 
-package com.nvidia.spark.rapids.shims.spark340;
+package com.nvidia.spark.rapids.shims.spark320;
 
-import com.nvidia.spark.rapids.{ShimLoader, SparkShimVersion}
+import com.nvidia.spark.rapids._
 import com.nvidia.spark.rapids.shims.SparkShimImpl
 import org.scalatest.FunSuite
 
-class Spark340ShimsSuite extends FunSuite {
+import org.apache.spark.sql.types.{DayTimeIntervalType, YearMonthIntervalType}
+
+class SparkShimsSuite extends FunSuite with FQSuiteName {
   test("spark shims version") {
-    assert(SparkShimImpl.getSparkShimVersion === SparkShimVersion(3, 4, 0))
+    assert(SparkShimImpl.getSparkShimVersion === SparkShimVersion(3, 2, 0))
   }
 
   test("shuffle manager class") {
     assert(ShimLoader.getRapidsShuffleManagerClass ===
-      classOf[com.nvidia.spark.rapids.spark340.RapidsShuffleManager].getCanonicalName)
+      classOf[com.nvidia.spark.rapids.spark320.RapidsShuffleManager].getCanonicalName)
+  }
+
+  test("TypeSig") {
+    val check = TypeSig.DAYTIME + TypeSig.YEARMONTH
+    assert(check.isSupportedByPlugin(DayTimeIntervalType()) == true)
+    assert(check.isSupportedByPlugin(YearMonthIntervalType()) == true)
   }
 
 }
