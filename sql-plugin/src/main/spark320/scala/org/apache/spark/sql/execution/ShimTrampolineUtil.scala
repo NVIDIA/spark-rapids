c087d2e3e0fad8e2f0968ab58c8af83cbaea9367
Gera Shegalov <gera@apache.org>
2023-03-06 09:58:13 -0800

    Automatic conversion to shimplified directory structure  [databricks] (#7222)
    Closes #7487 
    
    Command executed:
      
     ```bash
     mvn generate-sources -Dshimplify=true -Dshimplify.move=true
      ```
    Verified with:
        
    1) Build a multi-shim jar
    
    ```bash
    mvn clean
    echo 314 321 331 | xargs -t -n 1 bash -c 'mvn package -pl aggregator -am -Dbuildver=$1  -DskipTests -Dskip -Dmaven.jadadoc.skip' '{}'
    mvn package -pl dist -Dincluded_buildvers=314,321,331 -Ddist.jar.compress=false 
    ```
    
    2) Run a smoke test for all shims
    
    ```bash
    SPARK_HOME=~/gits/apache/spark NUM_LOCAL_EXECS=2 PYSP_TEST_spark_rapids_shuffle_mode=MULTITHREADED PYSP_TEST_spark_rapids_shuffle_multiThreaded_writer_threads=2 PYSP_TEST_spark_rapids_shuffle_multiThreaded_reader_threads=2 PYSP_TEST_spark_shuffle_manager=com.nvidia.spark.rapids.spark314.RapidsShuffleManager PYSP_TEST_spark_rapids_memory_gpu_minAllocFraction=0 PYSP_TEST_spark_rapids_memory_gpu_maxAllocFraction=0.1 PYSP_TEST_spark_rapids_memory_gpu_allocFraction=0.1 ./integration_tests/run_pyspark_from_build.sh -k test_hash_grpby_sum
    ```
    
    ```bash
    SPARK_HOME=~/dist/spark-3.2.1-bin-hadoop3.2 NUM_LOCAL_EXECS=2 PYSP_TEST_spark_rapids_shuffle_mode=MULTITHREADED PYSP_TEST_spark_rapids_shuffle_multiThreaded_writer_threads=2 PYSP_TEST_spark_rapids_shuffle_multiThreaded_reader_threads=2 PYSP_TEST_spark_shuffle_manager=com.nvidia.spark.rapids.spark321.RapidsShuffleManager PYSP_TEST_spark_rapids_memory_gpu_minAllocFraction=0 PYSP_TEST_spark_rapids_memory_gpu_maxAllocFraction=0.1 PYSP_TEST_spark_rapids_memory_gpu_allocFraction=0.1 ./integration_tests/run_pyspark_from_build.sh -k test_hash_grpby_sum
    ```
    
    ```bash
    SPARK_HOME=~/dist/spark-3.3.1-bin-hadoop3 NUM_LOCAL_EXECS=2 PYSP_TEST_spark_rapids_shuffle_mode=MULTITHREADED PYSP_TEST_spark_rapids_shuffle_multiThreaded_writer_threads=2 PYSP_TEST_spark_rapids_shuffle_multiThreaded_reader_threads=2 PYSP_TEST_spark_shuffle_manager=com.nvidia.spark.rapids.spark331.RapidsShuffleManager PYSP_TEST_spark_rapids_memory_gpu_minAllocFraction=0 PYSP_TEST_spark_rapids_memory_gpu_maxAllocFraction=0.1 PYSP_TEST_spark_rapids_memory_gpu_allocFraction=0.1 ./integration_tests/run_pyspark_from_build.sh -k test_hash_grpby_sum
    ```
    
    Separate PR will be posted  removing old sparkXYZ.sources definitions for the previous build. We'll merge it once we are confident in the transition robustness.
    
    
    Signed-off-by: Gera Shegalov <gera@apache.org>

---
+++ sql-plugin/src/main/spark320/scala/org/apache/spark/sql/execution/ShimTrampolineUtil.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2021-2023, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*** spark-rapids-shim-json-lines
+{"spark": "320"}
+{"spark": "321"}
+{"spark": "321cdh"}
+{"spark": "321db"}
+{"spark": "322"}
+{"spark": "323"}
+{"spark": "330"}
+{"spark": "330cdh"}
+{"spark": "331"}
+{"spark": "332"}
+spark-rapids-shim-json-lines ***/
+package org.apache.spark.sql.rapids.execution
+
+import org.apache.spark.sql.catalyst.plans.physical.{BroadcastMode, IdentityBroadcastMode}
+import org.apache.spark.sql.execution.joins.HashedRelationBroadcastMode
+import org.apache.spark.sql.types.{DataType, StructType}
+
+object ShimTrampolineUtil {
+
+  def unionLikeMerge(left: DataType, right: DataType): DataType =
+    StructType.unionLikeMerge(left, right)
+
+  def isSupportedRelation(mode: BroadcastMode): Boolean = mode match {
+    case _ : HashedRelationBroadcastMode => true
+    case IdentityBroadcastMode => true
+    case _ => false
+  }
+}
