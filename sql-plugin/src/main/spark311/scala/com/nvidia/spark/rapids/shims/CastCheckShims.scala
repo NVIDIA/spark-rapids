195f6c06b9637588cc53fcf8db8ec1137ccd6330
Andy Grove <andygrove@nvidia.com>
2023-04-18 17:34:57 -0600

    remove incorrect shim tags

--- sql-plugin/src/main/spark311/scala/com/nvidia/spark/rapids/shims/CastCheckShims.scala
+++ sql-plugin/src/main/spark311/scala/com/nvidia/spark/rapids/shims/CastCheckShims.scala
@@ -24,12 +24,6 @@
 {"spark": "321db"}
 {"spark": "322"}
 {"spark": "323"}
-{"spark": "330"}
-{"spark": "330cdh"}
-{"spark": "330db"}
-{"spark": "331"}
-{"spark": "332"}
-{"spark": "333"}
 spark-rapids-shim-json-lines ***/
 
 package com.nvidia.spark.rapids.shims
