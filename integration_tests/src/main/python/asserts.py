87ad05723eeb87e1af511bbe6427390aa5902dea
Andy Grove <andygrove@nvidia.com>
2023-04-19 17:23:55 -0600

    fix regression

--- integration_tests/src/main/python/asserts.py
+++ integration_tests/src/main/python/asserts.py
@@ -558,7 +558,36 @@ def run_with_cpu_and_gpu_return_df(func,
 def run_with_cpu_and_gpu(func,
     mode,
     conf={}):
-    from_cpu, _, from_gpu, _ = run_with_cpu_and_gpu_return_df(func, mode, conf)
+    (bring_back, collect_type) = _prep_func_for_compare(func, mode)
+    conf = _prep_incompat_conf(conf)
+
+    def run_on_cpu():
+        print('### CPU RUN ###')
+        global cpu_start
+        cpu_start = time.time()
+        global from_cpu
+        from_cpu = with_cpu_session(bring_back, conf=conf)
+        global cpu_end
+        cpu_end = time.time()
+
+    def run_on_gpu():
+        print('### GPU RUN ###')
+        global gpu_start
+        gpu_start = time.time()
+        global from_gpu
+        from_gpu = with_gpu_session(bring_back, conf=conf)
+        global gpu_end
+        gpu_end = time.time()
+
+    run_on_cpu()
+    run_on_gpu()
+
+    print('### {}: GPU TOOK {} CPU TOOK {} ###'.format(collect_type,
+        gpu_end - gpu_start, cpu_end - cpu_start))
+    if should_sort_locally():
+        from_cpu.sort(key=_RowCmp)
+        from_gpu.sort(key=_RowCmp)
+
     return (from_cpu, from_gpu)
 
 def assert_gpu_and_cpu_are_equal_collect(func, conf={}, is_cpu_first=True):
