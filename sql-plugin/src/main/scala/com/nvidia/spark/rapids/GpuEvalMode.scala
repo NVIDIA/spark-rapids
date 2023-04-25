322f8e4990212adef2bea41d8f75607eabfe6269
Andy Grove <andygrove@nvidia.com>
2023-04-25 10:53:35 -0600

    update copyright year

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuEvalMode.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuEvalMode.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021, NVIDIA CORPORATION.
+ * Copyright (c) 2023, NVIDIA CORPORATION.
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
