c2d7e659dbc31e458aba79706bbae783116b4c86
Raza Jafri <rjafri@nvidia.com>
2023-05-22 13:15:56 -0700

    add ColumnarToRowExec to allowed non gpu list
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>

--- integration_tests/src/main/python/cache_test.py
+++ integration_tests/src/main/python/cache_test.py
@@ -344,7 +344,7 @@ def test_aqe_cache_join(data_gen):
 # This test is testing cache when InMemoryTableScanExec fallsback to the CPU which causes the HostColumnarToGPU
 # to be pushed to the query plan which can cause ArrayIndexOutOfBoundsException
 @ignore_order
-@allow_non_gpu("InMemoryTableScanExec", "ProjectExec")
-def test_cache_count():
+@allow_non_gpu("InMemoryTableScanExec", "ProjectExec", "ColumnarToRowExec")
+def test_inmem_cache_count():
     conf={"spark.sql.session.timeZone": "America/Los_Angeles"}
     function_to_test_on_cached_df(with_gpu_session, lambda df: df.selectExpr("cast(a as timestamp)").cache().count(), int_gen, test_conf=conf)
