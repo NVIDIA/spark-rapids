NVIDIA/spark-rapids

commit 1b93b62cdea6709e090a12e92ed6e6f0436f9dcb

Fix reads for GZIP compressed Hive Text. (#8420)
* Fix reads for GZIP compressed Hive Text.

Fixes #8419.

This commit fixes how the GPU Hive Text reader determines whether the input
file can be split, for reads.
Before this change, the input was deemed splittable by default. This leads
to GZIP compressed Hive Text input to be split between tasks, causing read
failures like the following:
```
Caused by: java.io.IOException: Cannot seek in DeflateCodec compressed stream
        at org.apache.hadoop.mapreduce.lib.input.LineRecordReader.initialize(LineRecordReader.java:113)
        at org.apache.spark.sql.execution.datasources.HadoopFileLinesReader.<init>(HadoopFileLinesReader.scala:65)
        at com.nvidia.spark.rapids.GpuTextBasedPartitionReader.<init>(GpuTextBasedPartitionReader.scala:218)
        at com.nvidia.spark.rapids.CSVPartitionReaderBase.<init>(GpuCSVScan.scala:302)
        at org.apache.spark.sql.hive.rapids.GpuHiveDelimitedTextPartitionReader.<init>(GpuHiveTableScanExec.scala:496)
```
This commit adds logic to examine the compression codec of each input file,
and determine whether it is splittable.

Signed-off-by: MithunR <mythrocks@gmail.com>

sql-plugin/src/main/scala/org/apache/spark/sql/hive/rapids/GpuHiveTableScanExec.scala
@@ -28,6 +28,7 @@ import java.util.concurrent.TimeUnit.NANOSECONDS
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileStatus, Path}
 import org.apache.hadoop.hive.ql.metadata.{Partition => HivePartition}
+import org.apache.hadoop.io.compress.{CompressionCodecFactory, SplittableCompressionCodec}
 import scala.collection.JavaConverters._
 import scala.collection.immutable.HashSet
 import scala.collection.mutable
@@ -257,13 +258,20 @@ case class GpuHiveTableScanExec(requestedAttributes: Seq[Attribute],
 
     val maxSplitBytes      = FilePartition.maxSplitBytes(sparkSession, selectedPartitions)
 
+    def canBeSplit(file: FileStatus, hadoopConf: Configuration): Boolean = {
+      // Checks if `file` can be split.
+      // Uncompressed Hive Text files may be split. GZIP compressed files are not.
+      val codec = new CompressionCodecFactory(hadoopConf).getCodec(file.getPath)
+      codec == null || codec.isInstanceOf[SplittableCompressionCodec]
+    }
+
     val splitFiles = selectedPartitions.flatMap { partition =>
       partition.files.flatMap { f =>
         PartitionedFileUtil.splitFiles(
           sparkSession,
           f,
           f.getPath,
-          isSplitable = true,
+          isSplitable = canBeSplit(f, hadoopConf),
           maxSplitBytes,
           partition.values
         )

integration_tests/src/main/python/hive_delimited_text_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from asserts import assert_gpu_and_cpu_are_equal_collect, assert_gpu_and_cpu_sql_writes_are_equal_collect, assert_gpu_fallback_collect
+from asserts import assert_gpu_and_cpu_are_equal_collect, assert_gpu_and_cpu_are_equal_sql, assert_gpu_and_cpu_sql_writes_are_equal_collect, assert_gpu_fallback_collect
 from conftest import get_non_gpu_allowed
 from data_gen import *
 from enum import Enum
@@ -420,6 +420,37 @@ def test_custom_timestamp_formats_disabled(spark_tmp_path, data_gen, spark_tmp_t
         lambda spark: read_hive_text_table(spark, table_name),
         cpu_fallback_class_name=get_non_gpu_allowed()[0],
         conf=hive_text_enabled_conf)
+
+
+@pytest.mark.parametrize('codec', ['BZip2Codec',    # BZ2 compression, i.e. Splittable.
+                                   'DefaultCodec',  # DEFLATE, i.e. Gzip, without headers. Unsplittable.
+                                   'GzipCodec'])    # Gzip proper. Unsplittable.
+def test_read_compressed_hive_text(spark_tmp_table_factory, codec):
+    """
+    This tests whether compressed Hive Text tables are readable from spark-rapids.
+    For GZIP/DEFLATE compressed tables, spark-rapids should not attempt to split the input files.
+    Bzip2 compressed tables are splittable, and should remain readable.
+    """
+
+    table_name = spark_tmp_table_factory.get()
+
+    def create_table_with_compressed_files(spark):
+        spark.range(100000)\
+            .selectExpr("id",
+                        "cast(id as string) id_string")\
+            .repartition(1).write.format("hive").saveAsTable(table_name)
+
+    # Create Hive Text table with compression enabled.
+    with_cpu_session(create_table_with_compressed_files,
+                     {"hive.exec.compress.output": "true",
+                      "mapreduce.output.fileoutputformat.compress.codec":
+                          "org.apache.hadoop.io.compress.{}".format(codec)})
+
+    # Attempt to read from table with very small (2KB) splits.
+    assert_gpu_and_cpu_are_equal_collect(
+        lambda spark: spark.sql("SELECT COUNT(1) FROM {}".format(table_name)),
+        conf={"spark.sql.files.maxPartitionBytes": "2048b"}
+    )
 
 
 # Hive Delimited Text writer tests follow.

commit 7dc2582d54e04584edea36545b9efb0f7b79a9a7

Fix compressed Hive text read on Databricks. (#8462)
This commit fixes a compile failure on Databricks, introduced in #8420.

Partition.files is a `SerializableFileStatus` on Databricks, as opposed
to a `FileStatus` on Apache Spark.

This commit solves the problem, without resorting to Shims.

Signed-off-by: MithunR <mythrocks@gmail.com>

sql-plugin/src/main/scala/org/apache/spark/sql/hive/rapids/GpuHiveTableScanExec.scala
@@ -258,10 +258,13 @@ case class GpuHiveTableScanExec(requestedAttributes: Seq[Attribute],
 
     val maxSplitBytes      = FilePartition.maxSplitBytes(sparkSession, selectedPartitions)
 
-    def canBeSplit(file: FileStatus, hadoopConf: Configuration): Boolean = {
-      // Checks if `file` can be split.
+    def canBeSplit(filePath: Path, hadoopConf: Configuration): Boolean = {
+      // Checks if file at path `filePath` can be split.
       // Uncompressed Hive Text files may be split. GZIP compressed files are not.
-      val codec = new CompressionCodecFactory(hadoopConf).getCodec(file.getPath)
+      // Note: This method works on a Path, and cannot take a `FileStatus`.
+      //       partition.files is an Array[FileStatus] on vanilla Apache Spark,
+      //       but an Array[SerializableFileStatus] on Databricks.
+      val codec = new CompressionCodecFactory(hadoopConf).getCodec(filePath)
       codec == null || codec.isInstanceOf[SplittableCompressionCodec]
     }
 
@@ -271,7 +274,7 @@ case class GpuHiveTableScanExec(requestedAttributes: Seq[Attribute],
           sparkSession,
           f,
           f.getPath,
-          isSplitable = canBeSplit(f, hadoopConf),
+          isSplitable = canBeSplit(f.getPath, hadoopConf),
           maxSplitBytes,
           partition.values
         )