4afc593608fc3d95619bbd397d5998d498c54fb3
Andy Grove <andygrove73@gmail.com>
2023-06-06 15:38:51 -0600

    Update jenkins/databricks/install_deps.py
    Co-authored-by: Gera Shegalov <gshegalov@nvidia.com>

--- jenkins/databricks/install_deps.py
+++ jenkins/databricks/install_deps.py
@@ -140,10 +140,10 @@ def define_deps(spark_version, scala_version):
     return deps
 
 def install_deps(deps, spark_version_to_install_databricks_jars, m2_dir, jar_dir, file):
-    pom_xml_header = """<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-        <project xmlns=\"http://maven.apache.org/POM/4.0.0\"
-            xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
-            xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">
+    pom_xml_header = """<?xml version="1.0" encoding="UTF-8"?>
+        <project xmlns="http://maven.apache.org/POM/4.0.0"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
             <modelVersion>4.0.0</modelVersion>
             <groupId>com.nvidia</groupId>
             <artifactId>rapids-4-spark-databricks-deps-installer</artifactId>
