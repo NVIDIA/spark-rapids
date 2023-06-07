f6f2c952d29b0ef933eed7042b3a55e2f1db7f6a
Haoyang Li <haoyangl@nvidia.com>
2023-06-07 12:06:20 +0800

    Memoized DataGens in integration tests (#8441)
    * [WIP] memoizing datagens in integration tests
    * clean up
    * clean import
    * clean up
    * add class name for hash
    * add comments
    * rewrite __eq__ with __repr__s
    * recovery losed commit due to bad git ops...
    * refactor
    * add comments
    * remove cache_repr
    * clean up and add a comment
    * clean up
    * use _cache_repr for caching
    * clean up
    * add comment of memory usage
    * fix decimal repr, add sparkconf for cache key
    * Cache data instead of dataframes
    * Add cache_repr for new uniqueLongGen
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>

--- integration_tests/src/main/python/window_function_test.py
+++ integration_tests/src/main/python/window_function_test.py
@@ -22,6 +22,7 @@ from pyspark.sql.types import NumericType
 from pyspark.sql.window import Window
 import pyspark.sql.functions as f
 from spark_session import is_before_spark_320, is_before_spark_340, is_databricks113_or_later
+import warnings
 
 _grpkey_longs_with_no_nulls = [
     ('a', RepeatSeqGen(LongGen(nullable=False), length=20)),
@@ -1432,3 +1433,9 @@ def test_to_date_with_window_functions():
         FROM window_input
         """
     )
+
+def test_lru_cache_datagen():
+    # log cache info at the end of integration tests, not related to window functions
+    info = gen_df_help.cache_info()
+    warnings.warn("Cache info: {}".format(info))
+    gen_df_help.cache_clear()
