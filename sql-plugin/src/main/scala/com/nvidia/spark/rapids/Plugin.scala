b585361ff2cdaca018ed52a4f3fd0c049c5465d8
Chong Gao <res_life@163.com>
2023-06-12 19:30:48 +0800

    Modify FileCacheLocalityManager.init to pass in Spark context
    Signed-off-by: Chong Gao <res_life@163.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/Plugin.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/Plugin.scala
@@ -263,7 +263,7 @@ class RapidsDriverPlugin extends DriverPlugin with Logging {
       }
     }
 
-    FileCacheLocalityManager.init(pluginContext.conf())
+    FileCacheLocalityManager.init(sc, pluginContext.conf())
 
     logDebug("Loading extra driver plugins: " +
       s"${extraDriverPlugins.map(_.getClass.getName).mkString(",")}")
