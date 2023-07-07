0adaa4ecb9a2e9542cd210c17d2003916437be9f
Jason Lowe <jlowe@nvidia.com>
2023-07-07 15:17:21 -0500

    Only fail for missing data during precommit runs

--- integration_tests/src/main/python/parquet_testing_test.py
+++ integration_tests/src/main/python/parquet_testing_test.py
@@ -16,10 +16,11 @@
 # https://github.com/apache/parquet-testing
 
 from asserts import assert_gpu_and_cpu_are_equal_collect, assert_gpu_and_cpu_error
-from conftest import get_std_input_path, is_at_least_precommit_run
+from conftest import get_std_input_path, is_precommit_run
 from data_gen import copy_and_update
 from pathlib import Path
 import pytest
+import warnings
 
 _rebase_confs = {
     "spark.sql.legacy.parquet.datetimeRebaseModeInRead": "CORRECTED",
@@ -82,11 +83,13 @@ def locate_parquet_testing_files():
             files += p.glob(pattern)
         if files:
             return files
-    if is_at_least_precommit_run():
-        raise AssertionError("Cannot find parquet-testing data in any of: " +
-                             ", ".join([ p.joinpath(g).as_posix()
-                                         for p in places
-                                         for g in glob_patterns]))
+    locations = ", ".join([ p.joinpath(g).as_posix() for p in places for g in glob_patterns])
+    # TODO: Also fail for nightly tests when nightly scripts have been updated to initialize
+    #       the git submodules when pulling spark-rapids changes.
+    #       https://github.com/NVIDIA/spark-rapids/issues/8677
+    if is_precommit_run():
+        raise AssertionError("Cannot find parquet-testing data in any of: " + locations)
+    warnings.warn("Skipping parquet-testing tests. Unable to locate data in any of: " + locations)
     return []
 
 def gen_testing_params_for_errors():
