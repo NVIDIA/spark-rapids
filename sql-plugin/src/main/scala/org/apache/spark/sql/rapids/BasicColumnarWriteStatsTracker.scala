1c465c1b3e493d76290f30a449a6075cc3e81322
Haoyang Li <haoyangl@nvidia.com>
2023-07-21 17:11:50 +0800

    Test concurrent writer update file metrics
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/BasicColumnarWriteStatsTracker.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/BasicColumnarWriteStatsTracker.scala
@@ -158,8 +158,6 @@ class BasicColumnarWriteTaskStatsTracker(
   }
 
   override def getFinalStats(taskCommitTime: Long): WriteTaskStats = {
-    submittedFiles.foreach(updateFileStats)
-    submittedFiles.clear()
 
     // Reports bytesWritten and recordsWritten to the Spark output metrics.
     Option(TaskContext.get()).map(_.taskMetrics().outputMetrics).foreach { outputMetrics =>
