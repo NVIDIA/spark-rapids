54c83b3215062d5c3785219ca3c77d03f7f95f7f
Alessandro Bellina <abellina@nvidia.com>
2023-07-25 15:22:21 -0500

    Use RmmSparkRetrySuiteBase to reset rmm event handlers

--- tests/src/test/scala/com/nvidia/spark/rapids/DeviceMemoryEventHandlerSuite.scala
+++ tests/src/test/scala/com/nvidia/spark/rapids/DeviceMemoryEventHandlerSuite.scala
@@ -18,10 +18,9 @@ package com.nvidia.spark.rapids
 
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
-import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.mockito.MockitoSugar
 
-class DeviceMemoryEventHandlerSuite extends AnyFunSuite with MockitoSugar {
+class DeviceMemoryEventHandlerSuite extends RmmSparkRetrySuiteBase with MockitoSugar {
 
   test("a failed allocation should be retried if we spilled enough") {
     val mockCatalog = mock[RapidsBufferCatalog]
