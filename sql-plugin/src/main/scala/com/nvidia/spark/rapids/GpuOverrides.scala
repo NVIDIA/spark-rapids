dc3d67e27d77183b43570f287f24d8bcb0dec9f8
Haoyang Li <haoyangl@nvidia.com>
2023-07-28 22:28:51 +0800

    Fix collect_set_on_nested_type tests failed (#8783)
    * Fix collect_set_on_nested_type test failed
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * Fallback to cpu if double/float in nested type
    
    * Add fallback tests
    
    * Update fallback tests
    
    * add psnote and tests
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * verify
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    ---------
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
@@ -3259,23 +3259,20 @@ object GpuOverrides extends Logging {
         Seq(ParamCheck("input",
           (TypeSig.commonCudfTypes + TypeSig.DECIMAL_128 +
               TypeSig.NULL +
-              TypeSig.STRUCT +
-              TypeSig.ARRAY).nested(),
+              TypeSig.STRUCT.withPsNote(TypeEnum.STRUCT, "Structs containing " +
+              s"float/double will not be supported.") +
+              TypeSig.ARRAY.withPsNote(TypeEnum.ARRAY, "Arrays containing " +
+              s"floats/doubles will not be supported.")).nested(),
           TypeSig.all))),
       (c, conf, p, r) => new TypedImperativeAggExprMeta[CollectSet](c, conf, p, r) {
 
-        private def isNestedArrayType(dt: DataType): Boolean = {
-          dt match {
-            case StructType(fields) =>
-              fields.exists { field =>
-                field.dataType match {
-                  case sdt: StructType => isNestedArrayType(sdt)
-                  case _: ArrayType => true
-                  case _ => false
-                }
-              }
-            case ArrayType(et, _) => et.isInstanceOf[ArrayType] || et.isInstanceOf[StructType]
-            case _ => false
+        override def tagAggForGpu(): Unit = {
+          // Fall back to CPU if type is nested and contains floats or doubles in the type tree.
+          // Because NaNs in nested types are not supported yet. 
+          // See https://github.com/NVIDIA/spark-rapids/issues/8808
+          if (c.child.dataType != FloatType && c.child.dataType != DoubleType &&
+              isOrContainsFloatingPoint(c.child.dataType)) {
+            willNotWorkOnGpu("Float/Double in nested type is not supported")
           }
         }
 
