b1fb0e1d7375c7193cbf8f54285c295117ceac95
Alessandro Bellina <abellina@nvidia.com>
2023-08-10 11:21:27 -0500

    Use a single index in the scalar get_map_value test, add a fallback test for get_map_value, and add floats/doubles as keys we are testing

--- integration_tests/src/main/python/map_test.py
+++ integration_tests/src/main/python/map_test.py
@@ -18,7 +18,7 @@ from asserts import assert_gpu_and_cpu_are_equal_collect, assert_gpu_and_cpu_err
     assert_gpu_fallback_collect, assert_cpu_and_gpu_are_equal_collect_with_capture
 from data_gen import *
 from conftest import is_databricks_runtime
-from marks import allow_non_gpu
+from marks import allow_non_gpu, ignore_order
 from spark_session import is_before_spark_330, is_databricks104_or_later, is_databricks113_or_later, is_spark_33X, is_spark_340_or_later
 from pyspark.sql.functions import create_map, col, lit, row_number
 from pyspark.sql.types import *
@@ -32,9 +32,31 @@ basic_struct_gen = StructGen([
                                    BooleanGen(), DateGen(), TimestampGen(), null_gen] + decimal_gens)],
     nullable=False)
 
-maps_with_binary = [MapGen(IntegerGen(nullable=False), BinaryGen(max_length=5))]
+maps_with_binary_value = [MapGen(IntegerGen(nullable=False), BinaryGen(max_length=5))]
+# we need to fix https://github.com/NVIDIA/spark-rapids/issues/8985 and add to
+# map_keys, map_values, and map_entries tests
+maps_with_binary_key = [MapGen(BinaryGen(nullable=False), BinaryGen(max_length=5))]
+maps_with_array_key = [
+    MapGen(ArrayGen(IntegerGen(), nullable=False, max_length=5, convert_to_tuple=True),
+           IntegerGen())]
+maps_with_struct_key = [
+    MapGen(StructGen([['child0', IntegerGen()],
+                      ['child1', IntegerGen()]], nullable=False),
+           IntegerGen())]
 
-@pytest.mark.parametrize('data_gen', map_gens_sample + maps_with_binary + decimal_64_map_gens + decimal_128_map_gens, ids=idfn)
+supported_key_gens = \
+    map_gens_sample + \
+    maps_with_binary_value + \
+    decimal_64_map_gens + \
+    decimal_128_map_gens
+
+not_supported_get_map_value_keys = \
+    maps_with_binary_key + \
+    maps_with_array_key + \
+    maps_with_struct_key
+
+
+@pytest.mark.parametrize('data_gen', supported_key_gens, ids=idfn)
 def test_map_keys(data_gen):
     assert_gpu_and_cpu_are_equal_collect(
             lambda spark: unary_op_df(spark, data_gen).selectExpr(
@@ -45,7 +67,7 @@ def test_map_keys(data_gen):
                 'map_keys(a)'))
 
 
-@pytest.mark.parametrize('data_gen', map_gens_sample + maps_with_binary + decimal_64_map_gens + decimal_128_map_gens, ids=idfn)
+@pytest.mark.parametrize('data_gen', supported_key_gens, ids=idfn)
 def test_map_values(data_gen):
     assert_gpu_and_cpu_are_equal_collect(
             lambda spark: unary_op_df(spark, data_gen).selectExpr(
@@ -56,7 +78,7 @@ def test_map_values(data_gen):
                 'map_values(a)'))
 
 
-@pytest.mark.parametrize('data_gen', map_gens_sample  + maps_with_binary + decimal_64_map_gens + decimal_128_map_gens, ids=idfn)
+@pytest.mark.parametrize('data_gen', supported_key_gens, ids=idfn)
 def test_map_entries(data_gen):
     assert_gpu_and_cpu_are_equal_collect(
             lambda spark: unary_op_df(spark, data_gen).selectExpr(
@@ -113,24 +135,6 @@ numeric_key_gens = [
 numeric_key_map_gens = [MapGen(key, value(), max_length=6)
                         for key in numeric_key_gens for value in get_map_value_gens()]
 
-# commonCudfTypes
-supported_key_gens = \
-    [key(nullable=False)
-     for key in [
-         BooleanGen,
-         ByteGen,
-         ShortGen,
-         IntegerGen,
-         LongGen,
-         FloatGen,
-         DoubleGen,
-         DateGen,
-         TimestampGen,
-         StringGen]]
-
-supported_key_map_to_int_gens = [MapGen(key, value(), max_length=6)
-                                    for key in supported_key_gens for value in [IntegerGen]]
-
 
 @pytest.mark.parametrize('data_gen', numeric_key_map_gens, ids=idfn)
 def test_get_map_value_numeric_keys(data_gen):
@@ -145,13 +149,26 @@ def test_get_map_value_numeric_keys(data_gen):
             'a[999]'))
 
 
-@pytest.mark.parametrize('data_gen', supported_key_map_to_int_gens, ids=idfn)
+@pytest.mark.parametrize('data_gen', supported_key_gens, ids=idfn)
 def test_get_map_value_supported_keys(data_gen):
     key_gen = data_gen._key_gen
+    # first expression is not guaranteed to hit
+    # the second expression with map_keys will hit on the first key, or null
+    # on an empty dictionary generated in `a`
     assert_cpu_and_gpu_are_equal_collect_with_capture(
-        lambda spark: gen_df(spark, [("a", data_gen), ("ix", key_gen)])\
-                          .selectExpr('a[ix]'),
-        exist_classes="GpuGetMapValue")
+        lambda spark: gen_df(spark, [("a", data_gen), ("ix", key_gen)]) \
+            .selectExpr('a[ix]', 'a[map_keys(a)[0]]'),
+        exist_classes="GpuGetMapValue,GpuMapKeys")
+
+
+@allow_non_gpu("ProjectExec")
+@pytest.mark.parametrize('data_gen', not_supported_get_map_value_keys, ids=idfn)
+def test_get_map_value_fallback_keys(data_gen):
+    key_gen = data_gen._key_gen
+    assert_gpu_fallback_collect(
+        lambda spark: gen_df(spark, [("a", data_gen), ("ix", key_gen)]) \
+            .selectExpr('a[ix]'),
+        cpu_fallback_class_name="GetMapValue")
 
 
 @pytest.mark.parametrize('key_gen', numeric_key_gens, ids=idfn)
@@ -169,18 +186,18 @@ def test_basic_scalar_map_get_map_value(key_gen):
 
 
 @pytest.mark.parametrize('key_gen', supported_key_gens, ids=idfn)
-def test_map_scalars_supported_key_types(key_gen, key_ix):
+def test_map_scalars_supported_key_types(key_gen):
     def query_map_scalar(spark):
         key_df = gen_df(spark, [("key", key_gen)], length=100).orderBy(col("key"))\
             .select(col("key"),
                     row_number().over(Window().orderBy(col('key')))\
                         .alias("row_num"))
         key_df.select(col("key").alias("key_at_ix"))\
-            .where(col("row_num") == key_ix)\
+            .where(col("row_num") == 5)\
             .repartition(100)\
             .createOrReplaceTempView("single_key_tbl")
         key_df.select(col("key").alias("key_at_ix_next")) \
-            .where(col("row_num") == key_ix+1) \
+            .where(col("row_num") == 6) \
             .repartition(100) \
             .createOrReplaceTempView("single_key_tbl_next")
         # There will be a single row in single_key_tbl (the row that matched key_ix).
