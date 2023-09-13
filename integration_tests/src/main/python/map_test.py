5a31ba3f9a031e68b75cabc225f5ca1cbdca70c2
Robert (Bobby) Evans <bobby@apache.org>
2023-09-13 12:59:11 -0500

    Check for null keys when creating map
    Signed-off-by: Robert (Bobby) Evans <bobby@apache.org>

--- integration_tests/src/main/python/map_test.py
+++ integration_tests/src/main/python/map_test.py
@@ -293,6 +293,12 @@ def test_map_expr_expr_keys_dupe_exception():
             conf={'spark.sql.mapKeyDedupPolicy':'EXCEPTION'},
             error_message = "Duplicate map key")
 
+def test_map_keys_null_exception():
+    assert_gpu_and_cpu_error(
+            lambda spark: spark.sql(
+                "select map(x, -1) from (select explode(array(1,null)) as x)").collect(),
+            conf = {},
+            error_message = "Cannot use null as map key")
 
 def test_map_expr_literal_keys_dupe_last_win():
     data_gen = [('a', StringGen(nullable=False)), ('b', StringGen(nullable=False))]
