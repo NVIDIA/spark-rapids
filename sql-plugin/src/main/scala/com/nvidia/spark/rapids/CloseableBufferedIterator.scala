eb9aeeaf37eb172a5bcb7aca357e0b06ef768773
Alessandro Bellina <abellina@nvidia.com>
2023-09-20 13:33:18 -0500

    Roll our own BufferedIterator so we can close cleanly (#9267)
    * Roll our own BufferedIterator so we can close cleanly
    
    Signed-off-by: Alessandro Bellina <abellina@nvidia.com>
    
    * Add checks in ScalableTaskCompletion to throw if we are mutating the task completion callbacks from a callback
    
    * Fix test that relied on .buffered
    
    ---------
    
    Signed-off-by: Alessandro Bellina <abellina@nvidia.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/CloseableBufferedIterator.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/CloseableBufferedIterator.scala
@@ -21,14 +21,15 @@ import com.nvidia.spark.rapids.ScalableTaskCompletion.onTaskCompletion
 import org.apache.spark.TaskContext
 
 /**
- * Helper iterator that wraps a BufferedIterator of AutoCloseable subclasses.
+ * Helper iterator that wraps an Iterator of AutoCloseable subclasses.
  * This iterator also implements AutoCloseable, so it can be closed in case
- * of exceptions.
+ * of exceptions and when close is called on it, its buffered item will be
+ * closed as well.
  *
- * @param wrapped the buffered iterator
+ * @param wrapped the iterator we are wrapping for buffering
  * @tparam T an AutoCloseable subclass
  */
-class CloseableBufferedIterator[T <: AutoCloseable](wrapped: BufferedIterator[T])
+class CloseableBufferedIterator[T <: AutoCloseable](wrapped: Iterator[T])
   extends BufferedIterator[T] with AutoCloseable {
   // Don't install the callback if in a unit test
   Option(TaskContext.get()).foreach { tc =>
@@ -38,13 +39,38 @@ class CloseableBufferedIterator[T <: AutoCloseable](wrapped: BufferedIterator[T]
   }
 
   private[this] var isClosed = false
-  override def head: T = wrapped.head
-  override def headOption: Option[T] = wrapped.headOption
-  override def next: T = wrapped.next
-  override def hasNext: Boolean = wrapped.hasNext
+
+  private var hd: Option[T] = None
+
+  def head: T = {
+    if (hd.isEmpty) {
+      hd = Some(next())
+    }
+    hd.get
+  }
+
+  override def headOption: Option[T] = {
+    if (hasNext) {
+      Some(head)
+    } else {
+      None
+    }
+  }
+
+  override def next: T = if (hd.isDefined) {
+    val res = hd.get
+    hd = None
+    res
+  } else {
+    wrapped.next
+  }
+
+  override def hasNext: Boolean = !isClosed && (hd.isDefined || wrapped.hasNext)
+
   override def close(): Unit = {
     if (!isClosed) {
-      headOption.foreach(_.close())
+      hd.foreach(_.close()) // close a buffered head item
+      hd = None
       isClosed = true
     }
   }
