45000f92c242132bddfe157995d44c243330f1f3
MithunR <mythrocks@gmail.com>
2023-10-02 14:24:20 -0700

    Tests for reading GPU-written files.

--- integration_tests/src/main/python/fastparquet_compatibility_test.py
+++ integration_tests/src/main/python/fastparquet_compatibility_test.py
@@ -14,17 +14,11 @@
 
 import pytest
 
-from asserts import assert_gpu_and_cpu_are_equal_collect, assert_gpu_and_cpu_are_equal_sql, assert_gpu_fallback_collect, \
-    assert_gpu_sql_fallback_collect
+from asserts import assert_gpu_and_cpu_are_equal_collect
 from datetime import date, datetime, timezone
 from data_gen import *
-from marks import *
 from pyspark.sql.types import *
-from pyspark.sql.types import NumericType
-from pyspark.sql.window import Window
-import pyspark.sql.functions as f
-from spark_session import is_before_spark_320, is_databricks113_or_later, spark_version, with_cpu_session
-import warnings
+from spark_session import with_cpu_session, with_gpu_session
 
 rebase_write_corrected_conf = {
     'spark.sql.parquet.datetimeRebaseModeInWrite': 'CORRECTED',
@@ -34,7 +28,7 @@ rebase_write_corrected_conf = {
 
 def read_parquet(data_path):
     """
-    (Fetches a function that) reads Parquet from the specified `data_path`.
+    (Fetches a function that) Reads Parquet from the specified `data_path`.
     If the plugin is enabled, the read is done via Spark APIs, through the plugin.
     If the plugin is disabled, the data is read via `fastparquet`.
     :param data_path: Location of the (single) Parquet input file.
@@ -99,6 +93,52 @@ def test_read_fastparquet_single_column_tables(data_gen, spark_tmp_path, correct
 
 @pytest.mark.parametrize('column_gen', [
     ByteGen(nullable=False),
+    ShortGen(nullable=False),
+    IntegerGen(nullable=False),
+    pytest.param(IntegerGen(nullable=True),
+                 marks=pytest.mark.xfail(reason="Nullables cause merge errors, when converting to Spark dataframe")),
+    LongGen(nullable=False),
+    pytest.param(LongGen(nullable=True),
+                 marks=pytest.mark.xfail(reason="Nullables cause merge errors, when converting to Spark dataframe")),
+    FloatGen(nullable=False),
+    DoubleGen(nullable=False),
+    StringGen(nullable=False),
+    pytest.param(DecimalGen(nullable=False),
+                 marks=pytest.mark.xfail(reason="fastparquet reads Decimal columns as Float, as per "
+                                                "https://fastparquet.readthedocs.io/en/latest/details.html#data-types")),
+    pytest.param(DateGen(nullable=False,
+                         start=date(year=2020, month=1, day=1),
+                         end=date(year=2020, month=12, day=31)),
+                 marks=pytest.mark.xfail(reason="fastparquet reads dates as timestamps.")),
+    pytest.param(DateGen(nullable=False),
+                 marks=pytest.mark.xfail(reason="fastparquet reads far future dates (e.g. year=8705) incorrectly.")),
+    TimestampGen(nullable=False,
+                 start=datetime(2000, 1, 1, tzinfo=timezone.utc),
+                 end=datetime(2200, 12, 31, tzinfo=timezone.utc)),  # Vanilla case.
+    pytest.param(TimestampGen(nullable=False,
+                              start=datetime(1, 1, 1, tzinfo=timezone.utc),
+                              end=datetime(1899, 12, 31, tzinfo=timezone.utc)),
+                 marks=pytest.mark.xfail(reason="fastparquet reads timestamps preceding 1900 incorrectly.")),
+], ids=idfn)
+def test_reading_file_written_with_gpu(spark_tmp_path, column_gen):
+    data_path = spark_tmp_path + "/FASTPARQUET_TEST_GPU_WRITE_PATH"
+
+    gen = StructGen([('a', column_gen),
+                     ('part', IntegerGen(nullable=False))
+                     ], nullable=False)
+    # Write data out with Spark RAPIDS plugin.
+    with_gpu_session(
+        lambda spark: gen_df(spark, gen, 2048).repartition(1).write.mode('overwrite').parquet(data_path),
+        conf=rebase_write_corrected_conf
+    )
+
+    # TODO: Maybe make _assert_equal() available to compare dataframes, regardless of CPU vs GPU?
+    # For now, this compares the results of reading back the GPU-written data, via fastparquet and GPU.
+    assert_gpu_and_cpu_are_equal_collect(read_parquet(data_path), conf=rebase_write_corrected_conf)
+
+
+@pytest.mark.parametrize('column_gen', [
+    ByteGen(nullable=False),
     ByteGen(nullable=True),
     ShortGen(nullable=False),
     ShortGen(nullable=True),
@@ -138,16 +178,15 @@ def test_read_fastparquet_single_column_tables(data_gen, spark_tmp_path, correct
 ], ids=idfn)
 def test_reading_file_written_with_fastparquet(column_gen, spark_tmp_path):
     data_path = spark_tmp_path + "/FASTPARQUET_WRITE_PATH"
-    # data_path = "/tmp/FASTPARQUET_WRITE_PATH"
 
     def write_with_fastparquet(spark, data_gen):
-        #  TODO: Provide compression options.
+        #  TODO: (future) Compression settings?
         import fastparquet
         dataframe = gen_df(spark, data_gen, 2048)
         fastparquet.write(data_path, dataframe.toPandas())
 
     gen = StructGen([('a', column_gen),
-                     # ('part', IntegerGen(nullable=False))
+                     ('part', IntegerGen(nullable=False))
                      ], nullable=False)
     # Write data with CPU session.
     with_cpu_session(
