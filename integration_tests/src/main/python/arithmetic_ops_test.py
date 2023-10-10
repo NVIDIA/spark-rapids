e2901c040712a8658098ad2aa4bd9045974379e0
Navin Kumar <navink@nvidia.com>
2023-10-10 13:57:43 -0700

    Address feedback, refactor Arm, and add reference to SPARK JIRA in integration tests.

--- integration_tests/src/main/python/arithmetic_ops_test.py
+++ integration_tests/src/main/python/arithmetic_ops_test.py
@@ -471,7 +471,7 @@ def test_unary_minus(data_gen):
             lambda spark : unary_op_df(spark, data_gen).selectExpr('-a'))
 
 @pytest.mark.parametrize('data_gen', _arith_decimal_gens_high_precision, ids=idfn)
-@pytest.mark.skipif(is_scala213(), reason="Apache Spark built with Scala 2.13 produces inconsistent results at high precision")
+@pytest.mark.skipif(is_scala213(), reason="Apache Spark built with Scala 2.13 produces inconsistent results at high precision (SPARK-45438)")
 def test_unary_minus_decimal128(data_gen):
     assert_gpu_and_cpu_are_equal_collect(
             lambda spark : unary_op_df(spark, data_gen).selectExpr('-a'))
@@ -484,7 +484,7 @@ def test_unary_minus_ansi_no_overflow(data_gen):
             conf=ansi_enabled_conf)
 
 @pytest.mark.parametrize('data_gen', _arith_decimal_gens_high_precision, ids=idfn)
-@pytest.mark.skipif(is_scala213(), reason="Apache Spark built with Scala 2.13 produces inconsistent results at high precision")
+@pytest.mark.skipif(is_scala213(), reason="Apache Spark built with Scala 2.13 produces inconsistent results at high precision (SPARK-45438)")
 def test_unary_minus_ansi_no_overflow_decimal128(data_gen):
     assert_gpu_and_cpu_are_equal_collect(
             lambda spark : unary_op_df(spark, data_gen).selectExpr('-a'),
@@ -521,7 +521,7 @@ def test_abs(data_gen):
             lambda spark : unary_op_df(spark, data_gen).selectExpr('abs(a)'))
 
 @pytest.mark.parametrize('data_gen', _arith_decimal_gens_high_precision, ids=idfn)
-@pytest.mark.skipif(is_scala213(), reason="Apache Spark built with Scala 2.13 produces inconsistent results at high precision")
+@pytest.mark.skipif(is_scala213(), reason="Apache Spark built with Scala 2.13 produces inconsistent results at high precision (SPARK-45438)")
 def test_abs_decimal128(data_gen):
     assert_gpu_and_cpu_are_equal_collect(
             lambda spark : unary_op_df(spark, data_gen).selectExpr('abs(a)'))
