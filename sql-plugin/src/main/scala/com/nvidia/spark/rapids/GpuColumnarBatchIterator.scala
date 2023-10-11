1dbd5015abfce33db720e89a68dacbf3be881086
Partho Sarthi <psarthi@nvidia.com>
2023-10-10 21:16:53 -0700

    Extra line removal
    Signed-off-by: Partho Sarthi <psarthi@nvidia.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuColumnarBatchIterator.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuColumnarBatchIterator.scala
@@ -131,7 +131,6 @@ class GpuColumnarBatchWithPartitionValuesIterator(
     }
   }
 
-
   private[this] def computeValuesAndRowNumsForBatch(batchRowNum: Int):
       (Array[InternalRow], Array[Long]) = {
     val leftTotalRowNum = leftRowNums.sum
