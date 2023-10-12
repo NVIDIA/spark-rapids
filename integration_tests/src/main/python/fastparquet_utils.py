9da9e69635fb30cedc56e470e96a57cf85918130
Chong Gao <res_life@163.com>
2023-10-12 17:31:16 +0800

    change to flatten gpu result

--- integration_tests/src/main/python/fastparquet_utils.py
+++ integration_tests/src/main/python/fastparquet_utils.py
@@ -17,10 +17,9 @@ import pytest
 from pyspark.sql import Row
 
 def get_fastparquet_result_converter():
-    # get a converter which converts fastparquet result to Spark cpu type result
-    # the convert is used to convet flatted Row to nested Row
+    # Get a converter like fastparquet that flattens the GPU results.
     # e.g.:
-    # Row(a.b1 = 1, a.b2 = 2) => Row(a = Row(b1 = 1, b2 = 2))
+    # Row(a = Row(b1 = 1, b2 = 2)) => Row(a.b1 = 1, a.b2 = 2)
     return _convert_fastparquet_result
 
 def _convert_fastparquet_result(fastparquet_cpu, gpu):
@@ -29,113 +28,99 @@ def _convert_fastparquet_result(fastparquet_cpu, gpu):
         --- CPU OUTPUT
         +++ GPU OUTPUT
         @@ -1 +1 @@
-        -Row(a.first=-341142443, a.second=3.333994866005594e-37)
-        +Row(a=Row(first=-341142443, second=3.333994866005594e-37))
-    This method is used to canonicalize the fastparquet result
-    :param fastparquet_cpu: the fastparquet result, it's a Spark Row list,
+        -Row(a.first=-341142443, a.second=3.333994866005594e-37)    // fastparquet
+        +Row(a=Row(first=-341142443, second=3.333994866005594e-37)) // gpu
+    This method is used to canonicalize gpu result like fastparquet does
+    :param fastparquet_cpu: the fastparquet result, it's a Spark Row list with nested Rows
     :param gpu: the gpu result, it's a Spark Row list
-    :return: (converted cpu, gpu) tuple
+    :return: (fastparquet, converted_gpu) tuple
     """
-    assert(len(fastparquet_cpu) == len(gpu))
-    new_cpu = convert_fastparquet_result(fastparquet_cpu, gpu)
-    return (new_cpu, gpu)
+    new_gpu = [_convert_gpu_row(gpu_row) for gpu_row in gpu]
+    return (fastparquet_cpu, new_gpu)
 
-def convert_fastparquet_result(cpu, gpu):
-    return [_convert_fastparquet_row(cpu_row, gpu_row) for (cpu_row, gpu_row) in zip(cpu, gpu)]
+def _convert_gpu_row(gpu_row):
+    converted_dict = _get_converted_dict(gpu_row)
+    return Row(**converted_dict)
 
-def _convert_fastparquet_row(cpu_row, gpu_row):
-    cpu_dict = cpu_row.asDict()
-    _merge_cpu_dict(cpu_dict, gpu_row.asDict())
-    return Row(**cpu_dict)
-
-def _merge_cpu_dict(cpu_dict, gpu_dict, cpu_prefix=""):
-    # recursively merge items to sub Row, from bottom to top.
+def _get_converted_dict(gpu_row):
+    # recursively flatten Row, from bottom to top.
     # e.g.:
-    # step0: dict(a.b.c1 = 1, a.b.c2 = 2, a.b2 = 3)
-    # step1: dict(a.b = Row(c1 = 1, c2 = 2), a.b2 = 3)
-    # step2: dict(a = Row(b = Row(c1 = 1, c2 = 2), b2 = 3))
+    # step0:
+    #   Row(
+    #       a = Row(
+    #           b = Row(
+    #               c1 = 1,
+    #               c2 = 2),
+    #           b2 = 3))
+    # step1:
+    #   dict(
+    #       a = Row(
+    #           b.c1 = 1
+    #           b.c2 = 2,
+    #           b2 = 3))
+    # step2:
+    #   dict(
+    #       a.b.c1 = 1
+    #       a.b.c2 = 2,
+    #       a.b2 = 3)
+    gpu_dict = gpu_row.asDict()
+    updated_dict = {}
     for gpu_k, gpu_v in gpu_dict.items():
         if isinstance(gpu_v, Row):
-            cpu_prefix = cpu_prefix + gpu_k + "."
-            _merge_cpu_dict(cpu_dict, gpu_v.asDict(), cpu_prefix)
-            # reduce the dict for the `cpu_prefix`
-            _reduce_cpu_dict(cpu_dict, cpu_prefix)
+            d = _get_converted_dict(gpu_v)
+            for k, v in d.items():
+                # flatten and save to dict
+                updated_dict[gpu_k + '.' + k] = v
+        else:
+            updated_dict[gpu_k] = gpu_v
+    return updated_dict
 
-def _reduce_cpu_dict(cpu_dict, cpu_prefix):
-    """
-    Merge the items start with `cpu_prefix` in `cpu_dict` to a Spark Row.
-    e.g.:
-      cpu_dict = {"l1.l2.l3.key1": 1, "l1.l2.l3.key2": 2}, cpu_prefix = "l1.l2.l3."
-      ==>>
-      cpu_dict = {"l1.l2.l3": Row("key1": 1, "key2": 2)}
-    """
-    stripped_dict = {}
-    for_removes = set()
-    for cpu_k, cpu_v in cpu_dict.items():
-        if cpu_k.startswith(cpu_prefix):
-            stripped_key = cpu_k[len(cpu_prefix):]
-            stripped_dict[stripped_key] = cpu_v
-            for_removes.add(cpu_k)
-    # remove the items start with prefix
-    for for_remove_key in for_removes:
-        cpu_dict.pop(for_remove_key)
-    row = Row(**stripped_dict)
-    # trim the end dot, e.g.:  "l1.l2.l3." => "l1.l2.l3"
-    prefix_key = cpu_prefix[0 : len(cpu_prefix)-1]
-    # set the merge row
-    cpu_dict[prefix_key] = row
-
-
-def _test_convert_fastparquet_result_no_null():
+def _test_convert_gpu_row():
     """
     verify the method `_convert_fastparquet_result`
     """
-    cpu_dict = {'v1_1' : 11, 'v1_2.v2_1' : 21, 'v1_2.v2_2.v3_1' : 31, 'v1_2.v2_2.v3_2' : 32}
-    cpu_row = Row(**cpu_dict)
     gpu_row = Row(
-                v1_1 = 11,
-                v1_2 = Row(
-                    v2_1 = 21,
-                    v2_2 = Row(
-                        v3_1 = 31,
-                        v3_2 = 32)))
-
-    cpu = [cpu_row]
-    gpu = [gpu_row]
+            v1_1 = 11,
+            v1_2 = Row(
+                v2_1 = 21,
+                v2_2 = Row(
+                    v3_1 = 31,
+                    v3_2 = 32)))
+    expected_dict = {'v1_1' : 11, 'v1_2.v2_1' : 21, 'v1_2.v2_2.v3_1' : 31, 'v1_2.v2_2.v3_2' : 32}
+    expected_row = Row(**expected_dict)
     print()
     print("before:")
-    print(cpu, gpu)
-    (cpu, gpu) = _convert_fastparquet_result(cpu, gpu)
+    print("gpu row: " + str(gpu_row))
+    converted_gpu_row = _convert_gpu_row(gpu_row)
     print()
     print("after:")
-    print(cpu, gpu)
-    assert cpu == gpu
+    print("converted gpu row: " + str(converted_gpu_row))
+    print("expected: " + str(expected_row))
+    assert converted_gpu_row == expected_row
 
-def _test_convert_fastparquet_result_with_null():
+def _test_convert_gpu_row_with_null():
     """
     verify the method `_convert_fastparquet_result`
     """
-    cpu_dict = {'v1_1' : None, 'v1_2.v2_1' : None, 'v1_2.v2_2.v3_1' : None, 'v1_2.v2_2.v3_2' : 32}
-    cpu_row = Row(**cpu_dict)
     gpu_row = Row(
-                v1_1 = None,
-                v1_2 = Row(
-                    v2_1 = None,
-                    v2_2 = Row(
-                        v3_1 = None,
-                        v3_2 = 32)))
-
-    cpu = [cpu_row]
-    gpu = [gpu_row]
+            v1_1 = None,
+            v1_2 = Row(
+                v2_1 = None,
+                v2_2 = Row(
+                    v3_1 = None,
+                    v3_2 = 32)))
+    expected_dict = {'v1_1' : None, 'v1_2.v2_1' : None, 'v1_2.v2_2.v3_1' : None, 'v1_2.v2_2.v3_2' : 32}
+    expected_row = Row(**expected_dict)
     print()
     print("before:")
-    print(cpu, gpu)
-    (cpu, gpu) = _convert_fastparquet_result(cpu, gpu)
+    print("gpu row: " + str(gpu_row))
+    converted_gpu_row = _convert_gpu_row(gpu_row)
     print()
     print("after:")
-    print(cpu, gpu)
-    assert cpu == gpu
+    print("converted gpu row: " + str(converted_gpu_row))
+    print("expected: " + str(expected_row))
+    assert converted_gpu_row == expected_row
 
 if __name__ == "__main__":
-    _test_convert_fastparquet_result_no_null()
-    _test_convert_fastparquet_result_with_null()
+    _test_convert_gpu_row()
+    _test_convert_gpu_row_with_null()
