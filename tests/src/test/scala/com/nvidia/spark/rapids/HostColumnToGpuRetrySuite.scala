f558ba5c320ff246854652c33b2dafe815ef779a
Firestarman <firestarmanllc@gmail.com>
2023-10-16 05:09:34 +0000

    fix a leaking allocator issue
    Signed-off-by: Firestarman <firestarmanllc@gmail.com>

--- tests/src/test/scala/com/nvidia/spark/rapids/HostColumnToGpuRetrySuite.scala
+++ tests/src/test/scala/com/nvidia/spark/rapids/HostColumnToGpuRetrySuite.scala
@@ -19,7 +19,7 @@ package com.nvidia.spark.rapids
 import com.nvidia.spark.rapids.Arm.withResource
 import com.nvidia.spark.rapids.GpuColumnVector.GpuArrowColumnarBatchBuilder
 import com.nvidia.spark.rapids.jni.RmmSpark
-import org.apache.arrow.memory.RootAllocator
+import org.apache.arrow.memory.{BufferAllocator, RootAllocator}
 import org.apache.arrow.vector.IntVector
 
 import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
@@ -30,32 +30,35 @@ class HostColumnToGpuRetrySuite extends RmmSparkRetrySuiteBase {
   private val schema = StructType(Seq(StructField("a", IntegerType)))
   private val NUM_ROWS = 50
 
-  private def buildArrowIntColumn(): ColumnVector = {
-    val intVector = new IntVector("intVector", new RootAllocator())
+  private def buildArrowIntColumn(allocator: BufferAllocator): ColumnVector = {
+    val intVector = new IntVector("intVector", allocator)
     intVector.allocateNew(NUM_ROWS)
     (0 until NUM_ROWS).foreach { pos =>
       intVector.set(pos, pos * 10)
     }
+    intVector.setValueCount(NUM_ROWS)
     new ArrowColumnVector(intVector)
   }
 
   test("Arrow column builder with retry OOM") {
-    val batch = withResource(new GpuArrowColumnarBatchBuilder(schema)) { builder =>
-      withResource(buildArrowIntColumn()) { arrowColumn =>
-        builder.copyColumnar(arrowColumn, 0, NUM_ROWS)
+    withResource(new RootAllocator()) { allocator =>
+      val batch = withResource(new GpuArrowColumnarBatchBuilder(schema)) { builder =>
+        withResource(buildArrowIntColumn(allocator)) { arrowColumn =>
+          builder.copyColumnar(arrowColumn, 0, NUM_ROWS)
+        }
+        RmmSpark.forceRetryOOM(RmmSpark.getCurrentThreadId)
+        RmmRapidsRetryIterator.withRetryNoSplit[ColumnarBatch] {
+          builder.tryBuild(NUM_ROWS)
+        }
       }
-      RmmSpark.forceRetryOOM(RmmSpark.getCurrentThreadId)
-      RmmRapidsRetryIterator.withRetryNoSplit[ColumnarBatch] {
-        builder.tryBuild(NUM_ROWS)
-      }
-    }
-    withResource(batch) { _ =>
-      assertResult(NUM_ROWS)(batch.numRows())
-      assertResult(1)(batch.numCols())
-      withResource(batch.column(0).asInstanceOf[GpuColumnVector].copyToHost()) { hostCol =>
-        withResource(buildArrowIntColumn()) { arrowCol =>
-          (0 until NUM_ROWS).foreach { pos =>
-            assert(hostCol.getInt(pos) == arrowCol.getInt(pos))
+      withResource(batch) { _ =>
+        assertResult(NUM_ROWS)(batch.numRows())
+        assertResult(1)(batch.numCols())
+        withResource(batch.column(0).asInstanceOf[GpuColumnVector].copyToHost()) { hostCol =>
+          withResource(buildArrowIntColumn(allocator)) { arrowCol =>
+            (0 until NUM_ROWS).foreach { pos =>
+              assert(hostCol.getInt(pos) == arrowCol.getInt(pos))
+            }
           }
         }
       }
