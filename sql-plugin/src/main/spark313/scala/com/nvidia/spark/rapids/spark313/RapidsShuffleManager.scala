7494ba52f873cb28270910c6f1d195a96acba04e
Gera Shegalov <gera@apache.org>
2023-10-24 10:30:25 -0700

    merge fixes
    Signed-off-by: Gera Shegalov <gera@apache.org>

--- sql-plugin/src/main/spark313/scala/com/nvidia/spark/rapids/spark313/RapidsShuffleManager.scala
+++ sql-plugin/src/main/spark313/scala/com/nvidia/spark/rapids/spark313/RapidsShuffleManager.scala
@@ -20,7 +20,7 @@ spark-rapids-shim-json-lines ***/
 package com.nvidia.spark.rapids.spark313
 
 import org.apache.spark.SparkConf
-import org.apache.spark.shuffle.rapids.ProxyRapidsShuffleInternalManagerBase
+import org.apache.spark.sql.rapids.ProxyRapidsShuffleInternalManagerBase
 
 /** A shuffle manager optimized for the RAPIDS Plugin for Apache Spark. */
 sealed class RapidsShuffleManager(
