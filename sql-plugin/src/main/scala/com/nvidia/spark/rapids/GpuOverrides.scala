56d1be138defb06d161a37bda21b863197d2fe58
Haoyang Li <haoyangl@nvidia.com>
2023-11-01 09:49:55 +0800

    Support stack function (#9439)
    * wip
    
    * wip
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * wip
    
    * wip
    
    * [WIP] support stack
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * clean up
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * verify
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * order mismatch
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * address comments and fix it
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * update IT
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * address comments
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * Update sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuGenerateExec.scala
    
    Co-authored-by: Liangcai Li <firestarmanllc@gmail.com>
    
    * Update sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuGenerateExec.scala
    
    Co-authored-by: Liangcai Li <firestarmanllc@gmail.com>
    
    * Update sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuGenerateExec.scala
    
    Co-authored-by: Liangcai Li <firestarmanllc@gmail.com>
    
    * address comment
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * Update sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuGenerateExec.scala
    
    Co-authored-by: Liangcai Li <firestarmanllc@gmail.com>
    
    ---------
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    Co-authored-by: Liangcai Li <firestarmanllc@gmail.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
@@ -3300,6 +3300,19 @@ object GpuOverrides extends Logging {
         override val supportOuter: Boolean = true
         override def convertToGpu(): GpuExpression = GpuPosExplode(childExprs.head.convertToGpu())
       }),
+    expr[Stack](
+      "Separates expr1, ..., exprk into n rows.",
+      ExprChecks.projectOnly(
+        TypeSig.ARRAY.nested(TypeSig.commonCudfTypes + TypeSig.NULL + TypeSig.DECIMAL_128 +
+            TypeSig.ARRAY + TypeSig.STRUCT + TypeSig.MAP),
+        TypeSig.ARRAY.nested(TypeSig.all),
+        Seq(ParamCheck("n", TypeSig.lit(TypeEnum.INT), TypeSig.INT)),
+        Some(RepeatingParamCheck("expr",
+          (TypeSig.commonCudfTypes + TypeSig.NULL + TypeSig.DECIMAL_128 +
+              TypeSig.ARRAY + TypeSig.STRUCT + TypeSig.MAP).nested(),
+          TypeSig.all))),
+      (a, conf, p, r) => new GpuStackMeta(a, conf, p, r)
+    ),
     expr[ReplicateRows](
       "Given an input row replicates the row N times",
       ExprChecks.projectOnly(
