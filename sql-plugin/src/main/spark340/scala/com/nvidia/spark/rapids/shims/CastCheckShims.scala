d3629fd1993a391a4aac00d34edabfe58b28061c
Raza Jafri <razajafri@users.noreply.github.com>
2023-11-23 02:09:44 +0100

    Enable build for Databricks 13.3 [databricks] (#9677)
    * pom changes
    
    * pom changes
    
    * pom changes
    
    * add databricks13.3 to premerge
    
    * Added ToPrettyString support
    
    * xfail approximate percentile test
    
    * xfail failing udf tests
    
    * xfail failing tests due to WriteIntoDeltaCommand
    
    * xfail test_delta_atomic_create_table_as_select and test_delta_atomic_replace_table_as_select
    
    * Added 341db to shim-deps and removed from datagen/pom.xml
    
    * updated udf-compiler pom.xml
    
    * updated sql-plugin pom.xml
    
    * fixed multiple pom.xml
    
    * updated udf-compiler pom.xml
    
    * removed TODO
    
    * Signoff
    
    Signed-off-by: Raza Jafri <razajafri@users.noreply.github.com>
    
    * updated scala 2.13 poms
    
    * Revert "xfail failing tests due to WriteIntoDeltaCommand"
    
    This reverts commit 00b498ed3ea963605cc36560e8896fe27bd412d2.
    
    * Revert "xfail test_delta_atomic_create_table_as_select and test_delta_atomic_replace_table_as_select"
    
    This reverts commit ea2fd40b8215cdfa845074127a641af62052e947.
    
    * remove tests/pom.xml changes
    
    * reverted 2.13 generation of tests/pom.xml
    
    * removed 341db profile from tests as we don't run unit tests on databricks
    
    * fixed the xfail reason to point to the correct issue
    
    * removed diff.patch
    
    * Revert "xfail approximate percentile test"
    
    This reverts commit 0a7fa52dc06681a9ef8f1da6b36ed35ac2be79dc.
    
    * build fixes
    
    Signed-off-by: Jason Lowe <jlowe@nvidia.com>
    
    * Fix spark321db build
    
    * Skip UDF tests until UDF handling is updated
    
    * Remove xfail/skips eclipsed by module-level skip
    
    * xfail fastparquet tests due to nulls being introduced by pandas
    
    * Fix incorrect shimplify directives for 341db
    
    * Fix fallback test
    
    ---------
    
    Signed-off-by: Raza Jafri <razajafri@users.noreply.github.com>
    Signed-off-by: Jason Lowe <jlowe@nvidia.com>
    Co-authored-by: Jason Lowe <jlowe@nvidia.com>

--- sql-plugin/src/main/spark340/scala/com/nvidia/spark/rapids/shims/CastCheckShims.scala
+++ sql-plugin/src/main/spark340/scala/com/nvidia/spark/rapids/shims/CastCheckShims.scala
@@ -18,6 +18,7 @@
 /*** spark-rapids-shim-json-lines
 {"spark": "340"}
 {"spark": "341"}
+{"spark": "341db"}
 {"spark": "350"}
 spark-rapids-shim-json-lines ***/
 package com.nvidia.spark.rapids.shims
