bf38f0048936aab6cc5df71d58e567b0f89a1d52
Haoyang Li <haoyangl@nvidia.com>
2023-12-01 14:15:51 +0800

    clean up
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>

--- integration_tests/src/main/python/cast_test.py
+++ integration_tests/src/main/python/cast_test.py
@@ -158,7 +158,7 @@ def test_cast_string_ts_valid_format(data_gen):
     # In Spark 3.2.0+ the valid format changed, and we cannot support all of the format.
     # This provides values that are valid in all of those formats.
     assert_gpu_and_cpu_are_equal_collect(
-            lambda spark : unary_op_df(spark, data_gen, length=1000000).select(f.col('a').cast(TimestampType())),
+            lambda spark : unary_op_df(spark, data_gen).select(f.col('a').cast(TimestampType())),
             conf = {'spark.rapids.sql.hasExtendedYearValues': 'false',
                 'spark.rapids.sql.castStringToTimestamp.enabled': 'true'})
 
