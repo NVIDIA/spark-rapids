5f73433e6c24ff15ab583b5a9db7d0ec1bdd2ef5
YanxuanLiu <yanxuanl@nvidia.com>
2023-12-10 01:57:57 +0800

    add noSnapshots
    Signed-off-by: YanxuanLiu <yanxuanl@nvidia.com>

--- jenkins/spark-premerge-build.sh
+++ jenkins/spark-premerge-build.sh
@@ -235,7 +235,7 @@ nvidia-smi
 
 . jenkins/version-def.sh
 
-PREMERGE_PROFILES="-Ppre-merge"
+PREMERGE_PROFILES="-PnoSnapshots,pre-merge"
 
 # If possible create '~/.m2' cache from pre-created m2 tarball to minimize the impact of unstable network connection.
 # Please refer to job 'update_premerge_m2_cache' on Blossom about building m2 tarball details.
