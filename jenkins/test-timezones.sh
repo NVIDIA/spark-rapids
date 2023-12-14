ca5a289cef49d0bd63a046c7d3c17cbad8890026
sperlingxx <lovedreamf@gmail.com>
2023-12-14 12:46:52 +0900

    fix
    Signed-off-by: sperlingxx <lovedreamf@gmail.com>

--- jenkins/test-timezones.sh
+++ jenkins/test-timezones.sh
@@ -1,5 +1,5 @@
 # Test cases are used in both pre-merge test and nightly-build test.
-# We choose 7 cases so as to run through all time zones every week during nightly-build test.
+# We choose up to 7 cases so as to run through all time zones every week during nightly-build test.
 export time_zones_test_cases=(
   "Asia/Shanghai"   # CST
   "America/New_York"   # PST
@@ -7,5 +7,5 @@ export time_zones_test_cases=(
   "Canada/Newfoundland"   # contains may zone transitions as well
   "America/Belize"   # contains the most intra-hour zone transitions
   "America/Punta_Arenas"   # contains the most intra-minute zone transitions
-  "Africa/Casablanca"   # frequently changing transition rules between the release of Java 8 and Java 17
+  "Africa/Casablanca"   # frequently changing transition rules between the release of Java8 and Java17
 )
