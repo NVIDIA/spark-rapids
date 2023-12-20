f4e85d78969af8ea5536f6cbd186b53a52f5eb26
Haoyang Li <haoyangl@nvidia.com>
2023-12-20 09:54:50 +0800

    clean up
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>

--- integration_tests/src/main/python/date_time_test.py
+++ integration_tests/src/main/python/date_time_test.py
@@ -57,7 +57,7 @@ def test_timeadd_daytime_column():
         # timestamp column max year is 1000
         ('t', TimestampGen(end=datetime(1000, 1, 1, tzinfo=timezone.utc))),
         # max days is 8000 year, so added result will not be out of range
-        ('d', DayTimeIntervalGen(min_value=timedelta(days=1000 * 365), max_value=timedelta(days=1005 * 365)))]
+        ('d', DayTimeIntervalGen(min_value=timedelta(days=0), max_value=timedelta(days=8000 * 365)))]
     assert_gpu_and_cpu_are_equal_collect(
         lambda spark: gen_df(spark, gen_list).selectExpr("t + d", "t + INTERVAL '1 02:03:04' DAY TO SECOND"))
 
