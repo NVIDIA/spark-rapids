24001fa110a586c0840fab853874898f0a288973
Haoyang Li <haoyangl@nvidia.com>
2024-01-26 21:21:36 +0800

    Use parse_url kernel for QUERY literal and column key (#10211)
    * Use parse_url kernel for QUERY literal
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * fallback when regex keys
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * support column query and address comments
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    * address comment
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>
    
    ---------
    
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/RegexParser.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/RegexParser.scala
@@ -19,6 +19,7 @@ import java.sql.SQLException
 
 import scala.collection.mutable.ListBuffer
 
+import com.nvidia.spark.rapids.GpuOverrides.regexMetaChars
 import com.nvidia.spark.rapids.RegexParser.toReadableString
 
 /**
@@ -684,7 +685,6 @@ sealed class RegexRewriteFlags(val emptyRepetition: Boolean)
                 RegexSplitMode   if performing a split (string_split)
  */
 class CudfRegexTranspiler(mode: RegexMode) {
-  private val regexMetaChars = ".$^[]\\|?*+(){}"
   private val regexPunct = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
   private val escapeChars = Map('n' -> '\n', 'r' -> '\r', 't' -> '\t', 'f' -> '\f', 'a' -> '\u0007',
       'b' -> '\b', 'e' -> '\u001b')
