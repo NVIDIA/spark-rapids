b99a9d6b42505ee0ceb21edad1c81d5d64af5a38
Tim Liu <timl@nvidia.com>
2024-01-26 17:17:28 +0800

    Run '--packages' only with default cuda11 jar
    As '--packages' only works on the default cuda11 jar, it does not support classifier parameter,
    refer to issue: https://issues.apache.org/jira/browse/SPARK-20075
    
    We can not specify classifier jar to run plugin tests with '--packages', see below error log:
        Exception in thread "main" java.lang.IllegalArgumentException:
            requirement failed: Provided Maven Coordinates must be in the form 'groupId:artifactId:version'.
            The coordinate provided is: com.nvidia:rapids-4-spark_2.12:23.12.0:jar:cuda12
    
    Signed-off-by: Tim Liu <timl@nvidia.com>

--- jenkins/spark-tests.sh
+++ jenkins/spark-tests.sh
@@ -304,10 +304,14 @@ if [[ $TEST_MODE == "DEFAULT" ]]; then
   PYSP_TEST_spark_shuffle_manager=com.nvidia.spark.rapids.${SHUFFLE_SPARK_SHIM}.RapidsShuffleManager \
     ./run_pyspark_from_build.sh
 
-  SPARK_SHELL_SMOKE_TEST=1 \
-  PYSP_TEST_spark_jars_packages=com.nvidia:rapids-4-spark_${SCALA_BINARY_VER}:${PROJECT_VER} \
-  PYSP_TEST_spark_jars_repositories=${PROJECT_REPO} \
-    ./run_pyspark_from_build.sh
+  # As '--packages' only works on the default cuda11 jar, it does not support classifiers
+  # refer to issue : https://issues.apache.org/jira/browse/SPARK-20075
+  if [[ "$CLASSIFIER" == "" || "$CLASSIFIER" == "cuda11" ]]; then
+    SPARK_SHELL_SMOKE_TEST=1 \
+    PYSP_TEST_spark_jars_packages=com.nvidia:rapids-4-spark_${SCALA_BINARY_VER}:${PROJECT_VER} \
+    PYSP_TEST_spark_jars_repositories=${PROJECT_REPO} \
+      ./run_pyspark_from_build.sh
+  if
 
   # ParquetCachedBatchSerializer cache_test
   PYSP_TEST_spark_sql_cache_serializer=com.nvidia.spark.ParquetCachedBatchSerializer \
