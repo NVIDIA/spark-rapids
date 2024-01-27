e52e5710c9b5f59483d8721a8450acabe20ea4ac
Andy Grove <andygrove@nvidia.com>
2024-01-27 15:16:26 -0700

    experimental fix

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/GpuJsonToStructs.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/GpuJsonToStructs.scala
@@ -78,19 +78,16 @@ case class GpuJsonToStructs(
               withResource(isLiteralNull.ifElse(emptyRow, nullsReplaced)) { cleaned =>
                 checkForNewline(cleaned, "\n", "line separator")
                 checkForNewline(cleaned, "\r", "carriage return")
-                // if the last entry in a column is incomplete or invalid, then cuDF
-                // will drop the row rather than replace with null if there is no newline, so we
-                // add a newline here to prevent that
-                val joined = withResource(cudf.Scalar.fromString("\n")) { lineSep =>
-                  cleaned.joinStrings(lineSep, emptyRow)
-                }
-                val concat = withResource(joined) { _ =>
-                  withResource(ColumnVector.fromStrings("\n")) { newline =>
-                    ColumnVector.stringConcatenate(Array[ColumnView](joined, newline))
+
+                // add a newline to each JSON line and then join all the JSON lines into one string
+                val withNewline = withResource(cudf.Scalar.fromString("\n")) { lineSep =>
+                  withResource(ColumnVector.fromScalar(lineSep, cleaned.getRowCount.toInt)) {
+                      newLineCol =>
+                    ColumnVector.stringConcatenate(Array[ColumnView](cleaned, newLineCol))
                   }
                 }
 
-                (isNullOrEmptyInput, concat)
+                (isNullOrEmptyInput, withNewline.joinStrings(null, emptyRow))
               }
             }
           }
