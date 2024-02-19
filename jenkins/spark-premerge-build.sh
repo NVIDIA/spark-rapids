fb2a3fa1f412cc0f8997b12d0794b25a93195324
Alessandro Bellina <abellina@nvidia.com>
2024-02-19 15:53:38 -0800

    remove extra 320 build

--- jenkins/spark-premerge-build.sh
+++ jenkins/spark-premerge-build.sh
@@ -52,8 +52,8 @@ mvn_verify() {
         # build and run unit tests on one specific version for each sub-version (e.g. 320, 330) except base version
         # separate the versions to two ci stages (mvn_verify, ci_2) for balancing the duration
         if [[ "${SPARK_SHIM_VERSIONS_PREMERGE_UT_1[@]}" =~ "$version" ]]; then
-            env -u SPARK_HOME \
-              $MVN_CMD -U -B $MVN_URM_MIRROR -Dbuildver=$version clean install $MVN_BUILD_ARGS -Dpytest.TEST_TAGS='' -DskipTests
+            #env -u SPARK_HOME \
+            #  $MVN_CMD -U -B $MVN_URM_MIRROR -Dbuildver=$version clean install $MVN_BUILD_ARGS -Dpytest.TEST_TAGS='' -DskipTests
             # Run filecache tests
             #env -u SPARK_HOME SPARK_CONF=spark.rapids.filecache.enabled=true \
             #  $MVN_CMD -B $MVN_URM_MIRROR -Dbuildver=$version test -rf tests $MVN_BUILD_ARGS -Dpytest.TEST_TAGS='' \
