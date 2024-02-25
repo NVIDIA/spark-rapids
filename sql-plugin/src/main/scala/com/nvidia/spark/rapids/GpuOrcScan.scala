f3860fcb86902bc93221de5d043ea01f75b61a42
Gera Shegalov <gera@apache.org>
2024-02-24 20:26:06 -0800

    Add minValue overflow check in ORC double-to-timestamp cast
    Check for overflow towards negative Infinity
    
    Fixes #10431
    
    Signed-off-by: Gera Shegalov <gera@apache.org>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOrcScan.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOrcScan.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2023, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -408,6 +408,12 @@ object GpuOrcScan {
           withResource(milliseconds.max()) { maxValue =>
             if (maxValue.isValid) {
               testLongMultiplicationOverflow(maxValue.getDouble.toLong,
+                DateTimeConstants.MICROS_PER_MILLIS)
+            }
+          }
+          withResource(milliseconds.min()) { minValue =>
+            if (minValue.isValid) {
+              testLongMultiplicationOverflow(minValue.getDouble.toLong,
                 DateTimeConstants.MICROS_PER_MILLIS)
             }
           }
