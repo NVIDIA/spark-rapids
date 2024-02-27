fc63bf03ed0410b8f3b34b4b4e6e531c625bb4da
Chong Gao <res_life@163.com>
2024-02-27 10:14:51 +0800

    Remove copied Spark code; Mask JSON and Path
    Signed-off-by: Chong Gao <res_life@163.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/test/cpuJsonExpressions.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/test/cpuJsonExpressions.scala
@@ -16,29 +16,24 @@
 
 package org.apache.spark.sql.rapids.test
 
-import java.io.{ByteArrayOutputStream, FileWriter, StringWriter}
+import java.io.FileWriter
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Objects
 
-import scala.util.parsing.combinator.RegexParsers
-
 import ai.rapids.cudf.{ColumnVector, DType, HostColumnVector}
-import com.fasterxml.jackson.core.{JsonEncoding, JsonFactoryBuilder, JsonGenerator, JsonParser, JsonProcessingException, JsonToken}
-import com.fasterxml.jackson.core.json.JsonReadFeature
+import com.nvidia.spark.rapids.{GpuColumnVector, GpuScalar}
 import com.nvidia.spark.rapids.Arm.withResource
-import com.nvidia.spark.rapids.GpuColumnVector
 import com.univocity.parsers.csv.{CsvWriter, CsvWriterSettings}
-import java.util
 
-import org.apache.spark.sql.catalyst.json.CreateJacksonParser
+import org.apache.spark.sql.catalyst.expressions.{GetJsonObject, Literal}
+import org.apache.spark.sql.types.StringType
 import org.apache.spark.unsafe.types.UTF8String
-import org.apache.spark.util.Utils
 
 case class CsvWriterWrapper(filePath: String) extends AutoCloseable {
   // create file writer with append mode
-  val writer: FileWriter = new FileWriter(filePath, true)
-  val csvWriter: CsvWriter = new CsvWriter(writer, new CsvWriterSettings())
+  private val writer: FileWriter = new FileWriter(filePath, true)
+  private val csvWriter: CsvWriter = new CsvWriter(writer, new CsvWriterSettings())
 
   override def close(): Unit = {
     if (csvWriter != null) {
@@ -49,17 +44,98 @@ case class CsvWriterWrapper(filePath: String) extends AutoCloseable {
     }
   }
 
-  def writeHeaders(headers: util.Collection[String]): Unit = {
-    csvWriter.writeHeaders(headers)
-  }
-
   def writeRow(row: Array[String]): Unit = {
     csvWriter.writeRow(row)
   }
 }
 
+object GetJsonObjectMask {
+
+  /**
+   * Used by mask data
+   */
+  private val RETAIN_CHARS_FOR_JSON = Set[Char](
+    '{', '}', '[', ']', ',', ':', '"', '\'',
+    '\\', '/', 'b', 'f', 'n', 'r', 't', 'u',
+    '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'e', 'E',
+    'u', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f',
+    't', 'r', 'u', 'e',
+    'f', 'a', 'l', 's', 'e',
+    'n', 'u', 'l', 'l'
+  )
+
+  /**
+   * Mask data. RAPIDS Accelerator should now dump the original Customer data.
+   * This dump tool only care about the functionality of get-json-object, so just replace
+   * characters to a const 's' except the following cases:
+   *     { } [ ] , : " ' :  JSON structure chars, should not replace
+   *     \  :  escape char, should not replace
+   *     / b f n r t u : can follow \, should not replace
+   *     - :  used by number, should not replace
+   *     0-9  : used by number, should not replace
+   *     e E  : used by number, e,g,: 1.0E-3, should not replace
+   *     u A-F a-f : used by unicode: \u HEX HEX HEX HEX, should not replace
+   *     true :  should not replace
+   *     false :  should not replace
+   *     null :  should not replace
+   * To simplify more,
+   * Replace all the chars to 's' except above chars, including chars in true, false, null.
+   *
+   * @param jsonStr original JSON data
+   * @return Desensitized data
+   */
+  def maskData(jsonStr: String): String = {
+    replaceIfNotRetain(jsonStr, RETAIN_CHARS_FOR_JSON)
+  }
+
+  // used by mask path
+  private val RETAIN_CHARS_FOR_PATH = Set[Char]('$', '[', ']', '*', '.')
+
+  /**
+   * mask path
+   * @param pathStr original path
+   * @return masked path
+   */
+  def maskPath(pathStr: String): String = {
+    replaceIfNotRetain(pathStr, RETAIN_CHARS_FOR_PATH)
+  }
+
+  /**
+   * Replace chars not in `retainChars` to const char 's' to mask data
+   * @param originStr origin string
+   * @param retainChars retain chars
+   * @return masked string
+   */
+  private def replaceIfNotRetain(originStr: String, retainChars: Set[Char] ): String = {
+    if (originStr != null) {
+      val buf = new StringBuffer(originStr.length)
+      var idx = 0
+      while (idx < originStr.length) {
+        if (!retainChars.contains(originStr(idx))) {
+          // if it's not a retain char, replace to a const char 's'
+          buf.append('s')
+        } else {
+          buf.append(originStr(idx))
+        }
+        idx += 1
+      }
+      buf.toString
+    } else {
+      null
+    }
+  }
+}
+
 object CpuGetJsonObject {
-  // verify results from Cpu and Gpu, save diffs if have
+  /**
+   * verify results from Cpu and Gpu, save diffs if have
+   * @param dataCv original JSON data
+   * @param path the path to extract JSON data
+   * @param fromGpuCv result from GPU
+   * @param fromCpuHCV result from CPU
+   * @param savePathForVerify save path if have diffs
+   * @param saveRowsForVerify max diff rows to save, Note: only take effective for current data
+   */
   def verify(
       dataCv: ColumnVector,
       path: UTF8String,
@@ -69,6 +145,7 @@ object CpuGetJsonObject {
       saveRowsForVerify: Int): Unit = {
     withResource(dataCv.copyToHost()) { dataHCV =>
       withResource(fromGpuCv.copyToHost()) { fromGpuHCV =>
+        // save file is generated by date: yyyyMMdd.csv
         val savePath = savePathForVerify +
             DateTimeFormatter.ofPattern("yyyyMMdd").format(LocalDate.now()) + ".csv"
         withResource(CsvWriterWrapper(savePath)) { csvWriter =>
@@ -83,8 +160,15 @@ object CpuGetJsonObject {
             val gpuStr = if (fromGpuHCV.isNull(currRow)) null else fromGpuHCV.getJavaString(currRow)
             if (!Objects.equals(cpuStr, gpuStr)) { // if have diff
               diffRowsNum += 1
-              // write to csv file
-              csvWriter.writeRow(Array(pathStr, str, cpuStr, gpuStr))
+
+              // mask customer data
+              val maskedPath = GetJsonObjectMask.maskPath(pathStr)
+              val maskedOriginJson = GetJsonObjectMask.maskData(str)
+              val maskedCpuRet = GetJsonObjectMask.maskData(cpuStr)
+              val maskedGpuRet = GetJsonObjectMask.maskData(gpuStr)
+
+              // append to csv file: yyyyMMdd.csv
+              csvWriter.writeRow(Array(maskedPath, maskedOriginJson, maskedCpuRet, maskedGpuRet))
             }
             currRow += 1
           }
@@ -93,16 +177,26 @@ object CpuGetJsonObject {
     }
   }
 
-  // get_json_object on Cpu
-  def getJsonObjectOnCpu(dataCv: GpuColumnVector, path: UTF8String): HostColumnVector = {
+  /**
+   * Run get-json-object on CPU
+   * @param dataCv original JSON data
+   * @param pathScalar path scalar
+   * @return CPU result of get-json-object
+   */
+  def getJsonObjectOnCpu(dataCv: GpuColumnVector, pathScalar: GpuScalar): HostColumnVector = {
     withResource(dataCv.copyToHost()) { dataHCV =>
       withResource(HostColumnVector.builder(DType.STRING, dataHCV.getRowCount.toInt)) {
         resultBuilder =>
-          val cpuGetJsonObject = GetJsonObject(path)
+          val path = pathScalar.getValue.asInstanceOf[UTF8String]
+          val pathLiteral = Literal.create(path, StringType)
           for (i <- 0 until dataHCV.getRowCount.toInt) {
-            val s = dataHCV.getUTF8String(i)
-            // Call Cpu get_json_object
-            val utf8String = cpuGetJsonObject.eval(s)
+            val json = dataHCV.getUTF8String(i)
+            // In order to use `GetJsonObject` directly,
+            // here use a literal json and a literal path
+            val jsonLiteral = Literal.create(json, StringType)
+            val cpuGetJsonObject = GetJsonObject(jsonLiteral, pathLiteral)
+            // input null is safe because both json and path are literal
+            val utf8String = cpuGetJsonObject.eval(null)
             if (utf8String == null) {
               resultBuilder.appendNull()
             } else {
@@ -111,291 +205,6 @@ object CpuGetJsonObject {
           }
           resultBuilder.build()
       }
-    }
-  }
-}
-
-// below code is copied from Spark
-private[this] sealed trait PathInstruction
-
-private[this] object PathInstruction {
-  private[test] case object Subscript extends PathInstruction
-
-  private[test] case object Wildcard extends PathInstruction
-
-  private[test] case object Key extends PathInstruction
-
-  private[test] case class Index(index: Long) extends PathInstruction
-
-  private[test] case class Named(name: String) extends PathInstruction
-}
-
-private[this] sealed trait WriteStyle
-
-private[this] object WriteStyle {
-  private[test] case object RawStyle extends WriteStyle
-
-  private[test] case object QuotedStyle extends WriteStyle
-
-  private[test] case object FlattenStyle extends WriteStyle
-}
-
-private[this] object JsonPathParser extends RegexParsers {
-
-  import PathInstruction._
-
-  def root: Parser[Char] = '$'
-
-  def long: Parser[Long] = "\\d+".r ^? {
-    case x => x.toLong
-  }
-
-  // parse `[*]` and `[123]` subscripts
-  def subscript: Parser[List[PathInstruction]] =
-    for {
-      operand <- '[' ~> ('*' ^^^ Wildcard | long ^^ Index) <~ ']'
-    } yield {
-      Subscript :: operand :: Nil
-    }
-
-  // parse `.name` or `['name']` child expressions
-  def named: Parser[List[PathInstruction]] =
-    for {
-      name <- '.' ~> "[^\\.\\[]+".r | "['" ~> "[^\\'\\?]+".r <~ "']"
-    } yield {
-      Key :: Named(name) :: Nil
-    }
-
-  // child wildcards: `..`, `.*` or `['*']`
-  def wildcard: Parser[List[PathInstruction]] =
-    (".*" | "['*']") ^^^ List(Wildcard)
-
-  def node: Parser[List[PathInstruction]] =
-    wildcard |
-        named |
-        subscript
-
-  val expression: Parser[List[PathInstruction]] = {
-    phrase(root ~> rep(node) ^^ (x => x.flatten))
-  }
-
-  def parse(str: String): Option[List[PathInstruction]] = {
-    this.parseAll(expression, str) match {
-      case Success(result, _) =>
-        Some(result)
-
-      case _ =>
-        None
-    }
-  }
-}
-
-private[this] object SharedFactory {
-  val jsonFactory = new JsonFactoryBuilder()
-      // The two options below enabled for Hive compatibility
-      .enable(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS)
-      .enable(JsonReadFeature.ALLOW_SINGLE_QUOTES)
-      .build()
-}
-
-private[this] case class GetJsonObject(path: UTF8String) {
-
-  import com.fasterxml.jackson.core.JsonToken._
-
-  import PathInstruction._
-  import SharedFactory._
-  import WriteStyle._
-
-  private lazy val parsedPath = parsePath(path)
-
-  private[test] def eval(jsonStr: UTF8String): UTF8String = {
-    if (jsonStr == null) {
-      return null
-    }
-
-    val parsed = parsedPath
-
-    if (parsed.isDefined) {
-      try {
-        /* We know the bytes are UTF-8 encoded. Pass a Reader to avoid having Jackson
-          detect character encoding which could fail for some malformed strings */
-        withResource(CreateJacksonParser.utf8String(jsonFactory, jsonStr)) { parser =>
-          val output = new ByteArrayOutputStream()
-          val matched = withResource(
-            jsonFactory.createGenerator(output, JsonEncoding.UTF8)) { generator =>
-            parser.nextToken()
-            evaluatePath(parser, generator, RawStyle, parsed.get)
-          }
-          if (matched) {
-            UTF8String.fromBytes(output.toByteArray)
-          } else {
-            null
-          }
-        }
-      } catch {
-        case _: JsonProcessingException => null
-      }
-    } else {
-      null
-    }
-  }
-
-  private def parsePath(path: UTF8String): Option[List[PathInstruction]] = {
-    if (path != null) {
-      JsonPathParser.parse(path.toString)
-    } else {
-      None
-    }
-  }
-
-  // advance to the desired array index, assumes to start at the START_ARRAY token
-  private def arrayIndex(p: JsonParser, f: () => Boolean): Long => Boolean = {
-    case _ if p.getCurrentToken == END_ARRAY =>
-      // terminate, nothing has been written
-      false
-
-    case 0 =>
-      // we've reached the desired index
-      val dirty = f()
-
-      while (p.nextToken() != END_ARRAY) {
-        // advance the token stream to the end of the array
-        p.skipChildren()
-      }
-
-      dirty
-
-    case i if i > 0 =>
-      // skip this token and evaluate the next
-      p.skipChildren()
-      p.nextToken()
-      arrayIndex(p, f)(i - 1)
-  }
-
-  /**
-   * Evaluate a list of JsonPath instructions, returning a bool that indicates if any leaf nodes
-   * have been written to the generator
-   */
-  private def evaluatePath(
-      p: JsonParser,
-      g: JsonGenerator,
-      style: WriteStyle,
-      path: List[PathInstruction]): Boolean = {
-    (p.getCurrentToken, path) match {
-      case (VALUE_STRING, Nil) if style == RawStyle =>
-        // there is no array wildcard or slice parent, emit this string without quotes
-        if (p.hasTextCharacters) {
-          g.writeRaw(p.getTextCharacters, p.getTextOffset, p.getTextLength)
-        } else {
-          g.writeRaw(p.getText)
-        }
-        true
-
-      case (START_ARRAY, Nil) if style == FlattenStyle =>
-        // flatten this array into the parent
-        var dirty = false
-        while (p.nextToken() != END_ARRAY) {
-          dirty |= evaluatePath(p, g, style, Nil)
-        }
-        dirty
-
-      case (_, Nil) =>
-        // general case: just copy the child tree verbatim
-        g.copyCurrentStructure(p)
-        true
-
-      case (START_OBJECT, Key :: xs) =>
-        var dirty = false
-        while (p.nextToken() != END_OBJECT) {
-          if (dirty) {
-            // once a match has been found we can skip other fields
-            p.skipChildren()
-          } else {
-            dirty = evaluatePath(p, g, style, xs)
-          }
-        }
-        dirty
-
-      case (START_ARRAY, Subscript :: Wildcard :: Subscript :: Wildcard :: xs) =>
-        // special handling for the non-structure preserving double wildcard behavior in Hive
-        var dirty = false
-        g.writeStartArray()
-        while (p.nextToken() != END_ARRAY) {
-          dirty |= evaluatePath(p, g, FlattenStyle, xs)
-        }
-        g.writeEndArray()
-        dirty
-
-      case (START_ARRAY, Subscript :: Wildcard :: xs) if style != QuotedStyle =>
-        // retain Flatten, otherwise use Quoted... cannot use Raw within an array
-        val nextStyle = style match {
-          case RawStyle => QuotedStyle
-          case FlattenStyle => FlattenStyle
-          case QuotedStyle => throw new IllegalStateException()
-        }
-
-        // temporarily buffer child matches, the emitted json will need to be
-        // modified slightly if there is only a single element written
-        val buffer = new StringWriter()
-
-        var dirty = 0
-        Utils.tryWithResource(jsonFactory.createGenerator(buffer)) { flattenGenerator =>
-          flattenGenerator.writeStartArray()
-
-          while (p.nextToken() != END_ARRAY) {
-            // track the number of array elements and only emit an outer array if
-            // we've written more than one element, this matches Hive's behavior
-            dirty += (if (evaluatePath(p, flattenGenerator, nextStyle, xs)) 1 else 0)
-          }
-          flattenGenerator.writeEndArray()
-        }
-
-        val buf = buffer.getBuffer
-        if (dirty > 1) {
-          g.writeRawValue(buf.toString)
-        } else if (dirty == 1) {
-          // remove outer array tokens
-          g.writeRawValue(buf.substring(1, buf.length() - 1))
-        } // else do not write anything
-
-        dirty > 0
-
-      case (START_ARRAY, Subscript :: Wildcard :: xs) =>
-        var dirty = false
-        g.writeStartArray()
-        while (p.nextToken() != END_ARRAY) {
-          // wildcards can have multiple matches, continually update the dirty count
-          dirty |= evaluatePath(p, g, QuotedStyle, xs)
-        }
-        g.writeEndArray()
-
-        dirty
-
-      case (START_ARRAY, Subscript :: Index(idx) :: (xs@Subscript :: Wildcard :: _)) =>
-        p.nextToken()
-        // we're going to have 1 or more results, switch to QuotedStyle
-        arrayIndex(p, () => evaluatePath(p, g, QuotedStyle, xs))(idx)
-
-      case (START_ARRAY, Subscript :: Index(idx) :: xs) =>
-        p.nextToken()
-        arrayIndex(p, () => evaluatePath(p, g, style, xs))(idx)
-
-      case (FIELD_NAME, Named(name) :: xs) if p.getCurrentName == name =>
-        // exact field match
-        if (p.nextToken() != JsonToken.VALUE_NULL) {
-          evaluatePath(p, g, style, xs)
-        } else {
-          false
-        }
-
-      case (FIELD_NAME, Wildcard :: xs) =>
-        // wildcard field match
-        p.nextToken()
-        evaluatePath(p, g, style, xs)
-
-      case _ =>
-        p.skipChildren()
-        false
     }
   }
 }
