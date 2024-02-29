c1aa3b40c4bdada2e371175ae660a1f7b6bc0a98
Andy Grove <andygrove@nvidia.com>
2024-02-29 08:34:23 -0700

    Add shims for AQEShuffleReader

---
+++ sql-plugin/src/main/spark311/scala/com/nvidia/spark/rapids/shims/AQEShuffleReaderShims.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*** spark-rapids-shim-json-lines
+{"spark": "311"}
+{"spark": "312"}
+{"spark": "313"}
+spark-rapids-shim-json-lines ***/
+package com.nvidia.spark.rapids.shims
+
+import org.apache.spark.sql.catalyst.plans.physical.{Partitioning, UnknownPartitioning}
+import org.apache.spark.sql.execution.{CoalescedPartitionSpec, PartialMapperPartitionSpec, ShufflePartitionSpec}
+
+object AQEShuffleReaderShims {
+
+  def isLocalReader(partitionSpecs: Seq[ShufflePartitionSpec]): Boolean =
+    partitionSpecs.exists(_.isInstanceOf[PartialMapperPartitionSpec])
+
+  def isCoalescedSpec(spec: ShufflePartitionSpec): Boolean = {
+    spec.isInstanceOf[CoalescedPartitionSpec]
+  }
+
+  def coalescedReadOutputPartitioning(partitionSpecs: Seq[ShufflePartitionSpec],
+      outputPartitioning: Partitioning): Partitioning = {
+    UnknownPartitioning(partitionSpecs.length)
+  }
+}
