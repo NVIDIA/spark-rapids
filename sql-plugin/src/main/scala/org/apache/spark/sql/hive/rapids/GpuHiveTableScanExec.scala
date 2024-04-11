f5a00c8c809bb48704c6ae9b68dd78af8fd8e506
liurenjie1024 <liurenjie2008@gmail.com>
2024-04-11 10:52:42 +0800

    [Do not merge] Check build cmd [skip ci]
    Signed-off-by: liurenjie1024 <liurenjie2008@gmail.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/hive/rapids/GpuHiveTableScanExec.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/hive/rapids/GpuHiveTableScanExec.scala
@@ -283,6 +283,7 @@ case class GpuHiveTableScanExec(requestedAttributes: Seq[Attribute],
     val tableLocation: URI = hiveTableRelation.tableMeta.storage.locationUri.getOrElse{
       throw new UnsupportedOperationException("Table path not set.")
     }
+    println("Hello, world!")
     // No need to check if table directory exists.
     // FileSystem.listStatus() handles this for GpuHiveTableScanExec,
     // just like for Apache Spark.
