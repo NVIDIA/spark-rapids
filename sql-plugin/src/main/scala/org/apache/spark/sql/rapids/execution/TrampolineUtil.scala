75c683e3183630e8974642bcfc6e621f39ad594d
Raza Jafri <rjafri@nvidia.com>
2024-04-19 11:09:12 -0700

    Use cached ThreadPoolExecutor

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/execution/TrampolineUtil.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/execution/TrampolineUtil.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2023, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@
  */
 
 package org.apache.spark.sql.rapids.execution
+
+import java.util.concurrent.ThreadPoolExecutor
 
 import org.json4s.JsonAST
 
@@ -216,5 +218,13 @@ object TrampolineUtil {
       case _ =>
         1
     }
+  }
+
+  def newDaemonCachedThreadPool(
+      prefix: String,
+      maxThreadNumber: Int,
+      keepAliveSeconds: Int): ThreadPoolExecutor = {
+    org.apache.spark.util.ThreadUtils.newDaemonCachedThreadPool(prefix, maxThreadNumber,
+      keepAliveSeconds)
   }
 }
