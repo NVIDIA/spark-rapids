f455342b4f440a758ed3439cf5211deea6572c0f
Nghia Truong <nghiat@nvidia.com>
2024-04-23 08:32:08 -0700

    Change variables into functions
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
@@ -2550,9 +2550,9 @@ class RapidsConf(conf: Map[String, String]) extends Logging {
 
   lazy val shouldExplainAll: Boolean = explain.equalsIgnoreCase("ALL")
 
-  val chunkedReaderEnabled: Boolean = get(CHUNKED_READER)
+  def chunkedReaderEnabled: Boolean = get(CHUNKED_READER)
 
-  val limitChunkedReaderMemoryUsage: Boolean = {
+  def limitChunkedReaderMemoryUsage: Boolean = {
     val hasLimit = get(LIMIT_CHUNKED_READER_MEMORY_USAGE)
     val deprecatedConf = get(CHUNKED_SUBPAGE_READER)
     if (deprecatedConf.isDefined) {
