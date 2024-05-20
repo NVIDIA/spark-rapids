c6817985135c571388467a4d283bcdabb0813f60
liurenjie1024 <liurenjie2008@gmail.com>
2024-05-20 16:40:16 +0800

    Change it
    Signed-off-by: liurenjie1024 <liurenjie2008@gmail.com>

--- jenkins/spark-premerge-build.sh
+++ jenkins/spark-premerge-build.sh
@@ -206,10 +206,10 @@ ci_scala213() {
     cd .. # Run integration tests in the project root dir to leverage test cases and resource files
     export TEST_TAGS="not premerge_ci_1"
     export TEST_TYPE="pre-commit"
-    export TEST_PARALLEL=5
+    export TEST_PARALLEL=4
     # SPARK_HOME (and related) must be set to a Spark built with Scala 2.13
     SPARK_HOME=$SPARK_HOME PYTHONPATH=$PYTHONPATH \
-        ./integration_tests/run_pyspark_from_build.sh -k array_test.py
+        ./integration_tests/run_pyspark_from_build.sh -x
     # enable avro test separately
     SPARK_HOME=$SPARK_HOME PYTHONPATH=$PYTHONPATH \
         INCLUDE_SPARK_AVRO_JAR=true TEST='avro_test.py' ./integration_tests/run_pyspark_from_build.sh
