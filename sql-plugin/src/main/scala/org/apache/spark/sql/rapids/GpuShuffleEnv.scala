459ca9cabaaa552f1ce80e9bf4df3072340d1a9f
Zach Puller <zach.puller@gmail.com>
2024-05-28 12:34:10 -0700

    Update sql-plugin/src/main/scala/org/apache/spark/sql/rapids/GpuShuffleEnv.scala
    Co-authored-by: Alessandro Bellina <abellina@gmail.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/GpuShuffleEnv.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/GpuShuffleEnv.scala
@@ -116,7 +116,7 @@ object GpuShuffleEnv extends Logging {
         val rsmLoaderDirect = classOf[RapidsShuffleManagerLike].getClassLoader
  
         throw new IllegalStateException(s"Cannot initialize the RAPIDS Shuffle Manager " +
-          s"${shuffleManager}! Expected: an instance of  RapidsShuffleManagerLike loaded by " +
+          s"${shuffleManager}! Expected: an instance of RapidsShuffleManagerLike loaded by " +
           s"${rsmLoaderDirect}. Actual: ${shuffleManager} tagged with RapidsShuffleManagerLike " +
           s"loaded by: ${rsmLoaderViaShuffleManager}"
         )
