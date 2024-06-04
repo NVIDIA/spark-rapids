91b877c48384b7d8c1386c829a910b4bcefff175
Hongbin Ma (Mahone) <mahongbin@apache.org>
2024-06-04 10:03:35 +0800

    reduce premergeci2
    Signed-off-by: Hongbin Ma (Mahone) <mahongbin@apache.org>

--- jenkins/spark-premerge-build.sh
+++ jenkins/spark-premerge-build.sh
@@ -166,7 +166,7 @@ ci_2() {
 
     # Download a Scala 2.12 build of spark
     prepare_spark $SPARK_VER 2.12
-    ./integration_tests/run_pyspark_from_build.sh
+    ./integration_tests/run_pyspark_from_build.sh -k 'array_test.py and test_array_element_at_ansi_fail_invalid_index'
 
     # enable avro test separately
     INCLUDE_SPARK_AVRO_JAR=true TEST='avro_test.py' ./integration_tests/run_pyspark_from_build.sh
