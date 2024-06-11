169e15af6d7c2a4a8864efaeec262d85ec015e1d
Tim Liu <timl@nvidia.com>
2024-06-11 16:22:49 +0800

    Drop spark-3.1.x support for spark-rapids
    CI part for the issue: https://github.com/NVIDIA/spark-rapids/issues/10955
    
    We'll drop spark-3.1.x support from branch-24.08, change the default spark version string to 3.2.0
    
    This change needs to work together with the source code update for dropping spark-3.1.x support
    
    Will keep monitoring CI jobs status after all the related changes get merged
    
    Signed-off-by: Tim Liu <timl@nvidia.com>

--- jenkins/version-def.sh
+++ jenkins/version-def.sh
@@ -32,7 +32,7 @@ CUDA_CLASSIFIER=${CUDA_CLASSIFIER:-"cuda11"}
 CLASSIFIER=${CLASSIFIER:-"$CUDA_CLASSIFIER"} # default as CUDA_CLASSIFIER for compatibility
 PROJECT_VER=${PROJECT_VER:-"24.08.0-SNAPSHOT"}
 PROJECT_TEST_VER=${PROJECT_TEST_VER:-"24.08.0-SNAPSHOT"}
-SPARK_VER=${SPARK_VER:-"3.1.1"}
+SPARK_VER=${SPARK_VER:-"3.2.0"}
 SPARK_VER_213=${SPARK_VER_213:-"3.3.0"}
 # Make a best attempt to set the default value for the shuffle shim.
 # Note that SPARK_VER for non-Apache Spark flavors (i.e. databricks,
@@ -85,7 +85,7 @@ fi
 # PHASE_TYPE: CICD phase at which the script is called, to specify Spark shim versions.
 # regular: noSnapshots + snapshots
 # pre-release: noSnapshots only
-# *: shim versions to build, e.g., PHASE_TYPE="311 321"
+# *: shim versions to build, e.g., PHASE_TYPE="320 321"
 PHASE_TYPE=${PHASE_TYPE:-"regular"}
 case $PHASE_TYPE in
     # SPARK_SHIM_VERSIONS will be used for nightly artifact build
