8be7ddf68953068ea3aebdabd566099e4c518513
Hongbin Ma (Mahone) <mahongbin@apache.org>
2024-06-17 10:29:03 +0800

    address review comments
    Signed-off-by: Hongbin Ma (Mahone) <mahongbin@apache.org>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuExec.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuExec.scala
@@ -152,16 +152,34 @@ sealed abstract class GpuMetric extends Serializable {
   def +=(v: Long): Unit
   def add(v: Long): Unit
 
+  private var isTimerActive = false
+
+  final def tryActivateTimer(): Boolean = {
+    if (!isTimerActive) {
+      isTimerActive = true
+      true
+    } else {
+      false
+    }
+  }
+
+  final def deactivateTimer(duration: Long): Unit = {
+    if (isTimerActive) {
+      isTimerActive = false
+      add(duration)
+    }
+  }
+
   final def ns[T](f: => T): T = {
-    val needTrack = ThreadLocalMetrics.onMetricsEnter(this)
-    val start = System.nanoTime()
-    try {
-      f
-    } finally {
-      if (needTrack) {
-        add(System.nanoTime() - start)
-        ThreadLocalMetrics.onMetricsExit(this)
+    if (tryActivateTimer()) {
+      val start = System.nanoTime()
+      try {
+        f
+      } finally {
+        deactivateTimer(System.nanoTime() - start)
       }
+    } else {
+      f
     }
   }
 }
