f8bae930bd3f7b22bf703de0233e9ae63464f9ca
fejiang <fejiang@nvidia.com>
2024-07-11 19:45:06 +0800

    delimiter change to scalar type
    Signed-off-by: fejiang <fejiang@nvidia.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/stringFunctions.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/stringFunctions.scala
@@ -1616,7 +1616,7 @@ case class GpuSubstringIndex(strExpr: Expression,
       count: GpuScalar): ColumnVector = {
 
     val strColumnView = str.getBase
-    val delimiter = delim.getValue.asInstanceOf[UTF8String].toString
+    val delimiter = delim.getValue
     val cnt = count.getValue.asInstanceOf[Int]
     // Use withResource to manage the lifecycle of ColumnVector
     withResource(GpuSubstringIndexUtils.substringIndex(strColumnView, delimiter, cnt)) { result =>
