5d689fc80949a2b3a83019d436c682769257621f
YanxuanLiu <yanxuanl@nvidia.com>
2024-07-19 22:59:06 +0800

    fix error of script shell
    Signed-off-by: YanxuanLiu <yanxuanl@nvidia.com>

--- .github/workflows/mvn-verify-check/populate-daily-cache.sh
+++ .github/workflows/mvn-verify-check/populate-daily-cache.sh
@@ -16,23 +16,29 @@
 
 set -x
 max_retry=3; delay=30; i=1
-while true; do
-for pom in pom.xml 
-do
-    mvn $COMMON_MVN_FLAGS --file $pom help:evaluate -pl dist \
-    -Dexpression=included_buildvers \
-    -DforceStdout -PnoSnapshots -q | tr -d ',' | \
-        xargs -n 1 bash -c \
-        "mvn $COMMON_MVN_FLAGS --file $1 -Dbuildver=$2 de.qaware.maven:go-offline-maven-plugin:resolve-dependencies" _ $pom
-
-    # compile base versions to cache scala compiler and compiler bridge
-    mvn $COMMON_MVN_FLAGS --file $pom \
-    process-test-resources -pl sql-plugin-api -am
-done && break || {
-if [[ $i -le $max_retry ]]; then
-    echo "mvn command failed. Retry $i/$max_retry."; ((i++)); sleep $delay; ((delay=delay*2))
-else
-    echo "mvn command failed. Exit 1"; exit 1
+if [[ $SCALA_VER == '2.12' ]]; then
+    pom='pom.xml'
+    buildver='noSnapshots'
+elif [[ $SCALA_VER == '2.13' ]]; then
+    pom='scala2.13/pom.xml'
+    buildver='noSnapshotsScala213'
 fi
+while true; do
+    {
+        mvn $COMMON_MVN_FLAGS --file $pom help:evaluate -pl dist \
+            -Dexpression=included_buildvers \
+            -DforceStdout -P$buildver -q | tr -d ',' | \
+            xargs -n 1 -I {} bash -c \
+                "mvn $COMMON_MVN_FLAGS --file $pom -Dbuildver={} de.qaware.maven:go-offline-maven-plugin:resolve-dependencies"
+
+        # compile base versions to cache scala compiler and compiler bridge
+        mvn $COMMON_MVN_FLAGS --file $pom \
+            process-test-resources -pl sql-plugin-api -am
+    } && break || {
+    if [[ $i -le $max_retry ]]; then
+        echo "mvn command failed. Retry $i/$max_retry."; ((i++)); sleep $delay; ((delay=delay*2))
+    else
+        echo "mvn command failed. Exit 1"; exit 1
+    fi
 }
 done
