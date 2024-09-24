13784e932122710a09874f3edbc8c800145407f9
ustcfy <fengyan_@mail.ustc.edu.cn>
2024-09-24 14:55:31 +0800

    Improved resource management by using withResource.
    Signed-off-by: ustcfy <fengyan_@mail.ustc.edu.cn>

--- tests/src/test/scala/com/nvidia/spark/rapids/lore/GpuLoreSuite.scala
+++ tests/src/test/scala/com/nvidia/spark/rapids/lore/GpuLoreSuite.scala
@@ -17,6 +17,7 @@
 package com.nvidia.spark.rapids.lore
 
 import com.nvidia.spark.rapids.{FunSuiteWithTempDir, GpuColumnarToRowExec, RapidsConf, SparkQueryCompareTestSuite}
+import com.nvidia.spark.rapids.Arm.withResource
 import org.apache.hadoop.fs.Path
 
 import org.apache.spark.internal.Logging
