3bcffed2fb6e3ae3325367d4f61fd8767eee3e33
Robert (Bobby) Evans <bobby@apache.org>
2024-09-24 10:17:05 -0500

    Update test now that code is fixed
    Signed-off-by: Robert (Bobby) Evans <bobby@apache.org>

--- integration_tests/src/main/python/json_matrix_test.py
+++ integration_tests/src/main/python/json_matrix_test.py
@@ -791,7 +791,7 @@ def test_from_json_strings(std_input_path, input_file):
     "int_struct_formatted.json",
     "int_mixed_array_struct_formatted.json",
     "bad_whitespace.json",
-    pytest.param("escaped_strings.json", marks=pytest.mark.xfail(reason='https://github.com/NVIDIA/spark-rapids/issues/11386')),
+    "escaped_strings.json",
     pytest.param("nested_escaped_strings.json", marks=pytest.mark.xfail(reason='https://github.com/NVIDIA/spark-rapids/issues/11387')),
     pytest.param("repeated_columns.json", marks=pytest.mark.xfail(reason='https://github.com/NVIDIA/spark-rapids/issues/11361')),
     "mixed_objects.json",
