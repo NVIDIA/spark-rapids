15ef563b098b101a51f563f61e265430c4e7395c
Peixin Li <pxLi@nyu.edu>
2024-09-26 20:12:02 +0800

    replace conda with pip
    Signed-off-by: Peixin Li <pxLi@nyu.edu>

--- jenkins/spark-tests.sh
+++ jenkins/spark-tests.sh
@@ -108,12 +108,12 @@ rm -rf $TMP_PYTHON && mkdir -p $TMP_PYTHON && cp -r $SPARK_HOME/python $TMP_PYTH
 PY4J_FILE=$(find $TMP_PYTHON/python/lib -type f -iname "py4j*.zip")
 export PYTHONPATH=$TMP_PYTHON/python:$TMP_PYTHON/python/pyspark/:$PY4J_FILE
 
-# Extract 'value' from conda config string 'key: value'
-CONDA_ROOT=`conda config --show root_prefix | cut -d ' ' -f2`
-if [[ x"$CONDA_ROOT" != x ]]; then
+if command -v conda >/dev/null 2>&1; then
+  # Extract 'value' from conda config string 'key: value'
+  CONDA_ROOT=$(conda config --show root_prefix | cut -d ' ' -f2)
   # Put conda package path ahead of the env 'PYTHONPATH',
   # to import the right pandas from conda instead of spark binary path.
-  PYTHON_VER=`conda config --show default_python | cut -d ' ' -f2`
+  PYTHON_VER=$(conda config --show default_python | cut -d ' ' -f2)
   export PYTHONPATH="$CONDA_ROOT/lib/python$PYTHON_VER/site-packages:$PYTHONPATH"
 else
   # if no conda, then try with default python
