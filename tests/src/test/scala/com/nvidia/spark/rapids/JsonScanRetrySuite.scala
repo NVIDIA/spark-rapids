9a4a08731ad13376859c9c1efdf52d5121864397
Zach Puller <zpuller@nvidia.com>
2024-10-08 11:25:39 -0700

    undo Json change
    Signed-off-by: Zach Puller <zpuller@nvidia.com>

--- tests/src/test/scala/com/nvidia/spark/rapids/JsonScanRetrySuite.scala
+++ tests/src/test/scala/com/nvidia/spark/rapids/JsonScanRetrySuite.scala
@@ -16,26 +16,26 @@
 
 package com.nvidia.spark.rapids
 
-//import ai.rapids.cudf.JSONOptions
-//import com.nvidia.spark.rapids.jni.RmmSpark
-//
-//import org.apache.spark.sql.catalyst.json.rapids.JsonPartitionReader
-//import org.apache.spark.sql.types._
+import ai.rapids.cudf.JSONOptions
+import com.nvidia.spark.rapids.jni.RmmSpark
+
+import org.apache.spark.sql.catalyst.json.rapids.JsonPartitionReader
+import org.apache.spark.sql.types._
 
 class JsonScanRetrySuite extends RmmSparkRetrySuiteBase {
-//  test("test simple retry") {
-//    val bufferer = FilterEmptyHostLineBuffererFactory.createBufferer(100, Array('\n'.toByte))
-//    bufferer.add("{\"a\": 1, \"b\": 2".getBytes, 0, 14)
-//
-//    val cudfSchema = GpuColumnVector.from(StructType(Seq(StructField("a", IntegerType),
-//      StructField("b", IntegerType))))
-//    val opts = JSONOptions.builder().withLines(true).build()
-//    RmmSpark.forceRetryOOM(RmmSpark.getCurrentThreadId, 1,
-//      RmmSpark.OomInjectionType.GPU.ordinal, 0)
-//    val table = JsonPartitionReader.readToTable(bufferer, cudfSchema, NoopMetric,
-//      opts, "JSON", null)
-//    table.close()
-//    // We don't have any good way to verify that the retry was thrown, but we are going to trust
-//    // that it was.
-//  }
+  test("test simple retry") {
+    val bufferer = FilterEmptyHostLineBuffererFactory.createBufferer(100, Array('\n'.toByte))
+    bufferer.add("{\"a\": 1, \"b\": 2".getBytes, 0, 14)
+
+    val cudfSchema = GpuColumnVector.from(StructType(Seq(StructField("a", IntegerType),
+      StructField("b", IntegerType))))
+    val opts = JSONOptions.builder().withLines(true).build()
+    RmmSpark.forceRetryOOM(RmmSpark.getCurrentThreadId, 1,
+      RmmSpark.OomInjectionType.GPU.ordinal, 0)
+    val table = JsonPartitionReader.readToTable(bufferer, cudfSchema, NoopMetric,
+      opts, "JSON", null)
+    table.close()
+    // We don't have any good way to verify that the retry was thrown, but we are going to trust
+    // that it was.
+  }
 }
