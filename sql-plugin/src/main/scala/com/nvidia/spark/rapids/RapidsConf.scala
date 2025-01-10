6d888074f95cbc9e45e1af002361e9004b804be5
Hongbin Ma (Mahone) <mahongbin@apache.org>
2025-01-10 08:20:10 +0800

    make maxCpuBatchSize in GpuPartitioning configurable (#11929)
    This PR closes https://github.com/NVIDIA/spark-rapids/issues/11928
    
    <!--
    
    Thank you for contributing to RAPIDS Accelerator for Apache Spark!
    
    Here are some guidelines to help the review process go smoothly.
    
    1. Please write a description in this text box of the changes that are
    being
       made.
    
    2. Please ensure that you have written units tests for the changes
    made/features
       added.
    
    3. If you are closing an issue please use one of the automatic closing
    words as
    noted here:
    https://help.github.com/articles/closing-issues-using-keywords/
    
    4. If your pull request is not ready for review but you want to make use
    of the
    continuous integration testing facilities please label it with `[WIP]`.
    
    5. If your pull request is ready to be reviewed without requiring
    additional
       work on top of it, then remove the `[WIP]` label (if present).
    
    6. Once all work has been done and review has taken place please do not
    add
    features or make changes out of the scope of those requested by the
    reviewer
    (doing this just add delays as already reviewed code ends up having to
    be
    re-reviewed/it is hard to tell what is new etc!). Further, please avoid
    rebasing your branch during the review process, as this causes the
    context
    of any comments made by reviewers to be lost. If conflicts occur during
    review then they should be resolved by merging into the branch used for
       making the pull request.
    
    Many thanks in advance for your cooperation!
    
    -->
    
    ---------
    
    Signed-off-by: Hongbin Ma (Mahone) <mahongbin@apache.org>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
@@ -1976,6 +1976,20 @@ val SHUFFLE_COMPRESSION_LZ4_CHUNK_SIZE = conf("spark.rapids.shuffle.compression.
       .integerConf
       .createWithDefault(20)
 
+  val SHUFFLE_PARTITIONING_MAX_CPU_BATCH_SIZE =
+    conf("spark.rapids.shuffle.partitioning.maxCpuBatchSize")
+      .doc("The maximum size of a sliced batch output to the CPU side " +
+        "when GPU partitioning shuffle data. This can be used to limit the peak on-heap memory " +
+        "used by CPU to serialize the shuffle data, especially for skew data cases. " +
+        "The default value is maximum size of an Array minus 2k overhead (2147483639L - 2048L), " +
+        "user should only set a smaller value than default value to avoid subsequent failures.")
+      .internal()
+      .bytesConf(ByteUnit.BYTE)
+      .checkValue(v => v > 0 && v <= 2147483639L - 2048L,
+        s"maxCpuBatchSize must be positive and not exceed ${2147483639L - 2048L} bytes.")
+      // The maximum size of an Array minus a bit for overhead for metadata
+      .createWithDefault(2147483639L - 2048L)
+
   val SHUFFLE_MULTITHREADED_READER_THREADS =
     conf("spark.rapids.shuffle.multiThreaded.reader.threads")
         .doc("The number of threads to use for reading shuffle blocks per executor in the " +
@@ -3072,6 +3086,8 @@ class RapidsConf(conf: Map[String, String]) extends Logging {
   lazy val shuffleMultiThreadedWriterThreads: Int = get(SHUFFLE_MULTITHREADED_WRITER_THREADS)
 
   lazy val shuffleMultiThreadedReaderThreads: Int = get(SHUFFLE_MULTITHREADED_READER_THREADS)
+
+  lazy val shuffleParitioningMaxCpuBatchSize: Long = get(SHUFFLE_PARTITIONING_MAX_CPU_BATCH_SIZE)
 
   lazy val shuffleKudoSerializerEnabled: Boolean = get(SHUFFLE_KUDO_SERIALIZER_ENABLED)
 
