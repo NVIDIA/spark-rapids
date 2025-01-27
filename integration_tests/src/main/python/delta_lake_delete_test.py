9bc70251c715a693e0e79a4926470a8b430452ef
Raza Jafri <raza.jafri@gmail.com>
2025-01-27 11:04:20 -0800

    reverted unnecessary change

--- integration_tests/src/main/python/delta_lake_delete_test.py
+++ integration_tests/src/main/python/delta_lake_delete_test.py
@@ -86,6 +86,7 @@ def test_delta_delete_disabled_fallback(spark_tmp_path, disable_conf):
     assert_gpu_fallback_write(write_func, read_delta_path, data_path,
                               "ExecutedCommandExec", disable_conf)
 
+@allow_non_gpu("ExecutedCommandExec", *delta_meta_allow)
 @delta_lake
 @ignore_order
 @pytest.mark.parametrize("use_cdf", [True, False], ids=idfn)
