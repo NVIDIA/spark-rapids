b1901ab5dcd489674438551fcffcd44bc90be376
Niranjan Artal <nartal@nvidia.com>
2025-02-03 23:19:18 -0800

    Relax decimal metadata checks for mismatched precision/scale
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuCast.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuCast.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2024, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1616,7 +1616,7 @@ object GpuCast {
     }
   }
 
-  private def castDecimalToDecimal(
+  def castDecimalToDecimal(
       input: ColumnView,
       from: DecimalType,
       to: DecimalType,
