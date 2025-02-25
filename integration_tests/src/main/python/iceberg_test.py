21909feb9f9eae60feb51756f558fd9809873fc9
liurenjie1024 <liurenjie2008@gmail.com>
2025-02-25 16:14:25 +0800

    Skip iceberg tests

--- integration_tests/src/main/python/iceberg_test.py
+++ integration_tests/src/main/python/iceberg_test.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2023, NVIDIA CORPORATION.
+# Copyright (c) 2022-2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -37,6 +37,7 @@ iceberg_gens_list = [
 
 rapids_reader_types = ['PERFILE', 'MULTITHREADED', 'COALESCING']
 
+pytestmark = pytest.mark.skip(reason="Skipping all iceberg tests as it's under refactoring")
 
 @allow_non_gpu("BatchScanExec")
 @iceberg
