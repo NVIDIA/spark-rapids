f87d744bebb4d77f4dbdd9f723edfb72fe9734f0
sperlingxx <lovedreamf@gmail.com>
2025-03-05 16:31:13 +0900

    fix
    Signed-off-by: sperlingxx <lovedreamf@gmail.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/execution/GpuBroadcastNestedLoopJoinExecBase.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/execution/GpuBroadcastNestedLoopJoinExecBase.scala
@@ -793,7 +793,7 @@ abstract class GpuBroadcastNestedLoopJoinExecBase(
           Some(buildTime),
           Some(buildDataSize))
       }
-      val prjBuildBatch = postProjection.map(_(buildBatch)).getOrElse(buildBatch)
+      val prjBuildBatch = postProjectionAndClose.map(_(buildBatch)).getOrElse(buildBatch)
 
       val lazyStream = bufferedStream.map { cb =>
         withResource(cb) { cb =>
