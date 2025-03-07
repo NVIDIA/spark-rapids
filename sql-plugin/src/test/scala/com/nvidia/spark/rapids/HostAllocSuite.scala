b7d957be5a978bc10ebd39d1440206911fb2a19a
Hongbin Ma (Mahone) <mahongbin@apache.org>
2025-03-07 10:09:07 +0800

    further reduce size
    Signed-off-by: Hongbin Ma (Mahone) <mahongbin@apache.org>

--- sql-plugin/src/test/scala/com/nvidia/spark/rapids/HostAllocSuite.scala
+++ sql-plugin/src/test/scala/com/nvidia/spark/rapids/HostAllocSuite.scala
@@ -349,9 +349,9 @@ class HostAllocSuite extends AnyFunSuite with BeforeAndAfterEach with
       // put RMM back for other tests to use
       Rmm.initialize(RmmAllocationMode.CUDA_DEFAULT, null, 512 * 1024 * 1024)
     }
-    // a little less than 1 GiB, see more background at:
+    // less than 1 GiB, see more background at:
     // https://github.com/NVIDIA/spark-rapids/issues/12194#issuecomment-2703186601
-    PinnedMemoryPool.initialize(900 * 1024 * 1024)
+    PinnedMemoryPool.initialize(500 * 1024 * 1024)
     HostAlloc.initialize(-1)
   }
 
