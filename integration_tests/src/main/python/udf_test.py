117b11f3b3b22fc76acd2b9d2ecaec9460d2cb67
Firestarman <firestarmanllc@gmail.com>
2025-04-07 11:21:18 +0800

    Fix some udf failures on DB14.3
    Signed-off-by: Firestarman <firestarmanllc@gmail.com>

--- integration_tests/src/main/python/udf_test.py
+++ integration_tests/src/main/python/udf_test.py
@@ -16,7 +16,8 @@ import pytest
 from pyspark import BarrierTaskContext, TaskContext
 
 from conftest import is_at_least_precommit_run, is_databricks_runtime
-from spark_session import (is_before_spark_330, is_before_spark_331, is_before_spark_350, is_spark_341,
+from spark_session import (is_before_spark_330, is_before_spark_331, is_before_spark_350,
+                           is_databricks133_or_later, is_databricks143_or_later,
                            is_spark_400_or_later)
 
 from pyspark.sql.pandas.utils import require_minimum_pyarrow_version, require_minimum_pandas_version
@@ -176,8 +177,10 @@ pre_cur_win = Window\
 low_upper_win = Window.partitionBy('a').orderBy('b').rowsBetween(-3, 3)
 
 running_win_param = pytest.param(pre_cur_win, marks=pytest.mark.xfail(
-    condition=is_databricks_runtime() and is_spark_341(),
-    reason='DB13.3 wrongly uses RunningWindowFunctionExec to evaluate a PythonUDAF and it will fail even on CPU'))
+    condition=is_databricks133_or_later(),
+    reason="DB13.3 and 14.3 wrongly use RunningWindowFunctionExec to evaluate a PythonUDAF \
+and it will fail even on CPU"))
+
 udf_windows = [no_part_win, unbounded_win, cur_follow_win, running_win_param, low_upper_win]
 window_ids = ['No_Partition', 'Unbounded', 'Unbounded_Following', 'Unbounded_Preceding',
               'Lower_Upper']
@@ -393,7 +396,9 @@ def test_map_arrow_apply_udf(data_gen):
         conf=conf)
 
 
-map_in_pandas_node_name = 'MapInArrowExec' if is_spark_400_or_later() else 'PythonMapInArrowExec'
+map_in_pandas_node_name = 'MapInArrowExec' if is_spark_400_or_later() or is_databricks143_or_later() \
+    else 'PythonMapInArrowExec'
+
 @pytest.mark.parametrize('data_type', ['string', 'binary'], ids=idfn)
 @allow_non_gpu(map_in_pandas_node_name)
 @pytest.mark.skipif(is_before_spark_350(), reason='spark.sql.execution.arrow.useLargeVarTypes is introduced in Pyspark 3.5.0')
@@ -453,11 +458,14 @@ def test_map_in_pandas_with_barrier_mode(is_barrier):
                     reason='mapInArrow with barrier mode is introduced by Pyspark 3.5.0')
 @pytest.mark.parametrize('is_barrier', [True, False], ids=idfn)
 def test_map_in_arrow_with_barrier_mode(is_barrier):
+    # The "tc" here can be either a BarrierTaskContext or TaskContext on DB14.3,
+    # not sure any special change made by DB.
+    tc_types = (BarrierTaskContext, TaskContext) if is_databricks143_or_later() else BarrierTaskContext
     def func(iterator):
         tc = TaskContext.get()
         assert tc is not None
         if is_barrier:
-            assert isinstance(tc, BarrierTaskContext)
+            assert isinstance(tc, tc_types)
         else:
             assert not isinstance(tc, BarrierTaskContext)
 
