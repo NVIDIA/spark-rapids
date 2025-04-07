5dc5819cc3f5e1c2e119e23e54e1e7eab0dd5219
Jihoon Son <ghoonson@gmail.com>
2025-04-07 13:52:11 -0700

    Compute the actual max wait time and use it to compare with the max metric
    Signed-off-by: Jihoon Son <ghoonson@gmail.com>

--- sql-plugin/src/test/scala/com/nvidia/spark/rapids/io/async/ThrottlingExecutorSuite.scala
+++ sql-plugin/src/test/scala/com/nvidia/spark/rapids/io/async/ThrottlingExecutorSuite.scala
@@ -159,7 +159,7 @@ class ThrottlingExecutorSuite extends AnyFunSuite with BeforeAndAfterEach {
     assertCause(e2, classOf[RejectedExecutionException])
   }
 
-  ignore("test task metrics: https://github.com/NVIDIA/spark-rapids/issues/12410") {
+  test("task metrics") {
     val exec = Executors.newSingleThreadExecutor()
     // Run a task. Note that the first task never waits in ThrottlingExecutor.
     var runningTask = new TestTask
@@ -167,6 +167,7 @@ class ThrottlingExecutorSuite extends AnyFunSuite with BeforeAndAfterEach {
       override def run(): Unit = executor.submit(runningTask, 100)
     })
     var taskCount = 1
+    var actualMaxThrottleTimeNs = 0L
 
     for (i <- 0 to 9) {
       val sleepTimeMs = (i + 1) * 10L
@@ -195,11 +196,12 @@ class ThrottlingExecutorSuite extends AnyFunSuite with BeforeAndAfterEach {
       runningTask.latch.countDown()
       // Wait until the waitingTask is submitted.
       waitingTaskSubmitted.await(longTimeoutSec, TimeUnit.SECONDS)
+      actualMaxThrottleTimeNs = math.max(actualMaxThrottleTimeNs, actualWaitTimeNs.get())
       executor.updateMetrics()
 
       // Skip the check on the min throttle time as the first task never waits.
 
-      assert(actualWaitTimeNs.get() >=
+      assert(actualMaxThrottleTimeNs >=
         taskMetrics(GpuWriteJobStatsTracker.ASYNC_WRITE_MAX_THROTTLE_TIME_KEY).value
       )
 
