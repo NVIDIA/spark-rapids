f5fec913406c3db1ecc26b6b00764c5e18698b0b
Hongbin Ma (Mahone) <mahongbin@apache.org>
2025-04-21 09:22:58 +0800

    by default enable skip agg
    Signed-off-by: Hongbin Ma (Mahone) <mahongbin@apache.org>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
@@ -1606,7 +1606,7 @@ val GPU_COREDUMP_PIPE_PATTERN = conf("spark.rapids.gpu.coreDump.pipePattern")
         "Setting this to 1 essentially disables this feature.")
     .doubleConf
     .checkValue(v => v >= 0 && v <= 1, "The ratio value must be in [0, 1].")
-    .createWithDefault(1.0)
+    .createWithDefault(0.85)
 
   val FORCE_SINGLE_PASS_PARTIAL_SORT_AGG: ConfEntryWithDefault[Boolean] =
     conf("spark.rapids.sql.agg.forceSinglePassPartialSort")
