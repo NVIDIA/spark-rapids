6fde79595610af8751ea91e28be0f72c22bc9dfc
Raza Jafri <raza.jafri@gmail.com>
2025-05-05 13:48:41 -0700

    Skip the test for Spark without Deletion vectors

--- integration_tests/src/main/python/delta_lake_test.py
+++ integration_tests/src/main/python/delta_lake_test.py
@@ -125,6 +125,8 @@ def test_delta_read_column_mapping(spark_tmp_path, reader_confs, mapping, enable
     assert_gpu_and_cpu_are_equal_collect(lambda spark: spark.read.format("delta").load(data_path),
                                          conf=confs)
 
+
+@pytest.mark.skipif(not supports_delta_lake_deletion_vectors(), reason="This test only applies to deletion vectors")
 @allow_non_gpu('FileSourceScanExec')
 @delta_lake 
 def test_delta_read_with_deletion_vectors_enabled(spark_tmp_path):
@@ -133,8 +135,7 @@ def test_delta_read_with_deletion_vectors_enabled(spark_tmp_path):
     delta_conf = {"spark.rapids.sql.detectDeltaLogQueries": "false"}
     def create_delta(spark):
         df = gen_df(spark, gen_list).write.format("delta")
-        if supports_delta_lake_deletion_vectors():
-            df.option("delta.enableDeletionVectors", "true")
+        df.option("delta.enableDeletionVectors", "true")
         df.save(data_path)
 
     def read_delta_sql(data_path):
