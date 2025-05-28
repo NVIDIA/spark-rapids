27d454774ef4a5bc97768a231f89cff8d27a650a
liurenjie1024 <liurenjie2008@gmail.com>
2025-05-28 11:18:57 +0800

    Fix

--- tests/src/test/spark350/scala/com/nvidia/spark/rapids/iceberg/data/GpuDeleteFilterSuite.scala
+++ tests/src/test/spark350/scala/com/nvidia/spark/rapids/iceberg/data/GpuDeleteFilterSuite.scala
@@ -406,7 +406,7 @@ private object TestGpuDeleteLoader {
   def valueOf(col: HostColumnVectorCore, rowIdx: Int): AnyRef = {
     col.getType.getTypeId match {
       case DType.DTypeEnum.DECIMAL128 => col.getBigDecimal(rowIdx)
-      case _ => HOST_COL_GET_ELEMENT.invoke(col, rowIdx)
+      case _ => HOST_COL_GET_ELEMENT.invoke(col, rowIdx).asInstanceOf[AnyRef]
     }
   }
 
