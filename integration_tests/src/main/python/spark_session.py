fb0f22902daf04be5c2bdcdfabd0fd2856c1e193
Chong Gao <chongg@nvidia.com>
2025-05-31 07:45:41 +0800

    Fully support casting string with timezone to timestamp [databricks] (#12598)
    contributes to:
    * https://github.com/NVIDIA/spark-rapids-jni/issues/1654
    * https://github.com/NVIDIA/spark-rapids/issues/9653
    
    closes #11556
    
    
    Depends on:
    * https://github.com/NVIDIA/spark-rapids-jni/pull/3174
    * https://github.com/NVIDIA/spark-rapids-jni/pull/3333 
    
    Signed-off-by: Chong Gao <res_life@163.com>
    
    ---------
    
    Signed-off-by: Chong Gao <res_life@163.com>
    Co-authored-by: Chong Gao <res_life@163.com>

--- integration_tests/src/main/python/spark_session.py
+++ integration_tests/src/main/python/spark_session.py
@@ -47,7 +47,6 @@ _default_conf = {
     'spark.rapids.sql.castFloatToIntegralTypes.enabled': 'false',
     'spark.rapids.sql.castFloatToString.enabled': 'false',
     'spark.rapids.sql.castStringToFloat.enabled': 'false',
-    'spark.rapids.sql.castStringToTimestamp.enabled': 'false',
     'spark.rapids.sql.fast.sample': 'false',
     'spark.rapids.sql.hasExtendedYearValues': 'true',
     'spark.rapids.sql.hashOptimizeSort.enabled': 'false',
@@ -177,6 +176,9 @@ def is_before_spark_314():
 
 def is_before_spark_320():
     return spark_version() < "3.2.0"
+
+def is_before_spark_321():
+    return spark_version() < "3.2.1"
 
 def is_before_spark_322():
     return spark_version() < "3.2.2"
