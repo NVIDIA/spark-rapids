a628767ecf1786cf29508a3995a59134e245beea
Ray Liu <liurenjie2008@gmail.com>
2025-06-03 15:46:02 +0800

    Iceberg move out

--- integration_tests/src/main/python/iceberg.py
+++ integration_tests/src/main/python/iceberg.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This file provides some utility methods for iceberg tests.
+
 import os
 import tempfile
 from itertools import combinations
