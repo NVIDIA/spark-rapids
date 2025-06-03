10031217c47a32fdf310c34705e6a432e4fcef2f
Firestarman <firestarmanllc@gmail.com>
2025-06-03 10:50:05 +0800

    Add the NV license to relevant py files
    Signed-off-by: Firestarman <firestarmanllc@gmail.com>

--- python/rapids/daemon_databricks.py
+++ python/rapids/daemon_databricks.py
@@ -1,4 +1,3 @@
-# Copyright (c) 2024-2025, NVIDIA CORPORATION.
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
@@ -15,6 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+#
+# NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+# property and proprietary rights in and to this material, related
+# documentation and any modifications thereto. Any use, reproduction,
+# disclosure or distribution of this material and related documentation
+# without an express license agreement from NVIDIA CORPORATION or
+# its affiliates is strictly prohibited.
 
 import os
 import signal
