2d39d69a3a013928b0259e29b24f94d0fb1d4102
Yinqing Hao <haoyinqing@gmail.com>
2025-06-30 14:56:26 +0800

    Remove redundant tests to accelerate premerge CI
    Signed-off-by: Yinqing Hao <haoyinqing@gmail.com>

--- jenkins/test-timezones.sh
+++ jenkins/test-timezones.sh
@@ -22,9 +22,4 @@ set -ex
 export time_zones_test_cases=(
   "Asia/Shanghai"   # CST
   "America/New_York"   # PST
-  "Asia/Hebron"   # contains the most zone transitions
-  "Canada/Newfoundland"   # contains may zone transitions as well
-  "America/Belize"   # contains the most intra-hour zone transitions
-  "America/Punta_Arenas"   # contains the most intra-minute zone transitions
-  "Africa/Casablanca"   # frequently changing transition rules between the release of Java8 and Java17
 )
