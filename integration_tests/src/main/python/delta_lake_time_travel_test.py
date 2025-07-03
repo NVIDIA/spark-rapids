8fe1a67143a91a6f6307b97b04278e0899a5d31e
liurenjie1024 <liurenjie2008@gmail.com>
2025-07-03 15:43:56 +0800

    Better name

--- integration_tests/src/main/python/delta_lake_time_travel_test.py
+++ integration_tests/src/main/python/delta_lake_time_travel_test.py
@@ -25,6 +25,8 @@ from spark_session import with_gpu_session
 from delta.tables import *
 
 
+in_commit_ts_param_id = functools.partial(idfn_with_name, name="in_commit_ts")
+
 @delta_lake
 def test_time_travel_on_non_existing_table():
     def time_travel_on_non_existing_table():
@@ -125,7 +127,7 @@ def test_time_travel_df_timestamp(spark_tmp_path, spark_tmp_table_factory, in_co
 @delta_lake
 @ignore_order(local=True)
 @pytest.mark.parametrize("in_commit_ts", enable_in_commit_ts(),
-                         ids=functools.partial(idfn_with_name, name="in_commit_ts"))
+                         ids=in_comit_ts_param_id)
 def test_time_travel_sql_timestamp(spark_tmp_path, spark_tmp_table_factory, in_commit_ts):
     table_path = do_set_up_tables_for_time_travel(spark_tmp_path, spark_tmp_table_factory,
                                                   in_commit_ts,
