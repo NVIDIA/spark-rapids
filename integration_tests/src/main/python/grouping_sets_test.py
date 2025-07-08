7cc1cabf3b7e0795e9b46032ad1b43c116a78332
Niranjan Artal <50492963+nartal1@users.noreply.github.com>
2025-07-08 09:42:43 -0700

    Spark-4.0: Enable ansi mode for some of the integration tests [databricks] (#13043)
    This PR contributes to
    https://github.com/NVIDIA/spark-rapids/issues/11106
    
    After revisiting all the tests with disable_ansi_mode label, the tests
    in this PR can be enabled to run with ansi enabled for Spark-4.0. Most
    of the tests were disabled due to lack of ansi support for SUM
    aggregation. It was added recently and the tests pass now.
    
    For `test_many_column_project` in parquet_test.py:
    - The ProjectExec would fallback to CPU if the dataframe is read from
    Scan ExistingRDD, so added allow_non_gpu for that test.
    
    
    Testing:
    
    Ran the integration tests on each file in this PR and there are no
    failures:
     `./integration_tests/run_pyspark_from_build.sh -k '<PYTHON_FILE_NAME>`
    
    ---------
    
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- integration_tests/src/main/python/grouping_sets_test.py
+++ integration_tests/src/main/python/grouping_sets_test.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020-2024, NVIDIA CORPORATION.
+# Copyright (c) 2020-2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -42,7 +42,6 @@ _grouping_set_sqls = [
 ]
 
 
-@disable_ansi_mode  # https://github.com/NVIDIA/spark-rapids/issues/5114
 # test nested syntax of grouping set, rollup and cube
 @ignore_order
 @pytest.mark.parametrize('data_gen', [_grouping_set_gen], ids=idfn)
