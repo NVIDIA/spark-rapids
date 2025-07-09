a8730a172cdf76326eb7cbf0493d538f31f149a5
Jihoon Son <ghoonson@gmail.com>
2025-07-08 17:31:14 -0700

    Add write round trip test for managed delta tables
    Signed-off-by: Jihoon Son <ghoonson@gmail.com>

--- integration_tests/src/main/python/delta_lake_write_test.py
+++ integration_tests/src/main/python/delta_lake_write_test.py
@@ -98,6 +98,31 @@ def test_delta_write_disabled_fallback(spark_tmp_path, disable_conf, enable_dele
         delta_write_fallback_check,
         conf=copy_and_update(writer_confs, disable_conf))
 
+
+@allow_non_gpu("AppendDataExecV1", *delta_meta_allow)
+@delta_lake
+@ignore_order(local=True)
+@pytest.mark.parametrize("enable_deletion_vectors", deletion_vector_values_with_350DB143_xfail_reasons(
+    enabled_xfail_reason="https://github.com/NVIDIA/spark-rapids/issues/12027"), ids=idfn)
+def test_delta_write_round_trip_managed(spark_tmp_table_factory, enable_deletion_vectors):
+    gen_list = [("c" + str(i), gen) for i, gen in enumerate(delta_write_gens)]
+    conf = copy_and_update(writer_confs, delta_writes_enabled_conf)
+    (cpu_table, gpu_table) = assert_gpu_and_cpu_save_as_table_are_equal_collect(
+        spark_tmp_table_factory,
+        lambda spark, table: get_writer_with_deletion_vector_property_set(
+            gen_df(spark, gen_list).coalesce(1).write.format("delta"), enable_deletion_vectors)
+            .saveAsTable(table),
+        conf=conf
+    )
+    cols = ["version", "userId", "userName", "operation", "operationParameters", "job", "notebook",
+            "clusterId", "readVersion", "isolationLevel", "isBlindAppend", "operationMetrics", "userMetadata"]
+    cpu_history = with_cpu_session(lambda spark: spark.sql("DESCRIBE HISTORY {}".format(cpu_table))
+                                   .select(cols).collect(), conf=conf)
+    gpu_history = with_cpu_session(lambda spark: spark.sql("DESCRIBE HISTORY {}".format(gpu_table))
+                                   .select(cols).collect(), conf=conf)
+    assert_equal(cpu_history, gpu_history)
+
+
 @allow_non_gpu(*delta_meta_allow)
 @delta_lake
 @ignore_order(local=True)
