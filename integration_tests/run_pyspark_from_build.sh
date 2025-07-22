a3c7abb9036395adec5efee37aadc3b632dc0f14
Yanxuan Liu <yanxuanl@nvidia.com>
2025-07-22 12:14:11 +0800

    fix issue
    Signed-off-by: Yanxuan Liu <yanxuanl@nvidia.com>

--- integration_tests/run_pyspark_from_build.sh
+++ integration_tests/run_pyspark_from_build.sh
@@ -263,7 +263,7 @@ else
     if [[ "${TESTS}" == "" ]]; then
         RUN_TESTS_COMMAND+=("${LOCAL_ROOTDIR}/src/main/python")
     else
-        read -ar RAW_TESTS <<< "${TESTS}"
+        read -ra RAW_TESTS <<< "${TESTS}"
         for raw_test in "${RAW_TESTS[@]}"; do
             RUN_TESTS_COMMAND+=("${LOCAL_ROOTDIR}/src/main/python/${raw_test}")
         done
@@ -276,9 +276,9 @@ else
           "$TEST_TAGS"
           --std_input_path="$STD_INPUT_PATH"
           --color=yes
-          $TEST_TYPE_PARAM
+          "$TEST_TYPE_PARAM"
           "$TEST_ARGS"
-          $RUN_TEST_PARAMS
+          "$RUN_TEST_PARAMS"
           --junitxml=TEST-pytest-`date +%s%N`.xml
           "$@")
 
@@ -353,7 +353,7 @@ else
         # We are limiting the number of tasks in local mode to 4 because it helps to reduce the
         # total memory usage, especially host memory usage because when copying data to the GPU
         # buffers as large as batchSizeBytes can be allocated, and the fewer of them we have the better.
-        LOCAL_PARALLEL=$(( $CPU_CORES > 4 ? 4 : $CPU_CORES ))
+        LOCAL_PARALLEL=$(( CPU_CORES > 4 ? 4 : CPU_CORES ))
         export PYSP_TEST_spark_master="local[$LOCAL_PARALLEL,$SPARK_TASK_MAXFAILURES]"
       fi
     fi
