cdb7f730f2315068fc5407c2b8ce77ae5bd3557a
Chong Gao <res_life@163.com>
2025-08-01 11:17:08 +0800

    Implements scalar * scalar overload function
    Signed-off-by: Chong Gao <res_life@163.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/arithmetic.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/arithmetic.scala
@@ -802,7 +802,9 @@ case class GpuMultiply(
   }
 
   override def doColumnar(numRows: Int, lhs: GpuScalar, rhs: GpuScalar): ColumnVector = {
-    throw new RuntimeException("Error in multiplication: Spark already did the constant folding")
+    withResource(GpuColumnVector.from(lhs, numRows, lhs.dataType)) { lhs_cv =>
+      doColumnar(lhs_cv, rhs)
+    }
   }
 }
 
