7f1c11173313489f3e1223217b07265b4b8ded07
Prabhjyot Singh <prabhjyotsingh@gmail.com>
2025-08-15 12:50:46 -0400

    decompose AcceldataShimVersion into proper version
    Signed-off-by: Prabhjyot Singh <prabhjyotsingh@gmail.com>

--- sql-plugin-api/src/main/scala/com/nvidia/spark/rapids/SparkShimVersion.scala
+++ sql-plugin-api/src/main/scala/com/nvidia/spark/rapids/SparkShimVersion.scala
@@ -29,7 +29,7 @@ case class ClouderaShimVersion(major: Int, minor: Int, patch: Int, clouderaVersi
 
 case class AcceldataShimVersion(major: Int, minor: Int, patch: Int, acceldataVersion: String)
   extends ShimVersion {
-  override def toString(): String = s"$major.$minor.$patch.$acceldataVersion"
+  override def toString(): String = s"$major.$minor.$patch-acceldata-$acceldataVersion"
 }
 
 case class DatabricksShimVersion(
