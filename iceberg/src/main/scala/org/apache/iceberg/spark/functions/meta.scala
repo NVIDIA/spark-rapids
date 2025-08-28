82c564b13db60213141663bbfaad6feff72b4578
liurenjie1024 <liurenjie2008@gmail.com>
2025-08-28 10:02:05 +0800

    Fix expr conversion

--- iceberg/src/main/scala/org/apache/iceberg/spark/functions/meta.scala
+++ iceberg/src/main/scala/org/apache/iceberg/spark/functions/meta.scala
@@ -1,14 +1,13 @@
 package org.apache.iceberg.spark.functions
 
-import com.nvidia.spark.rapids.{BaseExprMeta, DataFromReplacementRule, RapidsConf, RapidsMeta}
+import com.nvidia.spark.rapids.{DataFromReplacementRule, ExprMeta, GpuExpression, RapidsConf, RapidsMeta}
 
-import org.apache.spark.sql.catalyst.expressions.Expression
 import org.apache.spark.sql.catalyst.expressions.objects.StaticInvoke
 
 class GpuStaticInvokeMeta(expr: StaticInvoke,
   conf: RapidsConf,
   parent: Option[RapidsMeta[_, _, _]],
-  rule: DataFromReplacementRule) extends BaseExprMeta[StaticInvoke](expr, conf, parent, rule) {
+  rule: DataFromReplacementRule) extends ExprMeta[StaticInvoke](expr, conf, parent, rule) {
 
   override def tagExprForGpu(): Unit = {
     if (expr.staticObject != classOf[BucketFunction.BucketInt] &&
@@ -17,16 +16,8 @@ class GpuStaticInvokeMeta(expr: StaticInvoke,
     }
   }
 
-  override def convertToGpu(): Expression = {
-    expr.staticObject match {
-      case cls if cls == classOf[BucketFunction.BucketInt] =>
-        GpuBucketExpression(expr.arguments.head, expr.arguments(1))
-      case cls if cls == classOf[BucketFunction.BucketLong] =>
-        GpuBucketExpression(expr.arguments.head, expr.arguments(1))
-      case _ =>
-        throw new IllegalStateException(
-          s"only BucketFunction.BucketInt and BucketFunction.BucketLong are supported")
-    }
+  override def convertToGpu(): GpuExpression = {
+    val Seq(left, right) = childExprs.map(_.convertToGpu())
+    GpuBucketExpression(left, right)
   }
 }
-
