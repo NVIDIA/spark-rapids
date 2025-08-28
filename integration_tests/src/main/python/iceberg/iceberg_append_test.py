791caeac0c42e3202dc54dd55b849bdcd7347b94
liurenjie1024 <liurenjie2008@gmail.com>
2025-08-28 11:51:57 +0800

    fix test

--- integration_tests/src/main/python/iceberg/iceberg_append_test.py
+++ integration_tests/src/main/python/iceberg/iceberg_append_test.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Dict, Callable, Any
+
 import pytest
 
 from asserts import assert_equal
@@ -21,21 +23,14 @@ from marks import iceberg, ignore_order
 from spark_session import with_gpu_session, with_cpu_session
 
 
-@iceberg
-@ignore_order(local=True)
-@pytest.mark.parametrize("format_version", ["1", "2"], ids=lambda x: f"format_version={x}")
-@pytest.mark.parametrize("write_distribution_mode", ["none", "hash", "range"],
-                         ids=lambda x: f"write_distribution_mode={x}")
-def test_insert_into_unpartitioned_table_all_cols(spark_tmp_table_factory, format_version, write_distribution_mode):
+def do_test_insert_into_table_sql(spark_tmp_table_factory,
+                                  create_table_func: Callable[[str], Any]):
     base_table_name = get_full_table_name(spark_tmp_table_factory)
     cpu_table_name = f"{base_table_name}_cpu"
     gpu_table_name = f"{base_table_name}_gpu"
 
-    table_prop = {"format-version": format_version,
-                  "write.distribution-mode": write_distribution_mode}
-
-    create_iceberg_table(cpu_table_name, table_prop=table_prop)
-    create_iceberg_table(gpu_table_name, table_prop=table_prop)
+    create_table_func(cpu_table_name)
+    create_table_func(gpu_table_name)
 
     def insert_data(spark, table_name: str):
         df = gen_df(spark, list(zip(iceberg_base_table_cols, iceberg_gens_list)))
@@ -58,34 +53,32 @@ def test_insert_into_unpartitioned_table_all_cols(spark_tmp_table_factory, forma
 @iceberg
 @ignore_order(local=True)
 @pytest.mark.parametrize("format_version", ["1", "2"], ids=lambda x: f"format_version={x}")
+@pytest.mark.parametrize("write_distribution_mode", ["none", "hash", "range"],
+                         ids=lambda x: f"write_distribution_mode={x}")
+def test_insert_into_unpartitioned_table_all_cols(spark_tmp_table_factory, format_version, write_distribution_mode):
+    table_prop = {"format-version": format_version,
+                  "write.distribution-mode": write_distribution_mode}
+
+    do_test_insert_into_table_sql(
+        spark_tmp_table_factory,
+        lambda table_name: create_iceberg_table(table_name, table_prop=table_prop))
+
+
+@iceberg
+@ignore_order(local=True)
+@pytest.mark.parametrize("format_version", ["1", "2"], ids=lambda x: f"format_version={x}")
 @pytest.mark.parametrize("fanout", [True, False], ids=lambda x: f"fanout={x}")
 @pytest.mark.parametrize("write_distribution_mode", ["none", "hash", "range"],
                          ids=lambda x: f"write_distribution_mode={x}")
 def test_insert_into_partitioned_table_all_cols(spark_tmp_table_factory, format_version, fanout, write_distribution_mode):
-    base_table_name = get_full_table_name(spark_tmp_table_factory)
-    cpu_table_name = f"{base_table_name}_cpu"
-    gpu_table_name = f"{base_table_name}_gpu"
-
     table_prop = {"format-version": format_version,
                    "write.spark.fanout.enabled": str(fanout).lower(),
                   "write.distribution-mode": write_distribution_mode}
 
-    create_iceberg_table(cpu_table_name, table_prop=table_prop)
-    create_iceberg_table(gpu_table_name, table_prop=table_prop)
 
-    def insert_data(spark, table_name):
-        df = gen_df(spark, list(zip(iceberg_base_table_cols, iceberg_gens_list)))
-        view_name = spark_tmp_table_factory.get()
-        df.createOrReplaceTempView(view_name)
-        spark.sql(f"INSERT INTO {table_name} SELECT * FROM {view_name}")
-
-    with_gpu_session(lambda spark: insert_data(spark, gpu_table_name),
-                     conf = {"spark.sql.parquet.datetimeRebaseModeInWrite": "CORRECTED",
-                             "spark.sql.parquet.int96RebaseModeInWrite": "CORRECTED"})
-    with_cpu_session(lambda spark: insert_data(spark, cpu_table_name),
-                     conf = {"spark.sql.parquet.datetimeRebaseModeInWrite": "CORRECTED",
-                             "spark.sql.parquet.int96RebaseModeInWrite": "CORRECTED"})
-
-    cpu_data = with_cpu_session(lambda spark: spark.table(cpu_table_name).collect())
-    gpu_data = with_cpu_session(lambda spark: spark.table(gpu_table_name).collect())
-    assert_equal(cpu_data, gpu_data)
+    do_test_insert_into_table_sql(
+        spark_tmp_table_factory,
+        lambda table_name: create_iceberg_table(
+            table_name,
+            partition_col_sql="bucket(16, _c2), bucket(16, _c3)",
+            table_prop=table_prop))
