afd03065b38e69026ca60eb766ff0f8255e3fba4
liurenjie1024 <liurenjie2008@gmail.com>
2025-08-29 11:50:29 +0800

    Gpu iceberg bucket expression
    Signed-off-by: liurenjie1024 <liurenjie2008@gmail.com>

---
+++ iceberg/src/main/scala/org/apache/iceberg/spark/functions/bucket.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.iceberg.spark.functions
+
+import ai.rapids.cudf.{ColumnVector => CudfColumnVector, DType, Scalar}
+import com.nvidia.spark.rapids.{GpuBinaryExpression, GpuColumnVector, GpuExpression, GpuScalar}
+import com.nvidia.spark.rapids.Arm.withResource
+import com.nvidia.spark.rapids.jni.Hash
+import org.apache.iceberg.spark.functions.GpuBucketExpression.cast
+
+import org.apache.spark.sql.catalyst.expressions.Expression
+import org.apache.spark.sql.types.{ByteType, DataType, DataTypes, DateType, IntegerType, LongType, ShortType, TimestampNTZType, TimestampType}
+
+
+case class GpuBucketExpression(numBuckets: GpuExpression, value: GpuExpression)
+  extends GpuBinaryExpression {
+
+  require(numBuckets.dataType == DataTypes.IntegerType,
+    s"numBuckets must be an integer, got ${numBuckets.dataType}")
+
+  require(!value.nullable && GpuBucketExpression.supportedType(value.dataType),
+    s"Bucket function does not support type ${value.dataType} or nullable values")
+
+  override def doColumnar(lhs: GpuColumnVector, rhs: GpuColumnVector): CudfColumnVector = {
+    throw new IllegalStateException("GpuBucketExpression requires first argument to be scalar, " +
+      "second to be column, but both are columns")
+  }
+
+  override def doColumnar(numBuckets: GpuScalar, rhs: GpuColumnVector): CudfColumnVector = {
+    withResource(cast(rhs.getBase)) { rhsLong =>
+      withResource(Hash.murmurHash32(0, Array(rhsLong))) { hash =>
+        withResource(Scalar.fromInt(Integer.MAX_VALUE)) { intMax =>
+          withResource(hash.bitAnd(intMax)) { nonNegativeHash =>
+            nonNegativeHash.mod(numBuckets.getBase, DType.INT32)
+          }
+        }
+      }
+    }
+  }
+
+  override def doColumnar(lhs: GpuColumnVector, rhs: GpuScalar): CudfColumnVector = {
+    throw new IllegalStateException("GpuBucketExpression requires first argument to be scalar, " +
+      "second to be column, but first is column, second is scalar")
+  }
+
+  override def doColumnar(numRows: Int, lhs: GpuScalar, rhs: GpuScalar): CudfColumnVector = {
+    throw new IllegalStateException("GpuBucketExpression requires first argument to be scalar, " +
+      "second to be column, but both are scalars")
+  }
+
+  override def left: Expression = numBuckets
+
+  override def right: Expression = value
+
+  override def dataType: DataType = DataTypes.IntegerType
+}
+
+object GpuBucketExpression {
+  def cast(cv: CudfColumnVector): CudfColumnVector = {
+    cv.getType match {
+      case d if d.isBackedByInt => cv.castTo(DType.INT64)
+      case _ => cv.incRefCount()
+    }
+  }
+
+  def supportedType(dataType: DataType): Boolean = {
+    dataType match {
+      case ByteType |  ShortType | IntegerType | DateType |
+           LongType | TimestampType | TimestampNTZType => true
+      case _ => false
+    }
+  }
+}
