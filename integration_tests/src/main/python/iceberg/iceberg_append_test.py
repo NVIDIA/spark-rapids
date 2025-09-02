a0a7f6731d4b48c1fcc137bbb54669384610a6a2
liurenjie1024 <liurenjie2008@gmail.com>
2025-09-02 10:45:30 +0800

    test

--- integration_tests/src/main/python/iceberg/iceberg_append_test.py
+++ integration_tests/src/main/python/iceberg/iceberg_append_test.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import pytest
 
 from asserts import assert_gpu_sql_fallback_collect
