a948374da3a2de19af383575d351ffac2a060a81
Niranjan Artal <nartal@nvidia.com>
2025-09-18 14:35:53 -0700

    refactor hash expressions
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- sql-plugin/src/main/spark401/scala/com/nvidia/spark/rapids/shims/SparkShims.scala
+++ sql-plugin/src/main/spark401/scala/com/nvidia/spark/rapids/shims/SparkShims.scala
@@ -20,71 +20,26 @@ spark-rapids-shim-json-lines ***/
 package com.nvidia.spark.rapids.shims
 
 import com.nvidia.spark.rapids._
-import com.nvidia.spark.rapids.jni.Hash
+import com.nvidia.spark.rapids.{HashExprChecks, Murmur3BaseExprMeta, XxHash64BaseExprMeta}
 
 import org.apache.spark.sql.catalyst.expressions.{CollationAwareMurmur3Hash, CollationAwareXxHash64, Expression}
-import org.apache.spark.sql.rapids.{GpuMurmur3Hash, GpuXxHash64, XxHash64Utils}
-import org.apache.spark.sql.rapids.execution.TrampolineUtil
-import org.apache.spark.sql.types.{ArrayType, StringType, StructType}
+import org.apache.spark.sql.rapids.{GpuMurmur3Hash, GpuXxHash64}
 
 object SparkShimImpl extends Spark400PlusCommonShims {
   override def getExprs: Map[Class[_ <: Expression], ExprRule[_ <: Expression]] = {
     val shimExprs: Map[Class[_ <: Expression], ExprRule[_ <: Expression]] = Seq(
       GpuOverrides.expr[CollationAwareMurmur3Hash](
         "Collation-aware murmur3 hash operator",
-        ExprChecks.projectOnly(TypeSig.INT, TypeSig.INT,
-          repeatingParamCheck = Some(RepeatingParamCheck("input",
-            (TypeSig.commonCudfTypes + TypeSig.NULL + TypeSig.DECIMAL_128 +
-                TypeSig.STRUCT + TypeSig.ARRAY).nested() +
-                TypeSig.psNote(TypeEnum.ARRAY, "Arrays of structs are not supported"),
-            TypeSig.all))),
-        (a, conf, p, r) => new ExprMeta[CollationAwareMurmur3Hash](a, conf, p, r) {
-          override val childExprs: Seq[BaseExprMeta[_]] = a.children
-            .map(GpuOverrides.wrapExpr(_, this.conf, Some(this)))
-
-          override def tagExprForGpu(): Unit = {
-            val arrayWithStructsHashing = a.children.exists(e =>
-              TrampolineUtil.dataTypeExistsRecursively(e.dataType,
-                {
-                  case ArrayType(_: StructType, _) => true
-                  case _ => false
-                })
-            )
-            if (arrayWithStructsHashing) {
-              willNotWorkOnGpu("hashing arrays with structs is not supported")
-            }
-          }
-
-          override def convertToGpu(): GpuExpression =
-            GpuMurmur3Hash(childExprs.map(_.convertToGpu()), a.seed)
+        HashExprChecks.murmur3ProjectChecks,
+        (a, conf, p, r) => new Murmur3BaseExprMeta[CollationAwareMurmur3Hash](a, conf, p, r) {
+          override protected def seedOf(e: CollationAwareMurmur3Hash): Int = e.seed
         }
       ),
       GpuOverrides.expr[CollationAwareXxHash64](
         "Collation-aware xxhash64 operator",
-        ExprChecks.projectOnly(TypeSig.LONG, TypeSig.LONG,
-          repeatingParamCheck = Some(RepeatingParamCheck("input",
-            XxHash64Shims.supportedTypes, TypeSig.all))),
-        (a, conf, p, r) => new ExprMeta[CollationAwareXxHash64](a, conf, p, r) {
-          override val childExprs: Seq[BaseExprMeta[_]] = a.children
-            .map(GpuOverrides.wrapExpr(_, this.conf, Some(this)))
-
-          override def tagExprForGpu(): Unit = {
-            val maxDepth = a.children.map(
-              c => XxHash64Utils.computeMaxStackSize(c.dataType)).max
-            if (maxDepth > Hash.MAX_STACK_DEPTH) {
-              willNotWorkOnGpu(s"The data type requires a stack depth of $maxDepth, " +
-                s"which exceeds the GPU limit of ${Hash.MAX_STACK_DEPTH}. " +
-                "The algorithm to calculate stack depth: " +
-                "1: Primitive type counts 1 depth; " +
-                "2: Array of Structure counts:  1  + depthOf(Structure); " +
-                "3: Array of Other counts: depthOf(Other); " +
-                "4: Structure counts: 1 + max of depthOf(child); " +
-                "5: Map counts: 2 + max(depthOf(key), depthOf(value)); "
-              )
-            }
-          }
-          override def convertToGpu(): GpuExpression =
-            GpuXxHash64(childExprs.map(_.convertToGpu()), a.seed)
+        HashExprChecks.xxhash64ProjectChecks,
+        (a, conf, p, r) => new XxHash64BaseExprMeta[CollationAwareXxHash64](a, conf, p, r) {
+          override protected def seedOf(e: CollationAwareXxHash64): Long = e.seed
         }
       )
     ).map(r => (r.getClassFor.asSubclass(classOf[Expression]), r)).toMap
