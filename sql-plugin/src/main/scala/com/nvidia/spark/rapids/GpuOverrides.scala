0729c4986d4761c92f409d69311b1507fa686e95
Niranjan Artal <nartal@nvidia.com>
2025-09-22 10:16:18 -0700

    fix build error
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
@@ -3452,11 +3452,11 @@ object GpuOverrides extends Logging {
     expr[Murmur3Hash](
       "Murmur3 hash operator",
       HashExprChecks.murmur3ProjectChecks,
-      Murmur3HashExprMeta),
+      Murmur3HashExprMeta.apply),
     expr[XxHash64](
       "xxhash64 hash operator",
       HashExprChecks.xxhash64ProjectChecks,
-      XxHash64ExprMeta),
+      XxHash64ExprMeta.apply),
     expr[HiveHash](
       "hive hash operator",
       ExprChecks.projectOnly(TypeSig.INT, TypeSig.INT,
