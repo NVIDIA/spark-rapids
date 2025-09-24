d8962fd293811163e818aa484d5aa3892e96468d
Jenkins Automation <70000568+nvauto@users.noreply.github.com>
2025-09-24 21:35:42 +0800

    Update dependency version JNI, private, hybrid to 25.12.0-SNAPSHOT (#13475)
    To fix: https://github.com/NVIDIA/spark-rapids/issues/13468
    
    Wait for the pre-merge CI job to SUCCEED
    
    Signed-off-by: nvauto <70000568+nvauto@users.noreply.github.com>

--- jenkins/databricks/init_cudf_udf.sh
+++ jenkins/databricks/init_cudf_udf.sh
@@ -20,8 +20,7 @@
 
 set -ex
 
-# TODO: https://github.com/NVIDIA/spark-rapids/issues/13468
-CUDF_VER=${CUDF_VER:-25.10}
+CUDF_VER=${CUDF_VER:-25.12}
 CUDA_VER=${CUDA_VER:-12.9}
 
 # Need to explicitly add conda into PATH environment, to activate conda environment.
