4384bf37e2af2be636db72095f952715f2a4f407
liurenjie1024 <liurenjie2008@gmail.com>
2025-09-26 14:07:23 +0800

    Remove test skip

--- integration_tests/src/main/python/iceberg/iceberg_append_test.py
+++ integration_tests/src/main/python/iceberg/iceberg_append_test.py
@@ -23,12 +23,8 @@ from iceberg import create_iceberg_table, iceberg_base_table_cols, iceberg_gens_
 from marks import iceberg, ignore_order, allow_non_gpu
 from spark_session import with_gpu_session, with_cpu_session, is_spark_35x
 
-pytestmark = [
-    pytest.mark.skipif(not is_spark_35x(),
-                       reason="Current spark-rapids only support spark 3.5.x"),
-    pytest.mark.skipif(is_iceberg_remote_catalog(),
-                        reason="https://github.com/NVIDIA/spark-rapids/issues/13471")
-]
+pytestmark = pytest.mark.skipif(not is_spark_35x(),
+                       reason="Current spark-rapids only support spark 3.5.x")
 
 
 def do_test_insert_into_table_sql(spark_tmp_table_factory,
