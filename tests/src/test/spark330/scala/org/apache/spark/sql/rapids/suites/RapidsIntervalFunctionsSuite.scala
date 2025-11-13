44ed0e71510d1a47c4322f54bbc35c20e2bdd486
Allen Xu <wjxiz1992@gmail.com>
2025-11-13 16:23:35 +0800

    Update tests/src/test/spark330/scala/org/apache/spark/sql/rapids/suites/RapidsIntervalFunctionsSuite.scala
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- tests/src/test/spark330/scala/org/apache/spark/sql/rapids/suites/RapidsIntervalFunctionsSuite.scala
+++ tests/src/test/spark330/scala/org/apache/spark/sql/rapids/suites/RapidsIntervalFunctionsSuite.scala
@@ -20,10 +20,10 @@ spark-rapids-shim-json-lines ***/
 package org.apache.spark.sql.rapids.suites
 
 import org.apache.spark.sql.IntervalFunctionsSuite
-import org.apache.spark.sql.rapids.utils.RapidsSQLTestsBaseTrait
+import org.apache.spark.sql.rapids.utils.RapidsSQLTestsTrait
 
 class RapidsIntervalFunctionsSuite
   extends IntervalFunctionsSuite
-  with RapidsSQLTestsBaseTrait {
+  with RapidsSQLTestsTrait {
 }
 
