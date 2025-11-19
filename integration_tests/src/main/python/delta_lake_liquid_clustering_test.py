e35bc223e628ece2df3bc25bc599de6e2e5de004
Jihoon Son <ghoonson@gmail.com>
2025-11-18 18:02:13 -0800

    fix test query

--- integration_tests/src/main/python/delta_lake_liquid_clustering_test.py
+++ integration_tests/src/main/python/delta_lake_liquid_clustering_test.py
@@ -34,14 +34,15 @@ from pyspark.sql.types import StringType, IntegerType
 from asserts import assert_gpu_fallback_write, assert_gpu_and_cpu_writes_are_equal_collect
 from conftest import is_databricks_runtime
 from data_gen import unary_op_df, int_gen, copy_and_update, SetValuesGen, string_gen, long_gen, \
-    gen_df
+    gen_df, append_unique_int_col_to_df
 from delta_lake_delete_test import delta_delete_enabled_conf
 from delta_lake_merge_test import delta_merge_enabled_conf
 from delta_lake_update_test import delta_update_enabled_conf
 from delta_lake_utils import delta_meta_allow, \
     delta_writes_enabled_conf, delta_write_fallback_allow, assert_gpu_and_cpu_delta_logs_equivalent
 from marks import allow_non_gpu, delta_lake, ignore_order, disable_ansi_mode
-from spark_session import is_databricks133_or_later, is_spark_353_or_later, is_spark_356_or_later, with_cpu_session
+from spark_session import is_databricks133_or_later, is_spark_353_or_later, is_spark_356_or_later, \
+    is_before_spark_353, with_cpu_session
 
 
 @allow_non_gpu(*delta_meta_allow)
@@ -81,7 +82,8 @@ def create_clustered_table_sql(spark, table_name, path):
              b string, 
              c string, 
              d int, 
-             e long)
+             e long,
+             unique_int int)
             USING DELTA
             LOCATION '{path}'
             CLUSTER BY (a, b, d)
@@ -95,6 +97,7 @@ def gen_df_and_replace_view(spark, view_name):
                 ("d", SetValuesGen(IntegerType(), [1, 2, 3])),
                 ("e", long_gen)]
     df = gen_df(spark, gen_list)
+    df = append_unique_int_col_to_df(spark, df)
     df.coalesce(1).createOrReplaceTempView(view_name)
 
 
@@ -269,8 +272,7 @@ def test_delta_insert_overwrite_replace_where_sql_liquid_clustering(spark_tmp_pa
 def do_test_delta_dml_sql_liquid_clustering(spark_tmp_path,
                                             spark_tmp_table_factory,
                                             conf: Dict[str, str],
-                                            sql_func: Callable[[str], str],
-                                            expect_fallback):
+                                            sql_func: Callable[[str], str]):
 
     base_data_path = spark_tmp_path + "/DELTA_LIQUID_CLUSTER"
     cpu_data_path = f"{base_data_path}/CPU"
@@ -289,18 +291,11 @@ def do_test_delta_dml_sql_liquid_clustering(spark_tmp_path,
         table_name = cpu_table_name if path == cpu_data_path else gpu_table_name
         spark.sql(sql_func(table_name))
 
-    if expect_fallback:
-        assert_gpu_fallback_write(modify_table,
-                                  lambda spark, path: spark.read.format("delta").load(path),
-                                  base_data_path,
-                                  "ExecutedCommandExec",
-                                  conf=conf)
-    else:
-        assert_gpu_and_cpu_writes_are_equal_collect(
-            modify_table,
-            lambda spark, path: spark.read.format("delta").load(path),
-            base_data_path,
-            conf=conf)
+    assert_gpu_fallback_write(modify_table,
+                              lambda spark, path: spark.read.format("delta").load(path),
+                              base_data_path,
+                              "ExecutedCommandExec",
+                              conf=conf)
 
 @allow_non_gpu(*delta_meta_allow, delta_write_fallback_allow, "CreateTableExec",
                "AppendDataExecV1")
@@ -315,8 +310,7 @@ def test_delta_delete_sql_liquid_clustering_fallback(spark_tmp_path,
 
     do_test_delta_dml_sql_liquid_clustering(
         spark_tmp_path, spark_tmp_table_factory, delta_delete_enabled_conf,
-        lambda table_name: f"DELETE FROM {table_name} WHERE a > 0",
-        expect_fallback=True)
+        lambda table_name: f"DELETE FROM {table_name} WHERE a > 0")
 
 @allow_non_gpu(*delta_meta_allow, delta_write_fallback_allow, "CreateTableExec",
                "AppendDataExecV1")
@@ -332,27 +326,81 @@ def test_delta_update_sql_liquid_clustering_fallback(spark_tmp_path,
 
     do_test_delta_dml_sql_liquid_clustering(
         spark_tmp_path, spark_tmp_table_factory, delta_update_enabled_conf,
-        lambda table_name: f"UPDATE {table_name} SET e = e+1 WHERE a > 0",
-        expect_fallback=True)
+        lambda table_name: f"UPDATE {table_name} SET e = e+1 WHERE a > 0")
 
-@allow_non_gpu(*delta_meta_allow)
+
+@allow_non_gpu(*delta_meta_allow, delta_write_fallback_allow, "CreateTableExec",
+               "AppendDataExecV1")
 @delta_lake
 @ignore_order
 @pytest.mark.skipif(is_databricks_runtime() and not is_databricks133_or_later(),
                     reason="Delta Lake liquid clustering is only supported on Databricks 13.3+")
-@pytest.mark.skipif(not is_spark_353_or_later(),
-                    reason="Create table with cluster by is only supported on delta 3.1+")
-def test_delta_merge_sql_liquid_clustering(spark_tmp_path,
-                                                     spark_tmp_table_factory):
+@pytest.mark.skipif(not is_databricks_runtime(),
+                    reason="Spark-RAPIDS plugin supports liquid clustering for Delta IO")
+def test_delta_merge_sql_liquid_clustering_fallback(spark_tmp_path,
+                                                    spark_tmp_table_factory):
 
     do_test_delta_dml_sql_liquid_clustering(
         spark_tmp_path, spark_tmp_table_factory, delta_merge_enabled_conf,
         lambda table_name: f"MERGE INTO {table_name} "
                            f"USING {table_name} as src_table "
                            f"ON {table_name}.a == src_table.a "
-                           f"WHEN NOT MATCHED THEN INSERT *",
-        expect_fallback=False)
+                           f"WHEN NOT MATCHED THEN INSERT *")
 
+
+@allow_non_gpu(*delta_meta_allow)
+@delta_lake
+@ignore_order
+@pytest.mark.skipif(is_databricks_runtime(),
+                    reason="Spark-RAPIDS plugin does not support liquid clustering for Databricks Delta")
+@pytest.mark.skipif(is_before_spark_353(),
+                    reason="rk-RAPIDS plugin supports liquid clustering for Delta IO 3.3+")
+def test_delta_merge_sql_liquid_clustering(spark_tmp_path, spark_tmp_table_factory):
+
+    base_source_path = spark_tmp_path + "/DELTA_MERGE_CLUSTERED_SOURCE"
+    cpu_source_path = f"{base_source_path}/CPU"
+    cpu_source_name = spark_tmp_table_factory.get()
+    gpu_source_path = f"{base_source_path}/GPU"
+    gpu_source_name = spark_tmp_table_factory.get()
+    base_target_path = spark_tmp_path + "/DELTA_MERGE_CLUSTERED_TARGET"
+    cpu_target_path = f"{base_target_path}/CPU"
+    cpu_target_name = spark_tmp_table_factory.get()
+    gpu_target_path = f"{base_target_path}/GPU"
+    gpu_target_name = spark_tmp_table_factory.get()
+
+    with_cpu_session(lambda spark: setup_clustered_table_sql(spark, cpu_source_path,
+                                                             cpu_source_name,
+                                                             spark_tmp_table_factory.get()))
+    with_cpu_session(lambda spark: setup_clustered_table_sql(spark, gpu_source_path,
+                                                             gpu_source_name,
+                                                             spark_tmp_table_factory.get()))
+    with_cpu_session(lambda spark: setup_clustered_table_sql(spark, cpu_target_path,
+                                                             cpu_target_name,
+                                                             spark_tmp_table_factory.get()))
+    with_cpu_session(lambda spark: setup_clustered_table_sql(spark, gpu_target_path,
+                                                             gpu_target_name,
+                                                             spark_tmp_table_factory.get()))
+
+    def merge_table(spark, path):
+        type = "cpu" if path == cpu_target_path else "gpu"
+        source_name = cpu_source_name if type == "cpu" else gpu_source_name
+        target_name = cpu_target_name if type == "cpu" else gpu_target_name
+        sql = f"""
+            MERGE INTO {target_name} as target
+            USING {source_name} as source
+            ON target.a == source.unique_int
+            WHEN MATCHED THEN
+              UPDATE SET *
+            WHEN NOT MATCHED THEN
+              INSERT *
+            """
+        spark.sql(sql).show()
+
+    assert_gpu_and_cpu_writes_are_equal_collect(
+        merge_table,
+        lambda spark, path: spark.read.format("delta").load(path),
+        base_target_path,
+        conf=delta_merge_enabled_conf)
 
 
 def create_clustered_delta_table_df(table_name, table_path):
