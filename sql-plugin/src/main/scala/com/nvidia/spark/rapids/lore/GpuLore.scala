3b62529e13f22621cb0b1548137fc921e1cd1773
Haoyang Li <haoyangl@nvidia.com>
2025-11-21 19:57:17 +0800

    fix style
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/lore/GpuLore.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/lore/GpuLore.scala
@@ -315,8 +315,9 @@ object GpuLore extends Logging {
                         registerCleanup(cleanupActions)(
                           subqueries -= sub.plan.canonicalized)
                       } else {
-                        throw new IllegalArgumentException("Subquery reuse is enabled, and we found" +
-                          " duplicated subqueries, which is currently not supported by LORE.")
+                        throw new IllegalArgumentException(
+                          "Subquery reuse is enabled, and we found duplicated subqueries, " +
+                            "which is currently not supported by LORE.")
                       }
                     }
                     tagSubqueryPlan(nextId, sub, loreOutputInfo, hadoopConf,
