3220146c53bf039e26d8d654606b69a301dfd8b8
Jihoon Son <ghoonson@gmail.com>
2025-11-24 04:37:09 -0800

    Delete support for clustered tables
    Signed-off-by: Jihoon Son <ghoonson@gmail.com>

--- integration_tests/src/main/python/delta_lake_liquid_clustering_test.py
+++ integration_tests/src/main/python/delta_lake_liquid_clustering_test.py
@@ -272,8 +272,7 @@ def test_delta_insert_overwrite_replace_where_sql_liquid_clustering(spark_tmp_pa
 def do_test_delta_dml_sql_liquid_clustering(spark_tmp_path,
                                                      spark_tmp_table_factory,
                                                      conf: Dict[str, str],
-                                                     sql_func: Callable[[str], str],
-                                                     expect_fallback):
+                                                     sql_func: Callable[[str], str]):
 
     base_data_path = spark_tmp_path + "/DELTA_LIQUID_CLUSTER"
     cpu_data_path = f"{base_data_path}/CPU"
@@ -292,34 +291,24 @@ def do_test_delta_dml_sql_liquid_clustering(spark_tmp_path,
         table_name = cpu_table_name if path == cpu_data_path else gpu_table_name
         spark.sql(sql_func(table_name))
 
-    if expect_fallback:
-        assert_gpu_fallback_write(modify_table,
-                                  lambda spark, path: spark.read.format("delta").load(path),
-                                  base_data_path,
-                                  "ExecutedCommandExec",
-                                  conf=conf)
-    else:
-        assert_gpu_and_cpu_writes_are_equal_collect(
-            modify_table,
-            lambda spark, path: spark.read.format("delta").load(path),
-            base_data_path,
-            conf=conf)
+    assert_gpu_and_cpu_writes_are_equal_collect(
+        modify_table,
+        lambda spark, path: spark.read.format("delta").load(path),
+        base_data_path,
+        conf=conf)
 
-@allow_non_gpu(*delta_meta_allow, delta_write_fallback_allow, "CreateTableExec",
-               "AppendDataExecV1")
+@allow_non_gpu(*delta_meta_allow, delta_write_fallback_allow)
 @delta_lake
 @ignore_order
 @pytest.mark.skipif(is_databricks_runtime() and not is_databricks133_or_later(),
                     reason="Delta Lake liquid clustering is only supported on Databricks 13.3+")
 @pytest.mark.skipif(not is_spark_353_or_later(),
                     reason="Create table with cluster by is only supported on delta 3.1+")
-def test_delta_delete_sql_liquid_clustering_fallback(spark_tmp_path,
-                                                     spark_tmp_table_factory):
+def test_delta_delete_sql_liquid_clustering(spark_tmp_path, spark_tmp_table_factory):
 
     do_test_delta_dml_sql_liquid_clustering(
         spark_tmp_path, spark_tmp_table_factory, delta_delete_enabled_conf,
-        lambda table_name: f"DELETE FROM {table_name} WHERE a > 0",
-        expect_fallback=True)
+        lambda table_name: f"DELETE FROM {table_name} WHERE a > 0")
 
 @allow_non_gpu(*delta_meta_allow, delta_write_fallback_allow, "CreateTableExec",
                "AppendDataExecV1")
@@ -335,8 +324,7 @@ def test_delta_update_sql_liquid_clustering(spark_tmp_path,
 
     do_test_delta_dml_sql_liquid_clustering(
         spark_tmp_path, spark_tmp_table_factory, delta_update_enabled_conf,
-        lambda table_name: f"UPDATE {table_name} SET e = e+1 WHERE a > 0",
-        expect_fallback=False)
+        lambda table_name: f"UPDATE {table_name} SET e = e+1 WHERE a > 0")
 
 
 @allow_non_gpu(*delta_meta_allow)
