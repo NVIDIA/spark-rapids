72f8f5e67e86acd497784b72d9ffa6fde5c4140b
Gary Shen <gashen@nvidia.com>
2025-12-04 11:53:54 +0800

    Add testRapids case to match GPU execution in RapidsDataFrameWindowFunctionsSuite (#13936)
    The original UT case is checking the CPU execution about the keys used
    in ShuffleExchangeExec.
    Copy the code to a new testRapids case to check the GPU execution which
    has the keys in GpuShuffleExchangeExec.
    
    Close https://github.com/NVIDIA/spark-rapids/issues/13765
    
    ---------
    
    Signed-off-by: Gary Shen <gashen@nvidia.com>

--- tests/src/test/spark330/scala/org/apache/spark/sql/rapids/suites/RapidsDataFrameWindowFunctionsSuite.scala
+++ tests/src/test/spark330/scala/org/apache/spark/sql/rapids/suites/RapidsDataFrameWindowFunctionsSuite.scala
@@ -19,10 +19,70 @@
 spark-rapids-shim-json-lines ***/
 package org.apache.spark.sql.rapids.suites
 
+import com.nvidia.spark.rapids.shims.GpuHashPartitioning
+import com.nvidia.spark.rapids.window.GpuWindowBaseExec
+
+import org.apache.spark.rapids.shims.GpuShuffleExchangeExec
 import org.apache.spark.sql.DataFrameWindowFunctionsSuite
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.expressions.{AttributeReference, Expression}
+import org.apache.spark.sql.execution.exchange.ENSURE_REQUIREMENTS
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.rapids.utils.RapidsSQLTestsTrait
 
 class RapidsDataFrameWindowFunctionsSuite
   extends DataFrameWindowFunctionsSuite
-  with RapidsSQLTestsTrait {}
+  with RapidsSQLTestsTrait {
+
+  import testImplicits._
+
+  testRapids("SPARK-38237: require all cluster " +
+    "keys for child required distribution for window query") {
+    def partitionExpressionsColumns(expressions: Seq[Expression]): Seq[String] = {
+      expressions.flatMap {
+        case ref: AttributeReference => Some(ref.name)
+      }
+    }
+
+    def isShuffleExecByRequirement(
+        plan: GpuShuffleExchangeExec,
+        desiredClusterColumns: Seq[String]): Boolean = plan match {
+      case GpuShuffleExchangeExec(op: GpuHashPartitioning, _, ENSURE_REQUIREMENTS, _) => {
+        partitionExpressionsColumns(op.expressions) === desiredClusterColumns
+      }
+      case _ => false
+    }
+
+    val df = Seq(("a", 1, 1), ("a", 2, 2), ("b", 1, 3), ("b", 1, 4)).toDF("key1", "key2", "value")
+    val windowSpec = Window.partitionBy("key1", "key2").orderBy("value")
+
+    withSQLConf(
+      SQLConf.ADAPTIVE_EXECUTION_ENABLED.key -> "false",
+      SQLConf.REQUIRE_ALL_CLUSTER_KEYS_FOR_DISTRIBUTION.key -> "true") {
+
+      val windowed = df
+        // repartition by subset of window partitionBy keys which satisfies ClusteredDistribution
+        .repartition($"key1")
+        .select(
+          lead($"key1", 1).over(windowSpec),
+          lead($"value", 1).over(windowSpec))
+
+      checkAnswer(windowed, Seq(Row("b", 4), Row(null, null), Row(null, null), Row(null, null)))
+
+      val shuffleByRequirement = windowed.queryExecution.executedPlan.exists {
+        case w: GpuWindowBaseExec =>
+          w.child.exists {
+            case s: GpuShuffleExchangeExec => isShuffleExecByRequirement(s, Seq("key1", "key2"))
+            case _ => false
+          }
+        case _ => false
+      }
+
+      assert(shuffleByRequirement, "Can't find desired shuffle node from the query plan")
+    }
+  }
+
+}
 
