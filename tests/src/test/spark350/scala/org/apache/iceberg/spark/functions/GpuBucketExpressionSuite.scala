298cfa42585f1138b2699eeed52b75d4c8029d24
Chong Gao <res_life@163.com>
2025-12-12 14:40:32 +0800

    Supports all types for Iceberg bucket transform
    Signed-off-by: Chong Gao <res_life@163.com>

--- tests/src/test/spark350/scala/org/apache/iceberg/spark/functions/GpuBucketExpressionSuite.scala
+++ tests/src/test/spark350/scala/org/apache/iceberg/spark/functions/GpuBucketExpressionSuite.scala
@@ -34,10 +34,12 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 
 import org.apache.spark.sql.catalyst.expressions.ExprId
-import org.apache.spark.sql.types.{DataType, DataTypes}
+import org.apache.spark.sql.types.{DataType, DataTypes, Decimal, DecimalType}
+import org.apache.spark.unsafe.types.UTF8String
 
 class GpuBucketExpressionSuite extends AnyFunSuite with BeforeAndAfterAll {
   private var seed = 2L
+  private val numBuckets = Int.MaxValue
 
   override def beforeAll(): Unit = {
     seed = System.currentTimeMillis()
@@ -45,29 +47,43 @@ class GpuBucketExpressionSuite extends AnyFunSuite with BeforeAndAfterAll {
   }
 
   test("Int bucket function") {
-    testBucketFunction[Int](DataTypes.IntegerType, 1000,
+    // Also covers DateType (stored as days since epoch, uses same BucketInt hash)
+    testBucketFunction[Int](DataTypes.IntegerType,
       (col, row) => col.getInt(row), BucketFunction.BucketInt.invoke)
   }
 
   test("Long bucket function") {
-    testBucketFunction[Long](DataTypes.LongType, 2000,
+    // Also covers TimestampType (stored as microseconds since epoch, uses same BucketLong hash)
+    testBucketFunction[Long](DataTypes.LongType,
       (col, row) => col.getLong(row), BucketFunction.BucketLong.invoke)
   }
 
+  test("String bucket function") {
+    testBucketFunction[UTF8String](DataTypes.StringType,
+      (col, row) => UTF8String.fromString(col.getJavaString(row)),
+      BucketFunction.BucketString.invoke)
+  }
+
+  test("Binary bucket function") {
+    testBucketFunctionBinary()
+  }
+
+  test("Decimal bucket function") {
+    testBucketFunctionDecimal(DecimalType(18, 2))
+  }
+
   private def testBucketFunction[T](dataType: DataType,
-    modValue: Int,
     colEval: (HostColumnVector, Int) => T,
     icebergBucket: (Int, T) => Int): Unit = {
-    // TODO: Fix this when spark rapids jni hash function supports nullable
-    val schema = createSchema(Seq(dataType), nullable = false)
+    val schema = createSchema(Seq(dataType), nullable = true)
     withResource(FuzzerUtils.createColumnarBatch(schema, 1000, seed = seed)) { batch =>
-      val inputRefExpr = GpuBoundReference(0, dataType, nullable = false)(ExprId(0), "input0")
-      val modExpr = GpuLiteral.create(modValue)
-      val expr = GpuBucketExpression(modExpr, inputRefExpr)
+      val inputRefExpr = GpuBoundReference(0, dataType, nullable = true)(ExprId(0), "input0")
+      val numBucketsExpr = GpuLiteral.create(numBuckets)
+      val expr = GpuBucketExpression(numBucketsExpr, inputRefExpr)
       withResource(expr.columnarEvalAny(batch).asInstanceOf[GpuColumnVector]) { gpuResult =>
         withResource(gpuResult.getBase.copyToHost()) { hostResult =>
           withResource(icebergEval(batch.column(0).asInstanceOf[GpuColumnVector],
-            modValue, colEval, icebergBucket)) { expected =>
+            colEval, icebergBucket)) { expected =>
             TestUtils.compareColumns(expected, hostResult)
           }
         }
@@ -75,7 +91,40 @@ class GpuBucketExpressionSuite extends AnyFunSuite with BeforeAndAfterAll {
     }
   }
 
-  private def icebergEval[T](col: GpuColumnVector, modValue: Int,
+  private def testBucketFunctionBinary(): Unit = {
+    val dataType = DataTypes.BinaryType
+    val schema = createSchema(Seq(dataType), nullable = true)
+    withResource(FuzzerUtils.createColumnarBatch(schema, 1000, seed = seed)) { batch =>
+      val inputRefExpr = GpuBoundReference(0, dataType, nullable = true)(ExprId(0), "input0")
+      val numBucketsExpr = GpuLiteral.create(numBuckets)
+      val expr = GpuBucketExpression(numBucketsExpr, inputRefExpr)
+      withResource(expr.columnarEvalAny(batch).asInstanceOf[GpuColumnVector]) { gpuResult =>
+        withResource(gpuResult.getBase.copyToHost()) { hostResult =>
+          withResource(icebergEvalBinary(batch.column(0).asInstanceOf[GpuColumnVector])) {
+            expected => TestUtils.compareColumns(expected, hostResult)
+          }
+        }
+      }
+    }
+  }
+
+  private def testBucketFunctionDecimal(dataType: DecimalType): Unit = {
+    val schema = createSchema(Seq(dataType), nullable = true)
+    withResource(FuzzerUtils.createColumnarBatch(schema, 1000, seed = seed)) { batch =>
+      val inputRefExpr = GpuBoundReference(0, dataType, nullable = true)(ExprId(0), "input0")
+      val numBucketsExpr = GpuLiteral.create(numBuckets)
+      val expr = GpuBucketExpression(numBucketsExpr, inputRefExpr)
+      withResource(expr.columnarEvalAny(batch).asInstanceOf[GpuColumnVector]) { gpuResult =>
+        withResource(gpuResult.getBase.copyToHost()) { hostResult =>
+          withResource(icebergEvalDecimal(batch.column(0).asInstanceOf[GpuColumnVector])) {
+            expected => TestUtils.compareColumns(expected, hostResult)
+          }
+        }
+      }
+    }
+  }
+
+  private def icebergEval[T](col: GpuColumnVector,
     colEval: (HostColumnVector, Int) => T,
     icebergBucket: (Int, T) => Int): HostColumnVector = {
     withResource(col.getBase.copyToHost()) { hostCol =>
@@ -86,7 +135,44 @@ class GpuBucketExpressionSuite extends AnyFunSuite with BeforeAndAfterAll {
           builder.appendNull()
         } else {
           val v = colEval(hostCol, row)
-          builder.append(icebergBucket(modValue, v))
+          builder.append(icebergBucket(numBuckets, v))
+        }
+      }
+      builder.build()
+    }
+  }
+
+  private def icebergEvalBinary(col: GpuColumnVector): HostColumnVector = {
+    withResource(col.getBase.copyToHost()) { hostCol =>
+      val builder = HostColumnVector.builder(DType.INT32, hostCol.getRowCount.toInt)
+
+      for (row <- 0 until hostCol.getRowCount.toInt) {
+        if (hostCol.isNull(row)) {
+          builder.appendNull()
+        } else {
+          val bytes = hostCol.getList(row).asInstanceOf[java.util.List[java.lang.Byte]]
+          val byteArray = new Array[Byte](bytes.size())
+          for (i <- 0 until bytes.size()) {
+            byteArray(i) = bytes.get(i)
+          }
+          builder.append(BucketFunction.BucketBinary.invoke(numBuckets, byteArray))
+        }
+      }
+      builder.build()
+    }
+  }
+
+  private def icebergEvalDecimal(col: GpuColumnVector): HostColumnVector = {
+    withResource(col.getBase.copyToHost()) { hostCol =>
+      val builder = HostColumnVector.builder(DType.INT32, hostCol.getRowCount.toInt)
+
+      for (row <- 0 until hostCol.getRowCount.toInt) {
+        if (hostCol.isNull(row)) {
+          builder.appendNull()
+        } else {
+          val bd = hostCol.getBigDecimal(row)
+          val sparkDecimal = Decimal(bd)
+          builder.append(BucketFunction.BucketDecimal.invoke(numBuckets, sparkDecimal))
         }
       }
       builder.build()
