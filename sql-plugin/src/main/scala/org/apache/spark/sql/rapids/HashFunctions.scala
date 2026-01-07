NVIDIA/spark-rapids

commit debf12a8e1d1026c1c0e5b7041b5ca5f0ec56d3e

[FEA] SHA-2 hash support. (#14038)
Depends on https://github.com/NVIDIA/spark-rapids-jni/pull/4091.

This commit adds support for the [SHA-2
family](https://spark.apache.org/docs/latest/api/sql/index.html#sha2) of
hash functions.
    
As with Apache Spark, the following data types are supported:
1. `BINARY`
2. `STRING`
    
Also, as with Apache Spark, the following bit-lengths are supported:
1. 224
2. 256
3. 384
4. 512

If the bitlength is specified as 0, a default of 256 is used.
If the bitlength differs from the list above, the returned row is null.
    
This implementation differs from Apache Spark in that it requires the
bitlength to be an INTEGER literal.  Apache Spark supports expressions
as bitlength, e.g.
```sql
SELECT SHA2(my_str, LENGTH(my_str)) FROM ...
```
This might be an exceedingly rare usecase, and can be addressed at a
later date if deemed a priority.
    
Signed-off-by: MithunR <mithunr@nvidia.com>

---------

Signed-off-by: MithunR <mithunr@nvidia.com>

sql-plugin/src/main/scala/org/apache/spark/sql/rapids/HashFunctions.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2025, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2026, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,15 +18,15 @@ package org.apache.spark.sql.rapids
 
 import java.util.Optional
 
-import ai.rapids.cudf.{BinaryOp, ColumnVector, ColumnView, DType}
-import com.nvidia.spark.rapids.{GpuColumnVector, GpuExpression, GpuProjectExec, GpuUnaryExpression}
+import ai.rapids.cudf.{BinaryOp, ColumnVector, ColumnView, DType, Scalar}
+import com.nvidia.spark.rapids.{BinaryExprMeta, DataFromReplacementRule, GpuBinaryExpression, GpuColumnVector, GpuExpression, GpuLiteral, GpuProjectExec, GpuScalar, GpuUnaryExpression, RapidsConf, RapidsMeta}
 import com.nvidia.spark.rapids.Arm.withResource
 import com.nvidia.spark.rapids.RapidsPluginImplicits._
 import com.nvidia.spark.rapids.jni.Hash
 import com.nvidia.spark.rapids.shims.{HashUtils, NullIntolerantShim, ShimExpression}
 
 import org.apache.spark.sql.catalyst.analysis.TypeCheckResult
-import org.apache.spark.sql.catalyst.expressions.{Expression, ImplicitCastInputTypes}
+import org.apache.spark.sql.catalyst.expressions.{Expression, ImplicitCastInputTypes, Sha2}
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.vectorized.ColumnarBatch
@@ -71,6 +71,99 @@ case class GpuSha1(child: Expression)
         fullResult.mergeAndSetValidity(BinaryOp.BITWISE_AND, normalizedStringCV)
       }
     }
+  }
+}
+
+/**
+ * GPU implementation of the SHA2 hash function set.
+ * Note:
+ *   - Only bit lengths of 0, 224, 256, 384, and 512 are supported.
+ *   - A bit length of 0 is treated as 256, following Spark behaviour.
+ *   - For the GPU implementation, the bit length must be provided as a literal.
+ *   - For unsupported (including negative) bit lengths, SHA2 returns null.
+ *     This differs from the CPU implementation, where the bit length can differ
+ *     per row.
+ * @see https://spark.apache.org/docs/latest/api/sql/index.html#sha2
+ * @param left The BINARY input column to be hashed.
+ * @param right The INTEGER literal bitlength for hashing.
+ */
+case class GpuSha2(left: Expression, right: Expression)
+  extends GpuBinaryExpression with ImplicitCastInputTypes with NullIntolerantShim {
+  private val childExpr: Expression = left
+  private val bitLengthExpr: Expression = right
+  override def toString: String = s"sha2($childExpr, $bitLength)"
+  override def inputTypes: Seq[AbstractDataType] = Seq(BinaryType, IntegerType)
+  override def dataType: DataType = StringType
+
+  private val bitLength: Option[Int] = bitLengthExpr match {
+    case lit: GpuLiteral if lit.value.isInstanceOf[Int] =>
+      lit.value.asInstanceOf[Int] match {
+        case 224 | 256 | 384 | 512 => Some(lit.value.asInstanceOf[Int])
+        case 0 => Some(256) // Spark default
+        case _ => None // SHA2 returns null for other bit lengths.
+      }
+    case _ => None
+  }
+
+  override def nullable: Boolean = childExpr.nullable || bitLength.isEmpty
+
+  override def doColumnar(lhs: GpuColumnVector, rhs: GpuScalar): ColumnVector = {
+    if (bitLength.isEmpty) {
+      // SHA2 returns null for "unsupported" bit lengths.
+      withResource(Scalar.fromNull(DType.STRING)) { nullStringExemplar =>
+        ColumnVector.fromScalar(nullStringExemplar, lhs.getRowCount.toInt)
+      }
+    } else {
+      bitLength match {
+        case Some(224) => withResource(GpuSha2.getStringViewOfBinaryColumn(lhs.getBase)) {
+          Hash.sha224NullsPreserved
+        }
+        case Some(256) => withResource(GpuSha2.getStringViewOfBinaryColumn(lhs.getBase)) {
+          Hash.sha256NullsPreserved
+        }
+        case Some(384) => withResource(GpuSha2.getStringViewOfBinaryColumn(lhs.getBase)) {
+          Hash.sha384NullsPreserved
+        }
+        case Some(512) => withResource(GpuSha2.getStringViewOfBinaryColumn(lhs.getBase)) {
+          Hash.sha512NullsPreserved
+        }
+        case unexpected =>
+          throw new UnsupportedOperationException(s"Unsupported bit length for SHA2: $unexpected")
+      }
+    }
+  }
+
+  override def doColumnar(numRows: Int, lhs: GpuScalar, rhs: GpuScalar): ColumnVector = {
+    withResource(GpuColumnVector.from(lhs, numRows, lhs.dataType)) {
+      doColumnar(_, rhs)
+    }
+  }
+
+  override def doColumnar(lhs: GpuColumnVector, rhs: GpuColumnVector): ColumnVector = {
+    throw new UnsupportedOperationException("Only literal bit lengths are supported for SHA2")
+  }
+
+  override def doColumnar(lhs: GpuScalar, rhs: GpuColumnVector): ColumnVector = {
+    throw new UnsupportedOperationException("Only literal bit lengths are supported for SHA2")
+  }
+}
+
+object GpuSha2 {
+
+  private def getStringViewOfBinaryColumn(col: ColumnView): ColumnView = {
+    withResource(col.getChildColumnView(0)) { dataCol =>
+      new ColumnView(DType.STRING, col.getRowCount,
+        Optional.of[java.lang.Long](col.getNullCount),
+        dataCol.getData, col.getValid, col.getOffsets)
+    }
+  }
+
+  class Meta(e: Sha2,
+              conf: RapidsConf,
+              p: Option[RapidsMeta[_, _, _]],
+              r: DataFromReplacementRule)
+    extends BinaryExprMeta[Sha2](e, conf, p, r) {
+    override def convertToGpu(lhs: Expression, rhs: Expression): GpuExpression = GpuSha2(lhs, rhs)
   }
 }
 

integration_tests/src/main/python/hashing_test.py
@@ -175,3 +175,28 @@ def test_str_special_characters_sha1():
     special_string_gen = StringGen().with_special_case('好').with_special_case('吃')
     assert_gpu_and_cpu_are_equal_collect(
         lambda spark: unary_op_df(spark, special_string_gen).selectExpr('sha1(a)'))
+
+
+@pytest.mark.parametrize("datagen", [binary_gen, string_gen])
+@pytest.mark.parametrize("bitlength", [-20, 0, 123, 224, 256, 384, 512], ids=idfn)
+def test_sha2_with_literal_bitlengths(bitlength, datagen):
+    """
+    Tests sha2 with bitlengths provided as literals, for string and binary inputs.
+    For bitlength==0, the output is the same as with 256.  Should run on the GPU.
+    For bitlength ∈ [-1, 123], the output should be nulls.  Should still run on the GPU.
+    All the other bitlengths are valid and should run on the GPU.
+    """
+    assert_gpu_and_cpu_are_equal_collect(
+        lambda spark: unary_op_df(spark, datagen).selectExpr(f'SHA2(a, {bitlength})'))
+    assert_gpu_and_cpu_are_equal_collect(
+        lambda spark: unary_op_df(spark, datagen).selectExpr(f'SHA2("asdf", {bitlength})'))
+
+
+@allow_non_gpu("ProjectExec")
+def test_sha2_bitlength_fallback():
+    """
+    Verifies that sha2 falls back to the CPU when the bitlength argument is not a literal.
+    """
+    assert_gpu_fallback_collect(
+        lambda spark: unary_op_df(spark, string_gen).selectExpr('SHA2(a, LENGTH(a))'),
+        "ProjectExec")

sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuOverrides.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2025, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2026, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -2567,6 +2567,15 @@ object GpuOverrides extends Logging {
       (a, conf, p, r) => new UnaryExprMeta[Sha1](a, conf, p, r) {
         override def convertToGpu(child: Expression): GpuExpression = GpuSha1(child)
       }),
+    expr[Sha2] (
+      "Sha2 hash operator",
+      ExprChecks.binaryProject(
+        TypeSig.STRING,
+        TypeSig.STRING,
+        ("input", TypeSig.BINARY, TypeSig.BINARY),
+        ("bitLength", TypeSig.lit(TypeEnum.INT), TypeSig.lit(TypeEnum.INT))),
+      (a, conf, p, r) => new GpuSha2.Meta(a, conf, p, r)
+    ),
     expr[Upper](
       "String uppercase operator",
       ExprChecks.unaryProjectInputMatchesOutput(TypeSig.STRING, TypeSig.STRING),

commit f66b18a91f65b89a440b240995f078c6d70ebd7c

Fix compile error in HashFunctions.scala
This commit fixes a compile error in HashFunctions.scala.

The error was introduced in #14038 (which introduced SHA2 support in
HashFunctions).  It was caused by a change in the signature of
`GpuColumnVector.from(GpuScalar, int...)` introduced in #14086.

The changes in #14086 were merged in the time between:
1. The CI build (passing) for #14038, and
2. The time #14038 was merged.

This commit uses the improved function signature.

Signed-off-by: MithunR <mithunr@nvidia.com>

sql-plugin/src/main/scala/org/apache/spark/sql/rapids/HashFunctions.scala
@@ -134,7 +134,7 @@ case class GpuSha2(left: Expression, right: Expression)
   }
 
   override def doColumnar(numRows: Int, lhs: GpuScalar, rhs: GpuScalar): ColumnVector = {
-    withResource(GpuColumnVector.from(lhs, numRows, lhs.dataType)) {
+    withResource(GpuColumnVector.from(lhs, numRows)) {
       doColumnar(_, rhs)
     }
   }