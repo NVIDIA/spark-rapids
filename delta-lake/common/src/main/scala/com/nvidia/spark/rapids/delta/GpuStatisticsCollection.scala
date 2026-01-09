e96e402d6437aaff190ecd21290febe13701b35e
Gera Shegalov <gshegalov@nvidia.com>
2026-01-09 10:54:58 -0800

    Copyright
    Signed-off-by: Gera Shegalov <gshegalov@nvidia.com>

--- delta-lake/common/src/main/scala/com/nvidia/spark/rapids/delta/GpuStatisticsCollection.scala
+++ delta-lake/common/src/main/scala/com/nvidia/spark/rapids/delta/GpuStatisticsCollection.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2025, NVIDIA CORPORATION.
+ * Copyright (c) 2022-2026, NVIDIA CORPORATION.
  *
  * This file was derived from StatisticsCollection.scala
  * in the Delta Lake project at https://github.com/delta-io/delta.
