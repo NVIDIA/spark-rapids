7df38c839beae3b3cecae168deb9e7e9c66ad84e
Chong Gao <res_life@163.com>
2026-01-13 16:41:33 +0800

    Fix
    Signed-off-by: Chong Gao <res_life@163.com>

--- sql-plugin/src/main/spark411/scala/com/nvidia/spark/rapids/shims/spark411/SparkShimServiceProvider.scala
+++ sql-plugin/src/main/spark411/scala/com/nvidia/spark/rapids/shims/spark411/SparkShimServiceProvider.scala
@@ -17,7 +17,7 @@
 /*** spark-rapids-shim-json-lines
 {"spark": "411"}
 spark-rapids-shim-json-lines ***/
-package com.nvidia.spark.rapids.shims.spark410
+package com.nvidia.spark.rapids.shims.spark411
 
 import com.nvidia.spark.rapids.SparkShimVersion
 
