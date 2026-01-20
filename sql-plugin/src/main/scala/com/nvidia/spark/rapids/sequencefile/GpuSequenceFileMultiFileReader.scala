984d88b19d53f3ead8c18be078887d551e7b05b5
Haoyang Li <haoyangl@nvidia.com>
2026-01-20 10:18:29 +0800

    Revert "fix"
    This reverts commit 4139c002a0975fbdbc785561c90aaab1fdc6bff4.

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/sequencefile/GpuSequenceFileMultiFileReader.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/sequencefile/GpuSequenceFileMultiFileReader.scala
@@ -599,20 +599,8 @@ class MultiFileSequenceFilePartitionReader(
       if (start > 0) {
         reader.sync(start - 1)
       }
-      // Check position BEFORE reading, and handle EOF gracefully
-      var continue = true
-      while (continue && reader.getPosition < end) {
-        try {
-          if (reader.next(key, value)) {
-            count += 1
-          } else {
-            continue = false
-          }
-        } catch {
-          case _: java.io.EOFException =>
-            // EOF reached - this can happen at split boundaries
-            continue = false
-        }
+      while (reader.getPosition < end && reader.next(key, value)) {
+        count += 1
       }
     } finally {
       reader.close()
