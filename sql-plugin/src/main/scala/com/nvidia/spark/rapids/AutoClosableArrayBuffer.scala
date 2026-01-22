9c395d434b28bc91b301a0b4715820cab27241e7
Firestarman <firestarmanllc@gmail.com>
2026-01-22 14:25:46 +0800

    revert a useless change
    Signed-off-by: Firestarman <firestarmanllc@gmail.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/AutoClosableArrayBuffer.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/AutoClosableArrayBuffer.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2026, NVIDIA CORPORATION.
+ * Copyright (c) 2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,8 +36,6 @@ class AutoClosableArrayBuffer[T <: AutoCloseable] extends AutoCloseable {
   def map[U](f: T => U): Seq[U] = data.map(f).toSeq
 
   def toArray[B >: T : ClassTag]: Array[B] = data.toArray
-
-  def toSeq: Seq[T] = data.toSeq
 
   def size(): Int = data.size
 
