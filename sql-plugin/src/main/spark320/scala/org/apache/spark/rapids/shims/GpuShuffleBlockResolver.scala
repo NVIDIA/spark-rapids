f97c234570c746f9085f8e8a97f431aca5aa31d3
Hongbin Ma (Mahone) <mahongbin@apache.org>
2026-01-29 00:50:34 +0800

    Revert "Rapids shuffle manager V2 phase 2" [skip ci] (#14219)
    This reverts commit 1ce1895d979be6d76fc03e8afa43539d2dd8870b to fix
    https://github.com/NVIDIA/spark-rapids/issues/14218

--- sql-plugin/src/main/spark320/scala/org/apache/spark/rapids/shims/GpuShuffleBlockResolver.scala
+++ sql-plugin/src/main/spark320/scala/org/apache/spark/rapids/shims/GpuShuffleBlockResolver.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2026, NVIDIA CORPORATION.
+ * Copyright (c) 2021-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,9 +58,7 @@ import org.apache.spark.shuffle.IndexShuffleBlockResolver
 import org.apache.spark.sql.rapids.GpuShuffleBlockResolverBase
 import org.apache.spark.storage.ShuffleMergedBlockId
 
-class GpuShuffleBlockResolver(
-    resolver: IndexShuffleBlockResolver,
-    catalog: ShuffleBufferCatalog)
+class GpuShuffleBlockResolver(resolver: IndexShuffleBlockResolver, catalog: ShuffleBufferCatalog)
     extends GpuShuffleBlockResolverBase(resolver, catalog) {
 
   /**
