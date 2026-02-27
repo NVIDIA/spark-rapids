267a476543914cd003e538db8246fef0d379761f
Niranjan Artal <nartal@nvidia.com>
2026-02-27 00:09:35 +0000

    remove desupported shims
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- sql-plugin/src/main/spark321/scala/org/apache/spark/sql/rapids/ShuffleManagerShims.scala
+++ sql-plugin/src/main/spark321/scala/org/apache/spark/sql/rapids/ShuffleManagerShims.scala
@@ -15,18 +15,11 @@
  */
 
 /*** spark-rapids-shim-json-lines
-{"spark": "320"}
 {"spark": "321"}
-{"spark": "321cdh"}
-{"spark": "322"}
-{"spark": "323"}
-{"spark": "324"}
 {"spark": "330"}
-{"spark": "330cdh"}
 {"spark": "330db"}
 {"spark": "331"}
 {"spark": "332"}
-{"spark": "332cdh"}
 {"spark": "332db"}
 {"spark": "333"}
 {"spark": "334"}
