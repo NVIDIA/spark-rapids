db8d0f6e2aeb0dd34dd4bddcc649f65e46142101
Niranjan Artal <nartal@nvidia.com>
2026-03-23 10:24:12 +0000

    Fix DB-17.3 integration tests in orc_write_test.py
    Signed-off-by: Niranjan Artal <nartal@nvidia.com>

--- sql-plugin/src/main/spark400db173/scala/com/nvidia/spark/rapids/shims/SparkShims.scala
+++ sql-plugin/src/main/spark400db173/scala/com/nvidia/spark/rapids/shims/SparkShims.scala
@@ -20,7 +20,9 @@ spark-rapids-shim-json-lines ***/
 package com.nvidia.spark.rapids.shims
 
 import com.nvidia.spark.rapids._
+import com.nvidia.spark.rapids.{HashExprChecks, Murmur3HashExprMeta, XxHash64ExprMeta}
 
+import org.apache.spark.sql.catalyst.expressions.{CollationAwareMurmur3Hash, CollationAwareXxHash64, Expression}
 import org.apache.spark.sql.execution.{OneRowRelationExec, SparkPlan}
 
 object SparkShimImpl extends Spark400PlusDBShims {
@@ -35,5 +37,21 @@ object SparkShimImpl extends Spark400PlusDBShims {
         (exec, conf, parent, rule) => new GpuOneRowRelationExecMeta(exec, conf, parent, rule))
     ).map(r => (r.getClassFor.asSubclass(classOf[SparkPlan]), r)).toMap
     super.getExecs ++ shimExecs
+  }
+
+  override def getExprs: Map[Class[_ <: Expression], ExprRule[_ <: Expression]] = {
+    val shimExprs: Map[Class[_ <: Expression], ExprRule[_ <: Expression]] = Seq(
+      GpuOverrides.expr[CollationAwareMurmur3Hash](
+        "Collation-aware murmur3 hash operator",
+        HashExprChecks.murmur3ProjectChecks,
+        Murmur3HashExprMeta.apply
+      ),
+      GpuOverrides.expr[CollationAwareXxHash64](
+        "Collation-aware xxhash64 operator",
+        HashExprChecks.xxhash64ProjectChecks,
+        XxHash64ExprMeta.apply
+      )
+    ).map(r => (r.getClassFor.asSubclass(classOf[Expression]), r)).toMap
+    super.getExprs ++ shimExprs
   }
 }
